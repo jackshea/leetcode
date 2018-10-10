66b1edbe2206370223382029d759ca9e02b9599a
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-10 17:29:32 +0800

    P0020

---
+++ LeetCode/Problems/P0020_ValidParentheses.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 有效的括号
+    /// https://leetcode-cn.com/problems/valid-parentheses/description/
+    public class P0020_ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                    continue;
+                }
+
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                if (c == ')' && stack.Pop() != '(')
+                {
+                    return false;
+                }
+
+                if (c == ']' && stack.Pop() != '[')
+                {
+                    return false;
+                }
+
+                if (c == '}' && stack.Pop() != '{')
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count != 0;
+
+            return true;
+        }
+    }
+}
