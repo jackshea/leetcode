d185211fc182cb7d6e29e354c93835d0451d7ad2
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-10 20:08:23 +0800

    P0021

---
+++ LeetCode/Problems/P0021_MergeTwoSortedLists.cs
@@ -0,0 +1,78 @@
+﻿namespace LeetCode.Problems
+{
+    public class P0021_MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode currentNode = null;
+            ListNode mergeListHeadNode = null;
+
+            if (l1 == null)
+            {
+                return l2;
+            }
+
+            if (l2 == null)
+            {
+                return l1;
+            }
+
+            while (true)
+            {
+                if (l1.val <= l2.val)
+                {
+                    if (currentNode == null)
+                    {
+                        currentNode = l1;
+                        mergeListHeadNode = currentNode;
+                    }
+                    else
+                    {
+                        currentNode.next = l1;
+                        currentNode = currentNode.next;
+                    }
+
+                    l1 = l1.next;
+
+                    if (l1 == null)
+                    {
+                        currentNode.next = l2;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (currentNode == null)
+                    {
+                        currentNode = l2;
+                        mergeListHeadNode = currentNode;
+                    }
+                    else
+                    {
+                        currentNode.next = l2;
+                        currentNode = currentNode.next;
+                    }
+
+                    l2 = l2.next;
+
+                    if (l2 == null)
+                    {
+                        currentNode.next = l1;
+                        break;
+                    }
+                }
+            }
+
+            return mergeListHeadNode;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+}
