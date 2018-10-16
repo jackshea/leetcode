69a76fe734eaa9926fbef2d3bf1ad742f88d40ec
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-16 15:05:53 +0800

    P0053 最大子序和

---
+++ LeetCode/Problems/P0053_MaximumSubarray.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Problems
+{
+    /// 最大子序和
+    /// https://leetcode-cn.com/problems/maximum-subarray/description/
+    public class P0053_MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int sum = nums[0];
+            int current = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (current < 0)
+                {
+                    current = nums[i];
+                }
+                else
+                {
+                    current += nums[i];
+                }
+
+                if (current > sum)
+                {
+                    sum = current;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
