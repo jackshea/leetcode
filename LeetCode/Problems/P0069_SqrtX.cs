a9b466a8a111f0d8e49739953a039b5c23cc6f62
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-17 20:14:50 +0800

    P0069 x 的平方根

---
+++ LeetCode/Problems/P0069_SqrtX.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// x 的平方根
+    /// https://leetcode-cn.com/problems/sqrtx/description/
+    public class P0069_SqrtX
+    {
+        public int MySqrt(int x)
+        {
+            if (x <= 1) return x;
+            int lo = 1, hi = x;
+            while (lo < hi)
+            {
+                int mid = lo + (hi - lo + 1) / 2;
+                if (mid <= x / mid)
+                {
+                    lo = mid;
+                }
+                else
+                {
+                    hi = mid - 1;
+                }
+            }
+            return lo;
+        }
+    }
+}
