1525586a8dc78fe81a7e2fdd02b31e2cfc85210d
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-24 09:49:44 +0800

    P0100 相同的树

---
+++ LeetCode/Problems/P0100_SameTree.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Problems
+{
+    public class P0100_SameTree
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p == null || q == null)
+            {
+                return false;
+            }
+
+            if (p.val != q.val)
+            {
+                return false;
+            }
+            else
+            {
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+            }
+        }
+
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+    }
+}
