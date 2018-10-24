4b1e94cada25708616e3a1397e9126722b5e47da
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-24 18:04:13 +0800

    P0110 平衡二叉树

---
+++ LeetCode/Tools/ConvertArrayToBinaryTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using LeetCode.Problems;
+
+namespace LeetCode.Tools
+{
+    public static class ConvertArrayToBinaryTree
+    {
+        public static TreeNode Build(int?[] values)
+        {
+            if (values == null || values.Length == 0 || values[0] == null)
+            {
+                return null;
+            }
+
+            TreeNode root = new TreeNode(values[0].Value);
+
+            List<TreeNode> nodeList = new List<TreeNode>(values.Length);
+            nodeList.Add(root);
+
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (!values[i].HasValue)
+                {
+                    nodeList.Add(null);
+                    continue;
+                }
+
+                var node = new TreeNode(values[i].Value);
+                var parentNode = nodeList[(i - 1) / 2];
+                if (i % 2 == 1)
+                {
+                    parentNode.left = node;
+                }
+                else
+                {
+                    parentNode.right = node;
+                }
+
+                nodeList.Add(node);
+            }
+
+            return root;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0110_BalancedBinaryTreeTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Tools;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0110_BalancedBinaryTreeTests
+    {
+        [Test()]
+        public void IsBalancedTest()
+        {
+            var values = new int?[] { 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, null, null, 5, 5 };
+            var root = ConvertArrayToBinaryTree.Build(values);
+
+            Assert.IsTrue(new P0110_BalancedBinaryTree().IsBalanced(root));
+        }
+    }
+}
