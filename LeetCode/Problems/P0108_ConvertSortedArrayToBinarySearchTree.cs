20ee6e255f86c142fa05293d55d634ae482a4d5a
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-24 15:40:28 +0800

    P0108 将有序数组转换为二叉搜索树

---
+++ LeetCode/Problems/P0108_ConvertSortedArrayToBinarySearchTree.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems
+{
+    /// 将有序数组转换为二叉搜索树
+    /// https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/description/
+    public class P0108_ConvertSortedArrayToBinarySearchTree
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return toBST(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode toBST(int[] nums, int left, int right)
+        {
+            if (right < left)
+            {
+                return null;
+            }
+
+            int mid = (left + right) / 2;
+            var node = new TreeNode(nums[mid]);
+            node.left = toBST(nums, left, mid - 1);
+            node.right = toBST(nums, mid + 1, right);
+            return node;
+        }
+    }
+}
