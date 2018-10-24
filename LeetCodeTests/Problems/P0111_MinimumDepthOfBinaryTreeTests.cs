61204d52ac7bcd4527b211e3abdbc9e7187b10be
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-24 19:47:56 +0800

    P0112 路径总和

---
+++ LeetCodeTests/Problems/P0111_MinimumDepthOfBinaryTreeTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Tools;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0111_MinimumDepthOfBinaryTreeTests
+    {
+        [Test()]
+        public void MinDepthTest()
+        {
+            var values = new int?[] { 1, 2 };
+
+            var root = ConvertArrayToBinaryTree.Build(values);
+
+            Assert.AreEqual(2, new P0111_MinimumDepthOfBinaryTree().MinDepth(root));
+        }
+    }
+}
