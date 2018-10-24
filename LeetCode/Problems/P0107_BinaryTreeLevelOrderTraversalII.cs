c6335415904282df224bc15d94541a9953f5aa55
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-24 15:13:05 +0800

    P0107 二叉树的层次遍历

---
+++ LeetCode/Problems/P0107_BinaryTreeLevelOrderTraversalII.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 二叉树的层次遍历
+    /// https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/description/
+    public class P0107_BinaryTreeLevelOrderTraversalII
+    {
+        private IList<IList<int>> nodes;
+
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            nodes = new List<IList<int>>();
+            Travel(root, 0);
+            return nodes;
+        }
+
+        private void Travel(TreeNode node, int level)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (nodes.Count <= level)
+            {
+                nodes.Insert(0, new List<int>());
+            }
+            Travel(node.left, level + 1);
+            Travel(node.right, level + 1);
+            nodes[nodes.Count - 1 - level].Add(node.val);
+        }
+    }
+}
