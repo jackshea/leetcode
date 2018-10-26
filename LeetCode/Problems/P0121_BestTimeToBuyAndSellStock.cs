82506276b0346788d9b600c2e0cbfbb133aa645a
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-26 10:07:22 +0800

    P0121 买卖股票的最佳时机

---
+++ LeetCode/Problems/P0121_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 买卖股票的最佳时机
+    /// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/description/
+    public class P0121_BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length <= 0)
+            {
+                return 0;
+            }
+            int soFarMaxProfit = 0;
+            int minPrice = Int32.MaxValue;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                soFarMaxProfit = Math.Max(soFarMaxProfit, prices[i] - minPrice);
+                minPrice = Math.Min(minPrice, prices[i]);
+            }
+
+            return soFarMaxProfit;
+        }
+    }
+}
