d71aaffdce432df76b8823f59c687692941c1423
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-26 10:56:52 +0800

    P0125 验证回文串

---
+++ LeetCode/Problems/P0125_ValidPalindrome.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 验证回文串
+    /// https://leetcode-cn.com/problems/valid-palindrome/description/
+    public class P0125_ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+            while (left < right)
+            {
+                if (!ValidChar(s[left]))
+                {
+                    left++;
+                    continue;
+                }
+
+                if (!ValidChar(s[right]))
+                {
+                    right--;
+                    continue;
+                }
+
+                if (!IgnoreCaseCompareChar(s[left], s[right]))
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        private bool ValidChar(char c)
+        {
+            return c >= '0' && c <= '9' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+        }
+
+        private bool IgnoreCaseCompareChar(char a, char b)
+        {
+            if (a >= 'A' && a <= 'Z')
+            {
+                a = (char)(a + 32);
+            }
+
+            if (b >= 'A' && b <= 'Z')
+            {
+                b = (char)(b + 32);
+            }
+
+            return a == b;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0125_ValidPalindromeTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0125_ValidPalindromeTests
+    {
+        [Test()]
+        public void IsPalindromeTest()
+        {
+            Assert.IsTrue(new P0125_ValidPalindrome().IsPalindrome("A man, a plan, a canal: Panama"));
+        }
+    }
+}
