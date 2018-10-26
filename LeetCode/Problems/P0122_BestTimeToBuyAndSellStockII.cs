38bea76fe2885caa10f382bbfa1b69f890cb724f
谢在奎 <zaikui@lixiaoqian.cn>
2018-10-26 10:22:21 +0800

    P0122 买卖股票的最佳时机 II

---
+++ LeetCode/Problems/P0122_BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 买卖股票的最佳时机 II
+    /// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+    public class P0122_BestTimeToBuyAndSellStockII
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length <= 0)
+            {
+                return 0;
+            }
+            int profit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                var currentProfit = prices[i] - prices[i - 1];
+                if (currentProfit > 0)
+                {
+                    profit += currentProfit;
+                }
+            }
+
+            return profit;
+        }
+    }
+}
