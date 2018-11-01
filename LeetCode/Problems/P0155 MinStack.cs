f6ccf8318e9d05aea2e9183d44845289e0b3338d
谢在奎 <zaikui@lixiaoqian.cn>
2018-11-01 16:14:11 +0800

    P0155 最小栈

---
+++ LeetCode/Problems/P0155 MinStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    public class MinStack
+    {
+        private LinkedList<StackNode> stack;
+
+        public MinStack()
+        {
+            stack = new LinkedList<StackNode>();
+        }
+
+        public void Push(int x)
+        {
+            if (stack.Count == 0)
+            {
+                stack.AddLast(new StackNode { Value = x, MinValue = x });
+            }
+            else
+            {
+                int min = Math.Min(GetMin(), x);
+                stack.AddLast(new StackNode { Value = x, MinValue = min });
+            }
+        }
+
+        public void Pop()
+        {
+            stack.RemoveLast();
+        }
+
+        public int Top()
+        {
+            return stack.Last.Value.Value;
+        }
+
+        public int GetMin()
+        {
+            return stack.Last.Value.MinValue;
+        }
+    }
+
+    public struct StackNode
+    {
+        public int Value;
+        public int MinValue;
+    }
+}
