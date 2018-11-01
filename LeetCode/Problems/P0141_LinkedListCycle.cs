b0f4791960e1fdd56fc1fb3264d693b64a3298ad
谢在奎 <zaikui@lixiaoqian.cn>
2018-11-01 15:45:30 +0800

    P0141 环形链表

---
+++ LeetCode/Problems/P0141_LinkedListCycle.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems
+{
+    /// 环形链表
+    /// https://leetcode-cn.com/problems/linked-list-cycle/description/
+    public class P0141_LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            ListNode oneStep = head;
+            ListNode twoStep = head;
+            while (twoStep.next != null && twoStep.next.next != null)
+            {
+                oneStep = oneStep.next;
+                twoStep = twoStep.next.next;
+                if (oneStep == twoStep)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
