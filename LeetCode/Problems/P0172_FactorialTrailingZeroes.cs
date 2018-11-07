5352a09b5636d43a98eba6813a212fc3db1f3354
谢在奎 <zaikui@lixiaoqian.cn>
2018-11-07 16:12:34 +0800

    P0172 阶乘后的零

---
+++ LeetCode/Problems/P0172_FactorialTrailingZeroes.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Tools
+{
+    /// 阶乘后的零
+    /// https://leetcode-cn.com/problems/factorial-trailing-zeroes/description/
+    public class P0172_FactorialTrailingZeroes
+    {
+        public int TrailingZeroes(int n)
+        {
+            return n == 0 ? 0 : n / 5 + TrailingZeroes(n / 5);
+        }
+    }
+}
