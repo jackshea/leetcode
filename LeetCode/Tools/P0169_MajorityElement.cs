561370f3f77c5e8d41f942dc24894e17ff8e9f97
谢在奎 <zaikui@lixiaoqian.cn>
2018-11-07 14:30:54 +0800

    P0169 求众数

---
+++ LeetCode/Tools/P0169_MajorityElement.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Tools
+{
+    /// 求众数
+    /// https://leetcode-cn.com/problems/majority-element/description/
+    public class P0169_MajorityElement
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int theMajorityElement = nums[0];
+            int count = 1;
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (count == 0)
+                {
+                    theMajorityElement = num;
+                }
+
+                if (num == theMajorityElement)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+
+            return theMajorityElement;
+        }
+    }
+}
