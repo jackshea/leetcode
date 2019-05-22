49d69e90e7ffa4f2059c1dc3de5bbb795790f795
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-22 20:06:39 +0800

    P0191 位1的个数

---
+++ LeetCode/Problems/P191_NumberOf1Bits.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems
+{
+    /// 位1的个数
+    /// https://leetcode-cn.com/problems/number-of-1-bits/
+    public class P191_NumberOf1Bits
+    {
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                if (n % 2 == 1)
+                {
+                    count++;
+                }
+
+                n = n >> 1;
+            }
+
+            return count;
+        }
+    }
+}
