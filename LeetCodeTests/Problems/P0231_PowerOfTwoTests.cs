7303884a3f980baf3d919330892a0a3324181a26
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 16:37:47 +0800

    P0231 2的幂

---
+++ LeetCodeTests/Problems/P0231_PowerOfTwoTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0231_PowerOfTwoTests
+    {
+        [Test()]
+        public void IsPowerOfTwoTest()
+        {
+            var input = -16;
+            var isPowerOfTwo = new P0231_PowerOfTwo().IsPowerOfTwo(input);
+            Assert.IsFalse(isPowerOfTwo);
+        }
+
+        [Test()]
+        public void IsPowerOfTwoTest_1()
+        {
+            var input = -24;
+            var isPowerOfTwo = new P0231_PowerOfTwo().IsPowerOfTwo(input);
+            Assert.IsFalse(isPowerOfTwo);
+        }
+    }
+}
