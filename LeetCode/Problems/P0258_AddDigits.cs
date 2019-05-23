2cf597afda5fa4a0f875a06ec416fbbc78e6b621
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 20:55:42 +0800

    P0258 各位相加

---
+++ LeetCode/Problems/P0258_AddDigits.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems
+{
+    /// 各位相加
+    /// https://leetcode-cn.com/problems/add-digits/comments/
+    public class P0258_AddDigits
+    {
+        public int AddDigits(int num)
+        {
+            while (num >= 10)
+            {
+                int temp = num;
+                num = 0;
+                while (temp > 0)
+                {
+                    num += temp % 10;
+                    temp /= 10;
+                }
+            }
+
+            return num;
+        }
+    }
+}
