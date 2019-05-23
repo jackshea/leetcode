b1dbb240ee6942d741b5e70aab4ce3b4010a5497
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 17:50:28 +0800

    P0235 二叉搜索树的最近公共祖先

--- LeetCode/Problems/P0235_LowestCommonAncestorOfABinarySearchTree.cs
+++ LeetCode/Problems/P0235_LowestCommonAncestorOfABinarySearchTree.cs
@@ -6,7 +6,17 @@
     {
         public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
         {
-            return null;
+            if (p.val < root.val && q.val < root.val)
+            {
+                return LowestCommonAncestor(root.left, p, q);
+            }
+
+            if (p.val > root.val && q.val > root.val)
+            {
+                return LowestCommonAncestor(root.right, p, q);
+            }
+
+            return root;
         }
     }
 }
