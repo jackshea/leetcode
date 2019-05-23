31a7c570932e1ce33ac729d2175345af4f529276
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 15:09:28 +0800

    P0217 存在重复元素

---
+++ LeetCode/Problems/P0217_ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    public class P0217_ContainsDuplicate
+    {
+        /// 存在重复元素
+        /// https://leetcode-cn.com/problems/contains-duplicate/submissions/
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> exists = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (exists.Contains(num))
+                {
+                    return true;
+                }
+
+                exists.Add(num);
+            }
+
+            return false;
+        }
+    }
+}
