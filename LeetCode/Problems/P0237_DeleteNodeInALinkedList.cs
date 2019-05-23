ae2c17c7d5f024a0d4ca9898127584f79c080448
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 18:15:42 +0800

    P0237 删除链表中的节点

---
+++ LeetCode/Problems/P0237_DeleteNodeInALinkedList.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Problems
+{
+    public class P0237_DeleteNodeInALinkedList
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
