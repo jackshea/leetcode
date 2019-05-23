0964858772135d47d7ddb6d14a89ed6f20b1dae7
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 18:31:30 +0800

    P0242 有效的字母异位词

--- LeetCode/Problems/P0237_DeleteNodeInALinkedList.cs
+++ LeetCode/Problems/P0237_DeleteNodeInALinkedList.cs
@@ -1,5 +1,7 @@
 ﻿namespace LeetCode.Problems
 {
+    /// 删除链表中的节点
+    /// https://leetcode-cn.com/problems/delete-node-in-a-linked-list/comments/
     public class P0237_DeleteNodeInALinkedList
     {
         public void DeleteNode(ListNode node)
---
+++ LeetCode/Problems/P0242_ValidAnagram.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 有效的字母异位词
+    /// https://leetcode-cn.com/problems/valid-anagram/
+    public class P0242_ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> chars2Count = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                if (chars2Count.ContainsKey(c))
+                {
+                    chars2Count[c]++;
+                }
+                else
+                {
+                    chars2Count[c] = 1;
+                }
+            }
+
+            foreach (var c in t)
+            {
+                if (!chars2Count.ContainsKey(c))
+                {
+                    return false;
+                }
+
+                chars2Count[c]--;
+                if (chars2Count[c] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
