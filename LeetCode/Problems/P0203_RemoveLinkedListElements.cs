3259cbe1e94a0b287a50c58381222ba21def6ef7
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 11:11:14 +0800

    P0203 移除链表元素

---
+++ LeetCode/Problems/P0203_RemoveLinkedListElements.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Problems
+{
+    /// 移除链表元素
+    /// https://leetcode-cn.com/problems/remove-linked-list-elements/
+    public class P0203_RemoveLinkedListElements
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            while (head != null && head.val == val)
+            {
+                head = head.next;
+            }
+
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.next != null && currentNode.next.val == val)
+                {
+                    currentNode.next = currentNode.next.next;
+                }
+                else
+                {
+                    currentNode = currentNode.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
