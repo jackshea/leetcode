728dda44e1cfe05252aa114a4ca97f8b1aa7d99b
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 15:47:58 +0800

    P0219 存在重复元素 II P0225 用队列实现栈

---
+++ LeetCodeTests/Problems/P0219_ContainsDuplicateIITests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0219_ContainsDuplicateIITests
+    {
+        [Test()]
+        public void ContainsNearbyDuplicateTest()
+        {
+            var input = new int[] { 1, 0, 1, 1 };
+            var k = 1;
+
+            Assert.IsTrue(new P0219_ContainsDuplicateII().ContainsNearbyDuplicate(input, k));
+        }
+    }
+}
