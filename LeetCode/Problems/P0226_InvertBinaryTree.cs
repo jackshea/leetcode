8ffad84589ef65a45033b806eb5c8acd50804ad3
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 16:06:22 +0800

    P0226 翻转二叉树

---
+++ LeetCode/Problems/P0226_InvertBinaryTree.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Problems
+{
+    /// 翻转二叉树
+    /// https://leetcode-cn.com/problems/invert-binary-tree/submissions/
+    public class P0226_InvertBinaryTree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            InvertTree(root.left);
+            InvertTree(root.right);
+            return root;
+        }
+    }
+}
