add8cd8bb802c2e2da1c005a6ff64e1493c9ff20
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-23 11:33:30 +0800

    P0204 计数质数

---
+++ LeetCodeTests/Problems/P0204_CountPrimesTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0204_CountPrimesTests
+    {
+        [Test()]
+        public void CountPrimesTest()
+        {
+            int input = 10;
+            var countPrimes = new P0204_CountPrimes().CountPrimes(input);
+            Assert.AreEqual(4, countPrimes);
+        }
+
+        [Test()]
+        public void CountPrimesTest1()
+        {
+            int input = 2;
+            var countPrimes = new P0204_CountPrimes().CountPrimes(input);
+            Assert.AreEqual(0, countPrimes);
+        }
+    }
+}
