6b06e5484d7292b6f26f3f993165cd4ea5225ca1
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-24 09:06:27 +0800

    P0263 丑数

---
+++ LeetCode/Problems/P0263_UglyNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems
+{
+    /// 丑数
+    /// https://leetcode-cn.com/problems/ugly-number/
+    public class P0263_UglyNumber
+    {
+        public bool IsUgly(int num)
+        {
+            if (num <= 0)
+            {
+                return false;
+            }
+
+            while (num % 2 == 0)
+            {
+                num /= 2;
+            }
+
+            while (num % 3 == 0)
+            {
+                num /= 3;
+            }
+
+            while (num % 5 == 0)
+            {
+                num /= 5;
+            }
+
+            return num == 1;
+        }
+    }
+}
