2322cdaa1323cbc5d04f9d18efe2f49338a7fc6e
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-27 18:09:20 +0800

    P0292 Nim 游戏

---
+++ LeetCode/Problems/P0292_NimGame.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems
+{
+    /// Nim 游戏
+    /// https://leetcode-cn.com/problems/nim-game/
+    public class P0292_NimGame
+    {
+        public bool CanWinNim(int n)
+        {
+            return n % 4 != 0;
+        }
+    }
+}
