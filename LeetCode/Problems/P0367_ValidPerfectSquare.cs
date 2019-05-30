0424e4064a28b3297a577114f2765adf506ea0a7
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-30 10:29:27 +0800

    P0367 有效的完全平方数

---
+++ LeetCode/Problems/P0367_ValidPerfectSquare.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Problems
+{
+    /// 有效的完全平方数
+    /// https://leetcode-cn.com/problems/valid-perfect-square/
+    public class P0367_ValidPerfectSquare
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            long left = 1;
+            long right = num;
+            while (left <= right)
+            {
+                long mid = left + (right - left) / 2;
+
+                long sqr = mid * mid;
+                if (sqr < num)
+                {
+                    left = mid + 1;
+                }
+                else if (sqr > num)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
