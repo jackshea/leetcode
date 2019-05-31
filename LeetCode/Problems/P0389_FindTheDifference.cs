89bbbbf47407d06ae9555072c3a3b30d84ed3a26
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-31 15:47:35 +0800

    P0389 找不同

---
+++ LeetCode/Problems/P0389_FindTheDifference.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Problems
+{
+    /// 找不同
+    /// https://leetcode-cn.com/problems/find-the-difference/
+    public class P0389_FindTheDifference
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            int sumS = 0;
+            foreach (var c in s)
+            {
+                sumS += c;
+            }
+
+            int sumT = 0;
+            foreach (var c in t)
+            {
+                sumT += c;
+            }
+
+            return (char) (sumT - sumS);
+        }
+    }
+}
