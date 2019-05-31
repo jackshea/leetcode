cb456e803d845b77df146d8588672da47a552cbf
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-31 15:28:29 +0800

    P0383 赎金信

---
+++ LeetCode/Problems/P0383_RansomNote.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems
+{
+    /// 赎金信
+    /// https://leetcode-cn.com/problems/ransom-note/
+    public class P0383_RansomNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            int[] magazineCharCount = new int[26];
+            foreach (var c in magazine)
+            {
+                magazineCharCount[c - 'a']++;
+            }
+
+            foreach (var c in ransomNote)
+            {
+                magazineCharCount[c - 'a']--;
+                if (magazineCharCount[c - 'a'] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
