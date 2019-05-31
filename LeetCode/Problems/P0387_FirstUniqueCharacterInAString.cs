7711fbd959ae75e79a8378d39b6ee0935394ee5a
谢在奎 <zaikui@lixiaoqian.cn>
2019-05-31 15:38:43 +0800

    P0387 字符串中的第一个唯一字符

---
+++ LeetCode/Problems/P0387_FirstUniqueCharacterInAString.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems
+{
+    /// 字符串中的第一个唯一字符
+    /// https://leetcode-cn.com/problems/first-unique-character-in-a-string/
+    public class P0387_FirstUniqueCharacterInAString
+    {
+        public int FirstUniqChar(string s)
+        {
+            int[] charCount = new int[26];
+            foreach (var c in s)
+            {
+                charCount[c - 'a']++;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount[s[i] - 'a'] == 1)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
