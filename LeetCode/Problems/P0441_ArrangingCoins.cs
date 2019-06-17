ef2651e5fe04ed0fbe58f3c473e4e7cb654dfa78
谢在奎 <zaikui@lixiaoqian.cn>
2019-06-17 17:25:37 +0800

    P0441 排列硬币

---
+++ LeetCode/Problems/P0441_ArrangingCoins.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Problems
+{
+    /// 排列硬币
+    /// https://leetcode-cn.com/problems/arranging-coins/
+    public class P0441_ArrangingCoins
+    {
+        public int ArrangeCoins(int n)
+        {
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                n -= i;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
