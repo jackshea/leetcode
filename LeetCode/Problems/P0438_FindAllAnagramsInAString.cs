e3b9bbbec81ce74a6da60bce96bac991c84c3528
谢在奎 <zaikui@lixiaoqian.cn>
2019-06-17 17:00:42 +0800

    P0438 找到字符串中所有字母异位词

---
+++ LeetCode/Problems/P0438_FindAllAnagramsInAString.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 找到字符串中所有字母异位词
+    /// https://leetcode-cn.com/problems/find-all-anagrams-in-a-string/
+    public class P0438_FindAllAnagramsInAString
+    {
+        private int[] _targetChars = new int[26];
+        private int[] _currentChars = new int[26];
+
+        public IList<int> FindAnagrams(string s, string p)
+        {
+            IList<int> result = new List<int>();
+
+            if (s.Length < p.Length)
+            {
+                return result;
+            }
+
+            foreach (var c in p)
+            {
+                _targetChars[c - 'a']++;
+            }
+
+            int leftIndex = 0;
+            int rightIndex = 0;
+
+            while (rightIndex < p.Length)
+            {
+                _currentChars[s[rightIndex++] - 'a']++;
+            }
+
+            if (IsSame())
+            {
+                result.Add(leftIndex);
+            }
+
+            while (rightIndex < s.Length)
+            {
+                _currentChars[s[leftIndex++] - 'a']--;
+                _currentChars[s[rightIndex++] - 'a']++;
+                if (IsSame())
+                {
+                    result.Add(leftIndex);
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsSame()
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                if (_targetChars[i] != _currentChars[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
