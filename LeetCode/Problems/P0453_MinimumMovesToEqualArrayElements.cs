10e82a1158de5175abba620acbb902c9851ca56c
谢在奎 <jackshea@163.com>
2019-09-03 00:42:30 +0800

    P0453 最小移动次数使数组元素相等

---
+++ LeetCode/Problems/P0453_MinimumMovesToEqualArrayElements.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems
+{
+    /// 最小移动次数使数组元素相等
+    /// https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements/
+    /// n-1 个数+1 相当于某个数-1. 即问题转化为数组中的数都转为最小的数需要执行多少次-1的操作。
+    /// 需要处理数组中的数大于整型最大值的值的情况
+    public class P0453_MinimumMovesToEqualArrayElements
+    {
+        public int MinMoves(int[] nums)
+        {
+            int min = nums.Min();
+            int result = 0;
+            foreach (var num in nums)
+            {
+                result += num - min;
+            }
+
+            return result;
+        }
+    }
+}
