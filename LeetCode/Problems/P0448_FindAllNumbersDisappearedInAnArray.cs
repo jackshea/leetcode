b812566dc56bb9285d14189c76516712e6431d06
谢在奎 <jackshea@163.com>
2019-09-03 00:18:46 +0800

    P0448 找到所有数组中消失的数字

---
+++ LeetCode/Problems/P0448_FindAllNumbersDisappearedInAnArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 找到所有数组中消失的数字
+    /// https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/
+    public class P0448_FindAllNumbersDisappearedInAnArray
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            IList<int> result = new List<int>();
+            foreach (var num in nums)
+            {
+                nums[Math.Abs(num) - 1] = -Math.Abs(nums[Math.Abs(num) - 1]);
+            }
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    result.Add(i + 1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
