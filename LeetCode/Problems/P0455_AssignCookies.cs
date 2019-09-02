0a1a4c8fa18eae8d3a2c0f0cbbaf1882b00434d4
谢在奎 <jackshea@163.com>
2019-09-03 01:07:25 +0800

    P0455 分发饼干

---
+++ LeetCode/Problems/P0455_AssignCookies.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 分发饼干
+    /// https://leetcode-cn.com/problems/assign-cookies/
+    public class P0455_AssignCookies
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+            int gIndex = 0;
+            int result = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (gIndex >= g.Length)
+                {
+                    break;
+                }
+
+                if (g[gIndex] <= s[i])
+                {
+                    result++;
+                    gIndex++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
