e6b4c3a65ce266e6ce4adc05285e33708147d1aa
谢在奎 <jackshea@163.com>
2019-09-04 20:42:34 +0800

    P0459 重复的子字符串

---
+++ LeetCode/Problems/P0459_RepeatedSubstringPattern.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems
+{
+    /// 重复的子字符串
+    /// https://leetcode-cn.com/problems/repeated-substring-pattern/comments/
+    public class P0459_RepeatedSubstringPattern
+    {
+        public bool RepeatedSubstringPattern(string s)
+        {
+            int len = s.Length;
+            for (int i = 1; i <= len / 2; i++)
+            {
+                if (len % i != 0)
+                {
+                    continue;
+                }
+
+                string substring = s.Substring(0, i);
+                if (IsRepeated(s,substring))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsRepeated(string source, string substring)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                var subLen = substring.Length;
+                if (source[i] != substring[i % subLen])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
