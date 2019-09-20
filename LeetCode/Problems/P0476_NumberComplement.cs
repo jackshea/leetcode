ab3d093948275fb878bb19f9b0b6559d556fc43b
谢在奎 <jackshea@163.com>
2019-09-20 22:26:42 +0800

    P0476 数字的补数

---
+++ LeetCode/Problems/P0476_NumberComplement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 数字的补数
+    /// https://leetcode-cn.com/problems/number-complement/
+    public class P0476_NumberComplement
+    {
+        public int FindComplement(int num)
+        {
+            long max = 2;
+
+            while (num >= max)
+            {
+                max *= 2;
+            }
+
+            return (int)(max - 1 - num);
+        }
+    }
+}
