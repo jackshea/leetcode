52a71c0bca76da70d3d50485d00a9b4986151aae
谢在奎 <jackshea@163.com>
2019-09-21 22:14:02 +0800

    P0492 构造矩形

---
+++ LeetCode/Problems/P0492_ConstructTheRectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 构造矩形
+    /// https://leetcode-cn.com/problems/construct-the-rectangle/submissions/
+    public class P0492_ConstructTheRectangle
+    {
+        public int[] ConstructRectangle(int area)
+        {
+            int width = (int)Math.Sqrt(area);
+            while (area % width != 0)
+            {
+                width--;
+            }
+
+            return new[] {area / width, width};
+        }
+    }
+}
