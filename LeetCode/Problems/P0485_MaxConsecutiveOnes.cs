20e27092169b1211fba38ff978c3ff1b292e77d1
谢在奎 <jackshea@163.com>
2019-09-21 21:58:15 +0800

    P0485 最大连续1的个数

---
+++ LeetCode/Problems/P0485_MaxConsecutiveOnes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 最大连续1的个数
+    /// https://leetcode-cn.com/problems/max-consecutive-ones/
+    public class P0485_MaxConsecutiveOnes
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int maxConsecutiveOnes = 0;
+            int consecutiveOnes = 0;
+            foreach (var num in nums)
+            {
+                if (num == 1)
+                {
+                    consecutiveOnes++;
+                    maxConsecutiveOnes = Math.Max(maxConsecutiveOnes, consecutiveOnes);
+                }
+                else
+                {
+                    consecutiveOnes = 0;
+                }
+            }
+
+            return maxConsecutiveOnes;
+        }
+    }
+}
