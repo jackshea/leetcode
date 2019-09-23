9ccb862a11673f937dd888c36f24e286397ee577
谢在奎 <jackshea@163.com>
2019-09-23 18:04:51 +0800

    P0504 七进制数

---
+++ LeetCode/Problems/P0504_Base7.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    /// 七进制数
+    /// https://leetcode-cn.com/problems/base-7/
+    public class P0504_Base7
+    {
+        public string ConvertToBase7(int num)
+        {
+            if (num == 0)
+            {
+                return "0";
+            }
+
+            bool isNegative = num < 0;
+            int absNum = Math.Abs(num);
+            StringBuilder sb = new StringBuilder();
+ 
+            while (absNum != 0)
+            {
+                sb.Append(absNum % 7);
+                absNum /= 7;
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            if (isNegative)
+            {
+                result.Append('-');
+            }
+
+            for (int i = sb.Length - 1; i >= 0; i--)
+            {
+                result.Append(sb[i]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
