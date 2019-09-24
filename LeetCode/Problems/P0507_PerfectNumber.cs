15bcaca0e010063c222e0f87bcaa5370a6e5f2c1
谢在奎 <jackshea@163.com>
2019-09-24 19:10:49 +0800

    P0507 完美数

---
+++ LeetCode/Problems/P0507_PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 完美数
+    /// https://leetcode-cn.com/problems/perfect-number/
+    public class P0507_PerfectNumber
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            if (num == 1)
+            {
+                return false;
+            }
+
+            int sum = 1;
+
+            double sqrt = Math.Sqrt(num);
+
+            for (int i = 2; i <= sqrt; i++)
+            {
+                if (i == sqrt)
+                {
+                    sum += i;
+                    break;
+                }
+
+                if (num % i == 0)
+                {
+                    sum += i;
+                    sum += num / i;
+                }
+            }
+
+            return sum == num;
+        }
+    }
+}
