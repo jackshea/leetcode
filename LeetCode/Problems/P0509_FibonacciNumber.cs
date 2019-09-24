eeeda3342db4f782a54863ed4c604aa320ad8dc4
谢在奎 <jackshea@163.com>
2019-09-24 19:25:27 +0800

    P0509 斐波那契数

---
+++ LeetCode/Problems/P0509_FibonacciNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems
+{
+    /// 斐波那契数
+    /// https://leetcode-cn.com/problems/fibonacci-number/
+    public class P0509_FibonacciNumber
+    {
+        public int Fib(int N)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            if (N < 2)
+            {
+                return N;
+            }
+
+            for (int i = 2; i <= N; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c;
+        }
+    }
+}
