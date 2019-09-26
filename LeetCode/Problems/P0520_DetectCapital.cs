4de42d30fa2fefca13bedf9e67ff2d9f48fd0f14
谢在奎 <jackshea@163.com>
2019-09-26 12:06:38 +0800

    P0520 检测大写字母

---
+++ LeetCode/Problems/P0520_DetectCapital.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCode.Problems
+{
+    /// 检测大写字母
+    /// https://leetcode-cn.com/problems/detect-capital/
+    public class P0520_DetectCapital
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            if (word.Length <= 1)
+            {
+                return true;
+            }
+
+            if (char.IsLower(word[0]))
+            {
+                // 验证是否是全小写的情况 
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (!char.IsLower(word[i]))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                if (char.IsUpper(word[1]))
+                {
+                    // 验证是否是全大写的情况
+                    for (int i = 2; i < word.Length; i++)
+                    {
+                        if (!char.IsUpper(word[i]))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+                else
+                {
+                    // 验证是否是首字母大写的情况
+                    for (int i = 2; i < word.Length; i++)
+                    {
+                        if (!char.IsLower(word[i]))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+            }
+        }
+    }
+}
