d9c8e895d356c8a94fd660fba075b8ea2085984e
谢在奎 <jackshea@163.com>
2019-09-26 12:13:42 +0800

    P0521 最长特殊序列 Ⅰ

---
+++ LeetCode/Problems/P0521_LongestUncommonSubsequenceI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 最长特殊序列 Ⅰ
+    /// https://leetcode-cn.com/problems/longest-uncommon-subsequence-i/
+    public class P0521_LongestUncommonSubsequenceI
+    {
+        public int FindLUSlength(string a, string b)
+        {
+            if (a == b)
+            {
+                return -1;
+            }
+
+            return Math.Max(a.Length, b.Length);
+        }
+    }
+}
