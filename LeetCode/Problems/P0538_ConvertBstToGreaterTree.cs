db0105e494152634e97e24a490bc255675b8d882
谢在奎 <jackshea@163.com>
2019-09-26 16:39:17 +0800

    P0538 把二叉搜索树转换为累加树

---
+++ LeetCode/Problems/P0538_ConvertBstToGreaterTree.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems
+{
+    /// 把二叉搜索树转换为累加树
+    /// https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+    public class P0538_ConvertBstToGreaterTree
+    {
+        private int sum = 0;
+
+        public TreeNode ConvertBST(TreeNode root)
+        {
+            Travel(root);
+            return root;
+        }
+
+        private void Travel(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Travel(node.right);
+            node.val += sum;
+            sum = node.val;
+            Travel(node.left);
+        }
+    }
+}
