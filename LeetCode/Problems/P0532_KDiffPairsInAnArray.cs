856afe0314d794d0588fa68a00c410eb9cadf83c
谢在奎 <jackshea@163.com>
2019-09-26 15:03:29 +0800

    P0532 数组中的K-diff数对

---
+++ LeetCode/Problems/P0532_KDiffPairsInAnArray.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 数组中的K-diff数对
+    /// https://leetcode-cn.com/problems/k-diff-pairs-in-an-array/
+    public class P0532_KDiffPairsInAnArray
+    {
+        public int FindPairs(int[] nums, int k)
+        {
+            if (k < 0)
+            {
+                return 0;
+            }
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    dic[num]++;
+                }
+                else
+                {
+                    dic.Add(num, 1);
+                }
+            }
+
+            int result = 0;
+            foreach (var num in dic.Keys)
+            {
+                if (k == 0)
+                {
+                    if (dic[num] > 1)
+                    {
+                        result++;
+                    }
+                }
+                else if (dic.ContainsKey(num + k))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
