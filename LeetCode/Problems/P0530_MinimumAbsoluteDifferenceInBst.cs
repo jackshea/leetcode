6dc1396f10c6c9afcc4a25118ff365523c59f37e
谢在奎 <jackshea@163.com>
2019-09-26 14:08:17 +0800

    P0530 二叉搜索树的最小绝对差

---
+++ LeetCode/Problems/P0530_MinimumAbsoluteDifferenceInBst.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 二叉搜索树的最小绝对差
+    /// https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+    public class P0530_MinimumAbsoluteDifferenceInBst
+    {
+        private TreeNode preNode;
+        private int miniDiff = Int32.MaxValue;
+        public int GetMinimumDifference(TreeNode root)
+        {
+            Travel(root);
+            return miniDiff;
+        }
+
+        private void Travel(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Travel(node.left);
+            if (preNode != null)
+            {
+                miniDiff = Math.Min(miniDiff, Math.Abs(node.val - preNode.val));
+            }
+            preNode = node;
+            Travel(node.right);
+        }
+    }
+}
