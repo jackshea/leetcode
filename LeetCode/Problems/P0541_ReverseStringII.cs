8c742d11df66bf4e505c2527423c55f08603ecac
谢在奎 <jackshea@163.com>
2019-09-27 16:08:00 +0800

    P0541 反转字符串 II

---
+++ LeetCode/Problems/P0541_ReverseStringII.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Problems
+{
+    /// 反转字符串 II
+    /// https://leetcode-cn.com/problems/reverse-string-ii/
+    public class P0541_ReverseStringII
+    {
+        public string ReverseStr(string s, int k)
+        {
+            var charArray = s.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                int m = i % (2 * k);
+                if (m < k)
+                {
+                    int n = i - m + k - m - 1;
+                    n = i - m + k - 1 < charArray.Length ? n : charArray.Length - m - 1;
+                    if (i < n)
+                    {
+                        Swap(ref charArray[i], ref charArray[n]);
+                    }
+                }
+            }
+
+            return new string(charArray);
+        }
+
+        private void Swap(ref char a, ref char b)
+        {
+            char temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0541_ReverseStringIITests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0541_ReverseStringIITests
+    {
+        [Test()]
+        public void ReverseStrTest()
+        {
+            var test = new P0541_ReverseStringII();
+
+            Assert.AreEqual("gfedcba", test.ReverseStr("abcdefg", 8));
+        }
+    }
+}
