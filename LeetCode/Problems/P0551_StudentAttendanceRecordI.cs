da34f6a14f9517abb112f3295a7c4f0f7bfc19f5
谢在奎 <jackshea@163.com>
2019-09-28 13:53:07 +0800

    P0551 学生出勤记录 I

---
+++ LeetCode/Problems/P0551_StudentAttendanceRecordI.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Problems
+{
+    /// 学生出勤记录 I
+    /// https://leetcode-cn.com/problems/student-attendance-record-i/
+    public class P0551_StudentAttendanceRecordI
+    {
+        public bool CheckRecord(string s)
+        {
+            int a = 0;
+            int contiL = 0;
+
+            foreach (var c in s)
+            {
+                if (c == 'A')
+                {
+                    a++;
+                    if (a > 1)
+                    {
+                        return false;
+                    }
+                }
+
+                if (c == 'L')
+                {
+                    contiL++;
+                    if (contiL>2)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    contiL = 0;
+                }
+            }
+
+            return true;
+        }
+    }
+}
