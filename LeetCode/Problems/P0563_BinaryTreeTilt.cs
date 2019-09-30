2a52290cdd0ef48e372628cabffa7a315e845d66
谢在奎 <jackshea@163.com>
2019-09-30 13:05:24 +0800

    P0563 二叉树的坡度

---
+++ LeetCode/Problems/P0563_BinaryTreeTilt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 二叉树的坡度
+    /// https://leetcode-cn.com/problems/binary-tree-tilt/
+    public class P0563_BinaryTreeTilt
+    {
+        private int result = 0;
+        public int FindTilt(TreeNode root)
+        {
+            Sum(root);
+            return result;
+        }
+
+        private int Sum(TreeNode node)
+        {
+            if (node==null)
+            {
+                return 0;
+            }
+
+            int left = Sum(node.left);
+            int right = Sum(node.right);
+            result += Math.Abs(left - right);
+            return left + right + node.val;
+        }
+    }
+}
