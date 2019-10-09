da62eb561f3de9073c8127ec8c9d927318260c63
谢在奎 <jackshea@163.com>
2019-10-09 09:59:01 +0800

    P0572 另一个树的子树

---
+++ LeetCode/Problems/P0572_SubtreeOfAnotherTree.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems
+{
+    /// 另一个树的子树
+    /// https://leetcode-cn.com/problems/subtree-of-another-tree/
+    public class P0572_SubtreeOfAnotherTree
+    {
+        public bool IsSubtree(TreeNode s, TreeNode t)
+        {
+            if (s == null)
+            {
+                return t == null;
+            }
+
+            return IsSameTree(s, t) || IsSubtree(s.left, t) || IsSubtree(s.right, t);
+        }
+
+        private bool IsSameTree(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null)
+            {
+                return true;
+            }
+
+            return s != null && t != null && s.val == t.val && IsSameTree(s.left, t.left) && IsSameTree(s.right, t.right);
+        }
+    }
+}
