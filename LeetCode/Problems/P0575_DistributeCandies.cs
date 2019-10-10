f8e879ae55f96ee823cf05ea06c3b71454be29c8
谢在奎 <jackshea@163.com>
2019-10-10 16:51:04 +0800

    P0575 分糖果

---
+++ LeetCode/Problems/P0575_DistributeCandies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 分糖果
+    /// https://leetcode-cn.com/problems/distribute-candies
+    public class P0575_DistributeCandies
+    {
+        public int DistributeCandies(int[] candies)
+        {
+            HashSet<int> kinds = new HashSet<int>();
+            foreach (var candy in candies)
+            {
+                kinds.Add(candy);
+            }
+
+            return Math.Min(kinds.Count, candies.Length / 2);
+        }
+    }
+}
