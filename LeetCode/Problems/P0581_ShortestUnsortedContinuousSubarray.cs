0ec966dd152c8a169c2e6301416206a2b8711739
谢在奎 <jackshea@163.com>
2019-10-10 17:46:53 +0800

    P0581 最短无序连续子数组

---
+++ LeetCode/Problems/P0581_ShortestUnsortedContinuousSubarray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 最短无序连续子数组
+    /// https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/
+    public class P0581_ShortestUnsortedContinuousSubarray
+    {
+        public int FindUnsortedSubarray(int[] nums)
+        {
+            int[] sorted = new int[nums.Length];
+            Array.Copy(nums, sorted, nums.Length);
+            Array.Sort(sorted);
+
+            int left = 0;
+            int right = nums.Length - 1;
+            for (left = 0; left < nums.Length; left++)
+            {
+                if (nums[left] != sorted[left])
+                {
+                    break;
+                }
+            }
+
+            for (right = nums.Length - 1; right > left; right--)
+            {
+                if (nums[right] != sorted[right])
+                {
+                    break;
+                }
+            }
+
+            return right - left + 1;
+        }
+    }
+}
