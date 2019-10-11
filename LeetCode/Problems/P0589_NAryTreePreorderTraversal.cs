0063471419bde32a5ec545eec36c792dc25d6828
谢在奎 <jackshea@163.com>
2019-10-11 11:22:04 +0800

    P0589 N叉树的前序遍历

---
+++ LeetCode/Problems/P0589_NAryTreePreorderTraversal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// N叉树的前序遍历
+    /// https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+    public class P0589_NAryTreePreorderTraversal
+    {
+        private List<int> result = new List<int>();
+
+        public IList<int> Preorder(Node root)
+        {
+            if (root == null)
+            {
+                return result;
+            }
+
+            result.Add(root.val);
+            foreach (var node in root.children)
+            {
+                Preorder(node);
+            }
+
+            return result;
+        }
+
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node()
+            {
+            }
+
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+        }
+    }
+}
