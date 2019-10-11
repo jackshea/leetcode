cfac723edac36e5e9758b6992170b522e60b2c54
谢在奎 <jackshea@163.com>
2019-10-11 11:28:50 +0800

    P0590 N叉树的后序遍历

---
+++ LeetCode/Problems/P0590_NAryTreePostorderTraversal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// N叉树的后序遍历
+    /// https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal/
+    public class P0590_NAryTreePostorderTraversal
+    {
+        private List<int> result = new List<int>();
+
+        public IList<int> Postorder(Node root)
+        {
+            if (root == null)
+            {
+                return result;
+            }
+
+            foreach (var node in root.children)
+            {
+                Postorder(node);
+            }
+
+            result.Add(root.val);
+            return result;
+        }
+
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node()
+            {
+            }
+
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+        }
+    }
+}
