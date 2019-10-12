7287a861aa99d67826a12a613997a020d5009b0a
谢在奎 <jackshea@163.com>
2019-10-12 13:51:44 +0800

    P0598 范围求和 II

---
+++ LeetCode/Problems/P0598_RangeAdditionII.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 范围求和 II
+    /// https://leetcode-cn.com/problems/range-addition-ii/
+    public class P0598_RangeAdditionII
+    {
+        public int MaxCount(int m, int n, int[][] ops)
+        {
+            int a = m;
+            int b = n;
+            foreach (int[] op in ops)
+            {
+                a = Math.Min(a, op[0]);
+                b = Math.Min(b, op[1]);
+            }
+
+            return a * b;
+        }
+    }
+}
