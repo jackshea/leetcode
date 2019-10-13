eee584699f10ee8322214e95239abac8443c53cc
谢在奎 <jackshea@163.com>
2019-10-13 16:39:30 +0800

    P0599 两个列表的最小索引总和

---
+++ LeetCode/Problems/P0599_MinimumIndexSumOfTwoLists.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 两个列表的最小索引总和
+    /// https://leetcode-cn.com/problems/minimum-index-sum-of-two-lists/
+    public class P0599_MinimumIndexSumOfTwoLists
+    {
+        public string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            for (var i = 0; i < list1.Length; i++)
+            {
+                dic[list1[i]] = i;
+            }
+
+            List<string> result = new List<string>();
+            int minIndexSum = Int32.MaxValue;
+            for (var i = 0; i < list2.Length; i++)
+            {
+                if (dic.ContainsKey(list2[i]))
+                {
+                    int indexSum = dic[list2[i]] + i;
+                    if (minIndexSum < indexSum)
+                    {
+                        continue;
+                    }
+                    else if (minIndexSum > indexSum)
+                    {
+                        result.Clear();
+                        result.Add(list2[i]);
+                        minIndexSum = indexSum;
+                    }
+                    else
+                    {
+                        result.Add(list2[i]);
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
