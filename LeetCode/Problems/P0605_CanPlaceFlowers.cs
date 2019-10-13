250336895e80b7793f2faec7f73aae83682ecf1e
谢在奎 <jackshea@163.com>
2019-10-13 17:07:10 +0800

    P0605 种花问题

---
+++ LeetCode/Problems/P0605_CanPlaceFlowers.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems
+{
+    /// 种花问题
+    /// https://leetcode-cn.com/problems/can-place-flowers/
+    public class P0605_CanPlaceFlowers
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0
+                    && (i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                    if (n <= 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return n <= 0;
+        }
+    }
+}
