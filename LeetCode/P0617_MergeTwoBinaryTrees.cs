4228919713a0f6d211622fa16b7f7a7a39f368ca
谢在奎 <jackshea@163.com>
2019-10-14 14:30:01 +0800

    P0617 合并二叉树

---
+++ LeetCode/P0617_MergeTwoBinaryTrees.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Problems;
+
+namespace LeetCode
+{
+    /// 合并二叉树
+    /// https://leetcode-cn.com/problems/merge-two-binary-trees/
+    public class P0617_MergeTwoBinaryTrees
+    {
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null)
+            {
+                return null;
+            }
+
+            var node = new TreeNode(0);
+            if (t1 == null)
+            {
+                node.val = t2.val;
+                node.left = MergeTrees(null, t2.left);
+                node.right = MergeTrees(null, t2.right);
+            }
+            else if (t2 == null)
+            {
+                node.val = t1.val;
+                node.left = MergeTrees(t1.left, null);
+                node.right = MergeTrees(t1.right, null);
+            }
+            else
+            {
+                node.val = t1.val + t2.val;
+                node.left = MergeTrees(t1.left, t2.left);
+                node.right = MergeTrees(t1.right, t2.right);
+            }
+
+            return node;
+        }
+    }
+}
