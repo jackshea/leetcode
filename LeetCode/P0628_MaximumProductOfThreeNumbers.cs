8850affab95cbb71a6b8fbadffc8efbe7a1ea2ef
谢在奎 <jackshea@163.com>
2019-10-15 14:20:24 +0800

    P0628 三个数的最大乘积

---
+++ LeetCode/P0628_MaximumProductOfThreeNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LeetCode
+{
+    /// 三个数的最大乘积
+    /// https://leetcode-cn.com/problems/maximum-product-of-three-numbers/
+    public class P0628_MaximumProductOfThreeNumbers
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            return nums[nums.Length - 1] * Math.Max(nums[0] * nums[1], nums[nums.Length - 3] * nums[nums.Length - 2]);
+        }
+    }
+}
