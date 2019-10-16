b7a70452a065bee35b1ae8f2e4aacce6ea84b068
谢在奎 <jackshea@163.com>
2019-10-16 16:59:58 +0800

    P0643 子数组最大平均数 I

---
+++ LeetCode/P0643_MaximumAverageSubarrayI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode
+{
+    /// 子数组最大平均数 I
+    /// https://leetcode-cn.com/problems/maximum-average-subarray-i/
+    public class P0643_MaximumAverageSubarrayI
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            int sum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                sum += nums[i];
+            }
+
+            double maxSum = sum;
+
+            for (int i = k; i < nums.Length; i++)
+            {
+                sum += -nums[i - k] + nums[i];
+                maxSum = Math.Max(maxSum, sum);
+            }
+
+            return maxSum / k;
+        }
+    }
+}
