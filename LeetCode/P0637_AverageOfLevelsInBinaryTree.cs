0998f8c04390bbb46625e9e5dcb773a2e3afe876
谢在奎 <jackshea@163.com>
2019-10-16 16:47:12 +0800

    P0637 二叉树的层平均值

---
+++ LeetCode/P0637_AverageOfLevelsInBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using LeetCode.Problems;
+
+namespace LeetCode
+{
+    /// 二叉树的层平均值
+    /// https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+    public class P0637_AverageOfLevelsInBinaryTree
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            IList<double> result = new List<double>();
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            while (q.Count != 0)
+            {
+                int count = q.Count;
+                double sum = 0;
+                for (int i = 0; i < count; i++)
+                {
+                    var top = q.Dequeue();
+                    sum += top.val;
+                    if (top.left != null)
+                    {
+                        q.Enqueue(top.left);
+                    }
+                    if (top.right != null)
+                    {
+                        q.Enqueue(top.right);
+                    }
+                }
+                result.Add(sum / count);
+            }
+
+            return result;
+        }
+    }
+}
