f7b6ef5b71aaf8559ce0809be07061f8f9e40ff1
谢在奎 <jackshea@163.com>
2019-10-17 17:00:28 +0800

    P0645 错误的集合

---
+++ LeetCode/Problems/P0645_SetMismatch.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Problems
+{
+    /// 错误的集合
+    /// https://leetcode-cn.com/problems/set-mismatch/
+    public class P0645_SetMismatch
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            int[] counter = new int[nums.Length + 1];
+            foreach (var n in nums)
+            {
+                counter[n]++;
+            }
+
+            int[] result = new int[2];
+            for (var i = 1; i < counter.Length; i++)
+            {
+                if (counter[i] == 0)
+                {
+                    result[1] = i;
+                }
+                else if (counter[i] == 2)
+                {
+                    result[0] = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
