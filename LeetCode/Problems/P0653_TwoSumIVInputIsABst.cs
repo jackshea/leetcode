f95d14fa89305dbd511798874bad2697a6ed7df7
谢在奎 <jackshea@163.com>
2019-10-17 17:38:29 +0800

    P0653 两数之和 IV - 输入 BST

---
+++ LeetCode/Problems/P0653_TwoSumIVInputIsABst.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 两数之和 IV - 输入 BST
+    /// https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/
+    public class P0653_TwoSumIVInputIsABst
+    {
+        private List<int> sorted = new List<int>();
+
+        public bool FindTarget(TreeNode root, int k)
+        {
+            Traversal(root);
+            int left = 0;
+            int right = sorted.Count - 1;
+            while (left < right)
+            {
+                var sum = sorted[left] + sorted[right];
+
+                if (sum == k)
+                {
+                    return true;
+                }
+                else if (sum < k)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return false;
+        }
+
+        private void Traversal(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Traversal(node.left);
+            sorted.Add(node.val);
+            Traversal(node.right);
+        }
+    }
+}
