35459ed055d0634ac650f63e1365f853a94d18fe
谢在奎 <jackshea@163.com>
2019-10-18 15:40:19 +0800

    P0657 机器人能否返回原点

---
+++ LeetCode/Problems/P0657_RobotReturnToOrigin.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Problems
+{
+    /// 机器人能否返回原点
+    /// https://leetcode-cn.com/problems/robot-return-to-origin/
+    public class P0657_RobotReturnToOrigin
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int h = 0;
+            int v = 0;
+            foreach (var move in moves)
+            {
+                switch (move)
+                {
+                    case 'R':
+                        h++;
+                        break;
+                    case 'L':
+                        h--;
+                        break;
+                    case 'U':
+                        v++;
+                        break;
+                    case 'D':
+                        v--;
+                        break;
+                }
+            }
+
+            return h == 0 && v == 0;
+        }
+    }
+}
