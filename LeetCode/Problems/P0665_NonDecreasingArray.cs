4250ed5cc06b08a93b8973cf1fb54b09426e8a0d
谢在奎 <jackshea@163.com>
2019-10-19 20:11:58 +0800

    P0665 非递减数列

---
+++ LeetCode/Problems/P0665_NonDecreasingArray.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Problems
+{
+    /// 非递减数列
+    /// https://leetcode-cn.com/problems/non-decreasing-array/
+    public class P0665_NonDecreasingArray
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            if (nums.Length < 3)
+            {
+                return true;
+            }
+
+            int count = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    count++;
+                    if (count > 1)
+                    {
+                        return false;
+                    }
+                    if (i > 0 && nums[i - 1] > nums[i + 1])
+                    {
+                        nums[i + 1] = nums[i];
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
