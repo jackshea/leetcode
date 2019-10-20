ffd1184280cb8c407773a964f761b43142529c30
谢在奎 <jackshea@163.com>
2019-10-20 16:58:29 +0800

    P0671 二叉树中第二小的节点

---
+++ LeetCode/Problems/P0671_SecondMinimumNodeInABinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 二叉树中第二小的节点
+    /// https://leetcode-cn.com/problems/second-minimum-node-in-a-binary-tree/
+    public class P0671_SecondMinimumNodeInABinaryTree
+    {
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            return Min(root, root.val);
+        }
+
+        private int Min(TreeNode tn, int mini)
+        {
+            if (tn == null)
+            {
+                return -1;
+            }
+
+            if (tn.val > mini)
+            {
+                return tn.val;
+            }
+
+            var left = Min(tn.left, mini);
+            var right = Min(tn.right, mini);
+            if (left == -1)
+            {
+                return right;
+            }
+            if (right == -1)
+            {
+                return left;
+            }
+
+            return Math.Min(left, right);
+        }
+    }
+}
