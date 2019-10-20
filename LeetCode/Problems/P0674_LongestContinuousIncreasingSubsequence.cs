1a15ae61847c7282fb81fca07d61c0295fd61d21
谢在奎 <jackshea@163.com>
2019-10-20 17:21:52 +0800

    P0674 最长连续递增序列

---
+++ LeetCode/Problems/P0674_LongestContinuousIncreasingSubsequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 最长连续递增序列
+    /// https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence/
+    public class P0674_LongestContinuousIncreasingSubsequence
+    {
+        public int FindLengthOfLCIS(int[] nums)
+        {
+            if (nums.Length < 2)
+            {
+                return nums.Length;
+            }
+
+            int result = 1;
+            int curSubsLength = 1;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] < nums[i + 1])
+                {
+                    curSubsLength++;
+                    result = Math.Max(result, curSubsLength);
+                }
+                else
+                {
+                    curSubsLength = 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
