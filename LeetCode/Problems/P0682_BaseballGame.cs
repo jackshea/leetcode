b04871aaa1db611b03005f316428c1f81e6bce46
谢在奎 <jackshea@163.com>
2019-10-28 20:58:21 +0800

    P0682 棒球比赛

---
+++ LeetCode/Problems/P0682_BaseballGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems
+{
+    /// 棒球比赛
+    /// https://leetcode-cn.com/problems/baseball-game/
+    public class P0682_BaseballGame
+    {
+        public int CalPoints(string[] ops)
+        {
+            Stack<int> score = new Stack<int>();
+            foreach (var op in ops)
+            {
+                switch (op)
+                {
+                    case "+":
+                        int temp = score.Pop();
+                        int currentScore = temp + score.Peek();
+                        score.Push(temp);
+                        score.Push(currentScore);
+                        break;
+                    case "D":
+                        score.Push(score.Peek() * 2);
+                        break;
+                    case "C":
+                        score.Pop();
+                        break;
+                    default:
+                        score.Push(int.Parse(op));
+                        break;
+                }
+            }
+
+            return score.Sum();
+        }
+    }
+}
