7987224552fd2980dfa0b4f189d1a11b648dcccb
谢在奎 <jackshea@163.com>
2019-10-29 16:35:05 +0800

    P0687 最长同值路径

---
+++ LeetCode/Problems/P0687_LongestUnivaluePath.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 最长同值路径
+    /// https://leetcode-cn.com/problems/longest-univalue-path/
+    public class P0687_LongestUnivaluePath
+    {
+        private int ans = 0;
+
+        public int LongestUnivaluePath(TreeNode root)
+        {
+            Length(root);
+            return ans;
+        }
+
+        private int Length(TreeNode tn)
+        {
+            if (tn == null)
+            {
+                return 0;
+            }
+
+            int left = Length(tn.left);
+            int right = Length(tn.right);
+            if (tn.left != null && tn.left.val == tn.val)
+            {
+                left++;
+            }
+            else
+            {
+                left = 0;
+            }
+
+            if (tn.right != null && tn.right.val == tn.val)
+            {
+                right++;
+            }
+            else
+            {
+                right = 0;
+            }
+
+            ans = Math.Max(ans, right + left);
+            return Math.Max(left, right);
+        }
+    }
+}
