17d7257efe98274e2bd39835f816f7e084595d80
谢在奎 <jackshea@163.com>
2019-10-29 15:44:14 +0800

    P0686 重复叠加字符串匹配

---
+++ LeetCode/Problems/P0686_RepeatedStringMatch.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Problems
+{
+    /// 重复叠加字符串匹配
+    /// https://leetcode-cn.com/problems/repeated-string-match/
+    public class P0686_RepeatedStringMatch
+    {
+        public int RepeatedStringMatch(string A, string B)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] != B[0])
+                    continue;
+
+                int aIndex = i + 1;
+                int result = 1;
+                bool isMatch = true;
+                for (int j = 1; j < B.Length; j++)
+                {
+                    if (aIndex >= A.Length)
+                    {
+                        aIndex -= A.Length;
+                        result++;
+                    }
+
+                    if (B[j] != A[aIndex])
+                    {
+                        isMatch = false;
+                        break;
+                    }
+
+                    aIndex++;
+                }
+
+                if (isMatch)
+                {
+                    return result;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
