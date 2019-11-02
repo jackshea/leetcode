7d41b1b74ecc5f491b763dd0b8297f9011b0a852
谢在奎 <jackshea@163.com>
2019-11-02 16:46:00 +0800

    P0704 二分查找

---
+++ LeetCode/Problems/P0704_BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems
+{
+    /// 二分查找
+    /// https://leetcode-cn.com/problems/binary-search/
+    public class P0704_BinarySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+
+                if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
