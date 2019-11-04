d328a24fd8173b5cd85daf5556e7a68cbdcc9e9d
谢在奎 <jackshea@163.com>
2019-11-04 19:05:45 +0800

    P0709 转换成小写字母

---
+++ LeetCode/Problems/P0709_ToLowerCase.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Problems
+{
+    /// 转换成小写字母
+    /// https://leetcode-cn.com/problems/to-lower-case/
+    public class P0709_ToLowerCase
+    {
+        public string ToLowerCase(string str)
+        {
+            var charArray = str.ToCharArray();
+            for (var i = 0; i < charArray.Length; i++)
+            {
+                char c = charArray[i];
+                if (char.IsUpper(c))
+                {
+                    charArray[i] = (char)(c + 32);
+                }
+            }
+
+            return new string(charArray);
+        }
+    }
+}
