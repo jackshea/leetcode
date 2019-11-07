3283026c171e42ad37a3116008c1f4742a3c64e6
谢在奎 <jackshea@163.com>
2019-11-07 16:28:39 +0800

    P0720 词典中最长的单词

---
+++ LeetCode/Problems/P0720_LongestWordInDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 词典中最长的单词
+    /// https://leetcode-cn.com/problems/longest-word-in-dictionary/
+    public class P0720_LongestWordInDictionary
+    {
+        public string LongestWord(string[] words)
+        {
+            Array.Sort(words);
+            string ans = string.Empty;
+            HashSet<string> set = new HashSet<string>();
+            set.Add(ans);
+
+            foreach (var word in words)
+            {
+                if (set.Contains(word.Substring(0, word.Length - 1)))
+                {
+                    if (word.Length > ans.Length)
+                    {
+                        ans = word;
+                    }
+
+                    set.Add(word);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
