31de9433a7d65312e67546d1c23f78e653e6471c
谢在奎 <jackshea@163.com>
2019-11-10 15:58:38 +0800

    P0748 最短完整词

---
+++ LeetCode/Problems/P0748_ShortestCompletingWord.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems
+{
+    /// 最短完整词
+    /// https://leetcode-cn.com/problems/shortest-completing-word/
+    public class P0748_ShortestCompletingWord
+    {
+        public string ShortestCompletingWord(string licensePlate, string[] words)
+        {
+            var licenseDic = GetDic(licensePlate);
+            int count = licenseDic.Values.Sum();
+
+            string ans = string.Empty;
+            int length = int.MaxValue;
+
+            foreach (var word in words)
+            {
+                if (word.Length < count)
+                {
+                    continue;
+                }
+
+                var wordDic = GetDic(word);
+                bool isMatch = true;
+                foreach (var kvp in licenseDic)
+                {
+                    int v = 0;
+                    if (!wordDic.TryGetValue(kvp.Key, out v) || v < kvp.Value)
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+
+                if (isMatch && word.Length < length)
+                {
+                    ans = word;
+                    length = word.Length;
+                }
+            }
+
+            return ans;
+        }
+
+        private Dictionary<char, int> GetDic(string word)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach (char c in word)
+            {
+                if (!char.IsLetter(c))
+                {
+                    continue;
+                }
+
+                char toLower = char.ToLower(c);
+                if (dic.ContainsKey(toLower))
+                {
+                    dic[toLower]++;
+                }
+                else
+                {
+                    dic[toLower] = 1;
+                }
+            }
+
+            return dic;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0748_ShortestCompletingWordTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0748_ShortestCompletingWordTests
+    {
+        [Test()]
+        public void ShortestCompletingWordTest()
+        {
+            var test = new P0748_ShortestCompletingWord();
+            string result = test.ShortestCompletingWord("GrC8950",
+                new String[]
+                    {"measure", "other", "every", "base", "according", "level", "meeting", "none", "marriage", "rest"});
+
+            Assert.AreEqual("according", result);
+        }
+    }
+}
