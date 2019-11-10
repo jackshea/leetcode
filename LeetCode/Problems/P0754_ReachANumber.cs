e953b54505338ac2ad7ff358ef8c039cb56cf7fc
谢在奎 <jackshea@163.com>
2019-11-10 16:21:31 +0800

    P0754 到达终点数字

---
+++ LeetCode/Problems/P0754_ReachANumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 到达终点数字
+    /// https://leetcode-cn.com/problems/reach-a-number/
+    public class P0754_ReachANumber
+    {
+        public int ReachNumber(int target)
+        {
+            target = Math.Abs(target);
+            int i = 0;
+            while (target >= 0)
+            {
+                target -= ++i;
+            }
+
+            return target % 2 == 0 ? i : i + 1 + i % 2;
+        }
+    }
+}
