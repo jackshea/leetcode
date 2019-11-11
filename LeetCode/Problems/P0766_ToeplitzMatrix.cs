5d531be8cba1058bbdf4b798d06013a632b6efa8
谢在奎 <jackshea@163.com>
2019-11-11 16:23:09 +0800

    P0766 托普利茨矩阵

---
+++ LeetCode/Problems/P0766_ToeplitzMatrix.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems
+{
+    /// 托普利茨矩阵
+    /// https://leetcode-cn.com/problems/toeplitz-matrix/
+    public class P0766_ToeplitzMatrix
+    {
+        public bool IsToeplitzMatrix(int[][] matrix)
+        {
+            for (int i = 1; i < matrix[0].Length; i++)
+            {
+                for (int j = 1; j < matrix.Length; j++)
+                {
+                    if (matrix[j][i] != matrix[j - 1][i - 1])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
