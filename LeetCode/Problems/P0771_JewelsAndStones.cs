e70b1723819a206b536777c5a4920839ef87da75
谢在奎 <jackshea@163.com>
2019-11-12 14:01:18 +0800

    P0771 宝石与石头

---
+++ LeetCode/Problems/P0771_JewelsAndStones.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 宝石与石头
+    /// https://leetcode-cn.com/problems/jewels-and-stones/
+    public class P0771_JewelsAndStones
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            HashSet<char> jewelType = new HashSet<char>();
+            foreach (var type in J)
+            {
+                jewelType.Add(type);
+            }
+
+            int count = 0;
+            foreach (var stone in S)
+            {
+                if (jewelType.Contains(stone))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
