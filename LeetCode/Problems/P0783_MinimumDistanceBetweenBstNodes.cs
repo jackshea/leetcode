d4945756e67f9ce9b08caf17b7ec5f253ecebb76
谢在奎 <jackshea@163.com>
2019-11-12 14:17:05 +0800

    P0783 二叉搜索树结点最小距离

---
+++ LeetCode/Problems/P0783_MinimumDistanceBetweenBstNodes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    public class P0783_MinimumDistanceBetweenBstNodes
+    {
+        private int minDist = Int32.MaxValue;
+        private TreeNode preNode;
+
+        public int MinDiffInBST(TreeNode root)
+        {
+            Traveral(root);
+            return minDist;
+        }
+
+        private void Traveral(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Traveral(node.left);
+            if (preNode != null)
+            {
+                minDist = Math.Min(minDist, node.val - preNode.val);
+            }
+
+            preNode = node;
+            Traveral(node.right);
+        }
+    }
+}
