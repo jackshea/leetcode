58c4e6d555b268c56f8be33dfb84587542b4ef7d
谢在奎 <jackshea@163.com>
2019-11-15 08:56:20 +0800

    P0796 旋转字符串

---
+++ LeetCode/Problems/P0796_RotateString.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Problems
+{
+    /// 旋转字符串
+    /// https://leetcode-cn.com/problems/rotate-string/
+    public class P0796_RotateString
+    {
+        public bool RotateString(string A, string B)
+        {
+            if (A.Length != B.Length)
+            {
+                return false;
+            }
+
+            return (A + A).Contains(B);
+        }
+    }
+}
