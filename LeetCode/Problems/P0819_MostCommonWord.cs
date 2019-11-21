831f271db06b0259c28e8d8917c4963394add9da
谢在奎 <jackshea@163.com>
2019-11-21 17:08:05 +0800

    P0819 最常见的单词

---
+++ LeetCode/Problems/P0819_MostCommonWord.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    /// 最常见的单词
+    /// https://leetcode-cn.com/problems/most-common-word/
+    public class P0819_MostCommonWord
+    {
+        private HashSet<string> _bannedSet;
+        private Dictionary<string, int> _words;
+        private StringBuilder _getWord;
+        private HashSet<char> _punctuations;
+
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            _bannedSet = new HashSet<string>();
+            foreach (var b in banned)
+            {
+                _bannedSet.Add(b);
+            }
+
+            _words = new Dictionary<string, int>();
+            _getWord = new StringBuilder();
+            _punctuations = new HashSet<char>("!?',;. ");
+
+            for (var i = 0; i < paragraph.Length; i++)
+            {
+                char c = paragraph[i];
+                if (_punctuations.Contains(c))
+                {
+                    if (_getWord.Length == 0)
+                    {
+                        continue;
+                    }
+
+                    AddWord();
+
+                    _getWord.Clear();
+                }
+                else
+                {
+                    _getWord.Append(char.ToLower(c));
+                    if (i == paragraph.Length - 1)
+                    {
+                        AddWord();
+                    }
+                }
+            }
+
+            return _words.First(p => p.Value == _words.Values.Max()).Key;
+        }
+
+        private void AddWord()
+        {
+            var word = _getWord.ToString();
+            if (!_bannedSet.Contains(word))
+            {
+                if (_words.ContainsKey(word))
+                {
+                    _words[word]++;
+                }
+                else
+                {
+                    _words[word] = 1;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0819_MostCommonWordTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0819_MostCommonWordTests
+    {
+        [Test()]
+        public void MostCommonWordTest()
+        {
+            var test = new P0819_MostCommonWord();
+            var actual = test.MostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new[] { "hit" });
+            Assert.AreEqual("ball", actual);
+        }
+    }
+}
