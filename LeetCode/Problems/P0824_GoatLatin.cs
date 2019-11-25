c60483a0df1854ed8b5997a3a36bb2f4b5c858b0
谢在奎 <jackshea@163.com>
2019-11-25 16:59:39 +0800

    P0824 山羊拉丁文

---
+++ LeetCode/Problems/P0824_GoatLatin.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    /// 山羊拉丁文
+    /// https://leetcode-cn.com/problems/goat-latin/
+    public class P0824_GoatLatin
+    {
+        HashSet<char> vowels = new HashSet<char>("aeiouAEIOU");
+
+        public string ToGoatLatin(string S)
+        {
+            var words = S.Split(' ');
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i != 0)
+                {
+                    sb.Append(' ');
+                }
+
+                sb.Append(Convert(words[i], i));
+            }
+
+            return sb.ToString();
+        }
+
+        public string Convert(string word, int index)
+        {
+            if (word.Length == 0)
+            {
+                return word;
+            }
+            StringBuilder sb = new StringBuilder();
+            if (vowels.Contains(word[0]))
+            {
+                sb.Append(word + "ma");
+            }
+            else
+            {
+                sb.Append(word.Substring(1, word.Length - 1) + word[0]);
+                sb.Append("ma");
+            }
+
+            for (int i = 0; i < index + 1; i++)
+            {
+                sb.Append('a');
+            }
+
+            return sb.ToString();
+        }
+    }
+}
