5909d8a15cf49492d4a8c6488c9b63d4953f743d
谢在奎 <jackshea@163.com>
2019-11-27 20:33:20 +0800

    P0832 翻转图像

---
+++ LeetCode/Problems/P0832_FlippingAnImage.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Problems
+{
+    /// 翻转图像
+    /// https://leetcode-cn.com/problems/flipping-an-image/
+    public class P0832_FlippingAnImage
+    {
+        public int[][] FlipAndInvertImage(int[][] A)
+        {
+            foreach (var row in A)
+            {
+                int left = 0;
+                int right = row.Length - 1;
+                while (left < right)
+                {
+                    int temp = Invert(row[left]);
+                    row[left] = Invert(row[right]);
+                    row[right] = temp;
+                    left++;
+                    right--;
+                }
+
+                if (left == right)
+                {
+                    row[left] = Invert(row[left]);
+                }
+            }
+
+            return A;
+        }
+
+        private int Invert(int a)
+        {
+            return a == 0 ? 1 : 0;
+        }
+    }
+}
