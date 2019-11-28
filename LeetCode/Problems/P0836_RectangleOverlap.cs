2043a55229e55f79045d7cf0c8da6c76f55f42d6
谢在奎 <jackshea@163.com>
2019-11-28 19:22:32 +0800

    P0836 矩形重叠

---
+++ LeetCode/Problems/P0836_RectangleOverlap.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems
+{
+    /// 矩形重叠
+    /// https://leetcode-cn.com/problems/rectangle-overlap/
+    public class P0836_RectangleOverlap
+    {
+        public bool IsRectangleOverlap(int[] rec1, int[] rec2)
+        {
+            return !(rec1[0] >= rec2[2] || rec2[0] >= rec1[0] || rec1[1] >= rec2[3] || rec2[1] >= rec1[3]);
+        }
+    }
+}
