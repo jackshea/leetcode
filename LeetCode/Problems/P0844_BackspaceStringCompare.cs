a8aef94d6a127f20d5dfcf56c47762112c5f4f15
谢在奎 <jackshea@163.com>
2019-11-30 17:35:51 +0800

    P0844 比较含退格的字符串

---
+++ LeetCode/Problems/P0844_BackspaceStringCompare.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 比较含退格的字符串
+    /// https://leetcode-cn.com/problems/backspace-string-compare/
+    public class P0844_BackspaceStringCompare
+    {
+        public bool BackspaceCompare(string S, string T)
+        {
+            var s = Convert(S);
+            var t = Convert(T);
+            if (s.Count != t.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < s.Count; i++)
+            {
+                if (s.Pop() != t.Pop())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private Stack<char> Convert(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (c == '#')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            return stack;
+        }
+    }
+}
