5bc974e712034770579561dc920c4ffb36ce40a9
谢在奎 <jackshea@163.com>
2019-12-01 16:33:47 +0800

    P0849 到最近的人的最大距离

---
+++ LeetCode/Problems/P0849_MaximizeDistanceToClosestPerson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 到最近的人的最大距离
+    /// https://leetcode-cn.com/problems/maximize-distance-to-closest-person/
+    public class P0849_MaximizeDistanceToClosestPerson
+    {
+        public int MaxDistToClosest(int[] seats)
+        {
+            int last = 0;
+            int maxDist = 0;
+            int dist = 0;
+            bool first = true;
+
+            for (int i = 0; i < seats.Length; i++)
+            {
+                if (seats[i] == 0)
+                    continue;
+
+                dist = i - last;
+                if (!first)
+                {
+                    dist /= 2;
+                }
+
+                maxDist = Math.Max(maxDist, dist);
+                last = i;
+                first = false;
+            }
+
+            var end = seats.Length - 1;
+            if (seats[end] == 0 && maxDist < end - last)
+            {
+                maxDist = end - last;
+            }
+
+            return maxDist;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0849_MaximizeDistanceToClosestPersonTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0849_MaximizeDistanceToClosestPersonTests
+    {
+        [Test()]
+        public void MaxDistToClosestTest()
+        {
+            var test = new P0849_MaximizeDistanceToClosestPerson();
+            var ans = test.MaxDistToClosest(new[] { 1, 0, 0, 0, 1, 0, 1 });
+            Assert.AreEqual(2, ans);
+        }
+
+        [Test()]
+        public void MaxDistToClosestTest1()
+        {
+            var test = new P0849_MaximizeDistanceToClosestPerson();
+            var ans = test.MaxDistToClosest(new[] { 1, 0, 0, 0 });
+            Assert.AreEqual(3, ans);
+        }
+
+        [Test()]
+        public void MaxDistToClosestTest2()
+        {
+            var test = new P0849_MaximizeDistanceToClosestPerson();
+            var ans = test.MaxDistToClosest(new[] { 0, 1 });
+            Assert.AreEqual(1, ans);
+        }
+
+        [Test()]
+        public void MaxDistToClosestTest3()
+        {
+            var test = new P0849_MaximizeDistanceToClosestPerson();
+            var ans = test.MaxDistToClosest(new[] { 0, 0, 1 });
+            Assert.AreEqual(2, ans);
+        }
+    }
+}
