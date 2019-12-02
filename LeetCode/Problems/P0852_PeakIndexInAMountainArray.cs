95e924198415c8cde343247ec69f2dc11f20ea0f
谢在奎 <jackshea@163.com>
2019-12-02 16:58:38 +0800

    P0852 山脉数组的峰顶索引

---
+++ LeetCode/Problems/P0852_PeakIndexInAMountainArray.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Problems
+{
+    /// 山脉数组的峰顶索引
+    /// https://leetcode-cn.com/problems/peak-index-in-a-mountain-array/
+    public class P0852_PeakIndexInAMountainArray
+    {
+        public int PeakIndexInMountainArray(int[] A)
+        {
+            int left = 0, right = A.Length - 1;
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1])
+                {
+                    return mid;
+                }
+
+                if (A[mid] < A[mid + 1])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left;
+        }
+    }
+}
