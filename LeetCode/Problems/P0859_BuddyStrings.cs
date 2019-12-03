0c87d98b228a940dd4b57b1eac105286534c2db8
谢在奎 <jackshea@163.com>
2019-12-03 22:08:44 +0800

    P0859 亲密字符串

---
+++ LeetCode/Problems/P0859_BuddyStrings.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 亲密字符串
+    /// https://leetcode-cn.com/problems/buddy-strings/
+    public class P0859_BuddyStrings
+    {
+        public bool BuddyStrings(string A, string B)
+        {
+            if (A.Length != B.Length)
+            {
+                return false;
+            }
+
+            int? diffIndex = null;
+            bool changed = false;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] != B[i])
+                {
+                    if (changed)
+                    {
+                        return false;
+                    }
+
+                    if (diffIndex.HasValue)
+                    {
+                        if (A[i] == B[diffIndex.Value] && A[diffIndex.Value] == B[i])
+                        {
+                            changed = true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        diffIndex = i;
+                    }
+                }
+            }
+
+            // 字符串相等的情况,必须要有重复字符
+            if (!changed)
+            {
+                HashSet<char> charSet = new HashSet<char>();
+                foreach (var c in A)
+                {
+                    if (charSet.Contains(c))
+                    {
+                        return true;
+                    }
+
+                    charSet.Add(c);
+                }
+            }
+
+            return changed;
+        }
+    }
+}
