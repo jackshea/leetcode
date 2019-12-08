6d690dc537c91c7cc9c230306e4341c1568416c8
谢在奎 <jackshea@163.com>
2019-12-08 16:55:04 +0800

    P0872 叶子相似的树

---
+++ LeetCode/Problems/P0872_LeafSimilarTrees.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 叶子相似的树
+    /// https://leetcode-cn.com/problems/leaf-similar-trees/
+    public class P0872_LeafSimilarTrees
+    {
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            var leafs1 = leafs(root1);
+            var leafs2 = leafs(root2);
+
+            if (leafs1.Count != leafs2.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < leafs1.Count; i++)
+            {
+                if (leafs1[i].val != leafs2[i].val)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private List<TreeNode> leafs(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            List<TreeNode> result = new List<TreeNode>();
+
+            if (root.left == null && root.right == null)
+            {
+                result.Add(root);
+                return result;
+            }
+
+            if (root.left != null)
+            {
+                result.AddRange(leafs(root.left));
+            }
+
+            if (root.right != null)
+            {
+                result.AddRange(leafs(root.right));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0872_LeafSimilarTreesTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Tools;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0872_LeafSimilarTreesTests
+    {
+        [Test()]
+        public void LeafSimilarTest()
+        {
+            var treeNode1 = ConvertArrayToBinaryTree.Build(new int?[] { 3, 5, 1, 6, 2, 9, 8, null, null, 7, 4 });
+            var treeNode2 = ConvertArrayToBinaryTree.Build(new int?[] { 3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8 });
+            var test = new P0872_LeafSimilarTrees();
+            var check = test.LeafSimilar(treeNode1, treeNode2);
+            Assert.AreEqual(true, check);
+        }
+    }
+}
