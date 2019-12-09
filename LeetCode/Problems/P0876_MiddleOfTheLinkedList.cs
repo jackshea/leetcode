dd979ae5fa56bc561aa5aa0da369221ef0212dc1
谢在奎 <jackshea@163.com>
2019-12-09 20:30:31 +0800

    P0876 链表的中间结点

---
+++ LeetCode/Problems/P0876_MiddleOfTheLinkedList.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems
+{
+    /// 链表的中间结点
+    /// https://leetcode-cn.com/problems/middle-of-the-linked-list/
+    public class P0876_MiddleOfTheLinkedList
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null)
+            {
+                fast = fast.next;
+                if (fast != null)
+                {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return slow;
+        }
+    }
+}
