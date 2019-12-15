c790107be8d72a350e233e66beab3cbd73117db6
谢在奎 <jackshea@163.com>
2019-12-15 14:52:02 +0800

    P0896 单调数列

---
+++ LeetCode/Problems/P0896_MonotonicArray.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems
+{
+    /// 单调数列
+    /// https://leetcode-cn.com/problems/monotonic-array/
+    public class P0896_MonotonicArray
+    {
+        public bool IsMonotonic(int[] A)
+        {
+            bool asc = true;
+            bool desc = true;
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                if (A[i] < A[i + 1])
+                {
+                    desc = false;
+                }
+
+                if (A[i] > A[i + 1])
+                {
+                    asc = false;
+                }
+            }
+
+            return asc || desc;
+        }
+    }
+}
