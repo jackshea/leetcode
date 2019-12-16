ef54a3dca128fb12a9ad6443ba409cabd0e08201
谢在奎 <jackshea@163.com>
2019-12-16 12:41:01 +0800

    P0897 递增顺序查找树

---
+++ LeetCode/Problems/P0897_IncreasingOrderSearchTree.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Problems
+{
+    /// 递增顺序查找树
+    /// https://leetcode-cn.com/problems/increasing-order-search-tree/
+    public class P0897_IncreasingOrderSearchTree
+    {
+        private TreeNode head;
+        private TreeNode current;
+        public TreeNode IncreasingBST(TreeNode root)
+        {
+            Traversal(root);
+            return head;
+        }
+
+        public void Traversal(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Traversal(node.left);
+
+            if (head == null)
+            {
+                head = new TreeNode(node.val);
+                current = head;
+            }
+            else
+            {
+                current.right = new TreeNode(node.val);
+                current = current.right;
+            }
+
+            Traversal(node.right);
+        }
+    }
+}
