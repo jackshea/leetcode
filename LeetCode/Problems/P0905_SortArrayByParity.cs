006fa68c4185acc78cfd210578d8824e25ebfef5
谢在奎 <jackshea@163.com>
2019-12-17 17:57:30 +0800

    P0905 按奇偶排序数组

---
+++ LeetCode/Problems/P0905_SortArrayByParity.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Problems
+{
+    /// 按奇偶排序数组
+    /// https://leetcode-cn.com/problems/sort-array-by-parity/
+    public class P0905_SortArrayByParity
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            int left = 0;
+            int right = A.Length - 1;
+            while (left < right)
+            {
+                if (A[left] % 2 == 0)
+                {
+                    left++;
+                    continue;
+                }
+
+                if (A[right] % 2 != 0)
+                {
+                    right--;
+                    continue;
+                }
+
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                left++;
+                right--;
+            }
+
+            return A;
+        }
+    }
+}
