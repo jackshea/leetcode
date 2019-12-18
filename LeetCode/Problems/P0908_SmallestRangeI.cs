d459c8a021cbcbf6574f6b22ef544c67595e0610
谢在奎 <jackshea@163.com>
2019-12-18 10:22:28 +0800

    P0908 最小差值 I

---
+++ LeetCode/Problems/P0908_SmallestRangeI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 最小差值 I
+    /// https://leetcode-cn.com/problems/smallest-range-i/
+    public class P0908_SmallestRangeI
+    {
+        public int SmallestRangeI(int[] A, int K)
+        {
+            int min = A[0];
+            int max = A[0];
+            foreach (var a in A)
+            {
+                if (min > a)
+                {
+                    min = a;
+                }
+
+                if (max < a)
+                {
+                    max = a;
+                }
+            }
+
+            var diff = max - min -2*K;
+            return Math.Max(0, diff);
+        }
+    }
+}
