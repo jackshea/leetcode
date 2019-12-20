14d91856436140c4781c41c9b037c3fd163084cd
谢在奎 <jackshea@163.com>
2019-12-20 11:18:15 +0800

    P0917 仅仅反转字母

---
+++ LeetCode/Problems/P0917_ReverseOnlyLetters.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Problems
+{
+    /// 仅仅反转字母
+    /// https://leetcode-cn.com/problems/reverse-only-letters/
+    public class P0917_ReverseOnlyLetters
+    {
+        public string ReverseOnlyLetters(string S)
+        {
+            var charArray = S.ToCharArray();
+            int left = 0;
+            int right = charArray.Length - 1;
+            while (left < right)
+            {
+                if (!char.IsLetter(charArray[left]))
+                {
+                    left++;
+                    continue;
+                }
+
+                if (!char.IsLetter(charArray[right]))
+                {
+                    right--;
+                    continue;
+                }
+
+                if (charArray[left] != charArray[right])
+                {
+                    char temp = charArray[left];
+                    charArray[left] = charArray[right];
+                    charArray[right] = temp;
+                }
+
+                left++;
+                right--;
+            }
+
+            return new string(charArray);
+        }
+    }
+}
