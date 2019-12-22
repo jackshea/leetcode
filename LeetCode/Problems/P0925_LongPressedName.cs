b200c45f19197b4bf9bc652bf9a584b16ced3f99
谢在奎 <jackshea@163.com>
2019-12-22 14:22:35 +0800

    P0925 长按键入

---
+++ LeetCode/Problems/P0925_LongPressedName.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems
+{
+    /// 长按键入
+    /// https://leetcode-cn.com/problems/long-pressed-name/
+    public class P0925_LongPressedName
+    {
+        public bool IsLongPressedName(string name, string typed)
+        {
+            int nameIndex = 0;
+            for (var i = 0; i < typed.Length; i++)
+            {
+                if (nameIndex < name.Length && name[nameIndex] == typed[i])
+                {
+                    nameIndex++;
+                }
+                else if (i > 0 && typed[i] == typed[i - 1])
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return nameIndex == name.Length;
+        }
+    }
+}
