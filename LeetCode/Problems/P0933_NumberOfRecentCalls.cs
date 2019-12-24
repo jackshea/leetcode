e7e630e780c7882a09325d5014c527871e37a91b
谢在奎 <jackshea@163.com>
2019-12-24 19:55:24 +0800

    P0933 最近的请求次数

---
+++ LeetCode/Problems/P0933_NumberOfRecentCalls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    public class RecentCounter
+    {
+        private Queue<int> lastPings;
+        public RecentCounter()
+        {
+            lastPings = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            while (lastPings.Count > 0 && lastPings.Peek() < t - 3000)
+            {
+                lastPings.Dequeue();
+            }
+            lastPings.Enqueue(t);
+            return lastPings.Count;
+        }
+    }
+}
