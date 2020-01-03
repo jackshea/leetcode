fff91bf354cae378eea5d284c80a2ee60b6f5168
谢在奎 <jackshea@163.com>
2020-01-03 20:28:53 +0800

    P0944 删列造序

---
+++ LeetCode/Problems/P0944_DeleteColumnsToMakeSorted.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Problems
+{
+    /// 删列造序
+    /// https://leetcode-cn.com/problems/delete-columns-to-make-sorted/
+    public class P0944_DeleteColumnsToMakeSorted
+    {
+        public int MinDeletionSize(string[] A)
+        {
+            int ans = 0;
+            for (int i = 0; i < A[0].Length; i++)
+            {
+                for (int j = 0; j < A.Length - 1; j++)
+                {
+                    if (A[j][i] > A[j + 1][i])
+                    {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
