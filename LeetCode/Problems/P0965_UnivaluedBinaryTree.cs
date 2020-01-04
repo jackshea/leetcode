9f1573524ff2e635937af0a4054d2216b958c4c1
谢在奎 <jackshea@163.com>
2020-01-05 01:34:19 +0800

    P0965 单值二叉树

---
+++ LeetCode/Problems/P0965_UnivaluedBinaryTree.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Problems
+{
+    /// 单值二叉树
+    /// https://leetcode-cn.com/problems/univalued-binary-tree/
+    public class P0965_UnivaluedBinaryTree
+    {
+        public bool IsUnivalTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            if (root.left != null && (root.val != root.left.val || !IsUnivalTree(root.left))) return false;
+            if (root.right != null && (root.val != root.right.val || !IsUnivalTree(root.right))) return false;
+            return true;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0965_UnivaluedBinaryTreeTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Tools;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0965_UnivaluedBinaryTreeTests
+    {
+        [Test()]
+        public void IsUnivalTreeTest()
+        {
+            var test = new P0965_UnivaluedBinaryTree();
+            var root = ConvertArrayToBinaryTree.Build(new int?[]{9, 9, 6, 9, 9});
+            Assert.IsFalse(test.IsUnivalTree(root));
+        }
+    }
+}
