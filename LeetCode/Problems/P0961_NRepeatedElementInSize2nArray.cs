c00ac2b8ba99ae08de824a248e9021a8e177a960
谢在奎 <jackshea@163.com>
2020-01-05 00:57:45 +0800

    P0961 重复 N 次的元素

---
+++ LeetCode/Problems/P0961_NRepeatedElementInSize2nArray.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems
+{
+    /// 重复 N 次的元素
+    /// https://leetcode-cn.com/problems/n-repeated-element-in-size-2n-array/
+    public class P0961_NRepeatedElementInSize2nArray
+    {
+        public int RepeatedNTimes(int[] A)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 0; j < A.Length - i; j++)
+                {
+                    if (A[j] == A[j + i])
+                    {
+                        return A[j];
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
