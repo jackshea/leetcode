c2d00d70c5ccbbe1873ed4a484e64dba063f5a84
谢在奎 <jackshea@163.com>
2020-01-04 23:10:08 +0800

    P0953 验证外星语词典

---
+++ LeetCode/Problems/P0953_VerifyingAnAlienDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 验证外星语词典
+    /// https://leetcode-cn.com/problems/verifying-an-alien-dictionary/
+    public class P0953_VerifyingAnAlienDictionary
+    {
+        private int[] dicOrder = new int[26];
+
+        public bool IsAlienSorted(string[] words, string order)
+        {
+            for (int i = 0; i < order.Length; i++)
+            {
+                int c = order[i] - 'a';
+                dicOrder[c] = i;
+            }
+
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                if (AlienWordComparer(words[i], words[i + 1]) > 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int AlienWordComparer(string a, string b)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i >= b.Length)
+                {
+                    return 1;
+                }
+
+                var compare = dicOrder[a[i] - 'a'] - dicOrder[b[i] - 'a'];
+                if (compare != 0)
+                {
+                    return Math.Sign(compare);
+                }
+            }
+
+            if (a.Length == b.Length)
+            {
+                return 0;
+            }
+            return -1;
+        }
+
+    }
+}
