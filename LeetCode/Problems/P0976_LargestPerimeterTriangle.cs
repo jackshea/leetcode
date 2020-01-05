00f190fc7101b32a654d2f5e2a19983bb773a45f
谢在奎 <jackshea@163.com>
2020-01-05 17:12:47 +0800

    P0976 三角形的最大周长

---
+++ LeetCode/Problems/P0976_LargestPerimeterTriangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 三角形的最大周长
+    /// https://leetcode-cn.com/problems/largest-perimeter-triangle/
+    public class P0976_LargestPerimeterTriangle
+    {
+        public int LargestPerimeter(int[] A)
+        {
+            Array.Sort(A, (a, b) => -a.CompareTo(b));
+
+            for (int i = 0; i < A.Length - 2; i++)
+            {
+                if (A[i + 1] + A[i + 2] > A[i])
+                {
+                    return A[i] + A[i + 1] + A[i + 2];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
