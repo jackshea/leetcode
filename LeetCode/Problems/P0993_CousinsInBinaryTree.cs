a07fa16d19a1bfdd8db76195bef7714c4198644f
谢在奎 <jackshea@163.com>
2020-01-06 17:45:04 +0800

    P0993 二叉树的堂兄弟节点

---
+++ LeetCode/Problems/P0993_CousinsInBinaryTree.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 二叉树的堂兄弟节点
+    /// https://leetcode-cn.com/problems/cousins-in-binary-tree/
+    public class P0993_CousinsInBinaryTree
+    {
+        private int xParent, xDepth;
+        private int yParent, yDepth;
+        public bool IsCousins(TreeNode root, int x, int y)
+        {
+            dfs(root.left, 1, root.val, x, y);
+            dfs(root.right, 1, root.val, x, y);
+            return xDepth == yDepth && xParent != yParent;
+        }
+
+        private void dfs(TreeNode root, int depth, int parent, int x, int y)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (root.val == x)
+            {
+                xParent = parent;
+                xDepth = depth;
+                return;
+            }
+            if (root.val == y)
+            {
+                yParent = parent;
+                yDepth = depth;
+                return;
+            }
+
+            dfs(root.left, 1 + depth, root.val, x, y);
+            dfs(root.right, 1 + depth, root.val, x, y);
+        }
+    }
+}
