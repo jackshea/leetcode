ca27eae091a3066504b42373165e6060ea840616
谢在奎 <jackshea@163.com>
2020-01-06 15:49:12 +0800

    P0977 有序数组的平方

---
+++ LeetCode/Problems/P0977_SquaresOfASortedArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 有序数组的平方
+    /// https://leetcode-cn.com/problems/squares-of-a-sorted-array/
+    public class P0977_SquaresOfASortedArray
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            var ans = new int[A.Length];
+            int left = 0, right = A.Length - 1;
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                if (Math.Abs(A[left]) > Math.Abs(A[right]))
+                {
+                    ans[i] = A[left] * A[left];
+                    left++;
+                }
+                else
+                {
+                    ans[i] = A[right] * A[right];
+                    right--;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
