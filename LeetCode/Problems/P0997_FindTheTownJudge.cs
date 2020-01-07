dc7e2d5bc8d53460207c31a8115e752c34a1a307
谢在奎 <jackshea@163.com>
2020-01-07 19:46:42 +0800

    P0997 找到小镇的法官

---
+++ LeetCode/Problems/P0997_FindTheTownJudge.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Problems
+{
+    /// 找到小镇的法官
+    /// https://leetcode-cn.com/problems/find-the-town-judge/
+    public class P0997_FindTheTownJudge
+    {
+        public int FindJudge(int N, int[][] trust)
+        {
+            var beTrusted = new int[N + 1];
+            var trustOther = new bool[N + 1];
+            foreach (var t in trust)
+            {
+                trustOther[t[0]] = true;
+                beTrusted[t[1]]++;
+            }
+
+            int ans = -1;
+            for (int i = 1; i <= N; i++)
+            {
+                if (beTrusted[i] == N - 1)
+                {
+                    ans = i;
+                }
+            }
+
+            if (ans > 0 && !trustOther[ans])
+            {
+                return ans;
+            }
+
+            return -1;
+        }
+    }
+}
