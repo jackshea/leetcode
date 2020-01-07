e4b50824e889eefae189e7c36c537c82625d7b5f
谢在奎 <jackshea@163.com>
2020-01-07 17:17:41 +0800

    P0994 腐烂的橘子

---
+++ LeetCode/Problems/P0994_RottingOranges.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 腐烂的橘子
+    /// https://leetcode-cn.com/problems/rotting-oranges/
+    public class P0994_RottingOranges
+    {
+        public int OrangesRotting(int[][] grid)
+        {
+            Queue<int[]> badOrange = new Queue<int[]>();
+            int ans = -1;
+            bool existFreshOrange = false;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        existFreshOrange = true;
+                    }
+
+                    if (grid[i][j] == 2)
+                    {
+                        badOrange.Enqueue(new int[] { i, j });
+                        ans = 0;
+                    }
+                }
+            }
+
+            if (!existFreshOrange)
+            {
+                return 0;
+            }
+
+            if (ans < 0)
+            {
+                return -1;
+            }
+
+            var nearly = new int[][] { new[] { 1, 0 }, new[] { -1, 0 }, new[] { 0, -1 }, new[] { 0, 1 } };
+
+            while (badOrange.Count > 0)
+            {
+                int count = badOrange.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var top = badOrange.Dequeue();
+                    foreach (var n in nearly)
+                    {
+                        int ii = top[0] + n[0];
+                        int jj = top[1] + n[1];
+                        if (ii >= 0 && ii < grid.Length && jj >= 0 && jj < grid[0].Length && grid[ii][jj] == 1)
+                        {
+                            grid[ii][jj] = 2;
+                            badOrange.Enqueue(new int[] { ii, jj });
+                        }
+                    }
+                }
+
+                if (badOrange.Count > 0)
+                {
+                    ans++;
+                }
+            }
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        return -1;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P0994_RottingOrangesTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P0994_RottingOrangesTests
+    {
+        [Test()]
+        public void OrangesRottingTest()
+        {
+            var input = new int[][] { new[] { 2, 1, 1 }, new[] { 1, 1, 0 }, new[] { 0, 1, 1 } };
+            var test = new P0994_RottingOranges();
+            Assert.AreEqual(4, test.OrangesRotting(input));
+        }
+
+        [Test()]
+        public void OrangesRottingTest_1()
+        {
+            var input = new int[][] { new[] { 1, 2 }, new[] { 2, 1 } };
+            var test = new P0994_RottingOranges();
+            Assert.AreEqual(1, test.OrangesRotting(input));
+        }
+    }
+}
