a6eec48dad8fa2ddd98d9b2aed49ffbcef062a36
谢在奎 <jackshea@163.com>
2020-01-08 21:17:42 +0800

    P1013 将数组分成和相等的三个部分

---
+++ LeetCode/Problems/P1013_PartitionArrayIntoThreePartsWithEqualSum.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems
+{
+    /// 将数组分成和相等的三个部分
+    /// https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum/
+    public class P1013_PartitionArrayIntoThreePartsWithEqualSum
+    {
+        public bool CanThreePartsEqualSum(int[] A)
+        {
+            var sum = A.Sum();
+            if (sum % 3 != 0)
+            {
+                return false;
+            }
+
+            var avg = sum / 3;
+            int left = 0;
+            int right = A.Length - 1;
+            int leftSum = 0;
+            int rightSum = 0;
+            while (left < right)
+            {
+                if (leftSum != avg)
+                {
+                    leftSum += A[left++];
+                }
+
+                if (rightSum != avg)
+                {
+                    rightSum += A[right--];
+                }
+
+                if (leftSum == avg && rightSum == avg)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P1013_PartitionArrayIntoThreePartsWithEqualSumTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P1013_PartitionArrayIntoThreePartsWithEqualSumTests
+    {
+        [Test()]
+        public void CanThreePartsEqualSumTest()
+        {
+            var test = new P1013_PartitionArrayIntoThreePartsWithEqualSum();
+            Assert.IsTrue(test.CanThreePartsEqualSum(new[] { 0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1 }));
+        }
+    }
+}
