e4ad5f29cad07842f31d93cde7efd11486cdb90e
谢在奎 <jackshea@163.com>
2020-01-08 14:37:32 +0800

    P1009 十进制整数的反码

---
+++ LeetCode/Problems/P1009_ComplementOfBase10Integer.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems
+{
+    /// 十进制整数的反码
+    /// https://leetcode-cn.com/problems/complement-of-base-10-integer/
+    public class P1009_ComplementOfBase10Integer
+    {
+        public int BitwiseComplement(int N)
+        {
+            if (N == 0)
+            {
+                return 1;
+            }
+
+            int ans = 0;
+            int b = 1;
+            while (N != 0)
+            {
+                ans += (N % 2 == 0 ? 1 : 0) * b;
+                N >>= 1;
+                b <<= 1;
+            }
+
+            return ans;
+        }
+    }
+}
