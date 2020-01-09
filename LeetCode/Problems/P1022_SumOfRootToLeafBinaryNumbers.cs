3a38212da1d34cccd5458dec40696cda7cd2c2c2
谢在奎 <jackshea@163.com>
2020-01-09 17:41:04 +0800

    P1022 从根到叶的二进制数之和

---
+++ LeetCode/Problems/P1022_SumOfRootToLeafBinaryNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 从根到叶的二进制数之和
+    /// https://leetcode-cn.com/problems/sum-of-root-to-leaf-binary-numbers/
+    public class P1022_SumOfRootToLeafBinaryNumbers
+    {
+        private int sum = 0;
+        private const int mod = 1000000000 + 9;
+        public int SumRootToLeaf(TreeNode root)
+        {
+            dfs(root, 0);
+            return sum;
+        }
+
+        private void dfs(TreeNode root, int parent)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            int v = (parent << 1) + root.val;
+            v %= mod;
+
+            if (root.left == null && root.right == null)
+            {
+                sum += v;
+                sum %= mod;
+            }
+
+            dfs(root.left, v);
+            dfs(root.right, v);
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P1022_SumOfRootToLeafBinaryNumbersTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Tools;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P1022_SumOfRootToLeafBinaryNumbersTests
+    {
+        [Test()]
+        public void SumRootToLeafTest()
+        {
+            var input = new int?[] { 1, 0, 1, 0, 1, 0, 1 };
+            var test = new P1022_SumOfRootToLeafBinaryNumbers();
+            Assert.AreEqual(22, test.SumRootToLeaf(ConvertArrayToBinaryTree.Build(input)));
+        }
+    }
+}
