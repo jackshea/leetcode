9ea6e9a8bb14a473c6fa6290937a4ed21ca86783
谢在奎 <jackshea@163.com>
2020-01-09 16:13:10 +0800

    P1018 可被 5 整除的二进制前缀

---
+++ LeetCode/Problems/P1018_BinaryPrefixDivisibleBy5.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 可被 5 整除的二进制前缀
+    /// https://leetcode-cn.com/problems/binary-prefix-divisible-by-5/
+    public class P1018_BinaryPrefixDivisibleBy5
+    {
+        public IList<bool> PrefixesDivBy5(int[] A)
+        {
+            IList<bool> ans = new List<bool>();
+            int n = 0;
+            foreach (var a in A)
+            {
+                n += a % 2;
+                n %= 5;
+                ans.Add(n == 0);
+                n <<= 1;
+            }
+
+            return ans;
+        }
+    }
+}
