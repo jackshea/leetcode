2246a1fc2ec84504018fbee3b575b67ffde44e69
谢在奎 <jackshea@163.com>
2020-01-09 17:57:35 +0800

    P1025 除数博弈

---
+++ LeetCode/Problems/P1025_DivisorGame.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems
+{
+    /// 除数博弈
+    /// https://leetcode-cn.com/problems/divisor-game/
+    public class P1025_DivisorGame
+    {
+        public bool DivisorGame(int N)
+        {
+            return N % 2 == 0;
+        }
+    }
+}
