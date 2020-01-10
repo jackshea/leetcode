0de2b57f34731f082ad3e0fecae687dbf0533918
谢在奎 <jackshea@163.com>
2020-01-10 21:59:51 +0800

    P1033 移动石子直到连续

---
+++ LeetCode/Problems/P1033_MovingStonesUntilConsecutive.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 移动石子直到连续
+    /// https://leetcode-cn.com/problems/moving-stones-until-consecutive/
+    public class P1033_MovingStonesUntilConsecutive
+    {
+        public int[] NumMovesStones(int a, int b, int c)
+        {
+            var pos = new[] { a, b, c };
+            Array.Sort(pos);
+            int min = 2;
+            int max = pos[2] - pos[0] - 2;
+
+            if (pos[0] + 1 == pos[1])
+            {
+                min--;
+            }
+            if (pos[1] + 1 == pos[2])
+            {
+                min--;
+            }
+
+            if (pos[0] + 2 == pos[1]|| pos[1] + 2 == pos[2])
+            {
+                min = 1;
+            }
+
+            return new[] {min, max};
+        }
+    }
+}
