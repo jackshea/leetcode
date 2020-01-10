27e791a39555eb6064f161c9f3e778c8020e251e
谢在奎 <jackshea@163.com>
2020-01-10 13:14:13 +0800

    P1029 两地调度

---
+++ LeetCode/Problems/P1029_TwoCityScheduling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 两地调度
+    /// https://leetcode-cn.com/problems/two-city-scheduling/
+    public class P1029_TwoCityScheduling
+    {
+        public int TwoCitySchedCost(int[][] costs)
+        {
+            // 按差值排序
+            Array.Sort(costs, (a, b) => { return (a[0] - a[1]).CompareTo(b[0] - b[1]); });
+            int sum = 0;
+            int half = costs.Length / 2;
+            for (int i = 0; i < costs.Length; i++)
+            {
+                if (i < half)
+                {
+                    sum += costs[i][0];
+                }
+                else
+                {
+                    sum += costs[i][1];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
