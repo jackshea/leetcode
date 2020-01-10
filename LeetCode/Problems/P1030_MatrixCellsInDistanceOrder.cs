4fd2c1c7fd0fe3fe87e1dd6e3d586f17a7e43080
谢在奎 <jackshea@163.com>
2020-01-10 14:06:37 +0800

    P1030 距离顺序排列矩阵单元格

---
+++ LeetCode/Problems/P1030_MatrixCellsInDistanceOrder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 距离顺序排列矩阵单元格
+    /// https://leetcode-cn.com/problems/matrix-cells-in-distance-order/
+    public class P1030_MatrixCellsInDistanceOrder
+    {
+        public int[][] AllCellsDistOrder(int R, int C, int r0, int c0)
+        {
+            var ans = new int[R * C][];
+            var ansIndex = 0;
+            for (int i = 0; i < R; i++)
+            {
+                for (int j = 0; j < C; j++)
+                {
+                    ans[ansIndex++] = new[] { i, j };
+                }
+            }
+
+            Array.Sort(ans, (a, b) => Dist(a[0], a[1], r0, c0).CompareTo(Dist(b[0], b[1], r0, c0)));
+            return ans;
+        }
+
+        private int Dist(int r, int c, int r0, int c0)
+        {
+            return Math.Abs(r - r0) + Math.Abs(c - c0);
+        }
+    }
+}
