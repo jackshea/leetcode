76515c7efd852d70011c76b7b5b46b1edb48bc76
谢在奎 <jackshea@163.com>
2020-01-10 22:26:06 +0800

    P1037 有效的回旋镖

---
+++ LeetCode/Problems/P1037_ValidBoomerang.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Problems
+{
+    /// 有效的回旋镖
+    /// https://leetcode-cn.com/problems/valid-boomerang/
+    public class P1037_ValidBoomerang
+    {
+        public bool IsBoomerang(int[][] points)
+        {
+            int x1 = points[1][0] - points[0][0];
+            int x2 = points[2][0] - points[0][0];
+
+            int y1 = points[1][1] - points[0][1];
+            int y2 = points[2][1] - points[0][1];
+
+            return x1 * y2 != x2 * y1;
+        }
+    }
+}
