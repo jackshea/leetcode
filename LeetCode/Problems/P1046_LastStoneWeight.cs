1fef7a36e54851d276780e7ac4568cc4b87b3d2e
谢在奎 <jackshea@163.com>
2020-01-12 00:57:35 +0800

    P1046 最后一块石头的重量

---
+++ LeetCode/Problems/P1046_LastStoneWeight.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 最后一块石头的重量
+    /// https://leetcode-cn.com/problems/last-stone-weight/
+    public class P1046_LastStoneWeight
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            Array.Sort(stones, (a, b) => -a.CompareTo(b));
+            int ans = stones[0];
+            for (int i = 1; i < stones.Length; i++)
+            {
+                ans = stones[i - 1] - stones[i];
+                if (ans == 0)
+                {
+                    if (i < stones.Length - 1)
+                    {
+                        ans = stones[i + 1];
+                    }
+
+                    i++;
+                }
+                else
+                {
+                    stones[i] = ans;
+                    for (int j = i + 1; j < stones.Length; j++)
+                    {
+                        if (stones[j] > ans)
+                        {
+                            stones[j - 1] = stones[j];
+                            stones[j] = ans;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P1046_LastStoneWeightTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P1046_LastStoneWeightTests
+    {
+        [Test()]
+        public void LastStoneWeightTest()
+        {
+            var test = new P1046_LastStoneWeight();
+            Assert.AreEqual(1, test.LastStoneWeight(new[] { 2, 7, 4, 1, 8, 1 }));
+        }
+
+        [Test()]
+        public void LastStoneWeightTest_1()
+        {
+            var test = new P1046_LastStoneWeight();
+            Assert.AreEqual(2, test.LastStoneWeight(new[] { 3, 7, 8 }));
+        }
+    }
+}
