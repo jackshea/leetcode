a9212fbe432775f559894e125e6da6afadcd446f
谢在奎 <jackshea@163.com>
2020-01-12 01:29:38 +0800

    P1047 删除字符串中的所有相邻重复项

---
+++ LeetCode/Problems/P1047_RemoveAllAdjacentDuplicatesInString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 删除字符串中的所有相邻重复项
+    /// https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/
+    public class P1047_RemoveAllAdjacentDuplicatesInString
+    {
+        public string RemoveDuplicates(string S)
+        {
+            int len = S.Length;
+            var charArray = S.ToCharArray();
+            int curIndex = S.Length;
+            while (curIndex >= 0)
+            {
+                if (curIndex >= len - 1)
+                {
+                    curIndex--;
+                    continue;
+                }
+
+                if (charArray[curIndex] == charArray[curIndex + 1])
+                {
+                    for (int j = curIndex + 2; j < len; j++)
+                    {
+                        charArray[j - 2] = charArray[j];
+                    }
+
+                    len -= 2;
+                }
+
+                curIndex--;
+            }
+
+            return new string(charArray, 0, len);
+        }
+    }
+}
