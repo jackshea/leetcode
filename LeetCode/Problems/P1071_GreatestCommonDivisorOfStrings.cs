8fb56dae8c77910bee67892225adde26815bb5b9
谢在奎 <jackshea@163.com>
2020-01-12 23:33:29 +0800

    P1071 字符串的最大公因子

---
+++ LeetCode/Problems/P1071_GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Problems
+{
+    /// 字符串的最大公因子
+    /// https://leetcode-cn.com/problems/greatest-common-divisor-of-strings/
+    public class P1071_GreatestCommonDivisorOfStrings
+    {
+        public string GcdOfStrings(string str1, string str2)
+        {
+            int len1 = str1.Length;
+            int len2 = str2.Length;
+            int div = len1 / len2;
+            int mod = len1 % len2;
+            int i1 = 0;
+            for (int i = 0; i < div; i++)
+            {
+                for (int j = 0; j < len2; j++)
+                {
+                    if (str1[i1++] != str2[j])
+                    {
+                        return string.Empty;
+                    }
+                }
+            }
+
+            if (mod == 0)
+            {
+                return str2;
+            }
+
+            return GcdOfStrings(str2, str1.Substring(len2 * div, mod));
+        }
+    }
+}
