f7aa5ccc6025da863c3a347981088375b28a505b
谢在奎 <jackshea@163.com>
2020-01-12 23:48:43 +0800

    P1089 复写零

---
+++ LeetCode/Problems/P1089_DuplicateZeros.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems
+{
+    /// 复写零
+    /// https://leetcode-cn.com/problems/duplicate-zeros/
+    public class P1089_DuplicateZeros
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    for (int j = arr.Length - 1; j > i; j--)
+                    {
+                        arr[j] = arr[j - 1];
+                    }
+
+                    i++;
+                }
+            }
+        }
+    }
+}
