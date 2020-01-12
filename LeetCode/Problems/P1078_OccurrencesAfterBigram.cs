07e06922cd275df5c0060e996e0a44be074acde8
谢在奎 <jackshea@163.com>
2020-01-12 23:40:00 +0800

    P1078 Bigram 分词

---
+++ LeetCode/Problems/P1078_OccurrencesAfterBigram.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// Bigram 分词
+    /// https://leetcode-cn.com/problems/occurrences-after-bigram/
+    public class P1078_OccurrencesAfterBigram
+    {
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            List<string> ans = new List<string>();
+            var words = text.Split(' ');
+            for (int i = 0; i < words.Length - 2; i++)
+            {
+                if (words[i] == first && words[i + 1] == second)
+                {
+                    ans.Add(words[i + 2]);
+                }
+            }
+
+            return ans.ToArray();
+        }
+    }
+}
