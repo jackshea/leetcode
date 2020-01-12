cc2298497842b6406aa712af12b5a6d97f7d7c8b
谢在奎 <jackshea@163.com>
2020-01-12 23:03:52 +0800

    P1051 高度检查器

---
+++ LeetCode/Problems/P1051_HeightChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 高度检查器
+    /// https://leetcode-cn.com/problems/height-checker/
+    public class P1051_HeightChecker
+    {
+        public int HeightChecker(int[] heights)
+        {
+            var sort = new int[heights.Length];
+            Array.Copy(heights, sort, heights.Length);
+            Array.Sort(sort);
+            int ans = 0;
+            for (int i = 0; i < sort.Length; i++)
+            {
+                if (heights[i] != sort[i])
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
