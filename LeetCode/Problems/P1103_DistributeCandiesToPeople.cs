c036d03647ac4ab64c6e62199f974632c055e337
谢在奎 <jackshea@163.com>
2020-01-13 19:50:18 +0800

    P1103 分糖果 II

---
+++ LeetCode/Problems/P1103_DistributeCandiesToPeople.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems
+{
+    /// 分糖果 II
+    /// https://leetcode-cn.com/problems/distribute-candies-to-people/
+    public class P1103_DistributeCandiesToPeople
+    {
+        public int[] DistributeCandies(int candies, int num_people)
+        {
+            var ans = new int[num_people];
+            int current = 1;
+            int index = 0;
+            while (current <= candies)
+            {
+                index %= num_people;
+                ans[index] += current;
+                candies -= current;
+                current++;
+                index++;
+            }
+
+            if (candies != 0)
+            {
+                index %= num_people;
+                ans[index] += candies;
+            }
+
+            return ans;
+        }
+    }
+}
