4ad26b6b608e9a54115bdd70dcad4ebdaf08c707
谢在奎 <jackshea@163.com>
2020-01-13 19:53:06 +0800

    P1108 IP 地址无效化

---
+++ LeetCode/Problems/P1108_DefangingAnIpAddress.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems
+{
+    /// IP 地址无效化
+    /// https://leetcode-cn.com/problems/defanging-an-ip-address/
+    public class P1108_DefangingAnIpAddress
+    {
+        public string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+    }
+}
