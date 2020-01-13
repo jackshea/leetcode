4b7fe884126265320e0aca4db3844284ca0f8244
谢在奎 <jackshea@163.com>
2020-01-13 21:02:51 +0800

    P1114 按序打印

---
+++ LeetCode/Problems/P1114_PrintInOrder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace LeetCode.Problems
+{
+    /// 按序打印
+    /// https://leetcode-cn.com/problems/print-in-order/
+    public class Foo
+    {
+        private AutoResetEvent second = new AutoResetEvent(false);
+        private AutoResetEvent third = new AutoResetEvent(false);
+        public Foo()
+        {
+
+        }
+
+        public void First(Action printFirst)
+        {
+
+            // printFirst() outputs "first". Do not change or remove this line.
+            printFirst();
+            second.Set();
+        }
+
+        public void Second(Action printSecond)
+        {
+            second.WaitOne();
+            // printSecond() outputs "second". Do not change or remove this line.
+            printSecond();
+            third.Set();
+        }
+
+        public void Third(Action printThird)
+        {
+            third.WaitOne();
+            // printThird() outputs "third". Do not change or remove this line.
+            printThird();
+        }
+    }
+}
