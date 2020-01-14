0369f75f9344054a20e92b3540fe6690f0140c55
谢在奎 <jackshea@163.com>
2020-01-14 15:49:49 +0800

    P1128 等价多米诺骨牌对的数量

---
+++ LeetCode/Problems/P1128_NumberOfEquivalentDominoPairs.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 等价多米诺骨牌对的数量
+    /// https://leetcode-cn.com/problems/number-of-equivalent-domino-pairs/
+    public class P1128_NumberOfEquivalentDominoPairs
+    {
+        public int NumEquivDominoPairs(int[][] dominoes)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int key = 0;
+            int ans = 0;
+            foreach (var domino in dominoes)
+            {
+                var a = domino[0];
+                var b = domino[1];
+                key = a < b ? a * 10 + b : b * 10 + a;
+                if (dic.ContainsKey(key))
+                {
+                    dic[key]++;
+                }
+                else
+                {
+                    dic[key] = 1;
+                }
+            }
+
+            foreach (var count in dic.Values)
+            {
+                if (count >= 2)
+                {
+                    ans += count * (count - 1) / 2;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
