0a26bed6ac093072e1f9457a10e2b38a9e56946a
谢在奎 <jackshea@163.com>
2020-01-14 15:55:41 +0800

    P1137 第 N 个泰波那契数

---
+++ LeetCode/Problems/P1137_NThTribonacciNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems
+{
+    /// 第 N 个泰波那契数
+    /// https://leetcode-cn.com/problems/n-th-tribonacci-number/
+    public class P1137_NThTribonacciNumber
+    {
+        public int Tribonacci(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            int a = 0;
+            int b = 1;
+            int c = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                int sum = a + b + c;
+                a = b;
+                b = c;
+                c = sum;
+            }
+
+            return c;
+        }
+    }
+}
