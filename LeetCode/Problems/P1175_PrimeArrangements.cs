c4faab75e10c7fa2b4711016f1d5817ed891ce83
谢在奎 <jackshea@163.com>
2020-01-15 23:08:21 +0800

    P1175 质数排列

---
+++ LeetCode/Problems/P1175_PrimeArrangements.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Problems
+{
+    /// 质数排列
+    /// https://leetcode-cn.com/problems/prime-arrangements/
+    public class P1175_PrimeArrangements
+    {
+        private const int Mod = 1000000007;
+
+        public int NumPrimeArrangements(int n)
+        {
+            int primeCount = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primeCount++;
+                }
+            }
+
+            var pc = Factorial(primeCount, 1L);
+            return (int)(Factorial(n - primeCount, pc));
+        }
+
+        private bool IsPrime(int n)
+        {
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private long Factorial(int n, long multiply)
+        {
+            long sum = multiply;
+            for (int i = 1; i <= n; i++)
+            {
+                sum = (sum * i) % Mod;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/P1175_PrimeArrangementsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestFixture()]
+    public class P1175_PrimeArrangementsTests
+    {
+        [Test()]
+        public void NumPrimeArrangementsTest()
+        {
+            var test = new P1175_PrimeArrangements();
+            Assert.AreEqual(612735986, test.NumPrimeArrangements(16));
+        }
+    }
+}
