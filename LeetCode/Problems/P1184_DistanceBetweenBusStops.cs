3030a9462b896b11e996992b5888e3975fa56ba1
谢在奎 <jackshea@163.com>
2020-01-22 20:59:31 +0800

    P1184 公交站间的距离

---
+++ LeetCode/Problems/P1184_DistanceBetweenBusStops.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 公交站间的距离
+    /// https://leetcode-cn.com/problems/distance-between-bus-stops/
+    public class P1184_DistanceBetweenBusStops
+    {
+        public int DistanceBetweenBusStops(int[] distance, int start, int destination)
+        {
+            int pathDist = 0;
+            int sumDist = 0;
+            if (start > destination)
+            {
+                int temp = start;
+                start = destination;
+                destination = temp;
+            }
+
+            for (int i = 0; i < distance.Length; i++)
+            {
+                sumDist += distance[i];
+                if (i >= start && i < destination)
+                {
+                    pathDist += distance[i];
+                }
+            }
+
+            return Math.Min(pathDist, sumDist - pathDist);
+        }
+    }
+}
