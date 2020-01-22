4532942ce46b1324b93d83ea007933bd68c413f3
谢在奎 <jackshea@163.com>
2020-01-22 21:57:56 +0800

    P1200 最小绝对差

---
+++ LeetCode/Problems/P1200_MinimumAbsoluteDifference.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 最小绝对差
+    /// https://leetcode-cn.com/problems/minimum-absolute-difference/
+    public class P1200_MinimumAbsoluteDifference
+    {
+        public IList<IList<int>> MinimumAbsDifference(int[] arr)
+        {
+            Array.Sort(arr);
+            int minDiff = Int32.MaxValue;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int diff = arr[i + 1] - arr[i];
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                }
+            }
+
+            IList<IList<int>> ans = new List<IList<int>>();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int diff = arr[i + 1] - arr[i];
+                if (diff == minDiff)
+                {
+                    ans.Add(new List<int>() { arr[i], arr[i + 1] });
+                }
+            }
+
+            return ans;
+        }
+    }
+}
