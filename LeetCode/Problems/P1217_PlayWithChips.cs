650acac754b8487082e40ac594ce1e61e4a9c17c
谢在奎 <jackshea@163.com>
2020-01-23 15:03:08 +0800

    P1217 玩筹码

---
+++ LeetCode/Problems/P1217_PlayWithChips.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 玩筹码
+    /// https://leetcode-cn.com/problems/play-with-chips/
+    public class P1217_PlayWithChips
+    {
+        public int MinCostToMoveChips(int[] chips)
+        {
+            int odd = 0;
+            int even = 0;
+            foreach (var chip in chips)
+            {
+                if (chip % 2 == 0)
+                {
+                    odd++;
+                }
+                else
+                {
+                    even++;
+                }
+            }
+
+            return Math.Min(odd, even);
+        }
+    }
+}
