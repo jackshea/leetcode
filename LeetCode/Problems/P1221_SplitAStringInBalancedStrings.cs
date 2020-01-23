122bd60985a5eb0f1828deb7a81e794f211c0c4f
谢在奎 <jackshea@163.com>
2020-01-23 15:20:45 +0800

    P1221 分割平衡字符串

---
+++ LeetCode/Problems/P1221_SplitAStringInBalancedStrings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 分割平衡字符串
+    /// https://leetcode-cn.com/problems/split-a-string-in-balanced-strings/
+    public class P1221_SplitAStringInBalancedStrings
+    {
+        public int BalancedStringSplit(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            int ans = 0;
+            foreach (var c in s)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(c);
+                    continue;
+                }
+
+                if (stack.Peek() != c)
+                {
+                    stack.Pop();
+                    if (stack.Count == 0)
+                    {
+                        ans++;
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
