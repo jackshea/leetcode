822c0a2187b03a41be6c129cae9afe11452b1364
谢在奎 <jackshea@163.com>
2020-01-23 14:55:02 +0800

    P1207 独一无二的出现次数

---
+++ LeetCode/Problems/P1207_UniqueNumberOfOccurrences.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 独一无二的出现次数
+    /// https://leetcode-cn.com/problems/unique-number-of-occurrences/
+    public class P1207_UniqueNumberOfOccurrences
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            var numberOfOccurrences = new Dictionary<int, int>();
+            foreach (var n in arr)
+            {
+                if (numberOfOccurrences.ContainsKey(n))
+                {
+                    numberOfOccurrences[n]++;
+                }
+                else
+                {
+                    numberOfOccurrences[n] = 1;
+                }
+            }
+
+            var exist = new HashSet<int>();
+            foreach (var v in numberOfOccurrences.Values)
+            {
+                if (exist.Contains(v))
+                {
+                    return false;
+                }
+
+                exist.Add(v);
+            }
+
+            return true;
+        }
+    }
+}
