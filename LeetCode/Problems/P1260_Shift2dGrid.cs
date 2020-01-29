8877fd8b94cf147539c1c66f8181429c1945148a
谢在奎 <jackshea@163.com>
2020-01-29 21:43:18 +0800

    P1260 二维网格迁移

---
+++ LeetCode/Problems/P1260_Shift2dGrid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    public class P1260_Shift2dGrid
+    {
+        /// 二维网格迁移
+        /// https://leetcode-cn.com/problems/shift-2d-grid/
+        public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+        {
+            var r = grid.Length;
+            var c = grid[0].Length;
+            int length = r * c;
+            k %= length;
+            var ans = new List<IList<int>>();
+            int newIndex = length - k;
+            for (int i = 0; i < r; i++)
+            {
+                IList<int> row = new List<int>();
+                for (int j = 0; j < c; j++)
+                {
+                    newIndex %= length;
+                    row.Add(grid[newIndex / c][newIndex % c]);
+                    newIndex++;
+                }
+                ans.Add(row);
+            }
+
+            return ans;
+        }
+    }
+}
