d95b4aba22124686abf3575904e8960392c2cbee
谢在奎 <jackshea@163.com>
2020-01-30 21:19:55 +0800

    P1287 有序数组中出现次数超过25%的元素

---
+++ LeetCode/Problems/P1287_ElementAppearingMoreThan25InSortedArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 有序数组中出现次数超过25%的元素
+    /// https://leetcode-cn.com/problems/element-appearing-more-than-25-in-sorted-array/
+    public class P1287_ElementAppearingMoreThan25InSortedArray
+    {
+        public int FindSpecialInteger(int[] arr)
+        {
+            int step = arr.Length / 4;
+            for (int i = 0; i < arr.Length - step; i++)
+            {
+                if (arr[i] == arr[i + step])
+                {
+                    return arr[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
