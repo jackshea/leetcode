0da05eefd0083f6e6829a7b54eabab4745c9c764
谢在奎 <jackshea@163.com>
2020-01-30 20:49:05 +0800

    P1281 整数的各位积和之差

---
+++ LeetCode/Problems/P1281_SubtractTheProductAndSumOfDigitsOfAnInteger.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Problems
+{
+    /// 整数的各位积和之差
+    /// https://leetcode-cn.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+    public class P1281_SubtractTheProductAndSumOfDigitsOfAnInteger
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            int sum = 0;
+            int product = 1;
+            while (n > 0)
+            {
+                var mod = n % 10;
+                sum += mod;
+                product *= mod;
+                n /= 10;
+            }
+
+            return product - sum;
+        }
+    }
+}
