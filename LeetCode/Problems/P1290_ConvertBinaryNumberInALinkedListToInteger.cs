d53dbc4be1bdb00b494011821d2e9be0050fb7b1
谢在奎 <jackshea@163.com>
2020-01-30 21:24:48 +0800

    P1290 二进制链表转整数

---
+++ LeetCode/Problems/P1290_ConvertBinaryNumberInALinkedListToInteger.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Problems
+{
+    /// 二进制链表转整数
+    /// https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+    public class P1290_ConvertBinaryNumberInALinkedListToInteger
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            int ans = 0;
+            ListNode cur = head;
+            while (cur != null)
+            {
+                ans <<= 1;
+                ans += cur.val;
+                cur = cur.next;
+            }
+
+            return ans;
+        }
+    }
+}
