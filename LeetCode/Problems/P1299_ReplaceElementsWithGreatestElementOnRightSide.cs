620f3cbece85a59077ce196346946d3c3e3e65bb
谢在奎 <jackshea@163.com>
2020-01-31 15:47:35 +0800

    P1299 将每个元素替换为右侧最大元素

---
+++ LeetCode/Problems/P1299_ReplaceElementsWithGreatestElementOnRightSide.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    /// 将每个元素替换为右侧最大元素
+    /// https://leetcode-cn.com/problems/replace-elements-with-greatest-element-on-right-side/
+    public class P1299_ReplaceElementsWithGreatestElementOnRightSide
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            int max = arr[arr.Length - 1];
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                if (arr[i] < max)
+                {
+                    arr[i] = max;
+                }
+                else
+                {
+                    int temp = arr[i];
+                    arr[i] = max;
+                    max = temp;
+                }
+            }
+            arr[arr.Length - 1] = -1;
+            return arr;
+        }
+    }
+}
