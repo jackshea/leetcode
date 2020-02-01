29357146296e06b15ec8695aba879b2f497d6254
谢在奎 <jackshea@163.com>
2020-02-01 15:14:00 +0800

    P1313 解压缩编码列表

---
+++ LeetCode/Problems/P1313_DecompressRunLengthEncodedList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// 解压缩编码列表
+    /// https://leetcode-cn.com/problems/decompress-run-length-encoded-list/
+    public class P1313_DecompressRunLengthEncodedList
+    {
+        public int[] DecompressRLElist(int[] nums)
+        {
+            List<int> ans = new List<int>();
+            for (int i = 0; i < nums.Length - 1; i += 2)
+            {
+                for (int j = 0; j < nums[i]; j++)
+                {
+                    ans.Add(nums[i + 1]);
+                }
+            }
+
+            return ans.ToArray();
+        }
+    }
+}
