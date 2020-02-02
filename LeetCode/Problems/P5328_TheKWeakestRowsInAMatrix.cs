c5c71ea1e554a2dbe344ad73cd0b2012939c71ef
谢在奎 <jackshea@163.com>
2020-02-02 15:18:09 +0800

    P5238 方阵中战斗力最弱的 K 行

---
+++ LeetCode/Problems/P5328_TheKWeakestRowsInAMatrix.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCode.Problems
+{
+    /// 方阵中战斗力最弱的 K 行
+    /// https://leetcode-cn.com/problems/the-k-weakest-rows-in-a-matrix/
+    public class P5328_TheKWeakestRowsInAMatrix
+    {
+        public int[] KWeakestRows(int[][] mat, int k)
+        {
+            List<PowerAndIndex> ranks = new List<PowerAndIndex>();
+            for (int i = 0; i < mat.Length; i++)
+            {
+                ranks.Add(new PowerAndIndex
+                {
+                    Power = GetPower(mat[i]),
+                    RawIndex = i
+                });
+            }
+            ranks.Sort((a, b) =>
+            {
+                if (a.Power != b.Power)
+                {
+                    return a.Power.CompareTo(b.Power);
+                }
+
+                return a.RawIndex.CompareTo(b.RawIndex);
+            });
+
+            int[] ans = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                ans[i] = ranks[i].RawIndex;
+            }
+
+            return ans;
+        }
+
+        public int GetPower(int[] row)
+        {
+            int count = 0;
+            for (int i = 0; i < row.Length; i++)
+            {
+                if (row[i] == 1)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public class PowerAndIndex
+        {
+            public int RawIndex;
+            public int Power;
+        }
+    }
+}
