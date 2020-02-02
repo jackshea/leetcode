f36a25bf8c3b0276b01f585f0733981bc1b15121
谢在奎 <jackshea@163.com>
2020-02-02 13:50:26 +0800

    P1332 删除回文子序列

---
+++ LeetCode/Problems/P1332_RemovePalindromicSubsequences.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems
+{
+    /// 删除回文子序列
+    /// https://leetcode-cn.com/problems/remove-palindromic-subsequences/
+    public class P1332_RemovePalindromicSubsequences
+    {
+        public int RemovePalindromeSub(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+
+            int left = 0;
+            int right = s.Length - 1;
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return 2;
+                }
+
+                left++;
+                right--;
+            }
+
+            return 1;
+        }
+    }
+}
