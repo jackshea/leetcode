4bb5e854fd275aa6a71a72e06029a0b2d4205edd
谢在奎 <jackshea@163.com>
2020-02-29 17:27:04 +0800

    P0003 无重复字符的最长子串

---
+++ LeetCode/Problems/Medium/P0003_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 无重复字符的最长子串
+    /// https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+    public class P0003_LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            if (s.Length == 1)
+            {
+                return 1;
+            }
+
+            var charArray = s.ToCharArray();
+            int maxLength = 0;
+            int left = 0, right = 0;
+            for (int i = 1; i < charArray.Length; i++)
+            {
+                for (int j = right; j >= left; j--)
+                {
+                    if (charArray[i] == charArray[j])
+                    {
+                        left = j + 1;
+                    }
+                }
+
+                right = i;
+                if (right - left + 1 > maxLength)
+                {
+                    maxLength = right - left + 1;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
