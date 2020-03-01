944903c281a81aa9a49580f532fd4ad5be6ccc4e
谢在奎 <jackshea@163.com>
2020-03-01 18:54:05 +0800

    P0005 最长回文子串

---
+++ LeetCode/Problems/Medium/P0005_LongestPalindromicSubstring.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 最长回文子串
+    /// https://leetcode-cn.com/problems/longest-palindromic-substring/
+    public class P0005_LongestPalindromicSubstring
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return "";
+            }
+            var chars = s.ToCharArray();
+            int start = 0, maxLenth = 0;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int len1 = Expand(chars, i);
+                int len2 = Expand2(chars, i);
+                int len = Math.Max(len1, len2);
+                if (len > maxLenth)
+                {
+                    maxLenth = len;
+                    start = i - (len - 1) / 2;
+                }
+            }
+
+            return s.Substring(start, maxLenth);
+        }
+
+        public int Expand(char[] chars, int mid)
+        {
+            int l = mid, r = mid;
+            while (l >= 0 && r < chars.Length && chars[l] == chars[r])
+            {
+                l--;
+                r++;
+            }
+
+            return r - l - 1;
+        }
+
+        public int Expand2(char[] chars, int mid)
+        {
+            int l = mid, r = mid + 1;
+            while (l >= 0 && r < chars.Length && chars[l] == chars[r])
+            {
+                l--;
+                r++;
+            }
+
+            return r - l - 1;
+        }
+    }
+}
