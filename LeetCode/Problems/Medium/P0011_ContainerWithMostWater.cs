d1876f388c0ff62bed08d7d9f9ecf90b4b78377e
谢在奎 <jackshea@163.com>
2020-03-04 15:16:06 +0800

    P0011 盛最多水的容器

---
+++ LeetCode/Problems/Medium/P0011_ContainerWithMostWater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 盛最多水的容器
+    /// https://leetcode-cn.com/problems/container-with-most-water/
+    public class P0011_ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                for (int j = i + 1; j < height.Length; j++)
+                {
+                    int area = (j - i) * Math.Min(height[i], height[j]);
+                    if (maxArea < area)
+                    {
+                        maxArea = area;
+                    }
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
