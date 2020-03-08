47c33714c4e415970327877bf881c10975471179
谢在奎 <jackshea@163.com>
2020-03-08 21:27:29 +0800

    P0016 最接近的三数之和

---
+++ LeetCode/Problems/Medium/P0016_3SumClosest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    public class P0016_3SumClosest
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            int minDiff = Int32.MaxValue;
+            int ans = 0;
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int left = i + 1;
+                int right = nums.Length - 1;
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if (sum > target)
+                    {
+                        right--;
+                    }
+                    else if (sum < target)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        return sum;
+                    }
+
+                    int diff = Math.Abs(sum - target);
+                    if (diff < minDiff)
+                    {
+                        minDiff = diff;
+                        ans = sum;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
