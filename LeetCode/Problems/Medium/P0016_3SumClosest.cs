e36e397f51fd4671ff0935d46a5c9a1dc6470fbd
谢在奎 <jackshea@163.com>
2020-03-08 21:28:24 +0800

    P0016 最接近的三数之和

--- LeetCode/Problems/Medium/P0016_3SumClosest.cs
+++ LeetCode/Problems/Medium/P0016_3SumClosest.cs
@@ -4,6 +4,8 @@ namespace LeetCode.Problems.Medium
 {
     public class P0016_3SumClosest
     {
+        /// 最接近的三数之和
+        /// https://leetcode-cn.com/problems/3sum-closest/
         public int ThreeSumClosest(int[] nums, int target)
         {
             int minDiff = Int32.MaxValue;
