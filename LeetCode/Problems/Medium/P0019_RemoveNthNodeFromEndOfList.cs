21eab15227d375496512f49c7bcd20e4a97f245a
谢在奎 <jackshea@163.com>
2020-03-10 18:54:47 +0800

    P0019 删除链表的倒数第N个节点

---
+++ LeetCode/Problems/Medium/P0019_RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 删除链表的倒数第N个节点
+    /// https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+    public class P0019_RemoveNthNodeFromEndOfList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (n <= 0)
+            {
+                return head;
+            }
+
+            ListNode forward = head;
+            ListNode last = head;
+            int index = 0;
+            while (forward != null)
+            {
+                if (index - n > 0)
+                {
+                    last = last.next;
+                }
+
+                forward = forward.next;
+                index++;
+            }
+
+            if (index - n <= 0)
+            {
+                head = head.next;
+            }
+            else
+            {
+                last.next = last.next.next;
+            }
+            return head;
+        }
+    }
+}
