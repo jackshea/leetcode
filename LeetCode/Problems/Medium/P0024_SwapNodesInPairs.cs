26bdb1a213d88e7c247b6b558c1a0788a3707539
谢在奎 <jackshea@163.com>
2020-03-12 22:46:25 +0800

    P0024 两两交换链表中的节点

---
+++ LeetCode/Problems/Medium/P0024_SwapNodesInPairs.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 两两交换链表中的节点
+    /// https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+    public class P0024_SwapNodesInPairs
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head?.next == null)
+            {
+                return head;
+            }
+
+            ListNode temp = head.next;
+            head.next = SwapPairs(temp.next);
+            temp.next = head;
+            return temp;
+        }
+    }
+}
