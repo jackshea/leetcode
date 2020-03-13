302f4bac0450adb6891a7c5b4d8b0b84814e1ce2
谢在奎 <jackshea@163.com>
2020-03-13 21:06:59 +0800

    P0050 Pow(x, n)

---
+++ LeetCode/Problems/Medium/P0050_PowXN.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// Pow(x, n)
+    /// https://leetcode-cn.com/problems/powx-n/
+    public class P0050_PowXN
+    {
+        public double MyPow(double x, int n)
+        {
+            return MyPowImpl(x, n);
+        }
+
+        private double MyPowImpl(double x, long ln)
+        {
+            if (ln == 1)
+            {
+                return x;
+            }
+
+            if (ln == 0)
+            {
+                return 1;
+            }
+
+            if (ln < 0)
+            {
+                return 1.0 / MyPowImpl(x, -ln);
+            }
+
+            double ans = MyPowImpl(x, ln / 2);
+            ans *= ans;
+
+            if (ln % 2 != 0)
+            {
+                ans *= x;
+            }
+
+            return ans;
+        }
+    }
+}
