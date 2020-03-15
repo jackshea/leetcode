3e6dd5fde57ba071aa587fd109d7580415f3986a
谢在奎 <jackshea@163.com>
2020-03-15 21:16:46 +0800

    P0120 三角形最小路径和

---
+++ LeetCode/Problems/Medium/P0120_Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 三角形最小路径和
+    /// https://leetcode-cn.com/problems/triangle/
+    /// 从下往上递推
+    public class P0120_Triangle
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            var n = triangle.Count;
+            int[] min = new int[n];
+            var endLine = triangle[n - 1];
+            for (var i = 0; i < endLine.Count; i++)
+            {
+                min[i] = endLine[i];
+            }
+
+            for (int i = triangle.Count - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < triangle[i].Count; j++)
+                {
+                    min[j] = triangle[i][j] + Math.Min(min[j], min[j + 1]);
+                }
+            }
+
+            return min[0];
+        }
+    }
+}
