87476b33478a4a158762fedd26bc15c91d3687f8
谢在奎 <jackshea@163.com>
2020-03-18 22:54:06 +0800

    P0047 全排列 II

---
+++ LeetCode/Problems/Medium/P0047_PermutationsII.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 全排列 II
+    /// https://leetcode-cn.com/problems/permutations-ii/
+    public class P0047_PermutationsII
+    {
+        private IList<IList<int>> ans = new List<IList<int>>();
+
+        public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            Array.Sort(nums);
+            DFS(nums, 0);
+            return ans;
+        }
+
+        private void DFS(int[] nums, int i)
+        {
+            if (i >= nums.Length - 1)
+            {
+                ans.Add(new List<int>(nums));
+                return;
+            }
+
+            for (int j = i; j < nums.Length; j++)
+            {
+                if (!CanSwap(nums, i, j))
+                {
+                    continue;
+                }
+
+                Swap(nums, i, j);
+                DFS(nums, i + 1);
+                Swap(nums, i, j);
+            }
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        /// 为了去重，可以交换的条件是mums[begin...end-1]没有出现过等于nums[end]的元素。
+        private bool CanSwap(int[] nums, int begin, int end)
+        {
+            for (int k = begin; k < end; k++)
+            {
+                if (nums[k] == nums[end])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
