bd15bc0392fcb76d41e2c1002e8c3f2522cb4351
谢在奎 <jackshea@163.com>
2020-03-18 17:15:50 +0800

    P0200 岛屿数量

---
+++ LeetCode/Problems/Medium/P0200_NumberOfIslands.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 岛屿数量
+    /// https://leetcode-cn.com/problems/number-of-islands/
+    public class P0200_NumberOfIslands
+    {
+        private int[] dx = { 1, -1, 0, 0 };
+        private int[] dy = { 0, 0, 1, -1 };
+        public int NumIslands(char[][] grid)
+        {
+            int ans = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        ans++;
+                        DFS(grid, i, j);
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+        private void DFS(char[][] grid, int x, int y)
+        {
+            if (x < 0 || x >= grid.Length || y < 0 || y >= grid[0].Length || grid[x][y] != '1')
+            {
+                return;
+            }
+
+            grid[x][y] = '0';
+
+            for (int i = 0; i < dx.Length; i++)
+            {
+                DFS(grid, x + dx[i], y + dy[i]);
+            }
+        }
+    }
+}
