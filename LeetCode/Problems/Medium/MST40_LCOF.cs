5e1d90d5a61dc10ec7cd920e720b83ef773b0575
谢在奎 <jackshea@163.com>
2020-03-21 00:50:21 +0800

    MST40 最小的k个数
    使用优先队列

--- LeetCode/Problems/Medium/MST40_LCOF.cs
+++ LeetCode/Problems/Medium/MST40_LCOF.cs
@@ -19,4 +19,114 @@ namespace LeetCode.Problems.Medium
             return ans;
         }
     }
+
+    /// 使用优先队列
+    public class MST40_LCOF_1
+    {
+        public int[] GetLeastNumbers(int[] arr, int k)
+        {
+            if (arr.Length <= k)
+            {
+                return arr;
+            }
+
+            var pq = new MaxPQ<int>(k + 1);
+            foreach (var v in arr)
+            {
+                pq.Insert(v);
+                if (pq.Size > k)
+                {
+                    pq.DelMax();
+                }
+            }
+
+            var ans = new int[k];
+            for (int i = 0; i < ans.Length; i++)
+            {
+                ans[i] = pq.DelMax();
+            }
+
+            return ans;
+        }
+    }
+
+    /// 优先队列，大根堆
+    public class MaxPQ<T> where T : IComparable<T>
+    {
+        private T[] pq; // pq[0] 不使用
+
+
+        public MaxPQ(int maxSize)
+        {
+            pq = new T[maxSize + 1];
+        }
+
+        public bool IsEmpty => Size == 0;
+
+        public int Size { get; private set; }
+
+        /// 插入
+        public void Insert(T v)
+        {
+            pq[++Size] = v;
+            Swim(Size);
+        }
+
+        /// 删除并返回最大值
+        public T DelMax()
+        {
+            T max = pq[1];
+            Swap(1, Size);
+            pq[Size] = default(T);
+            Size--;
+            Sink(1);
+            return max;
+        }
+
+        /// 返回最大值
+        public T GetMax()
+        {
+            return pq[1];
+        }
+
+        /// 交换两个索引的值
+        private void Swap(int i, int j)
+        {
+            T temp = pq[i];
+            pq[i] = pq[j];
+            pq[j] = temp;
+        }
+
+        /// 上浮，参数为索引
+        private void Swim(int i)
+        {
+            while (i > 1 && pq[i].CompareTo(pq[i / 2]) > 0)
+            {
+                Swap(i, i / 2);
+                i /= 2;
+            }
+        }
+
+        /// 下沉，参数为索引
+        private void Sink(int i)
+        {
+            while (2 * i <= Size)
+            {
+                int j = 2 * i;
+                if (j < Size && pq[j].CompareTo(pq[j + 1]) < 0)
+                {
+                    j++;
+                }
+
+                if (pq[i].CompareTo(pq[j]) >= 0)
+                {
+                    break;
+                }
+
+                Swap(i, j);
+                i = j;
+            }
+        }
+    }
+
 }
---
+++ LeetCodeTests/Problems/Medium/MST40_LCOF_1Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class MST40_LCOF_1Tests
+    {
+        [Test()]
+        public void GetLeastNumbersTest()
+        {
+            var test = new MST40_LCOF_1();
+            var res = test.GetLeastNumbers(new[] { 3, 2, 1 }, 2);
+            Assert.AreEqual(new[] { 1, 2 }, res);
+        }
+
+        [Test()]
+        public void GetLeastNumbersTest_1()
+        {
+            var test = new MST40_LCOF_1();
+            var res = test.GetLeastNumbers(new[] { 0, 0, 1, 2, 4, 2, 2, 3, 1, 4 }, 8);
+            Assert.AreEqual(new[] { 3, 2, 2, 2, 1, 1, 0, 0 }, res);
+        }
+    }
+}
