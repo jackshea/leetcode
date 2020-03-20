06cd97b67328ebdfef4e7c48cde8566f3ac36324
谢在奎 <jackshea@163.com>
2020-03-20 15:43:36 +0800

    MST40 最小的k个数

---
+++ LeetCode/Problems/Medium/MST40_LCOF.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 面试题40. 最小的k个数
+    /// https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+    public class MST40_LCOF
+    {
+        public int[] GetLeastNumbers(int[] arr, int k)
+        {
+            if (arr.Length <= k)
+            {
+                return arr;
+            }
+
+            Array.Sort(arr);
+            int[] ans = new int[k];
+            Array.Copy(arr, ans, k);
+            return ans;
+        }
+    }
+}
