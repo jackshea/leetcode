51fee700af816470e0087b7d6d0291a3c764b0a0
谢在奎 <jackshea@163.com>
2020-03-21 22:46:31 +0800

    P0365 水壶问题

---
+++ LeetCode/Problems/Medium/P0365_WaterAndJugProblem.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 水壶问题
+    /// https://leetcode-cn.com/problems/water-and-jug-problem/
+    public class P0365_WaterAndJugProblem
+    {
+        public bool CanMeasureWater(int x, int y, int z)
+        {
+            if (x + y < z)
+            {
+                return false;
+            }
+            var gcd = GCD(x, y);
+            if (gcd == 0)
+            {
+                return z == 0 || x + y == z;
+            }
+            return z % gcd == 0;
+        }
+
+        private int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+    }
+}
