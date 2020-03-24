93be5ff00ef247573dd74a1dd17bef37cbd1ee82
谢在奎 <jackshea@163.com>
2020-03-24 17:45:39 +0800

    P0198 打家劫舍

--- LeetCode/Problems/Easy/P0198_HouseRobber.cs
+++ LeetCode/Problems/Easy/P0198_HouseRobber.cs
@@ -1,4 +1,6 @@
-﻿namespace LeetCode.Problems.Easy
+﻿using System;
+
+namespace LeetCode.Problems.Easy
 {
     /// 打家劫舍
     /// https://leetcode-cn.com/problems/house-robber/
@@ -6,8 +8,22 @@
     {
         public int Rob(int[] nums)
         {
-            // todo: 学习动态规划
-            return 0;
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int dp0 = 0;// 前一个没选的最大值
+            int dp1 = nums[0];// 前一个选了的最大值
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int newDp0 = Math.Max(dp0, dp1);
+                dp1 = dp0 + nums[i];
+                dp0 = newDp0;
+            }
+
+            return Math.Max(dp0, dp1);
         }
     }
 }
