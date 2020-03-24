06c2d31d6f7169ca8fe4dc29b910bdbec8bcaafb
谢在奎 <jackshea@163.com>
2020-03-24 17:43:56 +0800

    MST1716 按摩师

---
+++ LeetCode/Problems/Medium/MST1716_TheMasseuselcci.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 面试题 17.16. 按摩师
+    /// https://leetcode-cn.com/problems/the-masseuse-lcci/
+    public class MST1716_TheMasseuselcci
+    {
+        public int Massage(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int dp0 = 0;// 前一个没选的最大值
+            int dp1 = nums[0];// 前一个选了的最大值
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int newDp0 = Math.Max(dp0, dp1);
+                dp1 = dp0 + nums[i];
+                dp0 = newDp0;
+            }
+
+            return Math.Max(dp0, dp1);
+        }
+    }
+
+    /// 递归，会超时
+    public class MST1716_TheMasseuselcci_0
+    {
+        public int Massage(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            return MassageMax(nums, 0);
+        }
+
+        private int MassageMax(int[] nums, int startIndex)
+        {
+            if (startIndex >= nums.Length)
+            {
+                return 0;
+            }
+
+            return Math.Max(nums[startIndex] + MassageMax(nums, startIndex + 2), MassageMax(nums, startIndex + 1));
+        }
+    }
+}
