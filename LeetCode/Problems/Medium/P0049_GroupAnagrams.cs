742a7a7de7967e7f26050f81d7476d1bb0113402
谢在奎 <jackshea@163.com>
2020-03-28 23:55:04 +0800

    P0049 字母异位词分组

---
+++ LeetCode/Problems/Medium/P0049_GroupAnagrams.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 字母异位词分组
+    /// https://leetcode-cn.com/problems/group-anagrams/
+    public class P0049_GroupAnagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+            foreach (var str in strs)
+            {
+                var keyCA = str.ToCharArray();
+                Array.Sort(keyCA);
+                string key = new string(keyCA);
+                if (dic.ContainsKey(key))
+                {
+                    dic[key].Add(str);
+                }
+                else
+                {
+                    dic[key] = new List<string> { str };
+                }
+            }
+            IList<IList<string>> ans = new List<IList<string>>();
+            foreach (var key in dic.Keys)
+            {
+                var row = new List<string>();
+                foreach (var s in dic[key])
+                {
+                    row.Add(s);
+                }
+                ans.Add(row);
+            }
+
+            return ans;
+        }
+    }
+}
