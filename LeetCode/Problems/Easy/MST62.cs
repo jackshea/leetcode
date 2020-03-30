dfed65d02c30085a12c9f8cbd5fc58a25adcb1fc
谢在奎 <jackshea@163.com>
2020-03-30 22:43:06 +0800

    MST62 圆圈中最后剩下的数字

---
+++ LeetCode/Problems/Easy/MST62.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 面试题62. 圆圈中最后剩下的数字
+    /// https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
+    public class MST62
+    {
+        public int LastRemaining(int n, int m)
+        {
+            if (n == 1)
+            {
+                return 0;
+            }
+
+            return (m + LastRemaining(n - 1, m)) % n;
+        }
+    }
+}
