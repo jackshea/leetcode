de38de120b65b472f84e24d7ac7354aab220a711
谢在奎 <jackshea@163.com>
2020-04-02 00:14:30 +0800

    P0059 螺旋矩阵 II

---
+++ LeetCode/Problems/Medium/P0059_SpiralMatrixII.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    public class P0059_SpiralMatrixII
+    {
+        /// 螺旋矩阵 II
+        /// https://leetcode-cn.com/problems/spiral-matrix-ii/
+        public int[][] GenerateMatrix(int n)
+        {
+            int[] dc = { 1, 0, -1, 0 }; // 行变化量
+            int[] dr = { 0, 1, 0, -1 }; // 列变化量
+            int start = 0;
+            int len = n - 1;
+            int v = 1;
+            int[][] ans = new int[n][];
+            int r = 0;
+            int c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ans[i] = new int[n];
+            }
+            while (len > 0)
+            {
+                for (int i = 0; i < dc.Length; i++)
+                {
+                    for (int j = 0; j < len; j++)
+                    {
+                        ans[r][c] = v;
+                        v++;
+                        r += dr[i];
+                        c += dc[i];
+                    }
+                }
+
+                start++;
+                len -= 2;
+                r = start;
+                c = start;
+            }
+
+            if (len == 0)
+            {
+                ans[r][c] = v;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0059_SpiralMatrixIITests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0059_SpiralMatrixIITests
+    {
+        [Test()]
+        public void GenerateMatrixTest()
+        {
+            var test = new P0059_SpiralMatrixII();
+            test.GenerateMatrix(4);
+            Assert.IsTrue(true);
+        }
+    }
+}
