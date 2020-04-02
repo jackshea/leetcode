36efc63603663181b28e93e268111f6d118f4d9e
谢在奎 <jackshea@163.com>
2020-04-02 19:45:38 +0800

    P0062 不同路径

---
+++ LeetCode/Problems/Medium/P0062_UniquePaths.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 不同路径
+    /// https://leetcode-cn.com/problems/unique-paths/
+    public class P0062_UniquePaths
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[,] grid = new int[m, n];
+            for (int i = 0; i < n; i++)
+            {
+                grid[m - 1, i] = 1;
+            }
+            for (int i = 0; i < m; i++)
+            {
+                grid[i, n - 1] = 1;
+            }
+
+            for (int i = m - 2; i >= 0; i--)
+            {
+                for (int j = n - 2; j >= 0; j--)
+                {
+                    grid[i, j] = grid[i + 1, j] + grid[i, j + 1];
+                }
+            }
+
+            return grid[0, 0];
+        }
+    }
+}
