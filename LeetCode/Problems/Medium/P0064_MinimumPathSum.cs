725641cf4467a99e59299745e6ca25f1779a1f7f
谢在奎 <jackshea@163.com>
2020-04-02 21:04:20 +0800

    P0064 最小路径和

--- LeetCode/Problems/Medium/P0064_MinimumPathSum.cs
+++ LeetCode/Problems/Medium/P0064_MinimumPathSum.cs
@@ -37,4 +37,32 @@ namespace LeetCode.Problems.Medium
             return cost[r, c];
         }
     }
+
+    /// 不需要额外空间
+    public class P0064_MinimumPathSum_1
+    {
+        public int MinPathSum(int[][] grid)
+        {
+            for (int i = grid.Length - 1; i >= 0; i--)
+            {
+                for (int j = grid[0].Length - 1; j >= 0; j--)
+                {
+                    if (i == grid.Length - 1 && j < grid[0].Length - 1)
+                    {
+                        grid[i][j] += grid[i][j + 1];
+                    }
+                    else if (i < grid.Length - 1 && j == grid[0].Length - 1)
+                    {
+                        grid[i][j] += grid[i + 1][j];
+                    }
+                    else if (i < grid.Length - 1 && j < grid[0].Length - 1)
+                    {
+                        grid[i][j] += Math.Min(grid[i][j + 1], grid[i + 1][j]);
+                    }
+                }
+            }
+
+            return grid[0][0];
+        }
+    }
 }
