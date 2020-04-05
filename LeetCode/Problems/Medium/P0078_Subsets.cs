a509c7c4711396922a42fe42f2348db7e76490c5
谢在奎 <jackshea@163.com>
2020-04-05 20:46:20 +0800

    P0078 子集

---
+++ LeetCode/Problems/Medium/P0078_Subsets.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 子集
+    /// https://leetcode-cn.com/problems/subsets/
+    public class P0078_Subsets
+    {
+        private int[] nums;
+        private IList<IList<int>> ans;
+
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            this.nums = nums;
+            ans = new List<IList<int>>();
+            ans.Add(new List<int>());
+            DFS(0, new List<int>());
+            return ans;
+        }
+
+        private void DFS(int start, List<int> selected)
+        {
+            if (start == nums.Length)
+            {
+                return;
+            }
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                selected.Add(nums[i]);
+                ans.Add(new List<int>(selected));
+                DFS(i + 1, selected);
+                selected.RemoveAt(selected.Count - 1);
+            }
+        }
+    }
+}
