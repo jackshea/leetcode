a2cde801b7bf2e1a4556ded54e1324eb027fd90e
谢在奎 <jackshea@163.com>
2020-04-05 19:21:29 +0800

    P0075 颜色分类

---
+++ LeetCode/Problems/Medium/P0075_SortColors.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 颜色分类(三路排序)
+    /// https://leetcode-cn.com/problems/sort-colors/
+    public class P0075_SortColors
+    {
+        public void SortColors(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            int cur = 1;
+            while (cur <= right)
+            {
+                if (nums[cur] < 1)
+                {
+                    Swap(nums, left++, cur++);
+                }
+                else if (nums[cur] > 1)
+                {
+                    Swap(nums, cur, right--);
+                }
+                else
+                {
+                    cur++;
+                }
+            }
+        }
+
+        public void Swap(int[] num, int a, int b)
+        {
+            int temp = num[a];
+            num[a] = num[b];
+            num[b] = temp;
+        }
+    }
+}
