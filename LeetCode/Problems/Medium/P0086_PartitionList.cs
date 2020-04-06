b305e0c98fd564b7a341d628471d5808268651b8
谢在奎 <jackshea@163.com>
2020-04-06 23:36:59 +0800

    P0086 分隔链表

--- LeetCode/Problems/Medium/P0086_PartitionList.cs
+++ LeetCode/Problems/Medium/P0086_PartitionList.cs
@@ -2,6 +2,8 @@
 
 namespace LeetCode.Problems.Medium
 {
+    /// 分隔链表
+    /// https://leetcode-cn.com/problems/partition-list/
     public class P0086_PartitionList
     {
         public ListNode Partition(ListNode head, int x)
@@ -42,6 +44,38 @@ namespace LeetCode.Problems.Medium
             }
 
             return fakeHead.next;
+        }
+    }
+
+    // 双链表实现，结构简单清晰
+    public class P0086_PartitionList_1
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            ListNode greaterHead = new ListNode(0);
+            ListNode lessHead = new ListNode(0);
+            ListNode greaterNode = greaterHead;
+            ListNode lessNode = lessHead;
+            ListNode cur = head;
+            while (cur != null)
+            {
+                if (cur.val < x)
+                {
+                    lessNode.next = cur;
+                    lessNode = lessNode.next;
+                }
+                else
+                {
+                    greaterNode.next = cur;
+                    greaterNode = greaterNode.next;
+                }
+
+                cur = cur.next;
+            }
+
+            greaterNode.next = null;
+            lessNode.next = greaterHead.next;
+            return lessHead.next;
         }
     }
 }
