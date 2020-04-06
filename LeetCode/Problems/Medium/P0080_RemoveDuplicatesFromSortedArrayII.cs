5b31eda845b189cb3577ad8729efb3acd4b44a0d
谢在奎 <jackshea@163.com>
2020-04-06 14:28:33 +0800

    P0080 删除排序数组中的重复项 II

---
+++ LeetCode/Problems/Medium/P0080_RemoveDuplicatesFromSortedArrayII.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 删除排序数组中的重复项 II
+    /// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+    public class P0080_RemoveDuplicatesFromSortedArrayII
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null)
+            {
+                return 0;
+            }
+
+            if (nums.Length <= 2)
+            {
+                return nums.Length;
+            }
+
+            int a = 2;
+            for (int i = 2; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[a - 2])
+                {
+                    nums[a++] = nums[i];
+                }
+            }
+
+            return a;
+        }
+    }
+}
