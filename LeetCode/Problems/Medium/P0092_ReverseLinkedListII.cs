e3e8b66cd670d6032a711c3b237990db66b74532
谢在奎 <jackshea@163.com>
2020-04-07 14:25:57 +0800

    P0092 反转链表 II

---
+++ LeetCode/Problems/Medium/P0092_ReverseLinkedListII.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 反转链表 II
+    /// https://leetcode-cn.com/problems/reverse-linked-list-ii/
+    public class P0092_ReverseLinkedListII
+    {
+        public ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+            ListNode fakeHead = new ListNode(0);
+            fakeHead.next = head;
+            ListNode cur = fakeHead;
+            for (int i = 1; i < m && cur != null; i++)
+            {
+                cur = cur.next;
+            }
+
+            if (cur == null)
+            {
+                return head;
+            }
+
+            ListNode revHead = cur;
+            cur = cur.next;
+            ListNode revTail = cur;
+            ListNode pre = null;
+            ListNode next = null;
+            for (int i = m; i <= n; i++)
+            {
+                next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+
+            revHead.next = pre;
+            revTail.next = cur;
+            return fakeHead.next;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0092_ReverseLinkedListIITests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0092_ReverseLinkedListIITests
+    {
+        [Test()]
+        public void ReverseBetweenTest()
+        {
+            var list = Utils.BuildList(new int[] { 1, 2, 3, 4, 5 });
+            var test = new P0092_ReverseLinkedListII();
+            var actual = test.ReverseBetween(list, 2, 4);
+            Assert.AreEqual(new int[] { 1, 4, 3, 2, 5 }, Utils.ListToArray(actual));
+        }
+    }
+}
