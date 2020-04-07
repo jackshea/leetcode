aeb54e70df34bc237ffd7a67ee4209092194e7d0
谢在奎 <jackshea@163.com>
2020-04-07 11:15:40 +0800

    P0090 子集 II

---
+++ LeetCode/Problems/Medium/P0090_SubsetsII.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 子集 II
+    /// https://leetcode-cn.com/problems/subsets-ii/
+    public class P0090_SubsetsII
+    {
+        IList<IList<int>> ans = new List<IList<int>>();
+        private int[] nums;
+
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            this.nums = nums;
+            Array.Sort(nums);
+            ans.Add(new List<int>());
+            DFS(0, new List<int>());
+            return ans;
+        }
+
+        private void DFS(int start, List<int> selected)
+        {
+            if (start == nums.Length)
+            {
+                return;
+            }
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                if (i == start || nums[i] != nums[i - 1])
+                {
+                    selected.Add(nums[i]);
+                    ans.Add(new List<int>(selected));
+                    DFS(i + 1, selected);
+                    selected.RemoveAt(selected.Count - 1);
+                }
+            }
+        }
+    }
+}
