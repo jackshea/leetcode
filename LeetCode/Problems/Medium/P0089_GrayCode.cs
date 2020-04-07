5ba0f7dd0862d938c1058aa62102f8577e7e5be2
谢在奎 <jackshea@163.com>
2020-04-07 10:24:37 +0800

    P0089 格雷编码

---
+++ LeetCode/Problems/Medium/P0089_GrayCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 格雷编码
+    /// https://leetcode-cn.com/problems/gray-code/
+    public class P0089_GrayCode
+    {
+        public IList<int> GrayCode(int n)
+        {
+            var ans = new List<int>();
+            ans.Add(0);
+            int max = (int)Math.Pow(2, n);
+            for (int i = 1; i <= max; i++)
+            {
+                ans.Add(i ^ (i / 2));
+            }
+
+            return ans;
+        }
+    }
+}
