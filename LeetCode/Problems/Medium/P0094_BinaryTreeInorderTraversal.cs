872bb56683cf117e998caa334edde44a7b90309d
谢在奎 <jackshea@163.com>
2020-04-08 16:00:43 +0800

    P0094 二叉树的中序遍历
    非递归

--- LeetCode/Problems/Medium/P0094_BinaryTreeInorderTraversal.cs
+++ LeetCode/Problems/Medium/P0094_BinaryTreeInorderTraversal.cs
@@ -25,4 +25,41 @@ namespace LeetCode.Problems.Medium
             DFS(node.right);
         }
     }
+
+    /// 非递归
+    public class P0094_BinaryTreeInorderTraversal_1
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            IList<int> ans = new List<int>();
+            if (root == null)
+            {
+                return ans;
+            }
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            TreeNode cur = root;
+
+            while (stack.Count != 0)
+            {
+                while (cur.left != null)
+                {
+                    stack.Push(cur.left);
+                    cur = cur.left;
+                }
+                while (stack.Count != 0)
+                {
+                    var node = stack.Pop();
+                    ans.Add(node.val);
+                    if (node.right != null)
+                    {
+                        stack.Push(node.right);
+                        cur = node.right;
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
 }
