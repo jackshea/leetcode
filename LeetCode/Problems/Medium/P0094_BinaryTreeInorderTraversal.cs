e4205997f9a082a5a4e98c53cdb9213625400e3a
谢在奎 <jackshea@163.com>
2020-04-08 14:51:03 +0800

    P0094 二叉树的中序遍历

---
+++ LeetCode/Problems/Medium/P0094_BinaryTreeInorderTraversal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 二叉树的中序遍历
+    /// https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+    public class P0094_BinaryTreeInorderTraversal
+    {
+        private IList<int> ans = new List<int>();
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            DFS(root);
+            return ans;
+        }
+
+        private void DFS(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            DFS(node.left);
+            ans.Add(node.val);
+            DFS(node.right);
+        }
+    }
+}
