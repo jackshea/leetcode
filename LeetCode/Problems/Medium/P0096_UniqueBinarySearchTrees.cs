04b68a022abf83f8735124ef112d5fa2fe39a845
谢在奎 <jackshea@163.com>
2020-04-08 17:12:13 +0800

    P0096 不同的二叉搜索树

---
+++ LeetCode/Problems/Medium/P0096_UniqueBinarySearchTrees.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 不同的二叉搜索树
+    /// https://leetcode-cn.com/problems/unique-binary-search-trees/
+    public class P0096_UniqueBinarySearchTrees
+    {
+        private int[] cache;
+        public int NumTrees(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            if (cache == null)
+            {
+                cache = new int[n + 1];
+            }
+
+            if (cache[n] != 0)
+            {
+                return cache[n];
+            }
+
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var left = NumTrees(i);
+                var right = NumTrees(n - 1 - i);
+                var p = left * right;
+                p = p == 0 ? left + right : p;
+                if (p == 0)
+                {
+                    p = 1;
+                }
+
+                ans += p;
+            }
+
+            cache[n] = ans;
+            return ans;
+        }
+    }
+}
