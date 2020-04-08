141554351325a1dfd145997e0a6e0b1c5a4f622a
谢在奎 <jackshea@163.com>
2020-04-08 14:45:50 +0800

    P0093 复原IP地址

---
+++ LeetCode/Problems/Medium/P0093_RestoreIpAddresses.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 复原IP地址
+    /// https://leetcode-cn.com/problems/restore-ip-addresses/
+    public class P0093_RestoreIpAddresses
+    {
+        private int[] ipAddr = new int[4];
+        private List<string> ans = new List<string>();
+        public IList<string> RestoreIpAddresses(string s)
+        {
+            DFS(0, 0, s);
+            return ans;
+        }
+
+        private void DFS(int ipIndex, int sIndex, string s)
+        {
+            if (ipIndex >= 4)
+            {
+                if (sIndex >= s.Length)
+                {
+                    ans.Add(ToString(ipAddr));
+                }
+                return;
+            }
+
+            for (int i = sIndex; i < sIndex + 3; i++)
+            {
+                var num = ToIpNumber(s, sIndex, i);
+                if (num >= 0)
+                {
+                    ipAddr[ipIndex] = num;
+                    DFS(ipIndex + 1, i + 1, s);
+                }
+            }
+        }
+
+        private string ToString(int[] ipAddr)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < ipAddr.Length; i++)
+            {
+                if (i != 0)
+                {
+                    sb.Append('.');
+                }
+
+                sb.Append(ipAddr[i]);
+            }
+
+            return sb.ToString();
+        }
+
+        private int ToIpNumber(string s, int start, int end)
+        {
+            if (end >= s.Length)
+            {
+                return -1;
+            }
+            var len = end - start + 1;
+            if (len < 1 || len > 3)
+            {
+                return -1;
+            }
+            if (len != 1 && s[start] == '0')
+            {
+                return -1;
+            }
+
+            int num = Convert.ToInt32(s.Substring(start, len));
+            if (num < 0 || num > 255)
+            {
+                return -1;
+            }
+
+            return num;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0093_RestoreIpAddressesTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0093_RestoreIpAddressesTests
+    {
+        [Test()]
+        public void RestoreIpAddressesTest()
+        {
+            var test = new P0093_RestoreIpAddresses();
+            var actual = test.RestoreIpAddresses("25525511135");
+            var expected = new List<string> { "255.255.11.135", "255.255.111.35" };
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void RestoreIpAddressesTest1()
+        {
+            var test = new P0093_RestoreIpAddresses();
+            var actual = test.RestoreIpAddresses("0000");
+            var expected = new List<string> { "0.0.0.0" };
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
