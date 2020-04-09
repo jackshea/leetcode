50fe4c7155dbdf36717651b4653ad8c2dd9a2373
谢在奎 <jackshea@163.com>
2020-04-09 11:31:56 +0800

    P0109 有序链表转换二叉搜索树

---
+++ LeetCode/Problems/Medium/P0109_ConvertSortedListToBinarySearchTree.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 有序链表转换二叉搜索树
+    /// https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/
+    public class P0109_ConvertSortedListToBinarySearchTree
+    {
+        private ListNode head;
+
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            this.head = head;
+            int size = 0;
+            ListNode cur = head;
+            while (cur != null)
+            {
+                cur = cur.next;
+                size++;
+            }
+
+            return BuildBST(0, size - 1);
+        }
+
+        private TreeNode BuildBST(int left, int right)
+        {
+            if (left > right)
+            {
+                return null;
+            }
+
+            int mid = left + (right - left) / 2;
+            var subLeftTree = BuildBST(left, mid - 1);
+            var root = new TreeNode(head.val);
+            root.left = subLeftTree;
+            head = head.next;
+            root.right = BuildBST(mid + 1, right);
+            return root;
+        }
+    }
+}
