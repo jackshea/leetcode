86f7d956b166e49b2fa17357a62705a6a153bd50
谢在奎 <jackshea@163.com>
2020-04-09 12:02:15 +0800

    P0114 二叉树展开为链表

---
+++ LeetCode/Problems/Medium/P0114_FlattenBinaryTreeToLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 二叉树展开为链表
+    /// https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
+    public class P0114_FlattenBinaryTreeToLinkedList
+    {
+        public void Flatten(TreeNode root)
+        {
+            while (root != null)
+            {
+                if (root.left != null)
+                {
+                    TreeNode pre = root.left;
+                    while (pre.right != null)
+                    {
+                        pre = pre.right;
+                    }
+
+                    pre.right = root.right;
+                    root.right = root.left;
+                    root.left = null;
+                }
+                else
+                {
+                    root = root.right;
+                }
+            }
+        }
+    }
+}
