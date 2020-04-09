946836e96b959e3c9dec4354376b96784b6a50ea
谢在奎 <jackshea@163.com>
2020-04-09 10:09:53 +0800

    P0103 二叉树的锯齿形层次遍历

---
+++ LeetCode/Problems/Medium/P0103_BinaryTreeZigzagLevelOrderTraversal.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 二叉树的锯齿形层次遍历
+    /// https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/
+    public class P0103_BinaryTreeZigzagLevelOrderTraversal
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            IList<IList<int>> ans = new List<IList<int>>();
+
+            if (root == null)
+            {
+                return ans;
+            }
+
+            Stack<TreeNode> stack1 = new Stack<TreeNode>();
+            Stack<TreeNode> stack2 = new Stack<TreeNode>();
+            stack1.Push(root);
+            while (stack1.Count != 0 || stack2.Count != 0)
+            {
+                List<int> row = new List<int>();
+                while (stack1.Count != 0)
+                {
+                    var node = stack1.Pop();
+                    row.Add(node.val);
+                    if (node.left != null)
+                    {
+                        stack2.Push(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        stack2.Push(node.right);
+                    }
+                }
+                if (row.Count > 0)
+                {
+                    ans.Add(row);
+                }
+                List<int> row2 = new List<int>();
+                while (stack2.Count != 0)
+                {
+                    var node = stack2.Pop();
+                    row2.Add(node.val);
+                    if (node.right != null)
+                    {
+                        stack1.Push(node.right);
+                    }
+                    if (node.left != null)
+                    {
+                        stack1.Push(node.left);
+                    }
+                }
+                if (row2.Count > 0)
+                {
+                    ans.Add(row2);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
