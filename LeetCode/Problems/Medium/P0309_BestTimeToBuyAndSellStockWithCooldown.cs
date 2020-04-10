970472ef75d10b88167f87f997cdb6fd545b50d7
谢在奎 <jackshea@163.com>
2020-04-10 16:23:11 +0800

    P0309 最佳买卖股票时机含冷冻期

---
+++ LeetCode/Problems/Medium/P0309_BestTimeToBuyAndSellStockWithCooldown.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 最佳买卖股票时机含冷冻期
+    /// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+    public class P0309_BestTimeToBuyAndSellStockWithCooldown
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int dp0 = 0; //  前一天不持有最大利润
+            int dp00 = 0; // 前两天不持有最大利润
+            int dp1 = int.MinValue; // 前一天持有最大利润
+            foreach (var price in prices)
+            {
+                dp1 = Math.Max(dp1, dp00 - price);
+                dp00 = dp0;
+                dp0 = Math.Max(dp0, dp1 + price);
+            }
+
+            return dp0;
+        }
+    }
+}
