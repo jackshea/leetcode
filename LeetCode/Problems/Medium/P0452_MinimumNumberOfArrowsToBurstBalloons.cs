b82959b918c6a0010ada209c894a338b2560e8f6
谢在奎 <jackshea@163.com>
2020-04-10 23:42:47 +0800

    P0452 用最少数量的箭引爆气球

---
+++ LeetCode/Problems/Medium/P0452_MinimumNumberOfArrowsToBurstBalloons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 用最少数量的箭引爆气球
+    /// https://leetcode-cn.com/problems/minimum-number-of-arrows-to-burst-balloons/
+    public class P0452_MinimumNumberOfArrowsToBurstBalloons
+    {
+        public int FindMinArrowShots(int[][] points)
+        {
+            if (points.Length <= 1)
+            {
+                return points.Length;
+            }
+            // 按结束端点从小到大排序
+            Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
+            int ans = 1;
+            int end = points[0][1];
+            for (int i = 1; i < points.Length; i++)
+            {
+                if (points[i][0] > end)
+                {
+                    ans++;
+                    end = points[i][1];
+                }
+            }
+
+            return ans;
+        }
+    }
+}
