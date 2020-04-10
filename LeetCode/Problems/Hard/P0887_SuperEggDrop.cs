74124ebf6ec9d207d5279658dc96b42bf6d807ac
谢在奎 <jackshea@163.com>
2020-04-10 19:40:53 +0800

    P0887 鸡蛋掉落

---
+++ LeetCode/Problems/Hard/P0887_SuperEggDrop.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Problems.Hard
+{
+    /// 鸡蛋掉落
+    /// https://leetcode-cn.com/problems/super-egg-drop/
+    public class P0887_SuperEggDrop
+    {
+        public int SuperEggDrop(int K, int N)
+        {
+            int[] dp = new int[K + 1];
+            int m = 0;
+            while (dp[K] < N)
+            {
+                for (int i = K; i >= 1; i--)
+                {
+                    dp[i] += dp[i - 1] + 1;
+                }
+
+                m++;
+            }
+
+            return m;
+        }
+    }
+}
