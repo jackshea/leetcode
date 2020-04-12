0b74be2db847578114883dddfa46605d443cded1
谢在奎 <jackshea@163.com>
2020-04-12 14:24:46 +0800

    P0494 目标和

---
+++ LeetCode/Problems/Medium/P0494_TargetSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 目标和
+    /// https://leetcode-cn.com/problems/target-sum/
+    public class P0494_TargetSum
+    {
+        public int FindTargetSumWays(int[] nums, int S)
+        {
+            int[] dp = new int[2001];
+            dp[nums[0] + 1000] += 1;
+            dp[-nums[0] + 1000] += 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int[] next = new int[2001];
+                for (int j = -1000; j < 1001; j++)
+                {
+                    if (dp[j + 1000] > 0)
+                    {
+                        next[j + nums[i] + 1000] += dp[j + 1000];
+                        next[j - nums[i] + 1000] += dp[j + 1000];
+                    }
+                }
+
+                dp = next;
+            }
+
+            return S > 1000 ? 0 : dp[S + 1000];
+        }
+    }
+}
