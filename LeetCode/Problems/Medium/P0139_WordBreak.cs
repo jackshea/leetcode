a747169c5d06cd821eee93d32fff918e23ceed1d
谢在奎 <jackshea@163.com>
2020-04-12 18:08:05 +0800

    P0139 单词拆分

---
+++ LeetCode/Problems/Medium/P0139_WordBreak.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 单词拆分
+    /// https://leetcode-cn.com/problems/word-break/
+    public class P0139_WordBreak
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            HashSet<string> set = new HashSet<string>();
+            foreach (var word in wordDict)
+            {
+                set.Add(word);
+            }
+
+            bool[] dp = new bool[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    bool isContains = set.Contains(s.Substring(j, i - j + 1));
+                    if (isContains && (j == 0 || j > 0 && dp[j - 1]))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[s.Length - 1];
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0139_WordBreakTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0139_WordBreakTests
+    {
+        [Test()]
+        public void WordBreakTest()
+        {
+            var test = new P0139_WordBreak();
+            var actual = test.WordBreak("leetcode", new List<string> { "leet", "code" });
+            Assert.IsTrue(actual);
+        }
+    }
+}
