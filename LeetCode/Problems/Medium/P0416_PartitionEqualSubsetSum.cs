a28af4481338c154b5d0c4b8ff4b3747db05a318
谢在奎 <jackshea@163.com>
2020-04-12 13:27:51 +0800

    P0416 分割等和子集

---
+++ LeetCode/Problems/Medium/P0416_PartitionEqualSubsetSum.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 分割等和子集
+    /// https://leetcode-cn.com/problems/partition-equal-subset-sum/
+    public class P0416_PartitionEqualSubsetSum
+    {
+        public bool CanPartition(int[] nums)
+        {
+            var sum = nums.Sum();
+            if (sum % 2 != 0)
+            {
+                return false;
+            }
+
+            var v = sum / 2;
+            bool[] dp = new bool[v + 1]; // 是否存在和为i的组合
+            if (nums[0] <= v)
+            {
+                dp[nums[0]] = true;
+            }
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                for (int j = v; j - nums[i] >= 0; j--)
+                {
+                    dp[j] |= dp[j - nums[i]];
+                }
+            }
+
+            return dp[v];
+        }
+    }
+}
