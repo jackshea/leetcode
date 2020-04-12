b8ef7fe67d433a8a2c5cea5035ed4ea39f39479a
谢在奎 <jackshea@163.com>
2020-04-12 15:14:28 +0800

    P0322 零钱兑换

---
+++ LeetCode/Problems/Medium/P0322_CoinChange.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 零钱兑换
+    /// https://leetcode-cn.com/problems/coin-change/
+    public class P0322_CoinChange
+    {
+        public int CoinChange(int[] coins, int amount)
+        {
+            // dp[m]==n 其中m为金额总数，n为方案数
+            // dp[m]=Min(dp[m],dp[m-coins[i]]+1)
+            int[] dp = new int[amount + 1];
+            for (int i = 1; i < dp.Length; i++)
+            {
+                dp[i] = amount + 1;
+            }
+
+            if (coins[0] == amount)
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                for (int j = coins[i]; j <= amount; j++)
+                {
+                    dp[j] = Math.Min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+
+            return dp[amount] == amount + 1 ? -1 : dp[amount];
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0322_CoinChangeTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0322_CoinChangeTests
+    {
+        [Test()]
+        public void CoinChangeTest()
+        {
+            var test = new P0322_CoinChange();
+            var count = test.CoinChange(new[] { 1, 2, 5 }, 11);
+            Assert.AreEqual(3, count);
+        }
+    }
+}
