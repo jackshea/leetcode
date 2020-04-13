b042da5514924a066558ce84f8b1af621e236be1
谢在奎 <jackshea@163.com>
2020-04-13 17:12:04 +0800

    P0264 丑数 II

---
+++ LeetCode/Problems/Medium/P0264_UglyNumberII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 丑数 II
+    /// https://leetcode-cn.com/problems/ugly-number-ii/
+    public class P0264_UglyNumberII
+    {
+        public int NthUglyNumber(int n)
+        {
+            List<int> uglyNums = new List<int>();
+            uglyNums.Add(1);
+            int n2 = 0, n3 = 0, n5 = 0;
+            while (uglyNums.Count < n)
+            {
+                int nextUglyNums = Math.Min(Math.Min(uglyNums[n2] * 2, uglyNums[n3] * 3), uglyNums[n5] * 5);
+                uglyNums.Add(nextUglyNums);
+                if (nextUglyNums == uglyNums[n2] * 2)
+                {
+                    n2++;
+                }
+                if (nextUglyNums == uglyNums[n3] * 3)
+                {
+                    n3++;
+                }
+                if (nextUglyNums == uglyNums[n5] * 5)
+                {
+                    n5++;
+                }
+            }
+
+            return uglyNums[n - 1];
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0264_UglyNumberIITests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Problems.Easy;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0264_UglyNumberIITests
+    {
+        [Test()]
+        public void NthUglyNumberTest()
+        {
+            var test = new P0264_UglyNumberII();
+            var actual = test.NthUglyNumber(10);
+            Assert.AreEqual(12, actual);
+        }
+    }
+}
