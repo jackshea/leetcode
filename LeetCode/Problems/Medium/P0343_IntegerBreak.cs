aba343a286d41fba0131d14a42bc32b7b4d58dc6
谢在奎 <jackshea@163.com>
2020-04-14 13:39:58 +0800

    P0343 整数拆分

---
+++ LeetCode/Problems/Medium/P0343_IntegerBreak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    public class P0343_IntegerBreak
+    {
+        public int IntegerBreak(int n)
+        {
+            if (n < 4)
+            {
+                return n - 1;
+            }
+
+            int[] dp = new int[n + 1]; // 和为n 的最大乘积，可以不拆分
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 3;
+            for (int i = 4; i <= n; i++)
+            {
+                int max = i;
+                for (int j = 2; j < i; j++)
+                {
+                    max = Math.Max(max, dp[j] * dp[i - j]);
+                }
+
+                dp[i] = max;
+            }
+
+            return dp[n];
+        }
+    }
+}
