ab7bd2df719f6f4f6ce55c74b2d89fd61963656d
谢在奎 <jackshea@163.com>
2020-04-14 15:18:32 +0800

    P0350 计算各个位数不同的数字个数

---
+++ LeetCode/Problems/Medium/P0357_CountNumbersWithUniqueDigits.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 计算各个位数不同的数字个数
+    /// https://leetcode-cn.com/problems/count-numbers-with-unique-digits/
+    public class P0357_CountNumbersWithUniqueDigits
+    {
+        public int CountNumbersWithUniqueDigits(int n)
+        {
+            int factor = 9;
+            int product = 9;
+            int ans = 1;
+            for (int i = 0; i < n; i++)
+            {
+                ans += product;
+                product *= factor--;
+            }
+
+            return ans;
+        }
+    }
+}
