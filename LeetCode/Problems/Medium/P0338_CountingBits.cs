f65b2844fca28330375b46232b107311afbe0b71
谢在奎 <jackshea@163.com>
2020-04-14 13:05:18 +0800

    P0338 比特位计数

---
+++ LeetCode/Problems/Medium/P0338_CountingBits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 比特位计数
+    /// https://leetcode-cn.com/problems/counting-bits/
+    public class P0338_CountingBits
+    {
+        public int[] CountBits(int num)
+        {
+            int[] ans = new int[num + 1];
+            for (int i = 1; i <= num; i++)
+            {
+                ans[i] = ans[i & (i - 1)] + 1;
+            }
+
+            return ans;
+        }
+    }
+}
