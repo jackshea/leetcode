948533f1560a06445cf3899754a9d2a14b28f1d0
谢在奎 <jackshea@163.com>
2020-04-15 13:07:02 +0800

    P0117 填充每个节点的下一个右侧节点指针 II

---
+++ LeetCode/Problems/Medium/P0117_PopulatingNextRightPointersInEachNodeII.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 填充每个节点的下一个右侧节点指针 II
+    /// https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/
+    public class P0117_PopulatingNextRightPointersInEachNodeII
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                Node next = null;
+                int count = q.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var top = q.Dequeue();
+                    top.next = next;
+                    next = top;
+
+                    if (top.right != null)
+                    {
+                        q.Enqueue(top.right);
+                    }
+                    if (top.left != null)
+                    {
+                        q.Enqueue(top.left);
+                    }
+                }
+            }
+
+            return root;
+        }
+
+        public class Node
+        {
+            public int val;
+            public Node left;
+            public Node right;
+            public Node next;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, Node _left, Node _right, Node _next)
+            {
+                val = _val;
+                left = _left;
+                right = _right;
+                next = _next;
+            }
+        }
+    }
+}
