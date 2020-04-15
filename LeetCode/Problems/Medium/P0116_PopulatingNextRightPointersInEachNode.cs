b34e8ce11bcabdee538f2df20b52e46f21371673
谢在奎 <jackshea@163.com>
2020-04-15 12:40:24 +0800

    P0116 填充每个节点的下一个右侧节点指针

---
+++ LeetCode/Problems/Medium/P0116_PopulatingNextRightPointersInEachNode.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 填充每个节点的下一个右侧节点指针
+    /// https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+    public class P0116_PopulatingNextRightPointersInEachNode
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null || root.left == null || root.right == null)
+            {
+                return root;
+            }
+
+            root.left.next = root.right;
+            if (root.next != null)
+            {
+                root.right.next = root.next.left;
+            }
+
+            Connect(root.left);
+            Connect(root.right);
+            return root;
+        }
+
+        public class Node
+        {
+            public int val;
+            public Node left;
+            public Node right;
+            public Node next;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, Node _left, Node _right, Node _next)
+            {
+                val = _val;
+                left = _left;
+                right = _right;
+                next = _next;
+            }
+        }
+    }
+}
