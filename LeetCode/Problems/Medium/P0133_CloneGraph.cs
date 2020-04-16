637511bd308e49eae6c4aa65f10297b4ae94b1b1
谢在奎 <jackshea@163.com>
2020-04-16 22:02:09 +0800

    P0133 克隆图

---
+++ LeetCode/Problems/Medium/P0133_CloneGraph.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 克隆图
+    /// https://leetcode-cn.com/problems/clone-graph/
+    public class P0133_CloneGraph
+    {
+        private Dictionary<Node, Node> visited = new Dictionary<Node, Node>();
+        public Node CloneGraph(Node node)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            if (visited.ContainsKey(node))
+            {
+                return visited[node];
+            }
+
+            var newNode = new Node(node.val);
+            visited[node] = newNode;
+            foreach (var neighbor in node.neighbors)
+            {
+                newNode.neighbors.Add(CloneGraph(neighbor));
+            }
+            return newNode;
+        }
+
+        public class Node
+        {
+            public int val;
+            public IList<Node> neighbors;
+
+            public Node()
+            {
+                val = 0;
+                neighbors = new List<Node>();
+            }
+
+            public Node(int _val)
+            {
+                val = _val;
+                neighbors = new List<Node>();
+            }
+
+            public Node(int _val, List<Node> _neighbors)
+            {
+                val = _val;
+                neighbors = _neighbors;
+            }
+        }
+    }
+}
