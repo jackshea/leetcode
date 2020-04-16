662a14c139e3ef4e335d6f382804e0a0fe332475
谢在奎 <jackshea@163.com>
2020-04-16 23:07:20 +0800

    P0134 加油站

---
+++ LeetCode/Problems/Medium/P0134_GasStation.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 加油站
+    /// https://leetcode-cn.com/problems/gas-station/
+    public class P0134_GasStation
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            int allDiff = 0;
+            int remain = 0;
+            int start = 0;
+            for (int i = 0; i < gas.Length; i++)
+            {
+                int diff = gas[i] - cost[i];
+                allDiff += diff;
+                remain += diff;
+                if (remain < 0)
+                {
+                    remain = 0;
+                    start = i + 1;
+                }
+            }
+
+            return allDiff < 0 ? -1 : start;
+        }
+    }
+}
