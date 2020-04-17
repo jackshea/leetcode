5aa780eac588400dd9bed00c37964b949f38e6a0
谢在奎 <jackshea@163.com>
2020-04-17 15:28:00 +0800

    P0297 二叉树的序列化与反序列化

---
+++ LeetCode/Problems/Hard/P0297_SerializeAndDeserializeBinaryTree.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Hard
+{
+    /// 二叉树的序列化与反序列化
+    /// https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/
+    public class Codec
+    {
+        // Encodes a tree to a single string.
+        public string serialize(TreeNode root)
+        {
+            if (root == null)
+            {
+                return "#,";
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append(root.val + ",");
+            sb.Append(serialize(root.left));
+            sb.Append(serialize(root.right));
+            return sb.ToString();
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(string data)
+        {
+            var strings = data.Split(new char[] { ',' });
+            int index = 0;
+            return deserialize(strings, ref index);
+        }
+
+        private TreeNode deserialize(string[] ss, ref int index)
+        {
+            if (ss[index] == "#")
+            {
+                return null;
+            }
+
+            var root = new TreeNode(int.Parse(ss[index]));
+            index++;
+            root.left = deserialize(ss, ref index);
+            index++;
+            root.right = deserialize(ss, ref index);
+            return root;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Hard/CodecTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Hard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Hard.Tests
+{
+    [TestFixture()]
+    public class CodecTests
+    {
+        [Test()]
+        public void serializeTest()
+        {
+            int?[] a = new int?[] { 1, 2, 3, null, null, 4, 5 };
+            var bt = ConvertArrayToBinaryTree.Build(a);
+            var test = new Codec();
+            Assert.AreEqual("1,2,#,#,3,4,#,#,5,#,#,", test.serialize(bt));
+        }
+    }
+}
