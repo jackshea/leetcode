854ae72cd6e8b20530fa0dfac74bec6e0305e5e9
谢在奎 <jackshea@163.com>
2020-04-18 13:34:28 +0800

    P0147 对链表进行插入排序

---
+++ LeetCode/Problems/Medium/P0147_InsertionSortList.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 对链表进行插入排序
+    /// https://leetcode-cn.com/problems/insertion-sort-list/
+    public class P0147_InsertionSortList
+    {
+        public ListNode InsertionSortList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode fakeHead = new ListNode(int.MinValue);
+            fakeHead.next = head;
+            ListNode sorted = head;
+            ListNode unsorted = head.next;
+            while (unsorted != null)
+            {
+                if (unsorted.val >= sorted.val)
+                {
+                    sorted = unsorted;
+                    unsorted = unsorted.next;
+                    continue;
+                }
+
+                head = fakeHead;
+                while (head.next.val < unsorted.val)
+                {
+                    head = head.next;
+                }
+
+                sorted.next = unsorted.next;
+                unsorted.next = head.next;
+                head.next = unsorted;
+                unsorted = sorted.next;
+            }
+
+            return fakeHead.next;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0147_InsertionSortListTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0147_InsertionSortListTests
+    {
+        [Test()]
+        public void InsertionSortListTest()
+        {
+            var list = Utils.BuildList(new[] { 4, 2, 1, 3 });
+            var test = new P0147_InsertionSortList();
+            var actual = test.InsertionSortList(list);
+            Assert.AreEqual(new[] { 1, 2, 3, 4 }, Utils.ListToArray(actual));
+        }
+    }
+}
