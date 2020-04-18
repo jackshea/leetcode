f0a687ceeba7c967aa463f780d7134fe6673ef2a
谢在奎 <jackshea@163.com>
2020-04-18 14:43:02 +0800

    P0150 逆波兰表达式求值

---
+++ LeetCode/Problems/Medium/P0150_EvaluateReversePolishNotation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 逆波兰表达式求值
+    /// https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/
+    public class P0150_EvaluateReversePolishNotation
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (var token in tokens)
+            {
+                switch (token)
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                        int num1 = stack.Pop();
+                        int num2 = stack.Pop();
+                        stack.Push(Operate(num2, num1, token));
+                        break;
+                    default:
+                        int num = int.Parse(token);
+                        stack.Push(num);
+                        break;
+                }
+            }
+
+            return stack.Pop();
+        }
+
+        public int Operate(int a, int b, string operate)
+        {
+            int ans = 0;
+            switch (operate)
+            {
+                case "+":
+                    ans = a + b;
+                    break;
+                case "-":
+                    ans = a - b;
+                    break;
+                case "*":
+                    ans = a * b;
+                    break;
+                case "/":
+                    ans = a / b;
+                    break;
+            }
+
+            return ans;
+        }
+    }
+}
