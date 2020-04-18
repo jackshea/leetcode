33c0b59f321c42f09268385794eab6c8a7a7d5b5
谢在奎 <jackshea@163.com>
2020-04-18 11:33:35 +0800

    P0144 二叉树的前序遍历

---
+++ LeetCode/Problems/Medium/P0144_BinaryTreePreorderTraversal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 二叉树的前序遍历
+    /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+    public class P0144_BinaryTreePreorderTraversal
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> ans = new List<int>();
+            if (root == null)
+            {
+                return ans;
+            }
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count != 0)
+            {
+                var top = stack.Pop();
+                ans.Add(top.val);
+                if (top.right != null)
+                {
+                    stack.Push(top.right);
+                }
+                if (top.left != null)
+                {
+                    stack.Push(top.left);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
