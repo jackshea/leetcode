ebf405cf751e1d44f8f5237da9bdcd278ad9df2e
谢在奎 <jackshea@163.com>
2020-04-19 19:10:07 +0800

    P0224 基本计算器

---
+++ LeetCode/Problems/Medium/P0224_BasicCalculator.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 基本计算器
+    /// https://leetcode-cn.com/problems/basic-calculator/
+    public class P0224_BasicCalculator
+    {
+        private int start = 0;
+        public int Calculate(string s)
+        {
+            int ans = 0;
+            int num = 0;
+            int sign = 1;
+            while (start < s.Length)
+            {
+                var c = s[start++];
+                if (c == ' ')
+                {
+                }
+                else if (char.IsDigit(c))
+                {
+                    num = num * 10 + (c - '0');
+                }
+                else if (c == '+')
+                {
+                    ans += num * sign;
+                    sign = 1;
+                    num = 0;
+                }
+                else if (c == '-')
+                {
+                    ans += num * sign;
+                    sign = -1;
+                    num = 0;
+                }
+                else if (c == '(')
+                {
+                    ans += Calculate(s) * sign;
+                    num = 0;
+                }
+                else if (c == ')')
+                {
+                    break;
+                }
+            }
+            ans += num * sign;
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0224_BasicCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0224_BasicCalculatorTests
+    {
+        [Test()]
+        public void CalculateTest()
+        {
+            var test = new P0224_BasicCalculator();
+            var actual = test.Calculate("(1+(4+5+2)-3)+(6+8)");
+            Assert.AreEqual(23, actual);
+        }
+    }
+}
