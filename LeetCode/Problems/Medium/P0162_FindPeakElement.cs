491ef51b598f0950595f8729d5deb1fd91d8d761
谢在奎 <jackshea@163.com>
2020-04-21 16:39:26 +0800

    P0162 寻找峰值

---
+++ LeetCode/Problems/Medium/P0162_FindPeakElement.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 寻找峰值
+    /// https://leetcode-cn.com/problems/find-peak-element/
+    public class P0162_FindPeakElement
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < nums[mid + 1])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+    }
+}
