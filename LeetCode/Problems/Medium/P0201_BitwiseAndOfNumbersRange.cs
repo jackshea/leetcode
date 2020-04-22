09826c3c1ca946d0e8fc5d232de8c6155afba02a
谢在奎 <jackshea@163.com>
2020-04-22 13:40:56 +0800

    P0201 数字范围按位与

---
+++ LeetCode/Problems/Medium/P0201_BitwiseAndOfNumbersRange.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 数字范围按位与
+    /// https://leetcode-cn.com/problems/bitwise-and-of-numbers-range/
+    public class P0201_BitwiseAndOfNumbersRange
+    {
+        public int RangeBitwiseAnd(int m, int n)
+        {
+            int i = 0;
+            while (m != n)
+            {
+                m >>= 1;
+                n >>= 1;
+                i++;
+            }
+
+            return m << i;
+        }
+    }
+}
