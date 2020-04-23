93a9c94379bc4b81a363290ea45c84d099d96ae2
谢在奎 <jackshea@163.com>
2020-04-23 12:24:56 +0800

    MST0811 面试题 08.11. 硬币

---
+++ LeetCode/Problems/Medium/MST0811.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 面试题 08.11. 硬币
+    /// https://leetcode-cn.com/problems/coin-lcci/
+    public class MST0811
+    {
+        public int WaysToChange(int n)
+        {
+            int[] dp = new int[n + 1];
+            int[] coins = { 25, 10, 5, 1 };
+            dp[0] = 1;
+            for (int i = 0; i < coins.Length; i++)
+            {
+                for (int j = coins[i]; j <= n; j++)
+                {
+                    dp[j] += dp[j - coins[i]];
+                    dp[j] %= 1000000007;
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
