450c138062bf7ec52b78a972ab0b10e483e88054
谢在奎 <jackshea@163.com>
2020-04-23 20:11:12 +0800

    P0216 组合总和 III

---
+++ LeetCode/Problems/Medium/P0216_CombinationSumIII.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 组合总和 III
+    /// https://leetcode-cn.com/problems/combination-sum-iii/
+    public class P0216_CombinationSumIII
+    {
+        private IList<IList<int>> ans = new List<IList<int>>();
+        public IList<IList<int>> CombinationSum3(int k, int n)
+        {
+            Backtrack(new List<int>(), 1, 0, k, n);
+            return ans;
+        }
+
+        public void Backtrack(IList<int> prefix, int start, int sum, int k, int n)
+        {
+            if (prefix.Count == k && sum == n)
+            {
+                ans.Add(new List<int>(prefix));
+                return;
+            }
+
+            for (int i = start; i < 10; i++)
+            {
+                prefix.Add(i);
+                Backtrack(prefix, i + 1, sum + i, k, n);
+                prefix.RemoveAt(prefix.Count - 1);
+            }
+        }
+    }
+}
