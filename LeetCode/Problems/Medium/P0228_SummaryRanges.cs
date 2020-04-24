fb9db7153628485ebacfa260a66f4436b0fbe232
谢在奎 <jackshea@163.com>
2020-04-24 16:11:53 +0800

    P0288 汇总区间

---
+++ LeetCode/Problems/Medium/P0228_SummaryRanges.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 汇总区间
+    /// https://leetcode-cn.com/problems/summary-ranges/
+    public class P0228_SummaryRanges
+    {
+        public IList<string> SummaryRanges(int[] nums)
+        {
+            IList<string> ans = new List<string>();
+            if (nums.Length == 0)
+            {
+                return ans;
+            }
+
+            int start = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == nums.Length - 1 || nums[i] != nums[i + 1] - 1)
+                {
+                    if (start == i)
+                    {
+                        ans.Add(nums[start].ToString());
+                    }
+                    else
+                    {
+                        ans.Add($"{nums[start]}->{nums[i]}");
+                    }
+
+                    start = i + 1;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
