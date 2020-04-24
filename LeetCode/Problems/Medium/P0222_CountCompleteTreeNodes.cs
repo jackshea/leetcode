b55184532eb5b96dae22971b1505d9971958d28f
谢在奎 <jackshea@163.com>
2020-04-24 15:10:51 +0800

    P0222 完全二叉树的节点个数

---
+++ LeetCode/Problems/Medium/P0222_CountCompleteTreeNodes.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 完全二叉树的节点个数
+    /// https://leetcode-cn.com/problems/count-complete-tree-nodes/
+    public class P0222_CountCompleteTreeNodes
+    {
+        public int CountNodes(TreeNode root)
+        {
+            if (IsPerfectTree(root))
+            {
+                int count = 1;
+                TreeNode right = root;
+                while (right != null)
+                {
+                    count *= 2;
+                    right = right.right;
+                }
+
+                return count - 1;
+            }
+
+            return CountNodes(root.left) + CountNodes(root.right) + 1;
+        }
+
+        public bool IsPerfectTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            TreeNode left = root;
+            TreeNode right = root;
+            if (left != null && right != null)
+            {
+                left = left.left;
+                right = right.right;
+            }
+
+            return left == null && right == null;
+        }
+    }
+}
