786db168de7c8a1b7f389f2685fcaa0a85aed1e7
谢在奎 <jackshea@163.com>
2020-04-27 00:00:11 +0800

    P0260 只出现一次的数字 III

---
+++ LeetCode/Problems/Medium/P0260_SingleNumberIII.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    public class P0260_SingleNumberIII
+    {
+        public int[] SingleNumber(int[] nums)
+        {
+            int bitmask = 0;
+            foreach (var num in nums)
+            {
+                bitmask ^= num;
+            }
+
+            int diff = bitmask & (-bitmask);
+            int x = 0;
+            foreach (var num in nums)
+            {
+                if ((num & diff) != 0)
+                {
+                    x ^= num;
+                }
+            }
+
+            return new[] { x, x ^ bitmask };
+        }
+    }
+}
