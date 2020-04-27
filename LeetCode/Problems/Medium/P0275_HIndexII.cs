015120e851938987b92b63468ccee999e3e44f1d
谢在奎 <jackshea@163.com>
2020-04-27 17:44:17 +0800

    P0275 H指数 II

---
+++ LeetCode/Problems/Medium/P0275_HIndexII.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// H指数 II
+    /// https://leetcode-cn.com/problems/h-index-ii/
+    public class P0275_HIndexII
+    {
+        public int HIndex(int[] citations)
+        {
+            int left = 0, right = citations.Length - 1;
+            int len = citations.Length;
+            int ans = 0;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                int count = len - mid;
+                if (citations[mid] < count)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                    ans = count;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
