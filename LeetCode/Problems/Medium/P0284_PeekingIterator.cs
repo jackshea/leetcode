2252ddbdeaabfe676654e9370a12047bfe60d2cb
谢在奎 <jackshea@163.com>
2020-04-27 17:55:38 +0800

    P0284 顶端迭代器

---
+++ LeetCode/Problems/Medium/P0284_PeekingIterator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 顶端迭代器
+    /// https://leetcode-cn.com/problems/peeking-iterator/
+    class PeekingIterator
+    {
+        private IEnumerator<int> iterator;
+        private bool hasNext;
+
+        // iterators refers to the first element of the array.
+        public PeekingIterator(IEnumerator<int> iterator)
+        {
+            this.iterator = iterator;
+            // initialize any member here.
+        }
+
+        // Returns the next element in the iteration without advancing the iterator.
+        public int Peek()
+        {
+            return iterator.Current;
+        }
+
+        // Returns the next element in the iteration and advances the iterator.
+        public int Next()
+        {
+            int ans = iterator.Current;
+            hasNext = iterator.MoveNext();
+            return ans;
+        }
+
+        // Returns false if the iterator is refering to the end of the array of true otherwise.
+        public bool HasNext()
+        {
+            return hasNext;
+        }
+    }
+}
