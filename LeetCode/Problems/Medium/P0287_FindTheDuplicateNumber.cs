904e898a1bacdf493ddfd2d74d7cc01953e81016
谢在奎 <jackshea@163.com>
2020-04-28 14:54:27 +0800

    P0287 寻找重复数

---
+++ LeetCode/Problems/Medium/P0287_FindTheDuplicateNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 寻找重复数
+    /// https://leetcode-cn.com/problems/find-the-duplicate-number/
+    public class P0287_FindTheDuplicateNumber
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            int fast = 0;
+            int slow = 0;
+            while (true)
+            {
+                fast = nums[nums[fast]];
+                slow = nums[slow];
+                if (slow == fast)
+                {
+                    fast = 0;
+                    while (nums[slow] != nums[fast])
+                    {
+                        fast = nums[fast];
+                        slow = nums[slow];
+                    }
+
+                    return nums[slow];
+                }
+            }
+        }
+    }
+}
