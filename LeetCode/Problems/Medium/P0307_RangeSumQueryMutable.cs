2937524e07a254025412588a2e7c0b86370a404f
谢在奎 <jackshea@163.com>
2020-04-28 19:05:13 +0800

    P0307 区域和检索 - 数组可修改

---
+++ LeetCode/Problems/Medium/P0307_RangeSumQueryMutable.cs
@@ -0,0 +1,88 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    public class NumArray
+    {
+        private SegTreeNode[] tree;
+        private int[] nums;
+
+        public NumArray(int[] nums)
+        {
+            this.nums = nums;
+            if (nums == null || nums.Length == 0)
+            {
+                return;
+            }
+            tree = new SegTreeNode[4 * this.nums.Length + 1];
+            BuildTree(1, 1, nums.Length);
+        }
+
+        public void Update(int i, int val)
+        {
+            Update(1, i + 1, val);
+        }
+
+        public int SumRange(int i, int j)
+        {
+            return Search(1, i + 1, j + 1);
+        }
+
+        private int BuildTree(int k, int left, int right)
+        {
+            tree[k] = new SegTreeNode { Left = left, Right = right };
+            if (left == right)
+            {
+                tree[k].Value = nums[left - 1];
+                return tree[k].Value;
+            }
+
+            int mid = (left + right) >> 1;
+
+
+            tree[k].Value = BuildTree(k * 2, left, mid) + BuildTree(k * 2 + 1, mid + 1, right);
+            return tree[k].Value;
+        }
+
+        private int Search(int k, int left, int right)
+        {
+            var node = tree[k];
+            if (node == null || node.Left > right || node.Right < left)
+            {
+                return 0;
+            }
+
+            if (node.Left >= left && node.Right <= right)
+            {
+                return node.Value;
+            }
+
+            return Search(k * 2, left, right) + Search(k * 2 + 1, left, right);
+        }
+
+        public void Update(int k, int i, int val)
+        {
+            if (tree[k].Left == tree[k].Right)
+            {
+                tree[k].Value = val;
+                return;
+            }
+
+            if (i <= tree[k * 2].Right)
+            {
+                Update(k * 2, i, val);
+            }
+            else
+            {
+                Update(k * 2 + 1, i, val);
+            }
+
+            tree[k].Value = tree[k * 2].Value + tree[k * 2 + 1].Value;
+        }
+    }
+
+    public class SegTreeNode
+    {
+        public int Left;
+        public int Right;
+        public int Value;
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/NumArrayTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class NumArrayTests
+    {
+        [Test()]
+        public void NumArrayTest()
+        {
+            var test = new NumArray(new[] { 1, 3, 5 });
+            Assert.AreEqual(9, test.SumRange(0, 2));
+            test.Update(1, 2);
+            Assert.AreEqual(8, test.SumRange(0, 2));
+        }
+
+        [Test()]
+        public void NumArrayTest_1()
+        {
+            var test = new NumArray(new[] { -1 });
+            Assert.AreEqual(-1, test.SumRange(0, 0));
+            test.Update(0, 1);
+            Assert.AreEqual(1, test.SumRange(0, 0));
+        }
+
+        [Test()]
+        public void NumArrayTest_2()
+        {
+            var test = new NumArray(new[] { 9, -8 });
+            test.Update(0, 3);
+            Assert.AreEqual(-8, test.SumRange(1, 1));
+            Assert.AreEqual(-5, test.SumRange(0, 1));
+            test.Update(1, -3);
+            Assert.AreEqual(0, test.SumRange(0, 1));
+        }
+
+        [Test()]
+        public void NumArrayTest_3()
+        {
+            var test = new NumArray(new[] { -28, -39, 53, 65, 11, -56, -65, -39, -43, 97 });
+            Assert.AreEqual(-56 - 65, test.SumRange(5, 6));
+            test.Update(9, 27);
+            Assert.AreEqual(118, test.SumRange(2, 3));
+            Assert.AreEqual(-104, test.SumRange(6, 7));
+            test.Update(1, -82);
+            test.Update(3, -72);
+            Assert.AreEqual(-221, test.SumRange(3, 7));
+            Assert.AreEqual(-293, test.SumRange(1, 8));
+            test.Update(5, 13);
+            test.Update(4, -67);
+        }
+    }
+}
