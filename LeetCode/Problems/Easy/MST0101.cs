1f9a4182d91f6365f0b421758f992b102ececeea
谢在奎 <jackshea@163.com>
2020-04-29 13:07:58 +0800

    MST0101 判定字符是否唯一

---
+++ LeetCode/Problems/Easy/MST0101.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 面试题 01.01. 判定字符是否唯一
+    /// https://leetcode-cn.com/problems/is-unique-lcci/
+    public class MST0101
+    {
+        public bool IsUnique(string astr)
+        {
+            bool[] contain = new bool[60];
+            foreach (var c in astr)
+            {
+                if (contain[c - 'A'])
+                {
+                    return false;
+                }
+
+                contain[c - 'A'] = true;
+            }
+
+            return true;
+        }
+    }
+}
