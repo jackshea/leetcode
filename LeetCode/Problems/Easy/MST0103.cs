7044d44c642c6b699cc6de285b692fed022145f4
谢在奎 <jackshea@163.com>
2020-04-29 13:23:29 +0800

    MST0103 URL化

---
+++ LeetCode/Problems/Easy/MST0103.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 面试题 01.03. URL化
+    /// https://leetcode-cn.com/problems/string-to-url-lcci/
+    public class MST0103
+    {
+        public string ReplaceSpaces(string S, int length)
+        {
+            char[] ans = new char[S.Length];
+            int i = 0;
+            for (var index = 0; index < length; index++)
+            {
+                char c = S[index];
+                if (c == ' ')
+                {
+                    ans[i++] = '%';
+                    ans[i++] = '2';
+                    ans[i++] = '0';
+                }
+                else
+                {
+                    ans[i++] = c;
+                }
+            }
+
+            return new string(ans, 0, i);
+        }
+    }
+}
