ce1295e450e48206d3bc9ddbfd84124658178a45
谢在奎 <jackshea@163.com>
2020-04-29 13:14:38 +0800

    MST0102 判定是否互为字符重排

---
+++ LeetCode/Problems/Easy/MST0102.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 面试题 01.02. 判定是否互为字符重排
+    /// https://leetcode-cn.com/problems/check-permutation-lcci/
+    public class MST0102
+    {
+        public bool CheckPermutation(string s1, string s2)
+        {
+            var chars1 = CountChars(s1);
+            var chars2 = CountChars(s2);
+            for (int i = 0; i < chars1.Length; i++)
+            {
+                if (chars1[i] != chars2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int[] CountChars(string s)
+        {
+            int[] chars = new int[60];
+            foreach (var c in s)
+            {
+                chars[c - 'A']++;
+            }
+
+            return chars;
+        }
+    }
+}
