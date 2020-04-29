86203aae50d9d986a01f4c9573152394e7f23882
谢在奎 <jackshea@163.com>
2020-04-29 14:23:37 +0800

    MST0105 一次编辑

---
+++ LeetCode/Problems/Medium/MST0105.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 一次编辑
+    /// https://leetcode-cn.com/problems/one-away-lcci/
+    public class MST0105
+    {
+        public bool OneEditAway(string first, string second)
+        {
+            if (Math.Abs(first.Length - second.Length) > 1)
+            {
+                return false;
+            }
+
+            int i = 0, j = 0;
+            int editLength = 0;
+            while (i < first.Length && j < second.Length)
+            {
+                if (first[i] == second[j])
+                {
+                    i++;
+                    j++;
+                    continue;
+                }
+
+                if (++editLength > 1)
+                {
+                    return false;
+                }
+
+                if (first.Length == second.Length)
+                {
+                    if (i == first.Length - 1 && j == second.Length - 1)
+                    {
+                        return true;
+                    }
+
+                    if (first[i + 1] == second[j + 1])
+                    {
+                        i++;
+                        j++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if (first.Length > second.Length)
+                {
+                    if (first[i + 1] == second[j])
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (first[i] == second[j + 1])
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
