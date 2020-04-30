9a86a68a05e4baf5e68f372119ffbe70c26e3b10
谢在奎 <jackshea@163.com>
2020-04-30 20:55:38 +0800

    MST0109 字符串轮转

---
+++ LeetCode/Problems/Easy/MST0109.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 字符串轮转
+    /// https://leetcode-cn.com/problems/string-rotation-lcci/
+    public class MST0109
+    {
+        public bool IsFlipedString(string s1, string s2)
+        {
+            return s1.Length == s2.Length && (s1 + s1).Contains(s2);
+        }
+    }
+}
