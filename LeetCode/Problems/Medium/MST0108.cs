e6ee611b55fa2b0ba59b781c7f6c86bc67408d4f
谢在奎 <jackshea@163.com>
2020-04-30 20:50:12 +0800

    MST0108 零矩阵

---
+++ LeetCode/Problems/Medium/MST0108.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 零矩阵
+    /// https://leetcode-cn.com/problems/zero-matrix-lcci/
+    public class MST0108
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+            if (matrix == null || matrix.Length == 0)
+            {
+                return;
+            }
+
+            HashSet<int> zeroR = new HashSet<int>();
+            HashSet<int> zeroC = new HashSet<int>();
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        zeroR.Add(i);
+                        zeroC.Add(j);
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] != 0 && (zeroR.Contains(i) || zeroC.Contains(j)))
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
