01d7f2b2e1d65bccab1d5218c7f2a0429146ecd3
谢在奎 <jackshea@163.com>
2020-05-01 14:19:26 +0800

    MST0202 返回倒数第 k 个节点

---
+++ LeetCode/Problems/Easy/MST0202.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 返回倒数第 k 个节点
+    /// https://leetcode-cn.com/problems/kth-node-from-end-of-list-lcci/
+    public class MST0202
+    {
+        public int KthToLast(ListNode head, int k)
+        {
+            int len = 0;
+            ListNode cur = head;
+            while (cur != null)
+            {
+                len++;
+                cur = cur.next;
+            }
+
+            cur = head;
+            for (int i = 1; i <= len - k; i++)
+            {
+                cur = cur.next;
+            }
+
+            return cur.val;
+        }
+    }
+}
