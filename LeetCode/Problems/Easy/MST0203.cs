8aba6dba83ab1a79ebc8025ed1abbf2b708d4ea5
谢在奎 <jackshea@163.com>
2020-05-01 14:27:48 +0800

    MST0203 删除中间节点

---
+++ LeetCode/Problems/Easy/MST0203.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 删除中间节点
+    /// https://leetcode-cn.com/problems/delete-middle-node-lcci/
+    public class MST0203
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
