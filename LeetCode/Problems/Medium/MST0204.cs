4fe9e5ccc6bb45360f3fef63e39900c89096ef64
谢在奎 <jackshea@163.com>
2020-05-01 14:49:45 +0800

    MST0204 分割链表

---
+++ LeetCode/Problems/Medium/MST0204.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 分割链表
+    /// https://leetcode-cn.com/problems/partition-list-lcci/
+    public class MST0204
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            ListNode greaterHead = new ListNode(0);
+            ListNode lessHead = new ListNode(0);
+            ListNode greaterNode = greaterHead;
+            ListNode lessNode = lessHead;
+            ListNode cur = head;
+            while (cur != null)
+            {
+                if (cur.val < x)
+                {
+                    lessNode.next = cur;
+                    lessNode = lessNode.next;
+                }
+                else
+                {
+                    greaterNode.next = cur;
+                    greaterNode = greaterNode.next;
+                }
+
+                cur = cur.next;
+            }
+
+            greaterNode.next = null;
+            lessNode.next = greaterHead.next;
+            return lessHead.next;
+        }
+    }
+}
