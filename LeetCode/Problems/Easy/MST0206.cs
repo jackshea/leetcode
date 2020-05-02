9cf9be2a471a4fae388c7e1447054d9b65e453b4
谢在奎 <jackshea@163.com>
2020-05-02 14:19:20 +0800

    MST0206 回文链表

---
+++ LeetCode/Problems/Easy/MST0206.cs
@@ -0,0 +1,54 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 回文链表
+    /// https://leetcode-cn.com/problems/palindrome-linked-list-lcci/
+    public class MST0206
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head?.next == null)
+            {
+                return true;
+            }
+
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast.next?.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            slow = ReverseList(slow.next);
+            while (slow != null)
+            {
+                if (head.val != slow.val)
+                {
+                    return false;
+                }
+
+                head = head.next;
+                slow = slow.next;
+            }
+
+            return true;
+        }
+
+        private ListNode ReverseList(ListNode head)
+        {
+            ListNode cur = head;
+            ListNode pre = null;
+            while (cur != null)
+            {
+                ListNode next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+
+            return pre;
+        }
+    }
+}
