0bb1598b550a588b3fb85785e4dd7ce73223a783
谢在奎 <jackshea@163.com>
2020-05-02 14:06:34 +0800

    MST0205 链表求和

---
+++ LeetCode/Problems/Medium/MST0205.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 链表求和
+    /// https://leetcode-cn.com/problems/sum-lists-lcci/
+    public class MST0205
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return AddTwoNumbers(l1, l2, 0);
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry)
+        {
+            if (l1 == null && l2 == null && carry == 0)
+            {
+                return null;
+            }
+
+            int v = carry;
+            if (l1 != null)
+            {
+                v += l1.val;
+            }
+
+            if (l2 != null)
+            {
+                v += l2.val;
+            }
+
+            carry = v / 10;
+            v %= 10;
+            ListNode ans = new ListNode(v);
+            ans.next = AddTwoNumbers(l1?.next, l2?.next, carry);
+            return ans;
+        }
+    }
+}
