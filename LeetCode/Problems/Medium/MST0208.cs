1e0cd6e23cbc8410045d1c8fe5a7e2b134ac3778
谢在奎 <jackshea@163.com>
2020-05-03 16:36:17 +0800

    MST0208 环路检测

---
+++ LeetCode/Problems/Medium/MST0208.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 环路检测
+    /// https://leetcode-cn.com/problems/linked-list-cycle-lcci/
+    public class MST0208
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast?.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                {
+                    fast = head;
+                    while (fast != slow)
+                    {
+                        fast = fast.next;
+                        slow = slow.next;
+                    }
+
+                    return fast;
+                }
+            }
+
+            return null;
+        }
+    }
+}
