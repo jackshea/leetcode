1089348dc490da3e130d6072a84d80514debc8ba
谢在奎 <jackshea@163.com>
2020-05-04 18:54:19 +0800

    MST0304 化栈为队

---
+++ LeetCode/Problems/Easy/MST0304.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 化栈为队
+    /// https://leetcode-cn.com/problems/implement-queue-using-stacks-lcci/
+    public class MyQueue
+    {
+        private Stack<int> outStack;
+        private Stack<int> inStack;
+
+        /** Initialize your data structure here. */
+        public MyQueue()
+        {
+            outStack = new Stack<int>();
+            inStack = new Stack<int>();
+        }
+
+        /** Push element x to the back of queue. */
+        public void Push(int x)
+        {
+            inStack.Push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int Pop()
+        {
+            if (outStack.Count == 0)
+            {
+                while (inStack.Count != 0)
+                {
+                    outStack.Push(inStack.Pop());
+                }
+            }
+
+            return outStack.Pop();
+        }
+
+        /** Get the front element. */
+        public int Peek()
+        {
+            if (outStack.Count == 0)
+            {
+                while (inStack.Count != 0)
+                {
+                    outStack.Push(inStack.Pop());
+                }
+            }
+
+            return outStack.Peek();
+        }
+
+        /** Returns whether the queue is empty. */
+        public bool Empty()
+        {
+            return inStack.Count == 0 && outStack.Count == 0;
+        }
+    }
+}
