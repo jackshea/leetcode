f157bbc95ffa9095b0797cc760b9725f29e981b6
谢在奎 <jackshea@163.com>
2020-05-04 19:06:14 +0800

    MST0305 栈排序

---
+++ LeetCode/Problems/Medium/MST0305.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 栈排序
+    /// https://leetcode-cn.com/problems/sort-of-stacks-lcci/
+    public class SortedStack
+    {
+        private Stack<int> main = new Stack<int>();
+        private Stack<int> minor = new Stack<int>();
+
+        public SortedStack()
+        {
+
+        }
+
+        public void Push(int val)
+        {
+            while (!IsEmpty() && main.Peek() < val)
+            {
+                minor.Push(main.Pop());
+            }
+            main.Push(val);
+            while (minor.Count > 0)
+            {
+                main.Push(minor.Pop());
+            }
+        }
+
+        public void Pop()
+        {
+            if (!IsEmpty())
+            {
+                main.Pop();
+            }
+        }
+
+        public int Peek()
+        {
+            if (!IsEmpty())
+            {
+                return main.Peek();
+            }
+
+            return -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return main.Count == 0;
+        }
+    }
+}
