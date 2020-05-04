9bc5181aa3431eadf1ee7c4e47c5650f530d6d6a
谢在奎 <jackshea@163.com>
2020-05-04 18:47:27 +0800

    P0045 跳跃游戏 II

---
+++ LeetCode/Problems/Hard/P0045_JumpGameII.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeetCode.Problems.Hard
+{
+    /// 跳跃游戏 II
+    /// https://leetcode-cn.com/problems/jump-game-ii/
+    public class P0045_JumpGameII
+    {
+        public int Jump(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1)
+            {
+                return 0;
+            }
+
+            int steps = 0;
+            int end = 0;
+            int far = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i <= end)
+                {
+                    far = Math.Max(far, i + nums[i]);
+                }
+
+                if (i == end)
+                {
+                    end = far;
+                    steps++;
+                }
+
+                if (end >= nums.Length - 1)
+                {
+                    break;
+                }
+            }
+
+            return steps;
+        }
+    }
+}
