7e812dee2f3380ef8fe177d7b8b8943c46bce196
谢在奎 <jackshea@163.com>
2020-05-05 13:17:13 +0800

    MST0402 最小高度树

---
+++ LeetCode/Problems/Easy/MST0402.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 最小高度树
+    /// https://leetcode-cn.com/problems/minimum-height-tree-lcci/
+    public class MST0402
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return Build(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode Build(int[] nums, int left, int right)
+        {
+            if (left > right)
+            {
+                return null;
+            }
+
+            if (left == right)
+            {
+                return new TreeNode(nums[left]);
+            }
+
+            int mid = left + (right - left) / 2;
+            var root = new TreeNode(nums[mid]);
+            root.left = Build(nums, left, mid - 1);
+            root.right = Build(nums, mid + 1, right);
+            return root;
+        }
+    }
+}
