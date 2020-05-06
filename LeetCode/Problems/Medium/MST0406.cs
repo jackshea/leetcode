e1b8e32eb97973ea765a71bad1c1934b5e6fcf30
谢在奎 <jackshea@163.com>
2020-05-06 14:02:41 +0800

    MST0406 后继者

---
+++ LeetCode/Problems/Medium/MST0406.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 后继者
+    /// https://leetcode-cn.com/problems/successor-lcci/
+    public class MST0406
+    {
+        public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            var left = InorderSuccessor(root.left, p);
+            if (left != null)
+            {
+                return left;
+            }
+            if (root.val > p.val)
+            {
+                return root;
+            }
+
+            return InorderSuccessor(root.right, p);
+        }
+    }
+}
