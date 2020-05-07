9f300328a8057aa7f546ed4e67cbcf5ad31bdeec
谢在奎 <jackshea@163.com>
2020-05-07 16:29:16 +0800

    MST0408 首个共同祖先

---
+++ LeetCode/Problems/Medium/MST0408.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 首个共同祖先
+    /// https://leetcode-cn.com/problems/first-common-ancestor-lcci/
+    public class MST0408
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || root == p || root == q)
+            {
+                return root;
+            }
+
+            TreeNode left = LowestCommonAncestor(root.left, p, q);
+            TreeNode right = LowestCommonAncestor(root.right, p, q);
+            if (left != null && right != null)
+            {
+                return root;
+            }
+
+            return left ?? right;
+        }
+    }
+}
