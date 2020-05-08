4afb0d46d3b5e8fddf9933f0c92342b73f858518
谢在奎 <jackshea@163.com>
2020-05-08 13:18:44 +0800

    MST0501 插入

---
+++ LeetCode/Problems/Medium/MST0501.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 插入
+    /// https://leetcode-cn.com/problems/insert-into-bits-lcci/
+    public class MST0501
+    {
+        public int InsertBits(int N, int M, int i, int j)
+        {
+            var mask = ((1 << (j - i + 1)) - 1) << i;
+            return (N & ~mask) | (M << i);
+        }
+    }
+}
