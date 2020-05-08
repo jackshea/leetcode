b2692bc883f7fdb2c3e0e20bc72f227ec51f0dbb
谢在奎 <jackshea@163.com>
2020-05-08 13:48:13 +0800

    MST0502 二进制数转字符串

--- LeetCode/Problems/Medium/MST0502.cs
+++ LeetCode/Problems/Medium/MST0502.cs
@@ -1,7 +1,35 @@
-﻿namespace LeetCode.Problems.Medium
+﻿using System.Text;
+
+namespace LeetCode.Problems.Medium
 {
+    /// 二进制数转字符串
+    /// https://leetcode-cn.com/problems/bianry-number-to-string-lcci/
     public class MST0502
     {
-        
+        public string PrintBin(double num)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("0.");
+            for (int i = 0; i < 30; i++)
+            {
+                num *= 2;
+                if (num >= 1)
+                {
+                    sb.Append('1');
+                    num -= 1;
+                }
+                else
+                {
+                    sb.Append('0');
+                }
+
+                if (num == 0)
+                {
+                    return sb.ToString();
+                }
+            }
+
+            return "ERROR";
+        }
     }
 }
