c5197b9fc38d128170475d1d2af1edb23e2d3ebd
谢在奎 <jackshea@163.com>
2020-05-09 15:49:16 +0800

    MST0506 整数转换

---
+++ LeetCode/Problems/Easy/MST0506.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    public class MST0506
+    {
+        /// 整数转换
+        /// https://leetcode-cn.com/problems/convert-integer-lcci/
+        public int ConvertInteger(int A, int B)
+        {
+            int x = A ^ B;
+            int count = 0;
+            while (x != 0)
+            {
+                x &= x - 1;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
