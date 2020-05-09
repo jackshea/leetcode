b06496553014a20c3bd131a172065497c2acc510
谢在奎 <jackshea@163.com>
2020-05-09 15:43:17 +0800

    MST0504 下一个数

---
+++ LeetCode/Problems/Medium/MST0504.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 下一个数
+    /// https://leetcode-cn.com/problems/closed-number-lcci/
+    public class MST0504
+    {
+        public int[] FindClosedNumbers(int num)
+        {
+            int greater = num + 1, less = num - 1;
+            var bitCount = BitCount(num);
+            while (greater <= int.MaxValue)
+            {
+                if (BitCount(greater++) == bitCount)
+                {
+                    break;
+                }
+            }
+
+            if (greater < 0)
+            {
+                greater = -1;
+            }
+            else
+            {
+                greater--;
+            }
+
+            while (less >= 0)
+            {
+                if (BitCount(less--) == bitCount)
+                {
+                    break;
+                }
+            }
+
+            if (less < 0)
+            {
+                less = -1;
+            }
+            else
+            {
+                less++;
+            }
+
+            return new int[] { greater, less };
+        }
+
+        private int BitCount(int n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                n &= (n - 1);
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
