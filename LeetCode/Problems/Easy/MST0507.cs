d624233fdc1343fc036ab9e7e822e54248691ff6
谢在奎 <jackshea@163.com>
2020-05-09 16:40:28 +0800

    MST0507 配对交换

---
+++ LeetCode/Problems/Easy/MST0507.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 配对交换
+    /// https://leetcode-cn.com/problems/exchange-lcci/
+    public class MST0507
+    {
+        public int ExchangeBits(int num)
+        {
+            return (int) (((num & 0x55555555) << 1) | ((num & 0xaaaaaaaa) >> 1));
+        }
+    }
+}
