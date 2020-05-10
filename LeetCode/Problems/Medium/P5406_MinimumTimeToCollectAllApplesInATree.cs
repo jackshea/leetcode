b454cd25abe34ed8839df1eacb71880c7857ca68
谢在奎 <jackshea@163.com>
2020-05-10 15:51:12 +0800

    P5406 收集树上所有苹果的最少时间

---
+++ LeetCode/Problems/Medium/P5406_MinimumTimeToCollectAllApplesInATree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 收集树上所有苹果的最少时间
+    /// https://leetcode-cn.com/contest/weekly-contest-188/problems/minimum-time-to-collect-all-apples-in-a-tree/
+    public class P5406_MinimumTimeToCollectAllApplesInATree
+    {
+        private IList<bool> hasApple;
+        private List<int>[] adj;
+
+        public int MinTime(int n, int[][] edges, IList<bool> hasApple)
+        {
+            this.hasApple = hasApple;
+            adj = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                adj[i] = new List<int>();
+            }
+
+            foreach (var edge in edges)
+            {
+                adj[edge[0]].Add(edge[1]);
+                adj[edge[1]].Add(edge[0]);
+            }
+
+            bool b = false;
+            return DFS(0, -1, out b);
+        }
+
+        private int DFS(int cur, int pre, out bool hasAppleInSub)
+        {
+            int ans = 0;
+            bool curHasApple = hasApple[cur];
+            foreach (var next in adj[cur])
+            {
+                if (next == pre)
+                {
+                    continue;
+                }
+
+                var nextCount = DFS(next, cur, out hasAppleInSub);
+                if (hasAppleInSub)
+                {
+                    ans += nextCount + 2;
+                    curHasApple = true;
+                }
+            }
+
+            hasAppleInSub = curHasApple;
+            return ans;
+        }
+    }
+}
