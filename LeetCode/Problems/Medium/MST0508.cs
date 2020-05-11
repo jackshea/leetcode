cafd22b3c8c90e644f47edfa9fa73e52e40920ff
谢在奎 <jackshea@163.com>
2020-05-11 13:28:51 +0800

    MST0508 绘制直线

---
+++ LeetCode/Problems/Medium/MST0508.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 绘制直线
+    /// https://leetcode-cn.com/problems/draw-line-lcci/
+    public class MST0508
+    {
+        public int[] DrawLine(int length, int w, int x1, int x2, int y)
+        {
+            int[] ans = new int[length];
+            for (int i = x1; i <= x2; i++)
+            {
+                ans[i / 32 + y * w / 32] |= (1 << (31 - i % 32));
+            }
+            return ans;
+        }
+    }
+}
