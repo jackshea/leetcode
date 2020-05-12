5e77e473e599a1a05aadc716682de58251d97056
谢在奎 <jackshea@163.com>
2020-05-12 13:38:23 +0800

    MST0805 递归乘法

---
+++ LeetCode/Problems/Medium/MST0805.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 递归乘法
+    /// https://leetcode-cn.com/problems/recursive-mulitply-lcci/
+    public class MST0805
+    {
+        public int Multiply(int A, int B)
+        {
+            if (B == 0) return 0;
+            if (B == 1) return A;
+            return (B % 2 == 0 ? 0 : A) + (Multiply(A, B >> 1) << 1);
+        }
+    }
+}
