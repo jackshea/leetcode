dc7e362e6a866e0dae33a77c4516ecca92c5555b
谢在奎 <jackshea@163.com>
2020-05-12 12:55:04 +0800

    MST0803 魔术索引

---
+++ LeetCode/Problems/Easy/MST0803.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 魔术索引
+    /// https://leetcode-cn.com/problems/magic-index-lcci/
+    public class MST0803
+    {
+        public int FindMagicIndex(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == i)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
