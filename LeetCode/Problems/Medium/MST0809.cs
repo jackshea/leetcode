b4f6181258ee218f4a6caae1033470f02bd4db12
谢在奎 <jackshea@163.com>
2020-05-14 13:22:37 +0800

    MST0809 括号

---
+++ LeetCode/Problems/Medium/MST0809.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 括号
+    /// https://leetcode-cn.com/problems/bracket-lcci/
+    public class MST0809
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> ans = new List<string>();
+            if (n == 0)
+            {
+                ans.Add(string.Empty);
+                return ans;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                IList<string> left = GenerateParenthesis(i);
+                foreach (var l in left)
+                {
+                    IList<string> right = GenerateParenthesis(n - i - 1);
+                    foreach (var r in right)
+                    {
+                        ans.Add($"({l}){r}");
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
