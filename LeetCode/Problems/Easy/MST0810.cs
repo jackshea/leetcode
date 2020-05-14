be54aea7e3814c92b487a3c12888d2925e6d62ac
谢在奎 <jackshea@163.com>
2020-05-14 13:36:49 +0800

    MST0810 颜色填充

---
+++ LeetCode/Problems/Easy/MST0810.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 颜色填充
+    /// https://leetcode-cn.com/problems/color-fill-lcci/
+    public class MST0810
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            if (image == null || image.Length == 0)
+            {
+                return image;
+            }
+
+            FloodFill(image, sr, sc, image[sr][sc], newColor);
+            return image;
+        }
+
+        private void FloodFill(int[][] image, int sr, int sc, int oldColor, int newColor)
+        {
+            if (oldColor == newColor)
+            {
+                return;
+            }
+
+            if (sr < 0 || sr >= image.Length || sc < 0 || sc >= image[0].Length)
+            {
+                return;
+            }
+
+            if (image[sr][sc] == oldColor)
+            {
+                image[sr][sc] = newColor;
+                FloodFill(image, sr + 1, sc, oldColor, newColor);
+                FloodFill(image, sr - 1, sc, oldColor, newColor);
+                FloodFill(image, sr, sc + 1, oldColor, newColor);
+                FloodFill(image, sr, sc - 1, oldColor, newColor);
+            }
+        }
+    }
+}
