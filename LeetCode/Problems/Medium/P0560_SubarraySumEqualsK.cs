6286fea6d78d1bfb9e28bb4118caf2560cfd39eb
谢在奎 <jackshea@163.com>
2020-05-15 12:44:47 +0800

    P0560 和为K的子数组

---
+++ LeetCode/Problems/Medium/P0560_SubarraySumEqualsK.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 和为K的子数组
+    /// https://leetcode-cn.com/problems/subarray-sum-equals-k/
+    public class P0560_SubarraySumEqualsK
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            dic.Add(0, 1);
+            int sum = 0, ans = 0;
+            foreach (var num in nums)
+            {
+                sum += num;
+
+                if (dic.TryGetValue(sum - k, out int sumk))
+                {
+                    ans += sumk;
+                }
+
+                int pre = 0;
+                dic.TryGetValue(sum, out pre);
+                dic[sum] = 1 + pre;
+
+            }
+
+            return ans;
+        }
+    }
+}
