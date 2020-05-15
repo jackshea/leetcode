2cf4dcd19921bade55f2fd88c064e0d659a87aef
谢在奎 <jackshea@163.com>
2020-05-15 15:39:20 +0800

    MST1002 变位词组

---
+++ LeetCode/Problems/Medium/MST1002.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 变位词组
+    /// https://leetcode-cn.com/problems/group-anagrams-lcci/
+    public class MST1002
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            IList<IList<string>> ans = new List<IList<string>>();
+            foreach (var str in strs)
+            {
+                bool isExistKey = false;
+                foreach (var list in ans)
+                {
+                    if (IsEqual(list[0], str))
+                    {
+                        isExistKey = true;
+                        list.Add(str);
+                    }
+                }
+
+                if (!isExistKey)
+                {
+                    ans.Add(new List<string>() { str });
+                }
+            }
+
+            return ans;
+        }
+
+        private int[] CharCounts(string s)
+        {
+            int[] ans = new int[26];
+            foreach (var c in s)
+            {
+                ans[c - 'a']++;
+            }
+            return ans;
+        }
+
+        private bool IsEqual(string strA, string strB)
+        {
+            if (strA.Length != strB.Length)
+            {
+                return false;
+            }
+
+            int[] a = CharCounts(strA);
+            int[] b = CharCounts(strB);
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
