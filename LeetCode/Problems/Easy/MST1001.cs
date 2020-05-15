83490a6bd0b5cd0c8103f460f8af034013ef9293
谢在奎 <jackshea@163.com>
2020-05-15 15:16:12 +0800

    MST1001 合并排序的数组

---
+++ LeetCode/Problems/Easy/MST1001.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 合并排序的数组
+    /// https://leetcode-cn.com/problems/group-anagrams-lcci/
+    public class MST1001
+    {
+        public void Merge(int[] A, int m, int[] B, int n)
+        {
+            m--;
+            n--;
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                if (n < 0 || (m >= 0 && A[m] > B[n]))
+                {
+                    A[i] = A[m--];
+                    continue;
+                }
+                A[i] = B[n--];
+            }
+        }
+    }
+}
