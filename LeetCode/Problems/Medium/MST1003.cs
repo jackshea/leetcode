a50d0cb73c95e3cccd70f307a54f453cc4303f33
谢在奎 <jackshea@163.com>
2020-05-16 13:42:48 +0800

    MST1003

---
+++ LeetCode/Problems/Medium/MST1003.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 搜索旋转数组
+    /// https://leetcode-cn.com/problems/search-rotate-array-lcci/
+    public class MST1003
+    {
+        public int Search(int[] arr, int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (target == arr[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
