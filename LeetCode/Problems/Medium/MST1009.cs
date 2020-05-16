1fb2d773390901ec8e7765977fc37b405599d988
谢在奎 <jackshea@163.com>
2020-05-16 15:06:33 +0800

    MST1009 排序矩阵查找

---
+++ LeetCode/Problems/Medium/MST1009.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 排序矩阵查找
+    /// https://leetcode-cn.com/problems/sorted-matrix-search-lcci/
+    public class MST1009
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            if (matrix == null || matrix.Length == 0)
+            {
+                return false;
+            }
+
+            int r = 0, c = matrix[0].Length - 1;
+            while (c >= 0 && r < matrix.Length)
+            {
+                if (target == matrix[r][c])
+                {
+                    return true;
+                }
+                else if (target > matrix[r][c])
+                {
+                    r++;
+                }
+                else
+                {
+                    c--;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/MST1009Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class MST1009Tests
+    {
+        [Test()]
+        public void SearchMatrixTest()
+        {
+            int[][] input = new int[][] {
+                new []{1, 4, 7, 11, 15},
+                new []{2, 5, 8, 12, 19},
+                new []{3, 6, 9, 16, 22},
+                new []{10, 13, 14, 17, 24},
+                new []{18, 21, 23, 26, 30}};
+            var test = new MST1009();
+            Assert.IsTrue(test.SearchMatrix(input, 5));
+            Assert.IsFalse(test.SearchMatrix(input, 20));
+        }
+    }
+}
