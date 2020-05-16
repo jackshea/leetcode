f444def33be24a6abb8f3778d384fcbb734c3e8b
谢在奎 <jackshea@163.com>
2020-05-16 15:33:55 +0800

    MST1601 交换数字

---
+++ LeetCode/Problems/Medium/MST1601.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 交换数字
+    /// https://leetcode-cn.com/problems/swap-numbers-lcci/
+    public class MST1601
+    {
+        public int[] SwapNumbers(int[] numbers)
+        {
+            numbers[0] ^= numbers[1];
+            numbers[1] ^= numbers[0];
+            numbers[0] ^= numbers[1];
+            return numbers;
+        }
+    }
+}
