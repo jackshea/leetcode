d336fe06c5e6273d54fa8fba30441b261007b0cf
谢在奎 <jackshea@163.com>
2020-05-17 19:38:40 +0800

    MST1605 阶乘尾数

---
+++ LeetCode/Problems/Easy/MST1605.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 阶乘尾数
+    /// https://leetcode-cn.com/problems/factorial-zeros-lcci/
+    public class MST1605
+    {
+        public int TrailingZeroes(int n)
+        {
+            return n == 0 ? 0 : n / 5 + TrailingZeroes(n / 5);
+        }
+    }
+}
