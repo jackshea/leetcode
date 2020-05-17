bc0f665a9216c456f4611ba914b22a6d2f997d7b
谢在奎 <jackshea@163.com>
2020-05-17 14:10:12 +0800

    MST1602 单词频率

---
+++ LeetCode/Problems/Medium/MST1602.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 单词频率
+    /// https://leetcode-cn.com/problems/words-frequency-lcci/
+    public class WordsFrequency
+    {
+        Dictionary<string, int> dic = new Dictionary<string, int>();
+        public WordsFrequency(string[] book)
+        {
+            foreach (var word in book)
+            {
+                dic.TryGetValue(word, out int count);
+                dic[word] = count + 1;
+            }
+        }
+
+        public int Get(string word)
+        {
+            dic.TryGetValue(word, out int count);
+            return count;
+        }
+    }
+}
