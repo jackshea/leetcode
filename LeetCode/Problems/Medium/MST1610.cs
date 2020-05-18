d9a589d7068be0423204d1ccd42c1b457bdd1bea
谢在奎 <jackshea@163.com>
2020-05-18 13:25:42 +0800

    MST1610 生存人数

---
+++ LeetCode/Problems/Medium/MST1610.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 生存人数
+    /// https://leetcode-cn.com/problems/living-people-lcci/
+    public class MST1610
+    {
+        public int MaxAliveYear(int[] birth, int[] death)
+        {
+            Array.Sort(birth);
+            Array.Sort(death);
+            int j = 0, alive = 0, max = 0, year = 0;
+            for (int i = 0; i < birth.Length;)
+            {
+                if (birth[i] <= death[j])
+                {
+                    alive++;
+                    if (max < alive)
+                    {
+                        max = alive;
+                        year = birth[i];
+                    }
+
+                    i++;
+                }
+                else
+                {
+                    alive--;
+                    j++;
+                }
+            }
+
+            return year;
+        }
+    }
+}
