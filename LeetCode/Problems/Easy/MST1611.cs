f3d32c1950c4b8f2c3621ba110d51b0af21cf03d
谢在奎 <jackshea@163.com>
2020-05-18 14:05:05 +0800

    MST1611

---
+++ LeetCode/Problems/Easy/MST1611.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 跳水板
+    /// https://leetcode-cn.com/problems/diving-board-lcci/
+    public class MST1611
+    {
+        public int[] DivingBoard(int shorter, int longer, int k)
+        {
+            if (k <= 0)
+            {
+                return new int[0];
+            }
+
+            if (shorter == longer)
+            {
+                return new int[] { k * shorter };
+            }
+
+            int[] ans = new int[k + 1];
+            for (int i = 0; i <= k; i++)
+            {
+                ans[i] = i * longer + (k - i) * shorter;
+            }
+
+            return ans;
+        }
+    }
+}
