0c693fe50b76d0ceafdd4dfa2b3a20d3e4ebd837
谢在奎 <jackshea@163.com>
2020-05-20 17:01:48 +0800

    MST1620 T9键盘

---
+++ LeetCode/Problems/Medium/MST1620.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// T9键盘
+    /// https://leetcode-cn.com/problems/t9-lcci/
+    public class MST1620
+    {
+        private string letter2Num = "22233344455566677778889999";
+
+        public IList<string> GetValidT9Words(string num, string[] words)
+        {
+            IList<string> ans = new List<string>();
+            foreach (var word in words)
+            {
+                if (WordToNum(word) == num)
+                {
+                    ans.Add(word);
+                }
+            }
+
+            return ans;
+        }
+
+        private string WordToNum(string word)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var c in word)
+            {
+                sb.Append(letter2Num[c - 'a']);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
