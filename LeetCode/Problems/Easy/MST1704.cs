58d193c1807762b627993f27a1a626c63575e63b
谢在奎 <jackshea@163.com>
2020-05-22 23:26:00 +0800

    MST1704 消失的数字

---
+++ LeetCode/Problems/Easy/MST1704.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 消失的数字
+    /// https://leetcode-cn.com/problems/missing-number-lcci/
+    public class MST1704
+    {
+        public int MissingNumber(int[] nums)
+        {
+            var sum = nums.Sum();
+            int n = nums.Length;
+            int expectSum = (0 + n) * (n + 1) / 2;
+            return expectSum - sum;
+        }
+    }
+}
