423754d960e1da1fd6d1343bc08bc1fcf75cf925
谢在奎 <jackshea@163.com>
2020-05-29 00:16:16 +0800

    P0394 字符串解码

---
+++ LeetCode/Problems/Medium/P0394_DecodeString.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 字符串解码
+    /// https://leetcode-cn.com/problems/decode-string/
+    public class P0394_DecodeString
+    {
+        public string DecodeString(string s)
+        {
+            int index = 0;
+            return DecodeString(s, ref index);
+        }
+
+        public string DecodeString(string s, ref int index)
+        {
+            StringBuilder ans = new StringBuilder();
+            int times = 0;
+            while (index < s.Length)
+            {
+                var c = s[index];
+                if (char.IsLetter(c))
+                {
+                    ans.Append(c);
+                    index++;
+                }
+                else if (char.IsDigit(c))
+                {
+                    times = times * 10 + (c - '0');
+                    index++;
+                }
+                else if (c == '[')
+                {
+                    ++index;
+                    ans.Append(Times(DecodeString(s, ref index), times));
+                    times = 0;
+                }
+                else if (c == ']')
+                {
+                    index++;
+                    break;
+                }
+            }
+
+            return ans.ToString();
+        }
+
+        private string Times(string s, int times)
+        {
+            if (times <= 1)
+            {
+                return s;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < times; i++)
+            {
+                sb.Append(s);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0394_DecodeStringTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0394_DecodeStringTests
+    {
+        [Test()]
+        public void DecodeStringTest()
+        {
+            var test = new P0394_DecodeString();
+            Assert.AreEqual("aaabcbc", test.DecodeString("3[a]2[bc]"));
+            Assert.AreEqual("accaccacc", test.DecodeString("3[a2[c]]"));
+            Assert.AreEqual("abcabccdcdcdef", test.DecodeString("2[abc]3[cd]ef"));
+        }
+    }
+}
