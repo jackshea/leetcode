6bc0546182a372f2de250464385a4ee1e2628851
谢在奎 <jackshea@163.com>
2020-05-29 23:54:30 +0800

    P0299 猜数字游戏

---
+++ LeetCode/Problems/Easy/P0299_BullsAndCows.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 猜数字游戏
+    /// https://leetcode-cn.com/problems/bulls-and-cows/
+    public class P0299_BullsAndCows
+    {
+        public string GetHint(string secret, string guess)
+        {
+            int bulls = 0;
+            int[] secretCounts = new int[10];
+            int[] guessCounts = new int[10];
+            for (int i = 0; i < secret.Length; i++)
+            {
+                secretCounts[secret[i] - '0']++;
+                guessCounts[guess[i] - '0']++;
+                if (secret[i] == guess[i])
+                {
+                    bulls++;
+                }
+            }
+
+            int bullsAndCows = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                bullsAndCows += Math.Min(secretCounts[i], guessCounts[i]);
+            }
+
+            return $"{bulls}A{bullsAndCows - bulls}B";
+        }
+    }
+}
