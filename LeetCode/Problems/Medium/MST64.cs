fd4579b50bf7d726b455bb9684c43ca092d01507
谢在奎 <jackshea@163.com>
2020-06-02 21:51:20 +0800

    面试题64. 求1+2+…+n

---
+++ LeetCode/Problems/Medium/MST64.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 面试题64. 求1+2+…+n
+    /// https://leetcode-cn.com/problems/qiu-12n-lcof/
+    public class MST64
+    {
+        public int SumNums(int n)
+        {
+            int sum = n;
+            bool flag = n > 0 && (sum += SumNums(n - 1)) > 0;
+            return sum;
+        }
+    }
+}
