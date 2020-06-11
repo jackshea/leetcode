3077e38c23e989fe74bc5d71d8043c2258b34dd0
谢在奎 <jackshea@163.com>
2020-06-11 12:56:22 +0800

    P0739 每日温度

---
+++ LeetCode/Problems/Medium/P0739_DailyTemperatures.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 每日温度
+    /// https://leetcode-cn.com/problems/daily-temperatures/
+    public class P0739_DailyTemperatures
+    {
+        public int[] DailyTemperatures(int[] T)
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] ans = new int[T.Length];
+            for (int i = 0; i < T.Length; i++)
+            {
+                int t = T[i];
+                while (stack.Count != 0)
+                {
+                    var topIndex = stack.Peek();
+                    if (T[topIndex] < t)
+                    {
+                        stack.Pop();
+                        ans[topIndex] = i - topIndex;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                stack.Push(i);
+            }
+
+            return ans;
+        }
+    }
+}
