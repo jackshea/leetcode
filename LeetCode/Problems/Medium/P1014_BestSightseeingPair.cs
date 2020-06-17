d150028ea7048150035ea5a00d0abbffd579de19
谢在奎 <jackshea@163.com>
2020-06-17 12:44:28 +0800

    P1014 最佳观光组合

---
+++ LeetCode/Problems/Medium/P1014_BestSightseeingPair.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 最佳观光组合
+    /// https://leetcode-cn.com/problems/best-sightseeing-pair/
+    public class P1014_BestSightseeingPair
+    {
+        public int MaxScoreSightseeingPair(int[] A)
+        {
+            int left = 0;
+            int ans = int.MinValue;
+            for (int i = 1; i < A.Length; i++)
+            {
+                var v = A[left] + left + A[i] - i;
+                if (ans < v)
+                {
+                    ans = v;
+                }
+                if (A[i] + i > left + A[left])
+                {
+                    left = i;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
