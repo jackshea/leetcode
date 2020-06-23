010feaad9d778668c797ea98ef5112156e452615
谢在奎 <jackshea@163.com>
2020-06-23 10:38:06 +0800

    P0374 猜数字大小

---
+++ LeetCode/Problems/Easy/P0374_GuessNumberHigherOrLower.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 猜数字大小
+    /// https://leetcode-cn.com/problems/guess-number-higher-or-lower/
+    public class P0374_GuessNumberHigherOrLower
+    {
+        public int GuessNumber(int n)
+        {
+            int left = 1, right = n;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                var cmp = guess(mid);
+                if (cmp == 1)
+                {
+                    left = mid + 1;
+                }
+                else if (cmp == -1)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return left;
+        }
+
+        private int guess(int num)
+        {
+            return 0;
+        }
+    }
+}
