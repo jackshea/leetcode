3baba95b2cf74baaba8dfbbbc1fc02c228032d09
谢在奎 <jackshea@163.com>
2020-06-23 10:48:17 +0800

    P0392 判断子序列

---
+++ LeetCode/Problems/Easy/P0392_IsSubsequence.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 判断子序列
+    /// https://leetcode-cn.com/problems/is-subsequence/
+    public class P0392_IsSubsequence
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int si = 0;
+            if (s.Length == 0)
+            {
+                return true;
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == s[si])
+                {
+                    si++;
+                    if (si == s.Length)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
