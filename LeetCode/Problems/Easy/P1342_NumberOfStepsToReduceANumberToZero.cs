240b954c64e2329683c6a4a05a0bc28ec88736c3
谢在奎 <jackshea@163.com>
2020-06-30 18:44:15 +0800

    P1342 将数字变成 0 的操作次数

---
+++ LeetCode/Problems/Easy/P1342_NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 将数字变成 0 的操作次数
+    /// https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+    public class P1342_NumberOfStepsToReduceANumberToZero
+    {
+        public int NumberOfSteps(int num)
+        {
+            int ans = 0;
+            while (num != 0)
+            {
+                ans += (num % 2 == 0 ? 1 : 2);
+                num >>= 1;
+            }
+
+            return ans > 0 ? ans - 1 : ans;
+        }
+    }
+}
