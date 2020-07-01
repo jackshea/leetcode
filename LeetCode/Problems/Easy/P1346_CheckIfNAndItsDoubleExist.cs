b76b0bcb6052060119632da09f116abdcbe6475a
谢在奎 <jackshea@163.com>
2020-07-01 09:45:34 +0800

    P1346 检查整数及其两倍数是否存在

---
+++ LeetCode/Problems/Easy/P1346_CheckIfNAndItsDoubleExist.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 检查整数及其两倍数是否存在
+    /// https://leetcode-cn.com/problems/check-if-n-and-its-double-exist/
+    public class P1346_CheckIfNAndItsDoubleExist
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            HashSet<int> exist = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var n = arr[i];
+                if (exist.Contains(n * 2) || n % 2 == 0 && exist.Contains(n / 2))
+                {
+                    return true;
+                }
+
+                exist.Add(n);
+            }
+
+            return false;
+        }
+    }
+}
