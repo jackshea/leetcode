2101b45b4ee89a948f19bbe50a1a95c993dd0861
谢在奎 <jackshea@163.com>
2020-07-01 10:22:36 +0800

    P1356 根据数字二进制下 1 的数目排序

---
+++ LeetCode/Problems/Easy/P1356_SortIntegersByTheNumberOf1Bits.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 根据数字二进制下 1 的数目排序
+    /// https://leetcode-cn.com/problems/sort-integers-by-the-number-of-1-bits/
+    public class P1356_SortIntegersByTheNumberOf1Bits
+    {
+        public int[] SortByBits(int[] arr)
+        {
+            Dictionary<int, int> hw = new Dictionary<int, int>();
+            foreach (var n in arr)
+            {
+                hw[n] = HammingWeight(n);
+            }
+
+            Array.Sort(arr, (a, b) =>
+            {
+                var ahw = hw[a];
+                var bhw = hw[b];
+                if (ahw == bhw)
+                {
+                    return a.CompareTo(b);
+                }
+
+                return ahw.CompareTo(bhw);
+            });
+            return arr;
+        }
+
+        private int HammingWeight(int n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                if (n % 2 == 1)
+                {
+                    count++;
+                }
+
+                n = n >> 1;
+            }
+
+            return count;
+        }
+    }
+}
