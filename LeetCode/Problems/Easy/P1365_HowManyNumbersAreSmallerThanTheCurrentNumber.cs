69044d87a30f213781f7e919f9fa111d38ad8d04
谢在奎 <jackshea@163.com>
2020-07-03 10:16:08 +0800

    P1365 有多少小于当前数字的数字

---
+++ LeetCode/Problems/Easy/P1365_HowManyNumbersAreSmallerThanTheCurrentNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 有多少小于当前数字的数字
+    /// https://leetcode-cn.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+    public class P1365_HowManyNumbersAreSmallerThanTheCurrentNumber
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (num < nums[j])
+                    {
+                        ans[j]++;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
