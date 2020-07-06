40ba529e29fdb5d1f8e9f696810f8ee9c86fc3f7
谢在奎 <jackshea@163.com>
2020-07-06 10:19:31 +0800

    P1370 上升下降字符串

---
+++ LeetCode/Problems/Easy/P1370_IncreasingDecreasingString.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 上升下降字符串
+    /// https://leetcode-cn.com/problems/increasing-decreasing-string/
+    public class P1370_IncreasingDecreasingString
+    {
+        public string SortString(string s)
+        {
+            bool[] selected = new bool[s.Length];
+            var ss = s.ToCharArray();
+            Array.Sort(ss);
+            StringBuilder ans = new StringBuilder();
+            char minChar = (char)('a' - 1);
+            char maxChar = (char)('z' + 1);
+            char lastChar = minChar;
+            bool isIncrease = true;
+            while (ans.Length < ss.Length)
+            {
+                if (isIncrease)
+                {
+                    for (int i = 0; i < ss.Length; i++)
+                    {
+                        if (selected[i])
+                        {
+                            continue;
+                        }
+
+                        if (lastChar < ss[i])
+                        {
+                            ans.Append(ss[i]);
+                            lastChar = ss[i];
+                            selected[i] = true;
+                        }
+                    }
+
+                    isIncrease = false;
+                    lastChar = maxChar;
+                }
+                else
+                {
+                    for (int i = ss.Length - 1; i >= 0; i--)
+                    {
+                        if (selected[i])
+                        {
+                            continue;
+                        }
+
+                        if (lastChar > ss[i])
+                        {
+                            ans.Append(ss[i]);
+                            lastChar = ss[i];
+                            selected[i] = true;
+                        }
+                    }
+
+                    isIncrease = true;
+                    lastChar = minChar;
+                }
+            }
+
+            return ans.ToString();
+        }
+    }
+}
