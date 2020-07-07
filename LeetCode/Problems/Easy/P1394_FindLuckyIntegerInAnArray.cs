8d8f79bc080831d4a0e01244997e84e61a376b32
谢在奎 <jackshea@163.com>
2020-07-07 11:40:08 +0800

    P1394 找出数组中的幸运数

---
+++ LeetCode/Problems/Easy/P1394_FindLuckyIntegerInAnArray.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 找出数组中的幸运数
+    /// https://leetcode-cn.com/problems/find-lucky-integer-in-an-array/
+    public class P1394_FindLuckyIntegerInAnArray
+    {
+        public int FindLucky(int[] arr)
+        {
+            int luckyNum = -1;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (var a in arr)
+            {
+                if (dic.ContainsKey(a))
+                {
+                    dic[a]++;
+                }
+                else
+                {
+                    dic[a] = 1;
+                }
+            }
+
+            foreach (var kvp in dic)
+            {
+                if (kvp.Key == kvp.Value && kvp.Value > luckyNum)
+                {
+                    luckyNum = kvp.Key;
+                }
+            }
+
+            return luckyNum;
+        }
+    }
+}
