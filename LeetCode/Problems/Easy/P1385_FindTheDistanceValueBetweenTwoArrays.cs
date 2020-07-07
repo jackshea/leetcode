02aee382cbbd0d625c9af789708a2036c3b0e381
谢在奎 <jackshea@163.com>
2020-07-07 11:01:31 +0800

    P1385 两个数组间的距离值

---
+++ LeetCode/Problems/Easy/P1385_FindTheDistanceValueBetweenTwoArrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 两个数组间的距离值
+    /// https://leetcode-cn.com/problems/find-the-distance-value-between-two-arrays/
+    public class P1385_FindTheDistanceValueBetweenTwoArrays
+    {
+        public int FindTheDistanceValue(int[] arr1, int[] arr2, int d)
+        {
+            int ans = 0;
+            foreach (var a1 in arr1)
+            {
+                bool check = true;
+                foreach (var a2 in arr2)
+                {
+                    if (Math.Abs(a1 - a2) <= d)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
