ccae8830295e979cf8f522b5c36c94b8a5b5cfb9
谢在奎 <jackshea@163.com>
2020-07-08 11:54:37 +0800

    P1403 非递增顺序的最小子序列

---
+++ LeetCode/Problems/Easy/P1403_MinimumSubsequenceInNonIncreasingOrder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 非递增顺序的最小子序列
+    /// https://leetcode-cn.com/problems/minimum-subsequence-in-non-increasing-order/
+    public class P1403_MinimumSubsequenceInNonIncreasingOrder
+    {
+        public IList<int> MinSubsequence(int[] nums)
+        {
+            var halfSum = nums.Sum() / 2;
+            Array.Sort(nums);
+            int selectSum = 0;
+            IList<int> ans = new List<int>();
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                selectSum += nums[i];
+                ans.Add(nums[i]);
+                if (selectSum > halfSum)
+                {
+                    break;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
