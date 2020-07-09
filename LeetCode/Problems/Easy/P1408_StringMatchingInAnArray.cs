54b8e5c4258b75972f4cc40d094c5ae1ccb91d17
谢在奎 <jackshea@163.com>
2020-07-09 09:42:12 +0800

    P1408 数组中的字符串匹配

---
+++ LeetCode/Problems/Easy/P1408_StringMatchingInAnArray.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 数组中的字符串匹配
+    /// https://leetcode-cn.com/problems/string-matching-in-an-array/
+    public class P1408_StringMatchingInAnArray
+    {
+        public IList<string> StringMatching(string[] words)
+        {
+            IList<string> ans = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (words[i].Length >= words[j].Length)
+                    {
+                        continue;
+                    }
+
+                    if (words[j].IndexOf(words[i]) != -1)
+                    {
+                        ans.Add(words[i]);
+                        break;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
