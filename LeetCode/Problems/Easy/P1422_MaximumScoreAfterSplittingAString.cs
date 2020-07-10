b2b9b0be12827762e02fd6134e096c02d0230295
谢在奎 <jackshea@163.com>
2020-07-10 10:17:45 +0800

    P1422 分割字符串的最大得分

---
+++ LeetCode/Problems/Easy/P1422_MaximumScoreAfterSplittingAString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 分割字符串的最大得分
+    /// https://leetcode-cn.com/problems/maximum-score-after-splitting-a-string/
+    public class P1422_MaximumScoreAfterSplittingAString
+    {
+        public int MaxScore(string s)
+        {
+            int[] left = new int[s.Length];
+            int[] right = new int[s.Length];
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    count++;
+                }
+
+                left[i] = count;
+            }
+
+            count = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                right[i] = count;
+                if (s[i] == '1')
+                {
+                    count++;
+                }
+            }
+
+            int ans = 0;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                ans = Math.Max(ans, left[i] + right[i]);
+            }
+
+            return ans;
+        }
+    }
+}
