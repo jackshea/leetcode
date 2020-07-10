c5d469bd088c0151d75701568d645a7fcd383691
谢在奎 <jackshea@163.com>
2020-07-10 09:48:00 +0800

    P1417 重新格式化字符串

---
+++ LeetCode/Problems/Easy/P1417_ReformatTheString.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 重新格式化字符串
+    /// https://leetcode-cn.com/problems/reformat-the-string/
+    public class P1417_ReformatTheString
+    {
+        public string Reformat(string s)
+        {
+            List<char> letters = new List<char>();
+            List<char> digits = new List<char>();
+            foreach (var c in s)
+            {
+                if (char.IsLetter(c))
+                {
+                    letters.Add(c);
+                }
+                else
+                {
+                    digits.Add(c);
+                }
+            }
+
+            if (Math.Abs(letters.Count - digits.Count) > 1)
+            {
+                return string.Empty;
+            }
+
+            List<char> longer;
+            List<char> shorter;
+            if (letters.Count > digits.Count)
+            {
+                longer = letters;
+                shorter = digits;
+            }
+            else
+            {
+                longer = digits;
+                shorter = letters;
+            }
+
+            StringBuilder ans = new StringBuilder();
+            for (int i = 0; i < shorter.Count; i++)
+            {
+                ans.Append(longer[i]);
+                ans.Append(shorter[i]);
+            }
+            ans.Append(longer[letters.Count - 1]);
+            return ans.ToString();
+        }
+    }
+}
