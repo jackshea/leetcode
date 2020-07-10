a6e6caed4c3d398ec684ef054e5019adce7487ae
谢在奎 <jackshea@163.com>
2020-07-10 10:36:40 +0800

    P1436 旅行终点站

---
+++ LeetCode/Problems/Easy/P1436_DestinationCity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 旅行终点站
+    /// https://leetcode-cn.com/problems/destination-city/
+    public class P1436_DestinationCity
+    {
+        public string DestCity(IList<IList<string>> paths)
+        {
+            Dictionary<string, string> nextCity = new Dictionary<string, string>();
+            foreach (var path in paths)
+            {
+                nextCity[path[0]] = path[1];
+            }
+
+            string cur = paths[0][0];
+            while (nextCity.ContainsKey(cur))
+            {
+                cur = nextCity[cur];
+            }
+
+            return cur;
+        }
+    }
+}
