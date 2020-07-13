20636a90e66b08b78d16b52f6e987277e47480fb
谢在奎 <jackshea@163.com>
2020-07-13 09:47:25 +0800

    P1446 连续字符

---
+++ LeetCode/Problems/Easy/P1446_ConsecutiveCharacters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 连续字符
+    /// https://leetcode-cn.com/problems/consecutive-characters/
+    public class P1446_ConsecutiveCharacters
+    {
+        public int MaxPower(string s)
+        {
+            int ans = 1;
+            int count = 1;
+            for (var i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    count++;
+                    ans = Math.Max(ans, count);
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
