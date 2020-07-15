0606853b33f7614bf6fb0262b9da246f8cb2a937
谢在奎 <jackshea@163.com>
2020-07-15 10:06:19 +0800

    P1486 数组异或操作

---
+++ LeetCode/Problems/Easy/P1486_XorOperationInAnArray.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 数组异或操作
+    /// https://leetcode-cn.com/problems/xor-operation-in-an-array/
+    public class P1486_XorOperationInAnArray
+    {
+        public int XorOperation(int n, int start)
+        {
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ans ^= (start + 2 * i);
+            }
+
+            return ans;
+        }
+    }
+}
