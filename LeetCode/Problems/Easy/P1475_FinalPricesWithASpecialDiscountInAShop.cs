4915cc6314353b9d15b4d1536589ec3504e93ef8
谢在奎 <jackshea@163.com>
2020-07-15 09:55:21 +0800

    P1475 商品折扣后的最终价格

---
+++ LeetCode/Problems/Easy/P1475_FinalPricesWithASpecialDiscountInAShop.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 商品折扣后的最终价格
+    /// https://leetcode-cn.com/problems/final-prices-with-a-special-discount-in-a-shop/
+    public class P1475_FinalPricesWithASpecialDiscountInAShop
+    {
+        public int[] FinalPrices(int[] prices)
+        {
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    if (prices[i] >= prices[j])
+                    {
+                        prices[i] -= prices[j];
+                        break;
+                    }
+                }
+            }
+
+            return prices;
+        }
+    }
+}
