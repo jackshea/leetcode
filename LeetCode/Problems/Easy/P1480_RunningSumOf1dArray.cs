2f37b8cc40b63f598ff027221af3928da6d1c9bf
谢在奎 <jackshea@163.com>
2020-07-15 10:01:51 +0800

    P1480 一维数组的动态和

---
+++ LeetCode/Problems/Easy/P1480_RunningSumOf1dArray.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 一维数组的动态和
+    /// https://leetcode-cn.com/problems/running-sum-of-1d-array/
+    public class P1480_RunningSumOf1dArray
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
