5eaa09be655ea8c973e7c2fe577258b586dac119
谢在奎 <jackshea@163.com>
2020-07-16 10:49:35 +0800

    P1491 去掉最低工资和最高工资后的工资平均值

---
+++ LeetCode/Problems/Easy/P1491_AverageSalaryExcludingTheMinimumAndMaximumSalary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 去掉最低工资和最高工资后的工资平均值
+    /// https://leetcode-cn.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+    public class P1491_AverageSalaryExcludingTheMinimumAndMaximumSalary
+    {
+        public double Average(int[] salary)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int sum = 0;
+            foreach (var s in salary)
+            {
+                max = Math.Max(max, s);
+                min = Math.Min(min, s);
+                sum += s;
+            }
+
+            return 1.0 * (sum - max - min) / (salary.Length - 2);
+        }
+    }
+}
