d630cf356ceb52c136a29bc67cc4658a4eb09146
谢在奎 <jackshea@163.com>
2020-07-16 10:30:46 +0800

    P0785 判断二分图

---
+++ LeetCode/Problems/Medium/P0785_IsGraphBipartite.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 判断二分图
+    /// https://leetcode-cn.com/problems/is-graph-bipartite/
+    public class P0785_IsGraphBipartite
+    {
+        public bool IsBipartite(int[][] graph)
+        {
+            int[] tags = new int[graph.Length];
+            Queue<int> q = new Queue<int>();
+            for (var i = 0; i < graph.Length; i++)
+            {
+                if (tags[i]!=0)
+                {
+                    continue;
+                }
+
+                q.Enqueue(i);
+                int tag = 1;
+                tags[i] = tag;
+                while (q.Count > 0)
+                {
+                    var count = q.Count;
+                    tag = tag % 2 + 1;
+                    for (int j = 0; j < count; j++)
+                    {
+                        var top = q.Dequeue();
+                        foreach (var next in graph[top])
+                        {
+                            if (tags[next] == 0)
+                            {
+                                q.Enqueue(next);
+                                tags[next] = tag;
+                            }
+                            else if (tags[next] == tag)
+                            {
+                                // do nothing
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return true;
+        }
+    }
+}
