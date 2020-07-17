5b8cd210bc850a428b738dde39266ca9df67411f
谢在奎 <jackshea@163.com>
2020-07-17 10:14:39 +0800

    P1502 判断能否形成等差数列

---
+++ LeetCode/Problems/Easy/P1502_CanMakeArithmeticProgressionFromSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 判断能否形成等差数列
+    /// https://leetcode-cn.com/problems/can-make-arithmetic-progression-from-sequence/
+    public class P1502_CanMakeArithmeticProgressionFromSequence
+    {
+        public bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+            int d = arr[1] - arr[0];
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i] - arr[i - 1] != d)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
