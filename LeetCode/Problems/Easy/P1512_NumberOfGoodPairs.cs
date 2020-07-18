dcd628a47384ad11cafadfdcfbde4d31da35d6f5
谢在奎 <jackshea@163.com>
2020-07-18 23:08:15 +0800

    P1512 好数对的数目

---
+++ LeetCode/Problems/Easy/P1512_NumberOfGoodPairs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 好数对的数目
+    /// https://leetcode-cn.com/problems/number-of-good-pairs/
+    public class P1512_NumberOfGoodPairs
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int ans = 0;
+            var dic = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    ans += dic[num];
+                    dic[num]++;
+                }
+                else
+                {
+                    dic[num] = 1;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
