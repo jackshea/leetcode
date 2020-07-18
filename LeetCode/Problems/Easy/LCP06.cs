1af2ea5826dd5496bed8ba09859954dec3151e0a
谢在奎 <jackshea@163.com>
2020-07-18 23:14:59 +0800

    LCP 06. 拿硬币

---
+++ LeetCode/Problems/Easy/LCP06.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// LCP 06. 拿硬币
+    /// https://leetcode-cn.com/problems/na-ying-bi/
+    public class LCP06
+    {
+        public int MinCount(int[] coins)
+        {
+            int ans = 0;
+            foreach (var coin in coins)
+            {
+                ans += coin % 2 + coin / 2;
+            }
+
+            return ans;
+        }
+    }
+}
