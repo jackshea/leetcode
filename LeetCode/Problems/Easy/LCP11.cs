4f6687e35f7d64cb3a84aefaf065e0bb8276f29f
谢在奎 <jackshea@163.com>
2020-07-19 12:22:13 +0800

    LCP 11. 期望个数统计

---
+++ LeetCode/Problems/Easy/LCP11.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// LCP 11. 期望个数统计
+    /// https://leetcode-cn.com/problems/qi-wang-ge-shu-tong-ji/
+    public class LCP11
+    {
+        public int ExpectNumber(int[] scores)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var score in scores)
+            {
+                if (dic.ContainsKey(score))
+                {
+                    dic[score]++;
+                }
+                else
+                {
+                    dic[score] = 1;
+                }
+            }
+
+            return dic.Count;
+        }
+    }
+}
