ecdbb080d3eb6e4796bee4bc94499e40af8aa663
谢在奎 <jackshea@163.com>
2020-07-19 12:27:07 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ LeetCode/Problems/Easy/Offer03.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 03. 数组中重复的数字
+    /// https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+    public class Offer03
+    {
+        public int FindRepeatNumber(int[] nums)
+        {
+            int[] freq = new int[nums.Length];
+            foreach (var num in nums)
+            {
+                freq[num]++;
+                if (freq[num] >= 2)
+                {
+                    return num;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
