927d3fd20b65bd5a0ab4ca6a3a7e26b02747dbc8
谢在奎 <jackshea@163.com>
2020-07-20 11:15:35 +0800

    P1518 换酒问题

---
+++ LeetCode/Problems/Easy/P1518_WaterBottles.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 换酒问题
+    /// https://leetcode-cn.com/problems/water-bottles/
+    public class P1518_WaterBottles
+    {
+        public int NumWaterBottles(int numBottles, int numExchange)
+        {
+            return numBottles + (numBottles - 1) / (numExchange - 1);
+        }
+    }
+}
