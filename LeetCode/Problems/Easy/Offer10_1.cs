c2f58e9b423f45315447e6bc42578c27bc6e0548
谢在奎 <jackshea@163.com>
2020-07-21 09:55:14 +0800

    剑指 Offer 10- I.斐波那契数列

---
+++ LeetCode/Problems/Easy/Offer10_1.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 10- I.斐波那契数列
+    /// https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+    public class Offer10_1
+    {
+        public int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            int a = 0;
+            int b = 1;
+            int c = a + b;
+            for (int i = 3; i <= n; i++)
+            {
+                a = b;
+                b = c;
+                c = (a + b) % 1000000007;
+            }
+
+            return c;
+        }
+    }
+}
