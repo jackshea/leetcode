5b977958d43b2aa0dc32cef479e32eca4848f3a9
谢在奎 <jackshea@163.com>
2020-07-21 10:00:16 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

---
+++ LeetCode/Problems/Easy/Offer10_II.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 10- II. 青蛙跳台阶问题
+    /// https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/
+    public class Offer10_II
+    {
+        public int NumWays(int n)
+        {
+            return Fib(n + 1);
+        }
+
+        public int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            int a = 0;
+            int b = 1;
+            int c = a + b;
+            for (int i = 3; i <= n; i++)
+            {
+                a = b;
+                b = c;
+                c = (a + b) % 1000000007;
+            }
+
+            return c;
+        }
+    }
+}
