5945d6d40b9d8d953fd13b773b5a2a17cfc0e3ad
谢在奎 <jackshea@163.com>
2020-07-21 09:45:31 +0800

    剑指 Offer 06. 从尾到头打印链表

---
+++ LeetCode/Problems/Easy/Offer06.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 06. 从尾到头打印链表
+    /// https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+    public class Offer06
+    {
+        public int[] ReversePrint(ListNode head)
+        {
+            Stack<int> stack = new Stack<int>();
+            ListNode cur = head;
+            while (cur != null)
+            {
+                stack.Push(cur.val);
+                cur = cur.next;
+            }
+
+            return stack.ToArray();
+        }
+    }
+}
