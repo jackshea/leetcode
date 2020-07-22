a635ed4be56a96a5542744ed6b1d6de8ca8bf38e
谢在奎 <jackshea@163.com>
2020-07-22 09:58:54 +0800

    剑指 Offer 11. 旋转数组的最小数字

---
+++ LeetCode/Problems/Easy/Offer11.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 11. 旋转数组的最小数字
+    /// https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/
+    public class Offer11
+    {
+        public int MinArray(int[] numbers)
+        {
+            int left = 0, right = numbers.Length - 1;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (numbers[mid] < numbers[right])
+                {
+                    right = mid;
+                }
+                else if (numbers[mid] > numbers[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return numbers[left];
+        }
+    }
+}
