cac0813b9814dea8de95691720db80b93b015185
谢在奎 <jackshea@163.com>
2020-07-23 09:39:07 +0800

    剑指 Offer 18. 删除链表的节点

---
+++ LeetCode/Problems/Easy/Offer18.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 18. 删除链表的节点
+    /// https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+    public class Offer18
+    {
+        public ListNode DeleteNode(ListNode head, int val)
+        {
+            if (head.val == val)
+            {
+                return head.next;
+            }
+
+            ListNode cur = head;
+            while (cur.next != null)
+            {
+                if (cur.next.val == val)
+                {
+                    cur.next = cur.next.next;
+                    break;
+                }
+
+                cur = cur.next;
+            }
+
+            return head;
+        }
+    }
+}
