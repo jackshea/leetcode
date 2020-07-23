6a23239d8fe3fcaaec031ff6a793fa7ea292f58e
谢在奎 <jackshea@163.com>
2020-07-23 09:48:42 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

---
+++ LeetCode/Problems/Easy/Offer21.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+    /// https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+    public class Offer21
+    {
+        public int[] Exchange(int[] nums)
+        {
+            int left = 0, right = nums.Length - 1;
+            while (left < right)
+            {
+                while (left < right && nums[left] % 2 != 0)
+                {
+                    left++;
+                }
+
+                while (left < right && nums[right] % 2 == 0)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    int temp = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = temp;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
