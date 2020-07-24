933892310df6c442a1908016344a32038a934bf0
谢在奎 <jackshea@163.com>
2020-07-24 10:05:14 +0800

    剑指 Offer 27. 二叉树的镜像

---
+++ LeetCode/Problems/Easy/Offer27.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 27. 二叉树的镜像
+    /// https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
+    public class Offer27
+    {
+        public TreeNode MirrorTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            TreeNode temp = root.left;
+            root.left = MirrorTree(root.right);
+            root.right = MirrorTree(temp);
+            return root;
+        }
+    }
+}
