84c7df5f45951274f0a96752234ee761270b1816
谢在奎 <jackshea@163.com>
2020-07-24 09:46:02 +0800

    剑指 Offer 24. 反转链表

---
+++ LeetCode/Problems/Easy/Offer24.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 24. 反转链表
+    /// https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+    public class Offer24
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode pre = null;
+            ListNode cur = head;
+            while (cur != null)
+            {
+                ListNode next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+
+            return pre;
+        }
+    }
+}
