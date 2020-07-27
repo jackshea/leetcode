7c563951f703c97dff76b62a65d2115ab8d9b10f
谢在奎 <jackshea@163.com>
2020-07-27 10:09:07 +0800

    剑指 Offer 28. 对称的二叉树

---
+++ LeetCode/Problems/Easy/Offer28.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 28. 对称的二叉树
+    /// https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof/
+    public class Offer28
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            return root == null || IsMirror(root.left, root.right);
+        }
+
+        private bool IsMirror(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+            {
+                return true;
+            }
+
+            if (left == null || right == null || left.val != right.val)
+            {
+                return false;
+            }
+
+            return IsMirror(left.left, right.right) && IsMirror(left.right, right.left);
+        }
+    }
+}
