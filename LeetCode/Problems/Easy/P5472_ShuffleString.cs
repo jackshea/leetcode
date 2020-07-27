10ce48f7ce423846e973b1c0f7b301575324178e
谢在奎 <jackshea@163.com>
2020-07-27 09:59:01 +0800

    P5472 重新排列字符串

---
+++ LeetCode/Problems/Easy/P5472_ShuffleString.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 重新排列字符串
+    /// https://leetcode-cn.com/problems/shuffle-string/
+    public class P5472_ShuffleString
+    {
+        public string RestoreString(string s, int[] indices)
+        {
+            char[] ans = new char[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                ans[indices[i]] = s[i];
+            }
+            return new string(ans);
+        }
+    }
+}
