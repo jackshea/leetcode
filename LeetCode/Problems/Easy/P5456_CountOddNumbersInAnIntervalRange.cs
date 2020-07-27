0987c524c220a1c14d6e3bbb0553beea33fd5f0f
谢在奎 <jackshea@163.com>
2020-07-27 09:46:12 +0800

    P5456 在区间范围内统计奇数数目

---
+++ LeetCode/Problems/Easy/P5456_CountOddNumbersInAnIntervalRange.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 在区间范围内统计奇数数目
+    /// https://leetcode-cn.com/problems/count-odd-numbers-in-an-interval-range/
+    public class P5456_CountOddNumbersInAnIntervalRange
+    {
+        public int CountOdds(int low, int high)
+        {
+            int ans = 0;
+            int diff = high - low + 1;
+            ans = diff / 2;
+            if (diff % 2 != 0 && high % 2 != 0)
+            {
+                ans++;
+            }
+
+            return ans;
+        }
+    }
+}
