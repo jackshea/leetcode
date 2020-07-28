c94b7f50ecc8be5872e6527649e5c1d9775c4915
谢在奎 <jackshea@163.com>
2020-07-28 09:58:54 +0800

    剑指 Offer 39. 数组中出现次数超过一半的数字

---
+++ LeetCode/Problems/Easy/Offer39.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 39. 数组中出现次数超过一半的数字
+    /// https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/
+    public class Offer39
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int n = -1;
+            int count = 0;
+            foreach (var num in nums)
+            {
+                if (count == 0)
+                {
+                    n = num;
+                    count = 1;
+                    continue;
+                }
+
+                if (n != num)
+                {
+                    count--;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            return n;
+        }
+    }
+}
