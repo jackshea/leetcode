be5cfb520f640fd28a0ab52fa7be3652eb12b751
谢在奎 <jackshea@163.com>
2020-07-29 09:49:56 +0800

    剑指 Offer 42. 连续子数组的最大和

---
+++ LeetCode/Problems/Easy/Offer42.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 42. 连续子数组的最大和
+    /// https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+    public class Offer42
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int ans = int.MinValue, sum = 0;
+            foreach (var num in nums)
+            {
+                sum += num;
+                if (sum > ans)
+                {
+                    ans = sum;
+                }
+                if (sum < 0)
+                {
+                    sum = 0;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
