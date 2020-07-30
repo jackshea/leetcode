abe3c1f07ec5fad052501ce1363422d2a254764d
谢在奎 <jackshea@163.com>
2020-07-30 11:18:29 +0800

    剑指 Offer 54. 二叉搜索树的第k大节点

---
+++ LeetCode/Problems/Easy/Offer54.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 54. 二叉搜索树的第k大节点
+    /// https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+    public class Offer54
+    {
+        private int ans = 0;
+        private int count = 0;
+        private bool isEnd = false;
+        public int KthLargest(TreeNode root, int k)
+        {
+            DFS(root, k);
+            return ans;
+        }
+
+        private void DFS(TreeNode root, int k)
+        {
+            if (root == null || isEnd)
+            {
+                return;
+            }
+            DFS(root.right, k);
+            count++;
+            if (count == k)
+            {
+                ans = root.val;
+                isEnd = true;
+            }
+            DFS(root.left, k);
+        }
+    }
+}
