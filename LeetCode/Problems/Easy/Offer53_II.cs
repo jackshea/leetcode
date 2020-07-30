81bc60e93390784ea0b1463bf02a548e669d2fc0
谢在奎 <jackshea@163.com>
2020-07-30 11:10:26 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

--- LeetCode/Problems/Easy/Offer53_II.cs
+++ LeetCode/Problems/Easy/Offer53_II.cs
@@ -1,7 +1,36 @@
-﻿namespace LeetCode.Problems.Easy
+﻿using System.Linq;
+
+namespace LeetCode.Problems.Easy
 {
+    /// 剑指 Offer 53 - II. 0～n-1中缺失的数字
+    /// https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
     public class Offer53_II
     {
-        
+        public int MissingNumber(int[] nums)
+        {
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == mid)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left;
+        }
+
+        public int MissingNumber_1(int[] nums)
+        {
+            var sum = nums.Sum();
+            int n = nums.Length + 1;
+            int expectSum = (n - 1) * n / 2;
+            return expectSum - sum;
+        }
     }
 }
