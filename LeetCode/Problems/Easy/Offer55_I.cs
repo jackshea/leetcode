b5f7f983fd41d64e251b039310d66099bdc05c50
谢在奎 <jackshea@163.com>
2020-07-31 09:40:05 +0800

    剑指 Offer 55 - I. 二叉树的深度

---
+++ LeetCode/Problems/Easy/Offer55_I.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 55 - I. 二叉树的深度
+    /// https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+    public class Offer55_I
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            return root == null ? 0 : 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        }
+    }
+}
