db7bd8d63421294b98b94b532b3786aa4ccf0a3f
谢在奎 <jackshea@163.com>
2020-07-31 10:28:39 +0800

    剑指 Offer 57. 和为s的两个数字

---
+++ LeetCode/Problems/Easy/Offer57.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 57. 和为s的两个数字
+    /// https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+    public class Offer57
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+            while (left < right)
+            {
+                int sum = nums[left] + nums[right];
+                if (sum == target)
+                {
+                    break;
+                }
+                else if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return new[] { nums[left], nums[right] };
+        }
+    }
+}
