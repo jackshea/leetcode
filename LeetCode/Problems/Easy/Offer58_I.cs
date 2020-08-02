0e4c56fdec103fd5aa0ef628ccb40c14ae5018a5
谢在奎 <jackshea@163.com>
2020-08-02 22:30:33 +0800

    剑指 Offer 58 - I. 翻转单词顺序

---
+++ LeetCode/Problems/Easy/Offer58_I.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems.Easy
+{
+    public class Offer58_I
+    {
+        /// 剑指 Offer 58 - I. 翻转单词顺序
+        /// https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof/
+        public string ReverseWords(string s)
+        {
+            var split = s.Split(' ');
+            StringBuilder sb = new StringBuilder();
+            for (var i = split.Length - 1; i >= 0; i--)
+            {
+                var word = split[i];
+                if (word.Length == 0)
+                {
+                    continue;
+                }
+                if (sb.Length != 0)
+                {
+                    sb.Append(" ");
+                }
+                sb.Append(word);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
