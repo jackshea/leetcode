f3c4d6420e576919cfb9a4c0e3fed3512156d999
谢在奎 <jackshea@163.com>
2020-08-03 09:51:57 +0800

    剑指 Offer 58 - II. 左旋转字符串

---
+++ LeetCode/Problems/Easy/Offer58_II.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 58 - II. 左旋转字符串
+    /// https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+    public class Offer58_II
+    {
+        public string ReverseLeftWords(string s, int n)
+        {
+            string left = s.Substring(0, n);
+            string right = s.Substring(n, s.Length - n);
+            return right + left;
+        }
+    }
+}
