e2cf97454d3e99f1efdf20a9a12fe095e8b818e1
谢在奎 <jackshea@163.com>
2020-08-04 09:42:36 +0800

    P1534 统计好三元组

---
+++ LeetCode/Problems/Easy/P1534_CountGoodTriplets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 统计好三元组
+    /// https://leetcode-cn.com/problems/count-good-triplets/
+    public class P1534_CountGoodTriplets
+    {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            int ans = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        if (Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b && Math.Abs(arr[i] - arr[k]) <= c)
+                        {
+                            ans++;
+                        }
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
