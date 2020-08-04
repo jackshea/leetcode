f525cdcfad1a2877404a1a0ff61655d12fcae7be
谢在奎 <jackshea@163.com>
2020-08-04 10:07:58 +0800

    剑指 Offer 65. 不用加减乘除做加法

---
+++ LeetCode/Problems/Easy/Offer65.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 剑指 Offer 65. 不用加减乘除做加法
+    /// https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/
+    public class Offer65
+    {
+        public int Add(int a, int b)
+        {
+            int c = a ^ b;
+            int carry = (a & b) << 1;
+            return carry != 0 ? Add(c, carry) : c;
+        }
+    }
+}
