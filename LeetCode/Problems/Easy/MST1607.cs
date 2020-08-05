feb8d0c4459f44c8e926518173382de09a8302f9
谢在奎 <jackshea@163.com>
2020-08-05 10:13:41 +0800

    面试题 16.07. 最大数值

---
+++ LeetCode/Problems/Easy/MST1607.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 面试题 16.07. 最大数值
+    /// https://leetcode-cn.com/problems/maximum-lcci/
+    public class MST1607
+    {
+        public int Maximum(int a, int b)
+        {
+            long x = (long)a - (long)b;
+            int k = (int)(x >> 63);
+
+            return (1 + k) * a - b * k;
+        }
+    }
+}
