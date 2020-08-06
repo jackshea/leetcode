aaa21d6049ea2eacc449adc7c6e21206cd1282c5
谢在奎 <jackshea@163.com>
2020-08-06 10:20:28 +0800

    面试题 17.12. BiNode

---
+++ LeetCode/Problems/Easy/MST1712.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 面试题 17.12. BiNode
+    /// https://leetcode-cn.com/problems/binode-lcci/
+    public class MST1712
+    {
+        public TreeNode ConvertBiNode(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            TreeNode left = ConvertBiNode(root.left);
+            if (left == null)
+            {
+                left = root;
+            }
+            else
+            {
+                TreeNode cur = left;
+                while (cur.right != null)
+                {
+                    cur = cur.right;
+                }
+
+                cur.right = root;
+            }
+
+            root.left = null;
+            root.right = ConvertBiNode(root.right);
+            return left;
+        }
+    }
+}
