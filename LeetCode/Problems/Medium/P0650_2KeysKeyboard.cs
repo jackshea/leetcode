4dc24bb07abaae23daa8d8d73363523d4664f22e
谢在奎 <jackshea@163.com>
2020-08-10 09:55:28 +0800

    P0650 只有两个键的键盘

---
+++ LeetCode/Problems/Medium/P0650_2KeysKeyboard.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 只有两个键的键盘
+    /// https://leetcode-cn.com/problems/2-keys-keyboard/
+    public class P0650_2KeysKeyboard
+    {
+        public int MinSteps(int n)
+        {
+            int ans = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    ans += i;
+                    n /= i;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
