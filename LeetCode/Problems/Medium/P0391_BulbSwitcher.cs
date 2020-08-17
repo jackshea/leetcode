e65fab8d1a22117e853fac48a0a66d631f7cec71
谢在奎 <jackshea@163.com>
2020-08-17 10:03:18 +0800

    P0319 灯泡开关

---
+++ LeetCode/Problems/Medium/P0391_BulbSwitcher.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 灯泡开关
+    /// https://leetcode-cn.com/problems/bulb-switcher/
+    public class P0391_BulbSwitcher
+    {
+        public int BulbSwitch(int n)
+        {
+            int ans = 0;
+            for (int i = 1; i * i <= n; i++)
+            {
+                ans++;
+            }
+
+            return ans;
+        }
+    }
+}
