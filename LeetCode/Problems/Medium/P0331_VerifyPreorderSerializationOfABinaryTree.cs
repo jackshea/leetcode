44ac5e07a812b49750cf4d0c8130515a2d3f6799
谢在奎 <jackshea@163.com>
2020-08-21 10:22:54 +0800

    P0331 验证二叉树的前序序列化

---
+++ LeetCode/Problems/Medium/P0331_VerifyPreorderSerializationOfABinaryTree.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 验证二叉树的前序序列化
+    /// https://leetcode-cn.com/problems/verify-preorder-serialization-of-a-binary-tree/
+    public class P0331_VerifyPreorderSerializationOfABinaryTree
+    {
+        public bool IsValidSerialization(string preorder)
+        {
+            var nodes = preorder.Split(',');
+            int slot = 1;
+            foreach (var node in nodes)
+            {
+                slot--;
+                if (slot < 0)
+                {
+                    return false;
+                }
+
+                if (node != "#")
+                {
+                    slot += 2;
+                }
+            }
+
+            return slot == 0;
+        }
+    }
+}
