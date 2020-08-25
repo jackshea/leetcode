04fcd06c61fd9961f26b31d66dc2e8c47a7cd29d
谢在奎 <jackshea@163.com>
2020-08-25 09:58:01 +0800

    P0491 递增子序列

---
+++ LeetCode/Problems/Medium/P0491_IncreasingSubsequences.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 递增子序列
+    /// https://leetcode-cn.com/problems/increasing-subsequences/
+    public class P0491_IncreasingSubsequences
+    {
+        private IList<IList<int>> ans = new List<IList<int>>();
+        private int n;
+        private int[] nums;
+
+        public IList<IList<int>> FindSubsequences(int[] nums)
+        {
+            this.nums = nums;
+            n = nums.Length;
+            DFS(0, int.MinValue, new List<int>());
+            return ans;
+        }
+
+        private void DFS(int start, int last, IList<int> row)
+        {
+            if (start >= n)
+            {
+                if (row.Count >= 2)
+                {
+                    ans.Add(new List<int>(row));
+                }
+
+                return;
+            }
+
+            if (nums[start] >= last)
+            {
+                row.Add(nums[start]);
+                DFS(start + 1, nums[start], row);
+                row.RemoveAt(row.Count - 1);
+            }
+
+            if (nums[start] != last)
+            {
+                DFS(start + 1, last, row);
+            }
+        }
+    }
+}
