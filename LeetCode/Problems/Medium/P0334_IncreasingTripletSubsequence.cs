eab137c74f7e6d62444f0bbf27e520da34cbf1ae
谢在奎 <jackshea@163.com>
2020-08-26 10:03:18 +0800

    P0334 递增的三元子序列

---
+++ LeetCode/Problems/Medium/P0334_IncreasingTripletSubsequence.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 递增的三元子序列
+    /// https://leetcode-cn.com/problems/increasing-triplet-subsequence/
+    public class P0334_IncreasingTripletSubsequence
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            int a = int.MaxValue, b = int.MaxValue;
+            foreach (var num in nums)
+            {
+                if (num <= a)
+                {
+                    a = num;
+                }
+                else if (num <= b)
+                {
+                    b = num;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
