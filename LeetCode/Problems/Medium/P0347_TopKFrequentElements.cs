a9ea87b8b7f9d23dfd06826778b1c5621523c4e2
谢在奎 <jackshea@163.com>
2020-08-28 10:17:07 +0800

    P0347 前 K 个高频元素

---
+++ LeetCode/Problems/Medium/P0347_TopKFrequentElements.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 前 K 个高频元素
+    /// https://leetcode-cn.com/problems/top-k-frequent-elements/
+    public class P0347_TopKFrequentElements
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> frequent = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                frequent.TryGetValue(num, out var count);
+                frequent[num] = count + 1;
+            }
+
+            var keyList = frequent.Keys.ToList();
+            keyList.Sort((a, b) => frequent[b].CompareTo(frequent[a]));
+            return keyList.Take(k).ToArray();
+        }
+    }
+}
