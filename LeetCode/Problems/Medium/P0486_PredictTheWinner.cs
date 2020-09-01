1cae5bf77d1d303d4fe4eafb03ff80c4f2959ab0
谢在奎 <jackshea@163.com>
2020-09-01 10:33:22 +0800

    P0486 预测赢家

---
+++ LeetCode/Problems/Medium/P0486_PredictTheWinner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 预测赢家
+    /// https://leetcode-cn.com/problems/predict-the-winner/
+    public class P0486_PredictTheWinner
+    {
+        public bool PredictTheWinner(int[] nums)
+        {
+            var n = nums.Length;
+            int[,] dp = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                dp[i, i] = nums[i];
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    dp[i, j] = Math.Max(nums[i] - dp[i + 1, j], nums[j] - dp[i, j - 1]);
+                }
+            }
+
+            return dp[0, n - 1] >= 0;
+        }
+    }
+}
