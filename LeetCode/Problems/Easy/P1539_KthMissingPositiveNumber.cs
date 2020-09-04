729c506ddda4e8fcef47dd57c5ce1ceb695db7a2
谢在奎 <jackshea@163.com>
2020-09-04 10:59:57 +0800

    P1539 第 k 个缺失的正整数

---
+++ LeetCode/Problems/Easy/P1539_KthMissingPositiveNumber.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 第 k 个缺失的正整数
+    /// https://leetcode-cn.com/problems/kth-missing-positive-number/
+    public class P1539_KthMissingPositiveNumber
+    {
+        public int FindKthPositive(int[] arr, int k)
+        {
+            if (arr[0] > k)
+            {
+                return k;
+            }
+
+            int left = 0, right = arr.Length;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                int v = int.MaxValue;
+                if (mid < arr.Length)
+                {
+                    v = arr[mid];
+                }
+
+                if (v - mid - 1 >= k)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return k + left;
+        }
+    }
+}
