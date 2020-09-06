f625154edb8256e7d3a0c9cfdca75f92f9d8baf0
谢在奎 <jackshea@163.com>
2020-09-06 14:08:18 +0800

    P1550 存在连续三个奇数的数组

---
+++ LeetCode/Problems/Easy/P1550_ThreeConsecutiveOdds.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 存在连续三个奇数的数组
+    /// https://leetcode-cn.com/problems/three-consecutive-odds/
+    public class P1550_ThreeConsecutiveOdds
+    {
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            int counter = 0;
+            foreach (var num in arr)
+            {
+                if (num % 2 == 0)
+                {
+                    counter = 0;
+                }
+                else
+                {
+                    counter++;
+                    if (counter >= 3)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
