b491e77bd8c0cf4bdabc1af9bc525998d780564f
谢在奎 <jackshea@163.com>
2020-09-09 10:17:09 +0800

    P0375 猜数字大小 II

---
+++ LeetCode/Problems/Medium/P0375_GuessNumberHigherOrLowerII.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 猜数字大小 II
+    /// https://leetcode-cn.com/problems/guess-number-higher-or-lower-ii/
+    public class P0375_GuessNumberHigherOrLowerII
+    {
+        private int[,] dp;
+        public int GetMoneyAmount(int n)
+        {
+            dp = new int[n + 1, n + 1];
+            for (var i = 0; i < dp.GetLength(0); i++)
+            {
+                for (int j = 0; j < dp.GetLength(1); j++)
+                {
+                    dp[i, j] = -1;
+                }
+            }
+
+            return calc(1, n);
+        }
+
+        private int calc(int low, int high)
+        {
+            if (low >= high)
+            {
+                return 0;
+            }
+
+            var ans = dp[low, high];
+            if (ans != -1)
+            {
+                return ans;
+            }
+
+            ans = int.MaxValue;
+            for (int i = (low + high) / 2; i <= high; i++)
+            {
+                int temp = i + Math.Max(calc(low, i - 1), calc(i + 1, high));
+                if (temp < ans)
+                {
+                    ans = temp;
+                }
+            }
+            dp[low, high] = ans;
+            return ans;
+        }
+    }
+}
