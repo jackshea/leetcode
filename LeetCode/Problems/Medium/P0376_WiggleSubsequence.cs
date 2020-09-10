767900943be1c696b913436c06a2788f80463d3a
谢在奎 <jackshea@163.com>
2020-09-10 10:15:04 +0800

    P0376 摆动序列

--- LeetCode/Problems/Medium/P0376_WiggleSubsequence.cs
+++ LeetCode/Problems/Medium/P0376_WiggleSubsequence.cs
@@ -8,6 +8,11 @@ namespace LeetCode.Problems.Medium
     {
         public int WiggleMaxLength(int[] nums)
         {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
             int up = 1, down = 1;
             for (int i = 1; i < nums.Length; i++)
             {
