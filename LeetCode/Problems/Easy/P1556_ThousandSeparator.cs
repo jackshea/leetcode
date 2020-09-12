30563e038d201d249bb97522be2a7620dd344d04
谢在奎 <jackshea@163.com>
2020-09-12 22:32:19 +0800

    P1556 千位分隔数

---
+++ LeetCode/Problems/Easy/P1556_ThousandSeparator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 千位分隔数
+    /// https://leetcode-cn.com/problems/thousand-separator/
+    public class P1556_ThousandSeparator
+    {
+        public string ThousandSeparator(int n)
+        {
+            if (n == 0)
+            {
+                return "0";
+            }
+            Stack<char> stack = new Stack<char>();
+            int count = 0;
+            do
+            {
+                var c = (char)('0' + n % 10);
+                n /= 10;
+                stack.Push(c);
+                if (++count % 3 == 0 && n != 0)
+                {
+                    stack.Push('.');
+                }
+            } while (n != 0);
+            return new string(stack.ToArray());
+        }
+    }
+}
