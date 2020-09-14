123e51cf651c8d07a9bea5cb1e05b1b40f20e25a
谢在奎 <jackshea@163.com>
2020-09-14 10:38:13 +0800

    P0380 常数时间插入、删除和获取随机元素

---
+++ LeetCode/Problems/Medium/P0380_InsertDeleteGetrandomO1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 常数时间插入、删除和获取随机元素
+    /// https://leetcode-cn.com/problems/insert-delete-getrandom-o1/
+    public class RandomizedSet
+    {
+        private Dictionary<int, int> dic = new Dictionary<int, int>();
+        private List<int> keys = new List<int>();
+
+        private Random random;
+
+        /** Initialize your data structure here. */
+        public RandomizedSet()
+        {
+            random = new Random();
+        }
+
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+        public bool Insert(int val)
+        {
+            if (!dic.ContainsKey(val))
+            {
+                dic.Add(val, keys.Count);
+                keys.Add(val);
+                return true;
+            }
+
+            return false;
+        }
+
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
+        public bool Remove(int val)
+        {
+            if (dic.ContainsKey(val))
+            {
+                keys[dic[val]] = keys[keys.Count - 1];
+                dic[keys[keys.Count - 1]] = dic[val];
+                keys.RemoveAt(keys.Count - 1);
+                dic.Remove(val);
+                return true;
+            }
+
+            return false;
+        }
+
+        /** Get a random element from the set. */
+        public int GetRandom()
+        {
+            var randomIndex = random.Next(keys.Count);
+            return keys[randomIndex];
+        }
+    }
+}
