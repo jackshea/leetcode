083354eb6117e775b1cf53fbbf0d3996f2fa9cd8
谢在奎 <jackshea@163.com>
2020-09-19 23:57:04 +0800

    P1566 重复至少 K 次且长度为 M 的模式

---
+++ LeetCode/Problems/Easy/P1566_DetectPatternOfLengthMRepeatedKOrMoreTimes.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 重复至少 K 次且长度为 M 的模式
+    /// https://leetcode-cn.com/problems/detect-pattern-of-length-m-repeated-k-or-more-times/
+    public class P1566_DetectPatternOfLengthMRepeatedKOrMoreTimes
+    {
+        public bool ContainsPattern(int[] arr, int m, int k)
+        {
+            for (int i = 0; i <= arr.Length - m * k; i++)
+            {
+                int j = i + m;
+                for (; j < i + m * k; j++)
+                {
+                    if (arr[j] != arr[j - m])
+                    {
+                        break;
+                    }
+                }
+
+                if (j == i + m * k)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
