174f437823e64e5c3fda05eb37cb5a19b433581c
谢在奎 <jackshea@163.com>
2020-09-21 10:08:58 +0800

    P0386 字典序排数

---
+++ LeetCode/Problems/Medium/P0386_LexicographicalNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 字典序排数
+    /// https://leetcode-cn.com/problems/lexicographical-numbers/
+    public class P0386_LexicographicalNumbers
+    {
+        private List<int> ans = new List<int>();
+        private int max;
+
+        public IList<int> LexicalOrder(int n)
+        {
+            max = n;
+            DFS(0);
+            return ans;
+        }
+
+        public void DFS(int start)
+        {
+            if (start > max)
+            {
+                return;
+            }
+
+            if (start > 0)
+            {
+                ans.Add(start);
+            }
+
+            start *= 10;
+            for (int i = 0; i <= 9; i++)
+            {
+                if (start + i == 0)
+                {
+                    continue;
+                }
+
+                DFS(start + i);
+            }
+        }
+    }
+}
