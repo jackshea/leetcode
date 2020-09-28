68f4293638f79081148235917ce1dd6be1c44e7e
谢在奎 <jackshea@163.com>
2020-09-28 09:52:03 +0800

    P0396 旋转函数

---
+++ LeetCode/Problems/Medium/P0396_RotateFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 旋转函数
+    /// https://leetcode-cn.com/problems/rotate-function/
+    public class P0396_RotateFunction
+    {
+        public int MaxRotateFunction(int[] A)
+        {
+            int sum = 0;
+            int f = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                sum += A[i];
+                f += i * A[i];
+            }
+
+            int ans = 0;
+            for (int i = 1; i < A.Length; i++)
+            {
+                f += sum - A.Length * A[A.Length - i];
+                ans = Math.Max(ans, f);
+            }
+
+            return ans;
+        }
+    }
+}
