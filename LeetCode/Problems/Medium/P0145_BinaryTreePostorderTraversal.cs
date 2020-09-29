cc524cc34bcf595085b634996d974100a9bec26b
谢在奎 <jackshea@163.com>
2020-09-29 10:01:12 +0800

    P0145 二叉树的后序遍历
    递归

---
+++ LeetCode/Problems/Medium/P0145_BinaryTreePostorderTraversal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 二叉树的后序遍历
+    /// https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+    public class P0145_BinaryTreePostorderTraversal
+    {
+        private IList<int> ans = new List<int>();
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            Traversal(root);
+            return ans;
+        }
+
+        public void Traversal(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Traversal(node.left);
+            Traversal(node.right);
+            ans.Add(node.val);
+        }
+    }
+}
