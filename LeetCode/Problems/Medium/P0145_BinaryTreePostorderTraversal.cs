55f230c4f79eb1602c1b915ed07843eb4c16e729
谢在奎 <jackshea@163.com>
2020-09-29 10:51:10 +0800

    P0145 二叉树的后序遍历
    非递归

--- LeetCode/Problems/Medium/P0145_BinaryTreePostorderTraversal.cs
+++ LeetCode/Problems/Medium/P0145_BinaryTreePostorderTraversal.cs
@@ -7,6 +7,46 @@ namespace LeetCode.Problems.Medium
     /// https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
     public class P0145_BinaryTreePostorderTraversal
     {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            IList<int> ans = new List<int>();
+            if (root == null)
+            {
+                return ans;
+            }
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode pre = null;
+            TreeNode cur = root;
+            while (cur != null || stack.Count != 0)
+            {
+                while (cur != null)
+                {
+                    stack.Push(cur);
+                    cur = cur.left;
+                }
+
+                cur = stack.Pop();
+                if (cur.right == null || cur.right == pre)
+                {
+                    ans.Add(cur.val);
+                    pre = cur;
+                    cur = null;
+                }
+                else
+                {
+                    stack.Push(cur);
+                    cur = cur.right;
+                }
+            }
+
+            return ans;
+        }
+    }
+
+
+    // 递归写法
+    public class P0145_BinaryTreePostorderTraversal1
+    {
         private IList<int> ans = new List<int>();
         public IList<int> PostorderTraversal(TreeNode root)
         {
