95b544fa04e8b11eadf0c750eedfdbe3bf1cb878
谢在奎 <jackshea@163.com>
2020-09-30 10:06:35 +0800

    P0701 二叉搜索树中的插入操作

---
+++ LeetCode/Problems/Medium/P0701_InsertIntoABinarySearchTree.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 二叉搜索树中的插入操作
+    /// https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
+    public class P0701_InsertIntoABinarySearchTree
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return new TreeNode(val);
+            }
+
+            if (val < root.val)
+            {
+                root.left = InsertIntoBST(root.left, val);
+            }
+            else
+            {
+                root.right = InsertIntoBST(root.right, val);
+            }
+
+            return root;
+        }
+    }
+}
