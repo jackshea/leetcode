9cd299c785a6446ac51f915c8d6e3035873c77af
谢在奎 <jackshea@163.com>
2020-10-03 13:44:29 +0800

    P0397 整数替换

---
+++ LeetCode/Problems/Medium/P0397_IntegerReplacement.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 整数替换
+    /// https://leetcode-cn.com/problems/integer-replacement/
+    public class P0397_IntegerReplacement
+    {
+        public int IntegerReplacement(int n)
+        {
+            int ans = 0;
+            int mark = 0;
+            while (n > 1)
+            {
+                if ((n & 1) == 0)
+                {
+                    n >>= 1;
+                }
+                else
+                {
+                    if (((n + 1) & 0b11) == 0 && n != 3)
+                    {
+                        n >>= 1;
+                        n++;
+                        ans++;
+                    }
+                    else
+                    {
+                        n--;
+                    }
+                }
+
+                ans++;
+            }
+
+            return ans;
+        }
+    }
+}
