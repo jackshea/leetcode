80d83447d0b97f7150372096af0b76c23de619a3
谢在奎 <jackshea@163.com>
2020-10-04 18:51:29 +0800

    P0398 随机数索引

---
+++ LeetCode/Problems/Medium/P0398_RandomPickIndex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium.P0398
+{
+    /// 随机数索引
+    /// https://leetcode-cn.com/problems/random-pick-index/
+    public class Solution
+    {
+        private int[] nums;
+
+        public Solution(int[] nums)
+        {
+            this.nums = nums;
+        }
+
+        public int Pick(int target)
+        {
+            int count = 0;
+            int ans = 0;
+            var random = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    count++;
+                    if (random.Next(count) == 0)
+                    {
+                        ans = i;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
