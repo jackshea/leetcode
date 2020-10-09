054cf93334439b25df3f065ce3f6107a963a028f
谢在奎 <jackshea@163.com>
2020-10-09 10:34:07 +0800

    P0406 根据身高重建队列

---
+++ LeetCode/Problems/Medium/P0406_QueueReconstructionByHeight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 根据身高重建队列
+    /// https://leetcode-cn.com/problems/queue-reconstruction-by-height/
+    public class P0406_QueueReconstructionByHeight
+    {
+        public int[][] ReconstructQueue(int[][] people)
+        {
+            Array.Sort(people, (a, b) =>
+            {
+                if (a[0] == b[0])
+                {
+                    return a[1].CompareTo(b[1]);
+                }
+
+                return -a[0].CompareTo(b[0]);
+            });
+
+            List<int[]> ans = new List<int[]>(people.Length);
+            foreach (var p in people)
+            {
+                ans.Insert(p[1], p);
+            }
+
+            return ans.ToArray();
+        }
+    }
+}
