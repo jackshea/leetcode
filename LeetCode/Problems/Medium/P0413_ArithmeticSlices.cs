eb7ef3aab5dcfe8a5c2b80cba0080c2effa7acdb
谢在奎 <jackshea@163.com>
2020-10-10 10:19:58 +0800

    P0413 等差数列划分

---
+++ LeetCode/Problems/Medium/P0413_ArithmeticSlices.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 等差数列划分
+    /// https://leetcode-cn.com/problems/arithmetic-slices/
+    public class P0413_ArithmeticSlices
+    {
+        public int NumberOfArithmeticSlices(int[] A)
+        {
+            int ans = 0;
+            int dp = 0;
+            for (int i = 2; i < A.Length; i++)
+            {
+                if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
+                {
+                    dp++;
+                    ans += dp;
+                }
+                else
+                {
+                    dp = 0;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
