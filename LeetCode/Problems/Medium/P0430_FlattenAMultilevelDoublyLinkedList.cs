17e91a5445fd77572107b3842c03d88d3bd50a4f
谢在奎 <jackshea@163.com>
2020-10-16 10:34:35 +0800

    P0430 扁平化多级双向链表

---
+++ LeetCode/Problems/Medium/P0430_FlattenAMultilevelDoublyLinkedList.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 扁平化多级双向链表
+    /// https://leetcode-cn.com/problems/flatten-a-multilevel-doubly-linked-list/
+    public class P0430_FlattenAMultilevelDoublyLinkedList
+    {
+        private Node tail;
+        public Node Flatten(Node head)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            Node cur = head;
+            while (cur != null)
+            {
+                tail = cur;
+                if (cur.child != null)
+                {
+                    var next = cur.next;
+                    var child = Flatten(cur.child);
+                    cur.next = child;
+                    child.prev = cur;
+                    tail.next = next;
+                    if (next != null)
+                    {
+                        next.prev = tail;
+                    }
+                }
+
+                cur.child = null;
+                cur = cur.next;
+            }
+
+            return head;
+        }
+
+        public class Node
+        {
+            public int val;
+            public Node prev;
+            public Node next;
+            public Node child;
+        }
+    }
+}
