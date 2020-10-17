a91688324e80dfbc56440aaeadce89326e7be37b
谢在奎 <jackshea@163.com>
2020-10-17 23:32:36 +0800

    第 37 场双周赛

---
+++ LeetCode/Problems/Easy/P5122.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    public class P5122
+    {
+        public double TrimMean(int[] arr)
+        {
+            Array.Sort(arr);
+            var topn = (int)(arr.Length * 0.05);
+            int sum = 0;
+            for (int i = topn; i < arr.Length - topn; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum * 1.0 / (arr.Length - 2 * topn);
+        }
+    }
+}
---
+++ LeetCode/Problems/Medium/P5528.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    public class P5528
+    {
+        public int[] BestCoordinate(int[][] towers, int radius)
+        {
+            int rr = radius * radius;
+            int max = 0;
+            int cur = 0;
+            int ansX = 0, ansY = 0;
+            for (int i = 0; i <= 50; i++)
+            {
+                for (int j = 0; j <= 50; j++)
+                {
+                    cur = 0;
+                    foreach (var tower in towers)
+                    {
+                        int x = tower[0];
+                        int y = tower[1];
+                        int q = tower[2];
+                        var dd = (x - i) * (x - i) + (y - j) * (y - j);
+                        if (dd <= rr)
+                        {
+                            cur += (int)(q / (1 + Math.Sqrt(dd)));
+                        }
+                    }
+                    if (max < cur)
+                    {
+                        max = cur;
+                        ansX = i;
+                        ansY = j;
+                    }
+                }
+            }
+            return new[] { ansX, ansY };
+        }
+    }
+}
