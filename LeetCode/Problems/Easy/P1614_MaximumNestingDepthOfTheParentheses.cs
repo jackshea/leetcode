fb39c3c3dae7851d3bedd42fd20eb957ec44c5b3
谢在奎 <jackshea@163.com>
2020-10-17 10:03:28 +0800

    P1614 括号的最大嵌套深度

---
+++ LeetCode/Problems/Easy/P1614_MaximumNestingDepthOfTheParentheses.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 括号的最大嵌套深度
+    /// https://leetcode-cn.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
+    public class P1614_MaximumNestingDepthOfTheParentheses
+    {
+        public int MaxDepth(string s)
+        {
+            int ans = 0;
+            int cur = 0;
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    cur++;
+                    ans = Math.Max(ans, cur);
+                }
+
+                if (c == ')')
+                {
+                    cur--;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
