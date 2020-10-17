5e63be8afda605011f27cb9a5fcade3d4eea3743
谢在奎 <jackshea@163.com>
2020-10-17 11:47:35 +0800

    P1616 分割两个字符串得到回文串

---
+++ LeetCode/Problems/Medium/P1616_SplitTwoStringsToMakePalindrome.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 分割两个字符串得到回文串
+    /// https://leetcode-cn.com/problems/split-two-strings-to-make-palindrome/
+    public class P1616_SplitTwoStringsToMakePalindrome
+    {
+        public bool CheckPalindromeFormation(string a, string b)
+        {
+            return Check(a, b) || Check(b, a);
+        }
+
+        private bool IsPalindrome(string a, int left, int right)
+        {
+            while (left < right)
+            {
+                if (a[left] != a[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        private bool Check(string a, string b)
+        {
+            int left = 0, right = a.Length - 1;
+            while (left < right)
+            {
+                if (a[left] != b[right])
+                {
+                    return IsPalindrome(a, left, right) || IsPalindrome(b, left, right);
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
