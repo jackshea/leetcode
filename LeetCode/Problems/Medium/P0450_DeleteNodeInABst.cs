d2da7e899a96d6f33d4c08e1064cbdfe236e047c
谢在奎 <jackshea@163.com>
2020-10-26 10:28:10 +0800

    P450 删除二叉搜索树中的节点

---
+++ LeetCode/Problems/Medium/P0450_DeleteNodeInABst.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 删除二叉搜索树中的节点
+    /// https://leetcode-cn.com/problems/delete-node-in-a-bst/
+    public class P0450_DeleteNodeInABst
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            if (root.val == key)
+            {
+                if (root.left == null)
+                {
+                    return root.right;
+                }
+
+                if (root.right == null)
+                {
+                    return root.left;
+                }
+
+                TreeNode cur = root.left;
+                TreeNode pre = null;
+                while (cur.right != null)
+                {
+                    pre = cur;
+                    cur = cur.right;
+                }
+
+                if (pre != null)
+                {
+                    pre.right = cur.left;
+                    cur.left = root.left;
+                    cur.right = root.right;
+                }
+                else
+                {
+                    cur.right = root.right;
+                }
+
+                return cur;
+            }
+
+            if (key < root.val)
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+            else
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+
+            return root;
+        }
+    }
+}
