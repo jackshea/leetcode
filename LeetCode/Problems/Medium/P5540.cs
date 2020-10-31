488a44b696d3f78d7f513bdc75cbd3aa598f3046
谢在奎 <jackshea@163.com>
2020-10-31 23:53:39 +0800

    P5540 两点之间不包含任何点的最宽垂直面积

---
+++ LeetCode/Problems/Medium/P5540.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 两点之间不包含任何点的最宽垂直面积
+    /// https://leetcode-cn.com/contest/biweekly-contest-38/problems/widest-vertical-area-between-two-points-containing-no-points/
+    public class P5540
+    {
+        public int MaxWidthOfVerticalArea(int[][] points)
+        {
+            Array.Sort(points, (a, b) => a[0].CompareTo(b[0]));
+            int maxDiff = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                var diff = points[i][0] - points[i - 1][0];
+                if (diff > maxDiff)
+                {
+                    maxDiff = diff;
+                }
+            }
+
+            return maxDiff;
+        }
+    }
+}
