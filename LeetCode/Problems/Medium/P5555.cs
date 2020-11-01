a85233baa5aab9d89d9a7221d1324d030018dbdf
谢在奎 <jackshea@163.com>
2020-11-01 11:12:58 +0800

    P5555 统计字典序元音字符串的数目

---
+++ LeetCode/Problems/Medium/P5555.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 统计字典序元音字符串的数目
+    /// https://leetcode-cn.com/contest/weekly-contest-213/problems/count-sorted-vowel-strings/
+    public class P5555
+    {
+        public int CountVowelStrings(int n)
+        {
+            int[] dp = new[] { 1, 1, 1, 1, 1 };
+            int sum = 5;
+            for (int i = 2; i <= n; i++)
+            {
+                sum = 1;
+                for (int j = 1; j < dp.Length; j++)
+                {
+                    dp[j] += dp[j - 1];
+                    sum += dp[j];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
