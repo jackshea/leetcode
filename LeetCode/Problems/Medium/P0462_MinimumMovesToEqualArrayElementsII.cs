c9f2a2b61d58900fb6c321399dfbe7e0633ed1d6
谢在奎 <jackshea@163.com>
2020-11-02 09:47:54 +0800

    P0462 最少移动次数使数组元素相等 II

---
+++ LeetCode/Problems/Medium/P0462_MinimumMovesToEqualArrayElementsII.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 最少移动次数使数组元素相等 II
+    /// https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements-ii/
+    public class P0462_MinimumMovesToEqualArrayElementsII
+    {
+        public int MinMoves2(int[] nums)
+        {
+            Array.Sort(nums);
+            int mid = nums[nums.Length / 2];
+            int ans = 0;
+            foreach (var num in nums)
+            {
+                ans += Math.Abs(num - mid);
+            }
+
+            return ans;
+        }
+    }
+}
