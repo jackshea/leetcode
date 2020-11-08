09375b155dc3bd9721f63eb6daccdc5ff2b77021
谢在奎 <jackshea@163.com>
2020-11-08 22:24:53 +0800

    P1592 重新排列单词间的空格

---
+++ LeetCode/Problems/Easy/P1592_RearrangeSpacesBetweenWords.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 重新排列单词间的空格
+    /// https://leetcode-cn.com/problems/rearrange-spaces-between-words/
+    public class P1592_RearrangeSpacesBetweenWords
+    {
+        public string ReorderSpaces(string text)
+        {
+            int spaceCount = 0;
+            foreach (var c in text)
+            {
+                if (c == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+
+            var words = text.Split(' ');
+            var wordsCount = words.Count(p => !string.IsNullOrWhiteSpace(p));
+            int space = 0;
+            if (wordsCount > 1)
+            {
+                space = (spaceCount) / (wordsCount - 1);
+            }
+            StringBuilder sb = new StringBuilder();
+            bool first = true;
+            foreach (var word in words)
+            {
+                if (string.IsNullOrWhiteSpace(word))
+                {
+                    continue;
+                }
+
+                if (!first)
+                {
+                    sb.Append(' ', space);
+                }
+
+                first = false;
+                sb.Append(word);
+            }
+
+            sb.Append(' ', spaceCount - space * (wordsCount - 1));
+            return sb.ToString();
+        }
+    }
+}
