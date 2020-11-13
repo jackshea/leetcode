fca81bcdeda345135a2d90d316d2639453321634
谢在奎 <jackshea@163.com>
2020-11-13 11:19:31 +0800

    P0473 火柴拼正方形

---
+++ LeetCode/Problems/Medium/P0473_MatchsticksToSquare.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 火柴拼正方形
+    /// https://leetcode-cn.com/problems/matchsticks-to-square/
+    public class P0473_MatchsticksToSquare
+    {
+        public bool Makesquare(int[] nums)
+        {
+            if (nums.Length < 4)
+            {
+                return false;
+            }
+            var sum = nums.Sum();
+            if (sum == 0 || sum % 4 != 0)
+            {
+                return false;
+            }
+
+            int[] sides = new int[4];
+            var sideLen = sum / 4;
+            Array.Sort(nums, (a, b) => -a.CompareTo(b));
+            return Backtracking(nums, sides, 0, sideLen);
+        }
+
+        private bool Backtracking(int[] nums, int[] sides, int startIndex, int maxSideLen)
+        {
+            if (startIndex == nums.Length)
+            {
+                foreach (var side in sides)
+                {
+                    if (side != maxSideLen)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (sides[i] + nums[startIndex] > maxSideLen)
+                {
+                    continue;
+                }
+
+                sides[i] += nums[startIndex];
+                if (Backtracking(nums, sides, startIndex + 1, maxSideLen))
+                {
+                    return true;
+                }
+                sides[i] -= nums[startIndex];
+            }
+
+            return false;
+        }
+    }
+}
