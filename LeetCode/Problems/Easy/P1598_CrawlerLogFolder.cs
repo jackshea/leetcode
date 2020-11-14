fa3aba3b130cace6ca198dee395762f530e3001d
谢在奎 <jackshea@163.com>
2020-11-14 21:27:38 +0800

    P1598 文件夹操作日志搜集器

---
+++ LeetCode/Problems/Easy/P1598_CrawlerLogFolder.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 文件夹操作日志搜集器
+    /// https://leetcode-cn.com/problems/crawler-log-folder/
+    public class P1598_CrawlerLogFolder
+    {
+        public int MinOperations(string[] logs)
+        {
+            int len = 0;
+            foreach (var log in logs)
+            {
+                if (log == "../")
+                {
+                    if (len > 0)
+                    {
+                        len--;
+                    }
+                }
+                else if (log == "./")
+                {
+                    continue;
+                }
+                else
+                {
+                    len++;
+                }
+            }
+
+            return len;
+        }
+    }
+}
