c6eb8c88453898ccfe4794bec04f747f860b293c
谢在奎 <jackshea@163.com>
2020-11-17 09:58:05 +0800

    P0478 在圆内随机生成点

---
+++ LeetCode/Problems/Medium/P0478_GenerateRandomPointInACircle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium.P0478
+{
+    /// 在圆内随机生成点
+    /// https://leetcode-cn.com/problems/generate-random-point-in-a-circle/
+    public class Solution
+    {
+        private double rr;
+        private double _radius;
+        private double _xCenter;
+        private double _yCenter;
+        private Random rand;
+
+        public Solution(double radius, double x_center, double y_center)
+        {
+            _yCenter = y_center;
+            _xCenter = x_center;
+            _radius = radius;
+            rr = radius * radius;
+            rand = new Random((int)DateTime.Now.Ticks);
+        }
+
+        public double[] RandPoint()
+        {
+            double x = 0;
+            double y = 0;
+            do
+            {
+                x = GetRandom(_xCenter - _radius, _xCenter + _radius);
+                y = GetRandom(_yCenter - _radius, _yCenter + _radius);
+            } while ((x - _xCenter) * (x - _xCenter) + (y - _yCenter) * (y - _yCenter) > rr);
+
+            return new[] { x, y };
+        }
+
+        private double GetRandom(double start, double end)
+        {
+            return start + (end - start) * rand.NextDouble();
+        }
+    }
+}
