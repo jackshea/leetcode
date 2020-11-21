b34114981affdc19f4cefa9a601ce297cc734aa9
谢在奎 <jackshea@163.com>
2020-11-21 22:06:02 +0800

    P1603 设计停车系统

---
+++ LeetCode/Problems/Easy/P1603_DesignParkingSystem.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 设计停车系统
+    /// https://leetcode-cn.com/problems/design-parking-system/
+    public class ParkingSystem
+    {
+        private int[] park;
+        public ParkingSystem(int big, int medium, int small)
+        {
+            park = new[] { 0, big, medium, small };
+        }
+
+        public bool AddCar(int carType)
+        {
+            if (park[carType] > 0)
+            {
+                park[carType]--;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
