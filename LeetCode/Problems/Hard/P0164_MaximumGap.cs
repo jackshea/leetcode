f0a2f71d3856651903c0446da6fb111e6acb3bce
谢在奎 <jackshea@163.com>
2020-11-26 09:59:55 +0800

    P0164 最大间距

---
+++ LeetCode/Problems/Hard/P0164_MaximumGap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Problems.Hard
+{
+    /// 最大间距
+    /// https://leetcode-cn.com/problems/maximum-gap/
+    public class P0164_MaximumGap
+    {
+        public int MaximumGap(int[] nums)
+        {
+            Array.Sort(nums);
+            int ans = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                ans = Math.Max(ans, nums[i] - nums[i - 1]);
+            }
+
+            return ans;
+        }
+    }
+}
