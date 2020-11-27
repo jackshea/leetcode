44dfc4db4f18ed10aa2b93ce6d54537b3784c163
谢在奎 <jackshea@163.com>
2020-11-27 09:47:19 +0800

    P0513 找树左下角的值

---
+++ LeetCode/Problems/Easy/P0513_FindBottomLeftTreeValue.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 找树左下角的值
+    /// https://leetcode-cn.com/problems/find-bottom-left-tree-value/
+    public class P0513_FindBottomLeftTreeValue
+    {
+        private int leftValue;
+        private int maxDepth = -1;
+        public int FindBottomLeftValue(TreeNode root)
+        {
+            DFS(root, 0);
+            return leftValue;
+        }
+
+        private void DFS(TreeNode root, int depth)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (maxDepth < depth)
+            {
+                leftValue = root.val;
+                maxDepth = depth;
+            }
+            DFS(root.left, depth + 1);
+            DFS(root.right, depth + 1);
+        }
+    }
+}
