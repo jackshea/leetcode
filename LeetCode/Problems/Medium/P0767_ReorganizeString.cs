90458d67928a3a4d1b5e645c166e07c160081677
谢在奎 <jackshea@163.com>
2020-11-30 10:23:18 +0800

    P0767 重构字符串

---
+++ LeetCode/Problems/Medium/P0767_ReorganizeString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 重构字符串
+    /// https://leetcode-cn.com/problems/reorganize-string/
+    public class P0767_ReorganizeString
+    {
+        public string ReorganizeString(string S)
+        {
+            if (S.Length <= 1)
+            {
+                return S;
+            }
+
+            int[] count = new int[26];
+            int maxCount = 0;
+            foreach (var c in S)
+            {
+                count[c - 'a']++;
+                maxCount = Math.Max(maxCount, count[c - 'a']);
+            }
+
+            if (maxCount > (S.Length + 1) / 2)
+            {
+                return string.Empty;
+            }
+
+            int evenIndex = 0, oddIndex = 1;
+            char[] ans = new char[S.Length];
+            for (int i = 0; i < 26; i++)
+            {
+                char c = (char)('a' + i);
+                while (count[i] > 0 && count[i] <= S.Length / 2 && oddIndex < S.Length)
+                {
+                    ans[oddIndex] = c;
+                    count[i]--;
+                    oddIndex += 2;
+                }
+
+                while (count[i] > 0)
+                {
+                    ans[evenIndex] = c;
+                    count[i]--;
+                    evenIndex += 2;
+                }
+            }
+
+            return new string(ans);
+        }
+    }
+}
