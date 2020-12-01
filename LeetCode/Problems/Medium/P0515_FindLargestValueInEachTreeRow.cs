2e523d7ff9888c4c2e997c59ab6e0e60cf5271cf
谢在奎 <jackshea@163.com>
2020-12-01 09:44:41 +0800

    P0515 在每个树行中找最大值

---
+++ LeetCode/Problems/Medium/P0515_FindLargestValueInEachTreeRow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 在每个树行中找最大值
+    /// https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row/
+    public class P0515_FindLargestValueInEachTreeRow
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            IList<int> ans = new List<int>();
+            if (root == null)
+            {
+                return ans;
+            }
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                int max = int.MinValue;
+                int count = q.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var top = q.Dequeue();
+                    max = Math.Max(max, top.val);
+                    if (top.left != null)
+                    {
+                        q.Enqueue(top.left);
+                    }
+                    if (top.right != null)
+                    {
+                        q.Enqueue(top.right);
+                    }
+                }
+                ans.Add(max);
+            }
+
+            return ans;
+
+        }
+    }
+}
