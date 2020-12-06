39c2f8b04ee7030e2bd37f668816f5b547497a8c
谢在奎 <jackshea@163.com>
2020-12-06 20:24:18 +0800

    P1656 设计有序流

---
+++ LeetCode/Problems/Easy/P1656.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy.P1656
+{
+    /// 设计有序流
+    /// https://leetcode-cn.com/problems/design-an-ordered-stream/
+    public class OrderedStream
+    {
+        private string[] stringArray;
+        private int ptr = 1;
+        public OrderedStream(int n)
+        {
+            stringArray = new string[n + 1];
+        }
+
+        public IList<string> Insert(int id, string value)
+        {
+            stringArray[id] = value;
+            IList<string> ans = new List<string>();
+            while (ptr < stringArray.Length && stringArray[ptr] != null)
+            {
+                ans.Add(stringArray[ptr]);
+                ptr++;
+            }
+
+            return ans;
+        }
+    }
+}
