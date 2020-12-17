da1a9fb102594de4666c6b374d94b9b584706f17
谢在奎 <jackshea@163.com>
2020-12-17 10:34:14 +0800

    P0526 优美的排列

---
+++ LeetCode/Problems/Medium/P0526_BeautifulArrangement.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 优美的排列
+    /// https://leetcode-cn.com/problems/beautiful-arrangement/
+    public class P0526_BeautifulArrangement
+    {
+        private int count = 0;
+        private bool[] visited;
+        public int CountArrangement(int N)
+        {
+            visited = new bool[N + 1];
+            Backtrack(N, 1);
+            return count;
+        }
+
+        private void Backtrack(int N, int pos)
+        {
+            if (pos > N)
+            {
+                count++;
+                return;
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (!visited[i] && (pos % i == 0 || i % pos == 0))
+                {
+                    visited[i] = true;
+                    Backtrack(N, pos + 1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
