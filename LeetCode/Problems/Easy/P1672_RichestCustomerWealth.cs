e3f04137d30db9da30553fe833b3f7b3414ba1a8
谢在奎 <jackshea@163.com>
2020-12-19 18:04:15 +0800

    P1672 最富有客户的资产总量

---
+++ LeetCode/Problems/Easy/P1672_RichestCustomerWealth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 最富有客户的资产总量
+    /// https://leetcode-cn.com/problems/richest-customer-wealth/
+    public class P1672_RichestCustomerWealth
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int ans = 0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < accounts[0].Length; j++)
+                {
+                    sum += accounts[i][j];
+                }
+
+                ans = Math.Max(ans, sum);
+            }
+
+            return ans;
+        }
+    }
+}
