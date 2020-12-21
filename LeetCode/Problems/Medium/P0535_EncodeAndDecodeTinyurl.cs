26c6f0ba98ed9788179d92d0a119d61fdbf338fe
谢在奎 <jackshea@163.com>
2020-12-21 10:01:48 +0800

    P0535 TinyURL 的加密与解密

---
+++ LeetCode/Problems/Medium/P0535_EncodeAndDecodeTinyurl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium.EncodeAndDecodeTinyurl
+{
+    /// TinyURL 的加密与解密
+    /// https://leetcode-cn.com/problems/encode-and-decode-tinyurl/
+    public class Codec
+    {
+        private int id = 0;
+        private Dictionary<int, string> dic = new Dictionary<int, string>();
+        private const string baseUrl = "http://tinyurl.com/";
+
+        // Encodes a URL to a shortened URL
+        public string encode(string longUrl)
+        {
+            dic[++id] = longUrl;
+            return baseUrl + id;
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public string decode(string shortUrl)
+        {
+            int getId = int.Parse(shortUrl.Substring(baseUrl.Length));
+            dic.TryGetValue(getId, out var url);
+            return url;
+        }
+    }
+}
