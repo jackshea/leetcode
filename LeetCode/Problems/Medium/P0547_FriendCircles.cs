4ae33cba732cbb62910c4f5ac43341d4fe7cebd1
谢在奎 <jackshea@163.com>
2020-12-28 10:02:24 +0800

    P0547 朋友圈

---
+++ LeetCode/Problems/Medium/P0547_FriendCircles.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 朋友圈
+    /// https://leetcode-cn.com/problems/friend-circles/
+    public class P0547_FriendCircles
+    {
+        private bool[] isVisited;
+        public int FindCircleNum(int[][] M)
+        {
+            int ans = 0;
+            isVisited = new bool[M.Length];
+            for (int i = 0; i < M.Length; i++)
+            {
+                if (!isVisited[i])
+                {
+                    DFS(M, i);
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+
+        private void DFS(int[][] M, int start)
+        {
+            for (int i = 0; i < M[start].Length; i++)
+            {
+                if (M[start][i] == 1 && !isVisited[i])
+                {
+                    isVisited[i] = true;
+                    DFS(M, i);
+                }
+            }
+        }
+    }
+}
