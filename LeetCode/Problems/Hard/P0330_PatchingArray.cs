85bd4603a27b32ffe858382481b22814b02a2d3d
谢在奎 <jackshea@163.com>
2020-12-29 10:26:10 +0800

    P0330 按要求补齐数组

---
+++ LeetCode/Problems/Hard/P0330_PatchingArray.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Hard
+{
+    /// 按要求补齐数组
+    /// https://leetcode-cn.com/problems/patching-array/
+    public class P0330_PatchingArray
+    {
+        public int MinPatches(int[] nums, int n)
+        {
+            long max = 1;
+            int ans = 0;
+            int index = 0;
+            while (max <= n)
+            {
+                if (index < nums.Length && nums[index] <= max)
+                {
+                    max += nums[index];
+                    index++;
+                }
+                else
+                {
+                    max *= 2;
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
