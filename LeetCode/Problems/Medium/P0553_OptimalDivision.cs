07ada2206b73dd0bea2ce69aac4fdb01087d72d4
谢在奎 <jackshea@163.com>
2020-12-30 09:51:53 +0800

    P0553 最优除法

---
+++ LeetCode/Problems/Medium/P0553_OptimalDivision.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 最优除法
+    /// https://leetcode-cn.com/problems/optimal-division/
+    public class P0553_OptimalDivision
+    {
+        public string OptimalDivision(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0].ToString();
+            }
+
+            if (nums.Length == 2)
+            {
+                return $"{nums[0]}/{nums[1]}";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 2; i < nums.Length; i++)
+            {
+                sb.Append("/" + nums[i]);
+            }
+
+            return $"{nums[0]}/({nums[1]}{sb})";
+        }
+    }
+}
