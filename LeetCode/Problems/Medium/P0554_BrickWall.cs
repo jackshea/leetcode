f04d2586ce4178acf070859ee06f53278481bc3b
谢在奎 <jackshea@163.com>
2020-12-31 10:43:17 +0800

    P0554 砖墙

---
+++ LeetCode/Problems/Medium/P0554_BrickWall.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 砖墙
+    /// https://leetcode-cn.com/problems/brick-wall/
+    public class P0554_BrickWall
+    {
+        public int LeastBricks(IList<IList<int>> wall)
+        {
+            int len = wall[0].Sum();
+            Dictionary<int, int> interval = new Dictionary<int, int>();
+            for (int i = 0; i < wall.Count; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < wall[i].Count; j++)
+                {
+                    sum += wall[i][j];
+                    if (sum >= len)
+                    {
+                        continue;
+                    }
+
+                    var cnt = 0;
+                    interval.TryGetValue(sum, out cnt);
+                    interval[sum] = cnt + 1;
+                }
+            }
+
+            int max = 0;
+            foreach (var value in interval.Values)
+            {
+                max = Math.Max(max, value);
+            }
+
+            return wall.Count - max;
+        }
+    }
+}
