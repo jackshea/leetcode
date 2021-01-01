4f1f4b51d31d29a0cc1017619664d64e5e844f75
谢在奎 <jackshea@163.com>
2021-01-01 19:17:49 +0800

    P1684 统计一致字符串的数目

---
+++ LeetCode/Problems/Easy/P1684_CountTheNumberOfConsistentStrings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 统计一致字符串的数目
+    /// https://leetcode-cn.com/problems/count-the-number-of-consistent-strings/
+    public class P1684_CountTheNumberOfConsistentStrings
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            HashSet<char> charSet = new HashSet<char>();
+            foreach (var c in allowed)
+            {
+                charSet.Add(c);
+            }
+
+            int ans = 0;
+            foreach (var word in words)
+            {
+                bool check = true;
+                foreach (char c in word)
+                {
+                    if (!charSet.Contains(c))
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if (check)
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
