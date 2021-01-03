593b6ecfe602a6d6da61794c77a2377591df99c8
谢在奎 <jackshea@163.com>
2021-01-03 22:49:25 +0800

    P1688 比赛中的配对次数

---
+++ LeetCode/Problems/Easy/P1688_CountOfMatchesInTournament.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 比赛中的配对次数
+    /// https://leetcode-cn.com/problems/count-of-matches-in-tournament/
+    public class P1688_CountOfMatchesInTournament
+    {
+        public int NumberOfMatches(int n)
+        {
+            int cnt = 0;
+            while (n > 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                    cnt += n;
+                }
+                else
+                {
+                    cnt += n / 2;
+                    n = n / 2 + 1;
+                }
+            }
+
+            return cnt;
+        }
+    }
+}
