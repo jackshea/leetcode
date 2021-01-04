651a8ec336c394caa737cd56a941e3ada41c778e
谢在奎 <jackshea@163.com>
2021-01-04 10:51:30 +0800

    P0556 下一个更大元素 III

---
+++ LeetCode/Problems/Medium/P0556_NextGreaterElementIII.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 下一个更大元素 III
+    /// https://leetcode-cn.com/problems/next-greater-element-iii/
+    public class P0556_NextGreaterElementIII
+    {
+        public int NextGreaterElement(int n)
+        {
+            var charArray = n.ToString().ToCharArray();
+            int findIdx = charArray.Length - 2;
+            while (findIdx >= 0 && charArray[findIdx] >= charArray[findIdx + 1])
+            {
+                findIdx--;
+            }
+
+            if (findIdx < 0)
+            {
+                return -1;
+            }
+
+            int justGt = charArray.Length - 1;
+            while (justGt >= 0 && charArray[justGt] <= charArray[findIdx])
+            {
+                justGt--;
+            }
+            Swap(charArray, findIdx, justGt);
+            Reverse(charArray, findIdx + 1, charArray.Length - 1);
+            int ans = 0;
+            try
+            {
+                ans = int.Parse(new string(charArray));
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+
+            return ans;
+        }
+
+        private void Swap(char[] charArray, int i, int j)
+        {
+            char c = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = c;
+        }
+
+        private void Reverse(char[] charArray, int i, int j)
+        {
+            while (i < j)
+            {
+                Swap(charArray, i, j);
+                i++;
+                j--;
+            }
+        }
+    }
+}
