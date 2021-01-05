f50516f48da5de25165e5c41fdb45421ca83d5f7
谢在奎 <jackshea@163.com>
2021-01-05 10:02:51 +0800

    P0565 数组嵌套

---
+++ LeetCode/Problems/Medium/P0565_ArrayNesting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 数组嵌套
+    /// https://leetcode-cn.com/problems/array-nesting/
+    public class P0565_ArrayNesting
+    {
+        public int ArrayNesting(int[] nums)
+        {
+            int ans = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < 0)
+                {
+                    continue;
+                }
+
+                int len = 0;
+                int jumpIdx = i;
+                while (nums[jumpIdx] >= 0)
+                {
+                    int next = nums[jumpIdx];
+                    nums[jumpIdx] = -1;
+                    jumpIdx = next;
+                    len++;
+                }
+
+                ans = Math.Max(ans, len);
+                if (ans >= nums.Length / 2)
+                {
+                    break;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
