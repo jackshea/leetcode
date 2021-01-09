3b01d87bb8d906da05ef6dda659f3b9e68843114
谢在奎 <jackshea@163.com>
2021-01-09 21:00:52 +0800

    P1694 重新格式化电话号码

---
+++ LeetCode/Problems/Easy/P1694_ReformatPhoneNumber.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 重新格式化电话号码
+    /// https://leetcode-cn.com/problems/reformat-phone-number/
+    public class P1694_ReformatPhoneNumber
+    {
+        public string ReformatNumber(string number)
+        {
+            List<char> numbers = new List<char>();
+            int cnt = 0;
+            foreach (var num in number)
+            {
+                if (num >= '0' && num <= '9')
+                {
+                    numbers.Add(num);
+                    cnt++;
+                    if (cnt % 3 == 0)
+                    {
+                        numbers.Add('-');
+                    }
+                }
+            }
+
+            switch (cnt % 3)
+            {
+                case 0:
+                    numbers.RemoveAt(numbers.Count - 1);
+                    break;
+                case 1:
+                    int a = numbers.Count - 3;
+                    char c = numbers[a];
+                    numbers[a] = numbers[a + 1];
+                    numbers[a + 1] = c;
+                    break;
+            }
+
+            return new string(numbers.ToArray());
+        }
+    }
+}
