247a41a2818384cedc6a5564f5a9516b485f97bf
谢在奎 <jackshea@163.com>
2021-01-11 13:56:35 +0800

    将项目改为 .net 5

--- LeetCode/Problems/Medium/P0056_MergeIntervals.cs
+++ LeetCode/Problems/Medium/P0056_MergeIntervals.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using NUnit.Framework;
 
 namespace LeetCode.Problems.Medium
 {
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LeetCode.Common;
-using LeetCode.Problems;
-using LeetCode.Problems.Easy;
-using LeetCode.Problems.Hard;
-using LeetCode.Problems.Medium;
 
 namespace LeetCode
 {
@@ -15,10 +6,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var test = new P1668_MaximumRepeatingSubstring();
-            //var ans = test.MaxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba");
-            var ans = test.MaxRepeating("ababc", "ba");
-            Console.WriteLine(ans);
+            Console.WriteLine("Hello World!");
         }
     }
 }
