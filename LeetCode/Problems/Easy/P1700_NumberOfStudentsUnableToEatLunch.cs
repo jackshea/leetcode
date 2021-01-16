8b86ab30e3f9f3cec5beed209e8bd4c5b61ad572
谢在奎 <jackshea@163.com>
2021-01-16 21:59:43 +0800

    P1700 无法吃午餐的学生数量

---
+++ LeetCode/Problems/Easy/P1700_NumberOfStudentsUnableToEatLunch.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 无法吃午餐的学生数量
+    /// https://leetcode-cn.com/problems/number-of-students-unable-to-eat-lunch/
+    public class P1700_NumberOfStudentsUnableToEatLunch
+    {
+        public int CountStudents(int[] students, int[] sandwiches)
+        {
+            int stu0 = 0, stu1 = 0;
+            foreach (var student in students)
+            {
+                if (student == 0)
+                {
+                    stu0++;
+                }
+                else
+                {
+                    stu1++;
+                }
+            }
+
+            foreach (var sandwich in sandwiches)
+            {
+                if (sandwich == 0)
+                {
+                    if (stu0 == 0)
+                    {
+                        break;
+                    }
+
+                    stu0--;
+                }
+                else
+                {
+                    if (stu1 == 0)
+                    {
+                        break;
+                    }
+
+                    stu1--;
+                }
+            }
+
+            return stu0 + stu1;
+        }
+    }
+}
