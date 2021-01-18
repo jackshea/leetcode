656615417aa1c18a251f7e2e282edc7ac6d4588f
谢在奎 <jackshea@163.com>
2021-01-18 22:12:58 +0800

    P1704 判断字符串的两半是否相似

---
+++ LeetCode/Problems/Easy/P1704_DetermineIfStringHalvesAreAlike.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 判断字符串的两半是否相似
+    /// https://leetcode-cn.com/problems/determine-if-string-halves-are-alike/
+    public class P1704_DetermineIfStringHalvesAreAlike
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            HashSet<char> aeiou = "aeiouAEIOU".ToHashSet();
+            int vowelCount = 0;
+            int halfLen = s.Length / 2;
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (!aeiou.Contains(c))
+                {
+                    continue;
+                }
+                if (i < halfLen)
+                {
+                    vowelCount++;
+                }
+                else
+                {
+                    vowelCount--;
+                    if (vowelCount < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return vowelCount == 0;
+        }
+    }
+}
