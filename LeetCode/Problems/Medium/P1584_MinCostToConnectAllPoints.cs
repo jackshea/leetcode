ae77482e547b805ba89e020051a1b227cb2b59e3
谢在奎 <jackshea@163.com>
2021-01-19 10:55:46 +0800

    P1584 连接所有点的最小费用

---
+++ LeetCode/Problems/Medium/P1584_MinCostToConnectAllPoints.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 连接所有点的最小费用
+    /// https://leetcode-cn.com/problems/min-cost-to-connect-all-points/
+    public class P1584_MinCostToConnectAllPoints
+    {
+        /// Kruskal 算法
+        public int MinCostConnectPoints(int[][] points)
+        {
+            var n = points.Length;
+            UnionFind uf = new UnionFind(n);
+            List<Edge> edges = new List<Edge>(n * (n - 1) / 2);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    edges.Add(new Edge { P1 = i, P2 = j, Length = Dist(points, i, j) });
+                }
+            }
+
+            edges.Sort((a, b) => a.Length.CompareTo(b.Length));
+            int ans = 0;
+            int joinedCnt = 1;
+            foreach (var edge in edges)
+            {
+                if (uf.Find(edge.P1) != uf.Find(edge.P2))
+                {
+                    uf.Union(edge.P1, edge.P2);
+                    ans += edge.Length;
+                    joinedCnt++;
+                }
+
+                if (joinedCnt == n)
+                {
+                    break;
+                }
+            }
+
+            return ans;
+        }
+
+        private int Dist(int[][] points, int p1, int p2)
+        {
+            return Math.Abs(points[p1][0] - points[p2][0]) + Math.Abs(points[p1][1] - points[p2][1]);
+        }
+
+        struct Edge
+        {
+            public int P1;
+            public int P2;
+            public int Length;
+        }
+
+        class UnionFind
+        {
+            private int[] ancestors;
+            public UnionFind(int n)
+            {
+                ancestors = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    ancestors[i] = i;
+                }
+            }
+
+            public int Find(int x)
+            {
+                if (x != ancestors[x])
+                {
+                    ancestors[x] = Find(ancestors[x]);
+                }
+
+                return ancestors[x];
+            }
+
+            public void Union(int a, int b)
+            {
+                ancestors[Find(a)] = Find(b);
+            }
+        }
+    }
+}
