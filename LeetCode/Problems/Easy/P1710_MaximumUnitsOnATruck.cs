f21acf72fd6da5d8d46f5f65f02b18992c43a4b3
谢在奎 <jackshea@163.com>
2021-01-24 22:35:31 +0800

    P1710 卡车上的最大单元数

---
+++ LeetCode/Problems/Easy/P1710_MaximumUnitsOnATruck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 卡车上的最大单元数
+    /// https://leetcode-cn.com/problems/maximum-units-on-a-truck/
+    public class P1710_MaximumUnitsOnATruck
+    {
+        public int MaximumUnits(int[][] boxTypes, int truckSize)
+        {
+            Array.Sort(boxTypes, (a, b) => -a[1].CompareTo(b[1]));
+            int ans = 0;
+            for (int i = 0; i < boxTypes.Length; i++)
+            {
+                if (boxTypes[i][0] <= truckSize)
+                {
+                    ans += boxTypes[i][1] * boxTypes[i][0];
+                    truckSize -= boxTypes[i][0];
+                }
+                else
+                {
+                    ans += boxTypes[i][1] * truckSize;
+                    break;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
