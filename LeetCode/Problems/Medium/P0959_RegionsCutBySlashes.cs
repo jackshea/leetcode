96e9637ce01c134c482789089882ac65b2ebe684
谢在奎 <jackshea@163.com>
2021-01-25 11:26:50 +0800

    P0959 由斜杠划分区域

---
+++ LeetCode/Problems/Medium/P0959_RegionsCutBySlashes.cs
@@ -0,0 +1,94 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 由斜杠划分区域
+    /// https://leetcode-cn.com/problems/regions-cut-by-slashes/
+    public class P0959_RegionsCutBySlashes
+    {
+        ///      0
+        ///    2   3
+        ///      1
+        public int RegionsBySlashes(string[] grid)
+        {
+            var length = grid[0].Length;
+            UnionFind uf = new UnionFind(grid.Length * length * 4);
+            int index = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    var c = grid[i][j];
+                    int pos = index * 4;
+                    if (c == '\\')
+                    {
+                        uf.Union(pos, pos + 3);
+                        uf.Union(pos + 1, pos + 2);
+                    }
+                    else if (c == '/')
+                    {
+                        uf.Union(pos, pos + 2);
+                        uf.Union(pos + 1, pos + 3);
+                    }
+                    else
+                    {
+                        uf.Union(pos, pos + 1);
+                        uf.Union(pos + 1, pos + 2);
+                        uf.Union(pos + 2, pos + 3);
+                    }
+
+                    if (i > 0)
+                    {
+                        uf.Union(pos + 2, ((i - 1) * length + j) * 4 + 3);
+                    }
+
+                    if (j > 0)
+                    {
+                        uf.Union(pos, (i * length + j - 1) * 4 + 1);
+                    }
+
+                    index++;
+                }
+            }
+
+            return uf.SetCount;
+        }
+
+        public class UnionFind
+        {
+            public int SetCount { get; private set; }
+            private int[] ancestor;
+
+            public UnionFind(int n)
+            {
+                SetCount = n;
+                ancestor = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    ancestor[i] = i;
+                }
+            }
+
+            public int Find(int x)
+            {
+                if (ancestor[x] != x)
+                {
+                    ancestor[x] = Find(ancestor[x]);
+                }
+
+                return ancestor[x];
+            }
+
+            public void Union(int a, int b)
+            {
+                var aa = Find(a);
+                var ba = Find(b);
+                if (aa == ba)
+                {
+                    return;
+                }
+
+                SetCount--;
+                ancestor[aa] = ba;
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0959_RegionsCutBySlashesTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0959_RegionsCutBySlashesTests
+    {
+        [Test()]
+        public void RegionsBySlashesTest()
+        {
+            var input = new[] { " /", "/ " };
+            var test = new P0959_RegionsCutBySlashes();
+            Assert.AreEqual(2, test.RegionsBySlashes(input));
+        }
+    }
+}
