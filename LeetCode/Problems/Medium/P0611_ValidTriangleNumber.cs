21340e839998903b89f95975acf60ae533295a5e
谢在奎 <jackshea@163.com>
2021-02-01 10:33:23 +0800

    P0611 有效三角形的个数

---
+++ LeetCode/Problems/Medium/P0611_ValidTriangleNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 有效三角形的个数
+    /// https://leetcode-cn.com/problems/valid-triangle-number/
+    public class P0611_ValidTriangleNumber
+    {
+        public int TriangleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int ans = 0;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    continue;
+                }
+
+                int k = i + 2;
+                for (int j = i + 1; j < nums.Length - 1; j++)
+                {
+                    while (k < nums.Length && nums[i] + nums[j] > nums[k])
+                    {
+                        k++;
+                    }
+
+                    ans += k - j - 1;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
