9e225bdb72b4353b9bbebf939f6d76a3f673526a
谢在奎 <jackshea@163.com>
2021-02-05 10:40:26 +0800

    P1208 尽可能使字符串相等

---
+++ LeetCode/Problems/Medium/P1208_GetEqualSubstringsWithinBudget.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 尽可能使字符串相等
+    /// https://leetcode-cn.com/problems/get-equal-substrings-within-budget/
+    public class P1208_GetEqualSubstringsWithinBudget
+    {
+        public int EqualSubstring(string s, string t, int maxCost)
+        {
+            int[] diff = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                diff[i] = Math.Abs(s[i] - t[i]);
+            }
+
+            int left = 0, right = 0, sum = 0, ans = 0;
+            while (right < s.Length)
+            {
+                sum += diff[right];
+                while (sum > maxCost)
+                {
+                    sum -= diff[left++];
+                }
+                ans = Math.Max(ans, right - left + 1);
+                right++;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P1208_GetEqualSubstringsWithinBudgetTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P1208_GetEqualSubstringsWithinBudgetTests
+    {
+        [Test()]
+        public void EqualSubstringTest()
+        {
+            var test = new P1208_GetEqualSubstringsWithinBudget();
+            Assert.AreEqual(3, test.EqualSubstring("abcd", "bcdf", 3));
+        }
+    }
+}
