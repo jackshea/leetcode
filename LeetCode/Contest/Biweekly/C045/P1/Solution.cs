119f7e2fb6adc83b10df347b26c7ac78f44f3dbb
谢在奎 <jackshea@163.com>
2021-02-05 17:24:09 +0800

    建立周赛目录

---
+++ LeetCode/Contest/Biweekly/C045/P1/Solution.cs
@@ -0,0 +1,7 @@
+﻿namespace LeetCode.Contest.Biweekly.C045.P1
+{
+    public class Solution
+    {
+        
+    }
+}
---
+++ LeetCode/Contest/Weekly/C227/P1/Solution.cs
@@ -0,0 +1,7 @@
+﻿namespace LeetCode.Contest.Weekly.C227.P1
+{
+    public class Solution
+    {
+        
+    }
+}
