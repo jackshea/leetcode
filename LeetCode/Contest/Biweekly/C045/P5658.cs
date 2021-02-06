3e548f3a876b2d027cf8f0d06b7594d29fcf99fd
谢在奎 <jackshea@163.com>
2021-02-06 22:48:49 +0800

    P5658 任意子数组和的绝对值的最大值

---
+++ LeetCode/Contest/Biweekly/C045/P5658.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCode.Contest.Biweekly.C045
+{
+    /// 任意子数组和的绝对值的最大值
+    /// https://leetcode-cn.com/contest/biweekly-contest-45/problems/maximum-absolute-sum-of-any-subarray/
+    public class P5658
+    {
+        public int MaxAbsoluteSum(int[] nums)
+        {
+            int min = 0, max = 0;
+            int positiveSum = 0, negativeSum = 0;
+            foreach (var num in nums)
+            {
+                positiveSum += num;
+                if (positiveSum > max)
+                {
+                    max = positiveSum;
+                }
+
+                if (positiveSum < 0)
+                {
+                    positiveSum = 0;
+                }
+                negativeSum += num;
+                if (negativeSum < min)
+                {
+                    min = negativeSum;
+                }
+                if (negativeSum > 0)
+                {
+                    negativeSum = 0;
+                }
+            }
+
+            return Math.Max(Math.Abs(min), max);
+        }
+    }
+}
