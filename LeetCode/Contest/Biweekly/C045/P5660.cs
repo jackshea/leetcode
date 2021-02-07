69e2c62a4c0120eeca7dbd558ac44f178351c9e0
谢在奎 <jackshea@163.com>
2021-02-07 18:39:59 +0800

    P5660 最多可以参加的会议数目 II

---
+++ LeetCode/Contest/Biweekly/C045/P5660.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LeetCode.Contest.Biweekly.C045
+{
+    /// 最多可以参加的会议数目 II
+    /// https://leetcode-cn.com/problems/maximum-number-of-events-that-can-be-attended-ii/
+    public class P5660
+    {
+        public int MaxValue(int[][] events, int k)
+        {
+            Array.Sort(events, (a, b) => a[1].CompareTo(b[1]));
+            int[][] dp = new int[events.Length][];
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = new int[k + 1];
+                Array.Fill(dp[i], int.MinValue);
+            }
+
+            dp[0][0] = 0;
+            dp[0][1] = events[0][2];
+            for (int i = 1; i < events.Length; i++)
+            {
+                // 参加会议i
+                int left = 0, right = i;
+                while (right - left > 1)
+                {
+                    int mid = (left + right) / 2;
+                    if (events[mid][1] >= events[i][0])
+                    {
+                        right = mid;
+                    }
+                    else
+                    {
+                        left = mid;
+                    }
+                }
+
+                if (events[left][1] >= events[i][0])
+                {
+                    dp[i][1] = Math.Max(dp[i][1], events[i][2]);
+                }
+                else
+                {
+                    for (int j = 1; j <= k; j++)
+                    {
+                        dp[i][j] = Math.Max(dp[i][j], dp[left][j - 1] + events[i][2]);
+                    }
+                }
+
+                // 不参加会议i
+                for (int j = 0; j <= k; j++)
+                {
+                    dp[i][j] = Math.Max(dp[i][j], dp[i - 1][j]);
+                }
+            }
+
+            int ans = 0;
+            for (int i = 0; i <= k; i++)
+            {
+                ans = Math.Max(ans, dp[events.Length - 1][i]);
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Contest/Biweekly/C045/P5660Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using LeetCode.Contest.Biweekly.C045;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Contest.Biweekly.C045.Tests
+{
+    [TestFixture()]
+    public class P5660Tests
+    {
+        [Test()]
+        public void MaxValueTest()
+        {
+            var test = new P5660();
+            var input = Utils.Read2DArray("[[1,2,4],[3,4,3],[2,3,1]]");
+            Assert.AreEqual(7, test.MaxValue(input, 2));
+        }
+
+        [Test()]
+        public void MaxValueTest1()
+        {
+            var test = new P5660();
+            var input = Utils.Read2DArray("[[1,2,4],[3,4,3],[2,3,10]]");
+            Assert.AreEqual(10, test.MaxValue(input, 2));
+        }
+
+        [Test()]
+        public void MaxValueTest2()
+        {
+            var test = new P5660();
+            var input = Utils.Read2DArray("[[1,1,1],[2,2,2],[3,3,3],[4,4,4]]");
+            Assert.AreEqual(9, test.MaxValue(input, 3));
+        }
+    }
+}
