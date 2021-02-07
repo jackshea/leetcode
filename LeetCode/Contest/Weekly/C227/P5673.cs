26f6bc7fbf48d85e289f81b198782663e3d98573
谢在奎 <jackshea@163.com>
2021-02-07 11:00:17 +0800

    P5673 移除石子的最大得分

---
+++ LeetCode/Contest/Weekly/C227/P5673.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Contest.Weekly.C227
+{
+    /// 移除石子的最大得分
+    /// https://leetcode-cn.com/contest/weekly-contest-227/problems/maximum-score-from-removing-stones/
+    public class P5673
+    {
+        public int MaximumScore(int a, int b, int c)
+        {
+            int[] heap = new[] { a, b, c };
+            Array.Sort(heap);
+            if (heap[0] + heap[1] <= heap[2])
+            {
+                return heap[0] + heap[1];
+            }
+            else
+            {
+                int min = Math.Min(heap[0], (heap[1] + heap[0] - heap[2]) / 2);
+                return heap[2] + min;
+            }
+        }
+    }
+}
