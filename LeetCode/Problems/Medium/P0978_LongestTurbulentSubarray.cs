e4949d043e7670ccc4ce62e908beb4e085573db7
谢在奎 <jackshea@163.com>
2021-02-08 11:20:00 +0800

    P0978 最长湍流子数组

---
+++ LeetCode/Problems/Medium/P0978_LongestTurbulentSubarray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 最长湍流子数组
+    /// https://leetcode-cn.com/problems/longest-turbulent-subarray/
+    public class P0978_LongestTurbulentSubarray
+    {
+        public int MaxTurbulenceSize(int[] arr)
+        {
+            if (arr.Length == 1)
+            {
+                return 1;
+            }
+
+            int left = 0, right = 0, ans = 0;
+            while (right < arr.Length - 1)
+            {
+                if (left == right)
+                {
+                    if (arr[left] == arr[left + 1])
+                    {
+                        left++;
+                    }
+
+                    right++;
+                }
+                else
+                {
+                    if (Check(arr, right))
+                    {
+                        right++;
+                    }
+                    else
+                    {
+                        left = right;
+                    }
+                }
+                ans = Math.Max(ans, right - left + 1);
+            }
+
+            return ans;
+        }
+
+        private bool Check(int[] array, int idx)
+        {
+            if (idx == 0 || idx >= array.Length - 1)
+            {
+                return false;
+            }
+
+            return array[idx - 1] < array[idx] && array[idx] > array[idx + 1]
+                || array[idx - 1] > array[idx] && array[idx] < array[idx + 1];
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0978_LongestTurbulentSubarrayTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0978_LongestTurbulentSubarrayTests
+    {
+        [Test()]
+        public void MaxTurbulenceSizeTest()
+        {
+            var test = new P0978_LongestTurbulentSubarray();
+            Assert.AreEqual(5, test.MaxTurbulenceSize(new[] { 2, 1, 2, 1, 2 }));
+        }
+
+        [Test()]
+        public void MaxTurbulenceSizeTest2()
+        {
+            var test = new P0978_LongestTurbulentSubarray();
+            Assert.AreEqual(5, test.MaxTurbulenceSize(new[] { 1, 2, 1, 2, 1 }));
+        }
+
+        [Test()]
+        public void MaxTurbulenceSizeTest3()
+        {
+            var test = new P0978_LongestTurbulentSubarray();
+            Assert.AreEqual(5, test.MaxTurbulenceSize(new[] { 9, 4, 2, 10, 7, 8, 8, 1, 9 }));
+        }
+
+        [Test()]
+        public void MaxTurbulenceSizeTest4()
+        {
+            var test = new P0978_LongestTurbulentSubarray();
+            Assert.AreEqual(2, test.MaxTurbulenceSize(new[] { 4, 8, 12, 16 }));
+        }
+
+        [Test()]
+        public void MaxTurbulenceSizeTest5()
+        {
+            var test = new P0978_LongestTurbulentSubarray();
+            Assert.AreEqual(1, test.MaxTurbulenceSize(new[] { 100 }));
+        }
+    }
+}
