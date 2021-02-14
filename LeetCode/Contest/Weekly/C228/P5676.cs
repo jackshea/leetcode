23f3333a3cea49870ed9a1bdde4e8df93f1210b9
谢在奎 <jackshea@163.com>
2021-02-14 10:38:11 +0800

    P5676 生成交替二进制字符串的最少操作数

---
+++ LeetCode/Contest/Weekly/C228/P5676.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.Contest.Weekly.C228
+{
+    /// 生成交替二进制字符串的最少操作数
+    /// https://leetcode-cn.com/contest/weekly-contest-228/problems/minimum-changes-to-make-alternating-binary-string/
+    public class P5676
+    {
+        public int MinOperations(string s)
+        {
+            var n = s.Length;
+            char mark = '1';
+            int diffCnt = 0;
+            foreach (var c in s)
+            {
+                if (c != mark)
+                {
+                    diffCnt++;
+                }
+
+                mark = mark == '1' ? '0' : '1';
+            }
+
+            return Math.Min(diffCnt, n - diffCnt);
+        }
+    }
+}
