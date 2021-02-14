7408dae1607480431e5998db55432ebeec1ecf0a
谢在奎 <jackshea@163.com>
2021-02-14 10:50:14 +0800

    P5677 统计同构子字符串的数目

---
+++ LeetCode/Contest/Weekly/C228/P5677.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Contest.Weekly.C228
+{
+    /// 统计同构子字符串的数目
+    /// https://leetcode-cn.com/contest/weekly-contest-228/problems/count-number-of-homogenous-substrings/
+    public class P5677
+    {
+        public int CountHomogenous(string s)
+        {
+            int count = 1;
+            int sum = 1;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+
+                sum += count;
+                sum %= 1000_000_007;
+            }
+
+            return sum;
+        }
+    }
+}
