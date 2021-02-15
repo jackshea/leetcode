7f826489e232424ca6cab88e8bf3bff7438aa8e6
谢在奎 <jackshea@163.com>
2021-02-15 22:14:02 +0800

    P1720 解码异或后的数组

---
+++ LeetCode/Problems/Easy/P1720_DecodeXoredArray.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 解码异或后的数组
+    /// https://leetcode-cn.com/problems/decode-xored-array/
+    public class P1720_DecodeXoredArray
+    {
+        public int[] Decode(int[] encoded, int first)
+        {
+            int[] ans = new int[encoded.Length + 1];
+            ans[0] = first;
+            for (int i = 1; i < ans.Length; i++)
+            {
+                ans[i] = encoded[i - 1] ^ ans[i - 1];
+            }
+
+            return ans;
+        }
+    }
+}
