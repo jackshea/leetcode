ff8501592541274adad2227447664757aee5d945
谢在奎 <jackshea@163.com>
2021-02-17 20:17:28 +0800

    P1732 找到最高海拔

---
+++ LeetCode/Problems/Easy/P1732_FindTheHighestAltitude.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 找到最高海拔
+    /// https://leetcode-cn.com/problems/find-the-highest-altitude/
+    public class P1732_FindTheHighestAltitude
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int ans = 0;
+            int curr = 0;
+            foreach (var g in gain)
+            {
+                curr += g;
+                if (ans < curr)
+                {
+                    ans = curr;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
