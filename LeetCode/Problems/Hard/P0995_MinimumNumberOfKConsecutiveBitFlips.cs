e426880e5f5605398885374a463042357aed6cdc
谢在奎 <jackshea@163.com>
2021-02-18 11:27:57 +0800

    P0995 K 连续位的最小翻转次数

---
+++ LeetCode/Problems/Hard/P0995_MinimumNumberOfKConsecutiveBitFlips.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Hard
+{
+    /// K 连续位的最小翻转次数
+    /// https://leetcode-cn.com/problems/minimum-number-of-k-consecutive-bit-flips/
+    public class P0995_MinimumNumberOfKConsecutiveBitFlips
+    {
+        public int MinKBitFlips(int[] A, int K)
+        {
+            Queue<int> q = new Queue<int>();
+            int ans = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (q.TryPeek(out var top) && i >= top + K)
+                {
+                    q.Dequeue();
+                }
+                if (q.Count % 2 == A[i])
+                {
+                    if (i + K > A.Length)
+                    {
+                        return -1;
+                    }
+                    q.Enqueue(i);
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Hard/P0995_MinimumNumberOfKConsecutiveBitFlipsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Hard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Hard.Tests
+{
+    [TestFixture()]
+    public class P0995_MinimumNumberOfKConsecutiveBitFlipsTests
+    {
+        [Test()]
+        public void MinKBitFlipsTest()
+        {
+            var test = new P0995_MinimumNumberOfKConsecutiveBitFlips();
+            Assert.AreEqual(3, test.MinKBitFlips(new[] { 0, 1, 0, 0, 1, 0 }, 4));
+        }
+    }
+}
