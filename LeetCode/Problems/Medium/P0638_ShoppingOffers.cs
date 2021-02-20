31f50e86ab370dc275dcd464689c19b0b4b5c521
谢在奎 <jackshea@163.com>
2021-02-20 11:31:51 +0800

    P0638 大礼包

---
+++ LeetCode/Problems/Medium/P0638_ShoppingOffers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 大礼包
+    /// https://leetcode-cn.com/problems/shopping-offers/
+    public class P0638_ShoppingOffers
+    {
+        public int ShoppingOffers(IList<int> price, IList<IList<int>> special, IList<int> needs)
+        {
+            return DFS(price, special, needs);
+        }
+
+        public int DFS(IList<int> price, IList<IList<int>> special, IList<int> needs)
+        {
+            int totalCost = 0;
+            for (int i = 0; i < needs.Count; i++)
+            {
+                totalCost += price[i] * needs[i];
+            }
+
+            foreach (var pack in special)
+            {
+                var currentNeeds = new List<int>(needs);
+                bool check = true;
+                for (var i = 0; i < currentNeeds.Count; i++)
+                {
+                    if (pack[i] > currentNeeds[i])
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    for (var i = 0; i < currentNeeds.Count; i++)
+                    {
+                        currentNeeds[i] -= pack[i];
+                    }
+
+                    totalCost = Math.Min(totalCost, pack[currentNeeds.Count] + DFS(price, special, currentNeeds));
+                }
+            }
+
+            return totalCost;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0638_ShoppingOffersTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0638_ShoppingOffersTests
+    {
+        [Test()]
+        public void ShoppingOffersTest()
+        {
+            var test = new P0638_ShoppingOffers();
+            Assert.AreEqual(14, test.ShoppingOffers(new List<int> { 2, 5 },
+                Utils.Read2DArray("[[3,0,5],[1,2,10]]"), new List<int> { 3, 2 }));
+        }
+    }
+}
