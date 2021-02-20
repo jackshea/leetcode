3c07d3840de833c92dc8941acb02edafac9a301f
谢在奎 <jackshea@163.com>
2021-02-21 00:20:28 +0800

    P5671 地图中的最高点
    BFS通过

--- LeetCode/Contest/Biweekly/C046/P5671.cs
+++ LeetCode/Contest/Biweekly/C046/P5671.cs
@@ -5,7 +5,59 @@ namespace LeetCode.Contest.Biweekly.C046
 {
     /// 地图中的最高点
     /// https://leetcode-cn.com/contest/biweekly-contest-46/problems/map-of-highest-peak/
-    public class P5671 // 超时
+    public class P5671 // BFS通过
+    {
+        private int[][] map;
+        private int m;
+        private int n;
+        private int[] dirX = { 1, 0, -1, 0 };
+        private int[] dirY = { 0, 1, 0, -1 };
+
+        public int[][] HighestPeak(int[][] isWater)
+        {
+            m = isWater.Length;
+            n = isWater[0].Length;
+            map = new int[m][];
+            for (int i = 0; i < m; i++)
+            {
+                map[i] = new int[n];
+            }
+
+            HashSet<int> visited = new HashSet<int>();
+            Queue<int[]> q = new Queue<int[]>();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (isWater[i][j] == 1)
+                    {
+                        q.Enqueue(new[] { i, j });
+                        visited.Add(i * 10000 + j);
+                    }
+                }
+            }
+
+            while (q.Count > 0)
+            {
+                var top = q.Dequeue();
+                for (int i = 0; i < dirX.Length; i++)
+                {
+                    int nx = top[0] + dirX[i];
+                    int ny = top[1] + dirY[i];
+                    if (0 <= nx && nx < m && 0 <= ny && ny < n && !visited.Contains(nx * 10000 + ny))
+                    {
+                        map[nx][ny] = map[top[0]][top[1]] + 1;
+                        q.Enqueue(new[] { nx, ny });
+                        visited.Add(nx * 10000 + ny);
+                    }
+                }
+            }
+            return map;
+        }
+    }
+
+    /// DFS超时
+    public class P5671_1 
     {
         private int[][] map;
         private int m;
@@ -59,7 +111,8 @@ namespace LeetCode.Contest.Biweekly.C046
         }
     }
 
-    public class P5671_1 // 超时
+    /// n^3 超时
+    public class P5671_2 
     {
         private int[][] map;
         private int m;
@@ -100,6 +153,10 @@ namespace LeetCode.Contest.Biweekly.C046
                     foreach (var waterCell in waterCells)
                     {
                         dist = Math.Min(dist, Math.Abs(waterCell[0] - i) + Math.Abs(waterCell[1] - j));
+                        if (dist == 1)
+                        {
+                            break;
+                        }
                     }
 
                     map[i][j] = dist;
