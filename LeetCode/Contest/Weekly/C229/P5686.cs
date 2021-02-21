edf868ec8c837bc18392cbc7aede4e25d0052840
谢在奎 <jackshea@163.com>
2021-02-21 10:51:42 +0800

    P5686 移动所有球到每个盒子所需的最小操作数

---
+++ LeetCode/Contest/Weekly/C229/P5686.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Contest.Weekly.C229
+{
+    /// 移动所有球到每个盒子所需的最小操作数
+    /// https://leetcode-cn.com/contest/weekly-contest-229/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
+    public class P5686
+    {
+        public int[] MinOperations(string boxes)
+        {
+            int[] ans = new int[boxes.Length];
+            for (int i = 0; i < boxes.Length; i++)
+            {
+                if (boxes[i] == '1')
+                {
+                    for (int j = 0; j < ans.Length; j++)
+                    {
+                        ans[j] += Math.Abs(j - i);
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
