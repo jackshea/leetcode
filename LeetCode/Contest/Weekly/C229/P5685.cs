8743952a15a6176b60b5efb9bcd7e216e36f9a2b
谢在奎 <jackshea@163.com>
2021-02-21 10:42:51 +0800

    P5685 交替合并字符串

---
+++ LeetCode/Contest/Weekly/C229/P5685.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Contest.Weekly.C229
+{
+    /// 交替合并字符串
+    /// https://leetcode-cn.com/contest/weekly-contest-229/problems/merge-strings-alternately/
+    public class P5685
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            char[] ans = new char[word1.Length + word2.Length];
+            int ansi = 0, w1 = 0, w2 = 0;
+            while (ansi < ans.Length)
+            {
+                if (w1 < word1.Length)
+                {
+                    ans[ansi++] = word1[w1++];
+                }
+
+                if (w2 < word2.Length)
+                {
+                    ans[ansi++] = word2[w2++];
+                }
+            }
+            return new string(ans);
+        }
+    }
+}
