3e5af84568c61c188a4e423a113cbae3d40d83eb
谢在奎 <jackshea@163.com>
2021-02-25 10:29:33 +0800

    P0647 回文子串

---
+++ LeetCodeTests/Problems/Medium/P0647_PalindromicSubstrings.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Problems.Medium.Tests
+{
+    /// 回文子串
+    /// https://leetcode-cn.com/problems/palindromic-substrings/
+    public class P0647_PalindromicSubstrings
+    {
+        public int CountSubstrings(string s)
+        {
+            var n = s.Length;
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int left = i, right = i;
+                while (left >= 0 && right < n)
+                {
+                    if (s[left--] == s[right++])
+                    {
+                        ans++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                left = i;
+                right = i + 1;
+                while (left >= 0 && right < n)
+                {
+                    if (s[left--] == s[right++])
+                    {
+                        ans++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
