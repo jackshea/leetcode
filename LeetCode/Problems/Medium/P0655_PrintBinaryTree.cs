ed4bac51252071ea0ade0e70c6acf6307aaf7ab6
谢在奎 <jackshea@163.com>
2021-03-04 10:56:49 +0800

    P0655 输出二叉树

---
+++ LeetCode/Problems/Medium/P0655_PrintBinaryTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 输出二叉树
+    /// https://leetcode-cn.com/problems/print-binary-tree/
+    public class P0655_PrintBinaryTree
+    {
+        private IList<IList<string>> ans = new List<IList<string>>();
+
+        public IList<IList<string>> PrintTree(TreeNode root)
+        {
+            var rowCount = MaxDepth(root);
+            var columnCount = MaxLength(rowCount);
+            for (int i = 0; i < rowCount; i++)
+            {
+                ans.Add(new List<string>(columnCount));
+                for (int j = 0; j < columnCount; j++)
+                {
+                    ans[i].Add("");
+                }
+            }
+            DFS(root, 0, columnCount - 1, 0);
+            return ans;
+        }
+
+        private int MaxDepth(TreeNode node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(MaxDepth(node.left), MaxDepth(node.right)) + 1;
+        }
+
+        private int MaxLength(int depth)
+        {
+            if (depth <= 1)
+            {
+                return 1;
+            }
+
+            return MaxLength(depth - 1) * 2 + 1;
+        }
+
+        private void DFS(TreeNode node, int left, int right, int depth)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (left > right)
+            {
+                return;
+            }
+
+            int mid = (left + right) / 2;
+            ans[depth][mid] = node.val.ToString();
+            DFS(node.left, left, mid - 1, depth + 1);
+            DFS(node.right, mid + 1, right, depth + 1);
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0655_PrintBinaryTreeTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0655_PrintBinaryTreeTests
+    {
+        [Test()]
+        public void PrintTreeTest()
+        {
+            var treeNode = ConvertArrayToBinaryTree.Build(new int?[] { 1, 2 });
+            var test = new P0655_PrintBinaryTree();
+            var printTree = test.PrintTree(treeNode);
+            Assert.AreEqual("[[,1,],[2,,]]", Utils.Write2DArray(printTree));
+        }
+    }
+}
