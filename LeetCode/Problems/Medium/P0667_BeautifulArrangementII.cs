813d239465aebc5ae7f7be6e7cd5a8155c8cbf68
谢在奎 <jackshea@163.com>
2021-03-07 18:04:21 +0800

    P0667 优美的排列 II

---
+++ LeetCode/Problems/Medium/P0667_BeautifulArrangementII.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 优美的排列 II
+    /// https://leetcode-cn.com/problems/beautiful-arrangement-ii/
+    public class P0667_BeautifulArrangementII
+    {
+        public int[] ConstructArray(int n, int k)
+        {
+            int[] ans = new int[n];
+            int a = 1, b = k + 1;
+            for (int i = 0; i < k + 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ans[i] = a;
+                    a++;
+                }
+                else
+                {
+                    ans[i] = b;
+                    b--;
+                }
+            }
+
+            for (int i = k + 1; i < n; i++)
+            {
+                ans[i] = i + 1;
+            }
+
+            return ans;
+        }
+    }
+}
