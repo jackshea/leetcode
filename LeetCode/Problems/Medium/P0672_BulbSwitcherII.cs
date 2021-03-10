cf1f480ae4bea51dccaf93ff61d0b3b4bd955729
谢在奎 <jackshea@163.com>
2021-03-10 10:06:32 +0800

    P0672 灯泡开关 Ⅱ

---
+++ LeetCode/Problems/Medium/P0672_BulbSwitcherII.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 灯泡开关 Ⅱ
+    /// https://leetcode-cn.com/problems/bulb-switcher-ii/
+    public class P0672_BulbSwitcherII
+    {
+        public int FlipLights(int n, int m)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (m == 0)
+            {
+                return 1;
+            }
+
+            if (n == 1)
+            {
+                return 2;
+            }
+
+            if (n == 2)
+            {
+                return m == 1 ? 3 : 4;
+            }
+
+            if (m == 1)
+            {
+                return 4;
+            }
+
+            if (m == 2)
+            {
+                return 7;
+            }
+
+            return 8;
+        }
+    }
+}
