8300323566ab3b5eacaa93b6a216131328d0f701
谢在奎 <jackshea@163.com>
2021-03-13 23:49:12 +0800

    P0676 实现一个魔法字典

---
+++ LeetCode/Problems/Medium/P0676_ImplementMagicDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 实现一个魔法字典
+    /// https://leetcode-cn.com/problems/implement-magic-dictionary/
+    public class MagicDictionary
+    {
+        private HashSet<string> dic = new HashSet<string>();
+        private HashSet<string> rawDic = new HashSet<string>();
+
+        /** Initialize your data structure here. */
+        public MagicDictionary()
+        {
+
+        }
+
+        public void BuildDict(string[] dictionary)
+        {
+            foreach (var word in dictionary)
+            {
+                rawDic.Add(word);
+                var charArray = word.ToCharArray();
+                for (var i = 0; i < charArray.Length; i++)
+                {
+                    var c = charArray[i];
+                    charArray[i] = '?';
+                    dic.Add(new string(charArray));
+                    charArray[i] = c;
+                }
+            }
+        }
+
+        public bool Search(string searchWord)
+        {
+            if (rawDic.Contains(searchWord))
+            {
+                return false;
+            }
+
+            var charArray = searchWord.ToCharArray();
+            for (var i = 0; i < charArray.Length; i++)
+            {
+                var c = charArray[i];
+                charArray[i] = '?';
+                if (dic.Contains(new string(charArray)))
+                {
+                    return true;
+                }
+                charArray[i] = c;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/MagicDictionaryTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class MagicDictionaryTests
+    {
+        [Test()]
+        public void SearchTest()
+        {
+            var test = new MagicDictionary();
+            test.BuildDict(new string[] { "hello", "leetcode" });
+            Assert.IsFalse(test.Search("hello"));
+            Assert.IsTrue(test.Search("hhllo"));
+            Assert.IsFalse(test.Search("hell"));
+            Assert.IsFalse(test.Search("leetcoded"));
+        }
+
+        [Test()]
+        public void SearchTest1()
+        {
+            var test = new MagicDictionary();
+            test.BuildDict(new string[] { "hello","hallo", "leetcode" });
+            Assert.IsTrue(test.Search("hello"));
+            Assert.IsTrue(test.Search("hhllo"));
+            Assert.IsFalse(test.Search("hell"));
+            Assert.IsFalse(test.Search("leetcoded"));
+        }
+    }
+}
