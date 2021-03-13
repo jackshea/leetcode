2cc5b62964adae7bac2fb4e1367e38acdd5e9e36
谢在奎 <jackshea@163.com>
2021-03-14 00:00:29 +0800

    P0676 实现一个魔法字典

--- LeetCode/Problems/Medium/P0676_ImplementMagicDictionary.cs
+++ LeetCode/Problems/Medium/P0676_ImplementMagicDictionary.cs
@@ -6,7 +6,7 @@ namespace LeetCode.Problems.Medium
     /// https://leetcode-cn.com/problems/implement-magic-dictionary/
     public class MagicDictionary
     {
-        private HashSet<string> dic = new HashSet<string>();
+        private Dictionary<string, int> dic = new Dictionary<string, int>();
         private HashSet<string> rawDic = new HashSet<string>();
 
         /** Initialize your data structure here. */
@@ -25,7 +25,9 @@ namespace LeetCode.Problems.Medium
                 {
                     var c = charArray[i];
                     charArray[i] = '?';
-                    dic.Add(new string(charArray));
+                    var key = new string(charArray);
+                    dic.TryGetValue(key, out var count);
+                    dic[key] = count + 1;
                     charArray[i] = c;
                 }
             }
@@ -33,21 +35,23 @@ namespace LeetCode.Problems.Medium
 
         public bool Search(string searchWord)
         {
-            if (rawDic.Contains(searchWord))
-            {
-                return false;
-            }
-
             var charArray = searchWord.ToCharArray();
             for (var i = 0; i < charArray.Length; i++)
             {
                 var c = charArray[i];
                 charArray[i] = '?';
-                if (dic.Contains(new string(charArray)))
+                var key = new string(charArray);
+                charArray[i] = c;
+                dic.TryGetValue(key, out var count);
+                if (count == 1 && rawDic.Contains(searchWord))
+                {
+                    continue;
+                }
+
+                if (count >= 1)
                 {
                     return true;
                 }
-                charArray[i] = c;
             }
 
             return false;
