47e5d7dd6d5d361af90bff24f717a4058f3baf80
谢在奎 <jackshea@163.com>
2021-03-19 11:22:45 +0800

    P0698 划分为k个相等的子集

---
+++ LeetCode/Problems/Medium/P0698_PartitionToKEqualSumSubsets.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 划分为k个相等的子集
+    /// https://leetcode-cn.com/problems/partition-to-k-equal-sum-subsets/
+    public class P0698_PartitionToKEqualSumSubsets
+    {
+        public bool CanPartitionKSubsets(int[] nums, int k)
+        {
+            int sum = 0, max = 0;
+            var n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                sum += nums[i];
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                }
+            }
+
+            int avg = sum / k;
+            if (avg * k != sum || max > avg)
+            {
+                return false;
+            }
+            bool[] used = new bool[n];
+            return BackTracking(nums, k, avg, 0, 0, used);
+        }
+
+        private bool BackTracking(int[] nums, int k, int target, int cur, int start, bool[] used)
+        {
+            if (k == 0) return true;
+            if (cur == target)
+            {
+                return BackTracking(nums, k - 1, target, 0, 0, used);
+            }
+            for (int i = start; i < nums.Length; i++)
+            {
+                if (!used[i] && cur + nums[i] <= target)
+                {
+                    used[i] = true;
+                    if (BackTracking(nums, k, target, cur + nums[i], i + 1, used)) return true;
+                    used[i] = false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
