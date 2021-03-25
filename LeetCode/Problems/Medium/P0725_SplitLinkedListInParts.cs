aa807e0341e24bb8aa546cdbfaf49ff3e926a4ae
谢在奎 <jackshea@163.com>
2021-03-25 11:14:12 +0800

    P0725 分隔链表

---
+++ LeetCode/Problems/Medium/P0725_SplitLinkedListInParts.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 分隔链表
+    /// https://leetcode-cn.com/problems/split-linked-list-in-parts/
+    public class P0725_SplitLinkedListInParts
+    {
+        public ListNode[] SplitListToParts(ListNode root, int k)
+        {
+            ListNode cur = root;
+            int len = 0;
+            while (cur != null)
+            {
+                len++;
+                cur = cur.next;
+            }
+
+            int subLen = len / k;
+            int remain = len % k;
+            ListNode[] ans = new ListNode[k];
+            cur = root;
+            for (int i = 0; i < k; i++)
+            {
+                ListNode head = new ListNode(-1);
+                ListNode subCur = head;
+                int extra = i < remain ? 1 : 0;
+                for (int j = 0; j < subLen + extra; j++)
+                {
+                    subCur.next = cur;
+                    cur = cur.next;
+                    subCur = subCur.next;
+                    if (j == subLen + extra - 1)
+                    {
+                        subCur.next = null;
+                    }
+                }
+
+                ans[i] = head.next;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0725_SplitLinkedListInPartsTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0725_SplitLinkedListInPartsTests
+    {
+        [Test()]
+        public void SplitListToPartsTest()
+        {
+            var test = new P0725_SplitLinkedListInParts();
+            var ans = test.SplitListToParts(Utils.BuildList(new[] { 1, 2, 3 }), 5);
+            int[][] actual = new int[5][];
+            for (int i = 0; i < actual.Length; i++)
+            {
+                actual[i] = Utils.ListToArray(ans[i]);
+            }
+
+            int[][] except = new[]
+            {
+                new[] {1},
+                new[] {2},
+                new[] {3},
+                new int[0],
+                new int[0]
+            };
+
+            CollectionAssert.AreEqual(except, actual);
+        }
+    }
+}
