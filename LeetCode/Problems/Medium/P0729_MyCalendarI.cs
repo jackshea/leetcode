bcc7fb3022b5b4661458748cc3d4153d5268505b
谢在奎 <jackshea@163.com>
2021-03-26 10:10:43 +0800

    P0729 我的日程安排表 I

---
+++ LeetCode/Problems/Medium/P0729_MyCalendarI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 我的日程安排表 I
+    /// https://leetcode-cn.com/problems/my-calendar-i/
+    public class MyCalendar
+    {
+        List<int[]> ranges = new List<int[]>();
+
+        public MyCalendar()
+        {
+
+        }
+
+        public bool Book(int start, int end)
+        {
+            foreach (var range in ranges)
+            {
+                if (range[0] < end && range[1] > start)
+                {
+                    return false;
+                }
+            }
+            ranges.Add(new[] { start, end });
+            return true;
+        }
+    }
+
+}
