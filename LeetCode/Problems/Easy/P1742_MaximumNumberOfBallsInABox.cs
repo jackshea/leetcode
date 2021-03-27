ea23f2c6b25dc2fb1f2c921c7b38c7ecc3cb398f
谢在奎 <jackshea@163.com>
2021-03-27 23:34:50 +0800

    P1742 盒子中小球的最大数量

---
+++ LeetCode/Problems/Easy/P1742_MaximumNumberOfBallsInABox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 盒子中小球的最大数量
+    /// https://leetcode-cn.com/problems/maximum-number-of-balls-in-a-box/
+    public class P1742_MaximumNumberOfBallsInABox
+    {
+        public int CountBalls(int lowLimit, int highLimit)
+        {
+            Dictionary<int, int> boxes = new Dictionary<int, int>();
+            int ans = 0;
+            for (int i = lowLimit; i <= highLimit; i++)
+            {
+                int num = i;
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+
+                boxes.TryGetValue(sum, out var count);
+                boxes[sum] = ++count;
+                if (ans < count)
+                {
+                    ans = count;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
