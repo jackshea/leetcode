11fbf375808881117b26355f5a3fa5e82f88679f
谢在奎 <jackshea@163.com>
2021-03-29 10:50:20 +0800

    P0735 行星碰撞

---
+++ LeetCode/Problems/Medium/P0735_AsteroidCollision.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 行星碰撞
+    /// https://leetcode-cn.com/problems/asteroid-collision/
+    public class P0735_AsteroidCollision
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            int len = 0;
+            for (int i = 0; i < asteroids.Length; i++)
+            {
+                if (asteroids[i] > 0)
+                {
+                    asteroids[len++] = asteroids[i];
+                }
+                else
+                {
+                    while (len > 0 && asteroids[len - 1] > 0 && asteroids[len - 1] + asteroids[i] < 0)
+                    {
+                        len--;
+                    }
+
+                    if (len > 0 && asteroids[len - 1] + asteroids[i] == 0)
+                    {
+                        len--;
+                    }
+                    else if (len <= 0 || asteroids[len - 1] < 0)
+                    {
+                        asteroids[len++] = asteroids[i];
+                    }
+                }
+            }
+            int[] ans = new int[len];
+            Array.Copy(asteroids, ans, len);
+            return ans;
+        }
+    }
+}
