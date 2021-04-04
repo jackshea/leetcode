07296f657219b74b3e658b47e0a7c2fa8f8cbd73
谢在奎 <jackshea@163.com>
2021-04-04 21:57:39 +0800

    P0781 森林中的兔子

---
+++ LeetCode/Problems/Medium/P0781_RabbitsInForest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 森林中的兔子
+    /// https://leetcode-cn.com/problems/rabbits-in-forest/
+    public class P0781_RabbitsInForest
+    {
+        public int NumRabbits(int[] answers)
+        {
+            if (answers.Length == 0)
+            {
+                return 0;
+            }
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            foreach (var answer in answers)
+            {
+                freq.TryGetValue(answer, out var count);
+                freq[answer] = count + 1;
+            }
+
+            int ans = 0;
+            foreach (var kvp in freq)
+            {
+                int groupCap = kvp.Key + 1;
+                int groupCount = (int)Math.Ceiling(1.0 * kvp.Value / groupCap);
+                ans += groupCap * groupCount;
+            }
+
+            return ans;
+        }
+    }
+}
