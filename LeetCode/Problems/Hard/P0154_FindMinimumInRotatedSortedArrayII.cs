f6de98359e8d7a43b671008bb410149f8942298a
谢在奎 <jackshea@163.com>
2021-04-09 10:36:32 +0800

    P0154 寻找旋转排序数组中的最小值 II

---
+++ LeetCode/Problems/Hard/P0154_FindMinimumInRotatedSortedArrayII.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Problems.Hard
+{
+    /// 寻找旋转排序数组中的最小值 II
+    /// https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/
+    public class P0154_FindMinimumInRotatedSortedArrayII
+    {
+        public int FindMin(int[] nums)
+        {
+            int left = 0, right = nums.Length - 1;
+            while (left < right)
+            {
+                while (left < right && nums[left] == nums[left + 1])
+                {
+                    left++;
+                }
+
+                while (left < right && nums[right] == nums[right - 1])
+                {
+                    right--;
+                }
+
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            return nums[left];
+        }
+    }
+}
