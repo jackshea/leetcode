74dd5ad96e91da51e8904df6b4e7e818c2e2a3f4
谢在奎 <jackshea@163.com>
2021-04-10 23:56:53 +0800

    P1779 找到最近的有相同 X 或 Y 坐标的点

---
+++ LeetCode/Problems/Easy/P1779_FindNearestPointThatHasTheSameXOrYCoordinate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 找到最近的有相同 X 或 Y 坐标的点
+    /// https://leetcode-cn.com/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
+    public class P1779_FindNearestPointThatHasTheSameXOrYCoordinate
+    {
+        public int NearestValidPoint(int x, int y, int[][] points)
+        {
+            int ans = -1;
+            int minDist = int.MaxValue;
+            for (var i = 0; i < points.Length; i++)
+            {
+                var point = points[i];
+                if (point[0] != x && point[1] != y)
+                {
+                    continue;
+                }
+
+                int dist = Math.Abs(x - point[0]) + Math.Abs(y - point[1]);
+                if (dist<minDist)
+                {
+                    minDist = dist;
+                    ans = i;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
