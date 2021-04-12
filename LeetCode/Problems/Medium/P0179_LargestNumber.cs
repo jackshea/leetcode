7f1feeb0affc2d2af4c274c51d3b75168415cb84
谢在奎 <jackshea@163.com>
2021-04-12 10:14:33 +0800

    P0179 最大数

---
+++ LeetCode/Problems/Medium/P0179_LargestNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 最大数
+    /// https://leetcode-cn.com/problems/largest-number/
+    public class P0179_LargestNumber
+    {
+        public string LargestNumber(int[] nums)
+        {
+            var list = nums.Select(p => p.ToString()).ToList();
+            list.Sort((a, b) => -String.Compare(a + b, b + a, StringComparison.Ordinal));
+            if (list[0] == "0")
+            {
+                return "0";
+            }
+            StringBuilder ans = new StringBuilder();
+            foreach (var item in list)
+            {
+                ans.Append(item);
+            }
+
+            return ans.ToString();
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0179_LargestNumberTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0179_LargestNumberTests
+    {
+        [Test()]
+        public void LargestNumberTest()
+        {
+            var test = new P0179_LargestNumber();
+            Assert.AreEqual("9534330", test.LargestNumber(new int[] { 3, 30, 34, 5, 9 }));
+        }
+    }
+}
