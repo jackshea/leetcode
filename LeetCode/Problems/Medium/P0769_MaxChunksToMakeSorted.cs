08cdd73b097229312aeb57361b5fd6d22db12b54
谢在奎 <jackshea@163.com>
2021-04-13 10:44:42 +0800

    P0768 最多能完成排序的块

---
+++ LeetCode/Problems/Medium/P0769_MaxChunksToMakeSorted.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 最多能完成排序的块
+    /// https://leetcode-cn.com/problems/max-chunks-to-make-sorted/
+    public class P0769_MaxChunksToMakeSorted
+    {
+        public int MaxChunksToSorted(int[] arr)
+        {
+            int max = 0;
+            int ans = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                max = Math.Max(max, arr[i]);
+                if (max==i)
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
