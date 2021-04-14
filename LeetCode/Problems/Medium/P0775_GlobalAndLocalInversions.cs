db08846fdf1f6193ba5f2a4924be6f91b357f25f
谢在奎 <jackshea@163.com>
2021-04-14 09:59:26 +0800

    P0775 全局倒置与局部倒置

---
+++ LeetCode/Problems/Medium/P0775_GlobalAndLocalInversions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 全局倒置与局部倒置
+    /// https://leetcode-cn.com/problems/global-and-local-inversions/
+    public class P0775_GlobalAndLocalInversions
+    {
+        public bool IsIdealPermutation(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (Math.Abs(i - nums[i]) > 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
