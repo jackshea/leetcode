2bb080cabff06c1eaa220f6b006950b24bc1652b
谢在奎 <jackshea@163.com>
2021-04-18 23:41:22 +0800

    P1784 检查二进制字符串字段

---
+++ LeetCode/Problems/Easy/P1784_CheckIfBinaryStringHasAtMostOneSegmentOfOnes.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 检查二进制字符串字段
+    /// https://leetcode-cn.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+    public class P1784_CheckIfBinaryStringHasAtMostOneSegmentOfOnes
+    {
+        public bool CheckOnesSegment(string s)
+        {
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == '0' && s[i + 1] == '1')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
