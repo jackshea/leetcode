f42dd495e8f3d57fe9b5f6354db32b2c9038b7c7
谢在奎 <jackshea@163.com>
2021-04-19 14:14:52 +0800

    P0779 第K个语法符号

---
+++ LeetCode/Problems/Medium/P0779_KThSymbolInGrammar.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 第K个语法符号
+    /// https://leetcode-cn.com/problems/k-th-symbol-in-grammar/
+    public class P0779_KThSymbolInGrammar
+    {
+        public int KthGrammar(int N, int K)
+        {
+            return Recursion(N, K - 1);
+        }
+
+        public int Recursion(int N, int K)
+        {
+            if (N == 1)
+            {
+                return 0;
+            }
+
+            int num = Recursion(N - 1, K / 2);
+            if (K % 2 == 0)
+            {
+                return num == 1 ? 1 : 0;
+            }
+            else
+            {
+                return num == 1 ? 0 : 1;
+            }
+        }
+    }
+}
