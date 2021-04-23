89f9b2960a3496c296012e9bb435ceb848cb9425
谢在奎 <jackshea@163.com>
2021-04-23 17:29:08 +0800

    P0789 逃脱阻碍者

---
+++ LeetCode/Problems/Medium/P0789_EscapeTheGhosts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 逃脱阻碍者
+    /// https://leetcode-cn.com/problems/escape-the-ghosts/
+    public class P0789_EscapeTheGhosts
+    {
+        public bool EscapeGhosts(int[][] ghosts, int[] target)
+        {
+            int dist = Math.Abs(target[0]) + Math.Abs(target[1]);
+            foreach (var ghost in ghosts)
+            {
+                if (Math.Abs(ghost[0] - target[0]) + Math.Abs(ghost[1] - target[1]) <= dist)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
