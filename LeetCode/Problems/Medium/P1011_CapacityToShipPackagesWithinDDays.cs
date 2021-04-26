d0d0db6815e431c9fa296f6e46ce115d5370eb86
谢在奎 <jackshea@163.com>
2021-04-26 10:44:26 +0800

    P1011 在 D 天内送达包裹的能力

---
+++ LeetCode/Problems/Medium/P1011_CapacityToShipPackagesWithinDDays.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 在 D 天内送达包裹的能力
+    /// https://leetcode-cn.com/problems/capacity-to-ship-packages-within-d-days/
+    public class P1011_CapacityToShipPackagesWithinDDays
+    {
+        public int ShipWithinDays(int[] weights, int D)
+        {
+            int left = weights.Max();
+            int right = weights.Sum();
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                int realDays = Days(weights, mid);
+                if (realDays > D)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+
+        // 已知载重量，求所需天数
+        public int Days(int[] weights, int cap)
+        {
+            int day = 1;
+            int sum = 0;
+            foreach (var weight in weights)
+            {
+                if (sum + weight <= cap)
+                {
+                    sum += weight;
+                }
+                else
+                {
+                    sum = weight;
+                    day++;
+                }
+            }
+
+            if (sum > cap)
+            {
+                day++;
+            }
+
+            return day;
+        }
+    }
+}
