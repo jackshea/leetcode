cea43f074007c376c87efe33d706a2d82a909d8e
谢在奎 <jackshea@163.com>
2021-04-28 10:31:20 +0800

    P0792 匹配子序列的单词数

---
+++ LeetCode/Problems/Medium/P0792_NumberOfMatchingSubsequences.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 匹配子序列的单词数
+    /// https://leetcode-cn.com/problems/number-of-matching-subsequences/
+    public class P0792_NumberOfMatchingSubsequences
+    {
+        public int NumMatchingSubseq(string s, string[] words)
+        {
+            List<Node>[] heads = new List<Node>[26];
+            int ans = 0;
+            for (int i = 0; i < heads.Length; i++)
+            {
+                heads[i] = new List<Node>();
+            }
+
+            foreach (var word in words)
+            {
+                heads[word[0] - 'a'].Add(new Node
+                {
+                    Word = word,
+                    Index = 0
+                });
+            }
+
+            foreach (var c in s)
+            {
+
+                List<Node> pre = heads[c - 'a'];
+                heads[c - 'a'] = new List<Node>();
+                foreach (var node in pre)
+                {
+                    node.Index++;
+                    if (node.Index == node.Word.Length)
+                    {
+                        ans++;
+                    }
+                    else
+                    {
+                        heads[node.Word[node.Index] - 'a'].Add(node);
+                    }
+                }
+                pre.Clear();
+            }
+
+            return ans;
+        }
+
+        private class Node
+        {
+            public string Word;
+            public int Index;
+        }
+    }
+}
