7e04c3f92b7da63c36a73058c380f86bb3f6a245
谢在奎 <jackshea@163.com>
2021-05-02 23:41:57 +0800

    P1796 字符串中第二大的数字

---
+++ LeetCode/Problems/Easy/P1796_SecondLargestDigitInAString.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 字符串中第二大的数字
+    /// https://leetcode-cn.com/problems/second-largest-digit-in-a-string/
+    public class P1796_SecondLargestDigitInAString
+    {
+        public int SecondHighest(string s)
+        {
+            bool[] hasNum = new bool[10];
+            foreach (var c in s)
+            {
+                if (char.IsNumber(c))
+                {
+                    hasNum[c - '0'] = true;
+                }
+            }
+
+            int top1 = -1;
+            for (int i = 9; i >= 0; i--)
+            {
+                if (hasNum[i])
+                {
+                    if (top1 == -1)
+                    {
+                        top1 = i;
+                    }
+                    else
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
