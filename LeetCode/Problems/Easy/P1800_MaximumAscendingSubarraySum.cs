0935a60850e593d2057eb430c3ef850dea4f672f
谢在奎 <jackshea@163.com>
2021-05-03 22:13:36 +0800

    P1800 最大升序子数组和

---
+++ LeetCode/Problems/Easy/P1800_MaximumAscendingSubarraySum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 最大升序子数组和
+    /// https://leetcode-cn.com/problems/maximum-ascending-subarray-sum/
+    public class P1800_MaximumAscendingSubarraySum
+    {
+        public int MaxAscendingSum(int[] nums)
+        {
+            int sum = nums[0];
+            int ans = sum;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    sum += nums[i];
+                }
+                else
+                {
+                    sum = nums[i];
+                }
+
+                ans = Math.Max(ans, sum);
+            }
+
+            return ans;
+        }
+    }
+}
