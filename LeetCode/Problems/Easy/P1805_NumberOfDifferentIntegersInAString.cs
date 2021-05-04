f19a33fef279d1837a92a8a15fe5dd664dfcb1db
谢在奎 <jackshea@163.com>
2021-05-04 21:54:27 +0800

    P1805 字符串中不同整数的数目

---
+++ LeetCode/Problems/Easy/P1805_NumberOfDifferentIntegersInAString.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 字符串中不同整数的数目
+    /// https://leetcode-cn.com/problems/number-of-different-integers-in-a-string/
+    public class P1805_NumberOfDifferentIntegersInAString
+    {
+        public int NumDifferentIntegers(string word)
+        {
+            HashSet<string> numbers = new HashSet<string>();
+            int start = 0, len = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsDigit(word[i]))
+                {
+                    len++;
+                }
+                else
+                {
+                    if (len != 0)
+                    {
+                        while (len > 0 && word[start] == '0')
+                        {
+                            start++;
+                            len--;
+                        }
+                        numbers.Add(word.Substring(start, len));
+                    }
+                    start = i + 1;
+                    len = 0;
+                }
+            }
+            if (len != 0)
+            {
+                while (len > 0 && word[start] == '0')
+                {
+                    start++;
+                    len--;
+                }
+                numbers.Add(word.Substring(start, len));
+            }
+
+            return numbers.Count;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Easy/P1805_NumberOfDifferentIntegersInAStringTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Easy.Tests
+{
+    [TestFixture()]
+    public class P1805_NumberOfDifferentIntegersInAStringTests
+    {
+        [Test()]
+        public void NumDifferentIntegersTest()
+        {
+            var test = new P1805_NumberOfDifferentIntegersInAString();
+            Assert.AreEqual(1,test.NumDifferentIntegers("a1b01c001"));
+        }
+
+        [Test()]
+        public void NumDifferentIntegersTest2()
+        {
+            var test = new P1805_NumberOfDifferentIntegersInAString();
+            Assert.AreEqual(1, test.NumDifferentIntegers("0a0"));
+        }
+    }
+}
