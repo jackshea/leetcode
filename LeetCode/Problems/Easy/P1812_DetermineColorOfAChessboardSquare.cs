29f903b5bb9b5758e9ef02f33275c57badf67c3a
谢在奎 <jackshea@163.com>
2021-05-05 21:48:29 +0800

    P1812 判断国际象棋棋盘中一个格子的颜色

---
+++ LeetCode/Problems/Easy/P1812_DetermineColorOfAChessboardSquare.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 判断国际象棋棋盘中一个格子的颜色
+    /// https://leetcode-cn.com/problems/determine-color-of-a-chessboard-square/
+    public class P1812_DetermineColorOfAChessboardSquare
+    {
+        public bool SquareIsWhite(string coordinates)
+        {
+            int x = coordinates[0] - 'a' + 1;
+            int y = coordinates[1] - '0';
+            return x % 2 != 0 ^ y % 2 != 0;
+        }
+    }
+}
