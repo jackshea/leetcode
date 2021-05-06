5b8da4529a6406b5ef68f89392fe02db6ed2dca4
谢在奎 <jackshea@163.com>
2021-05-06 11:40:15 +0800

    P0795 区间子数组个数

---
+++ LeetCode/Problems/Medium/P0795_NumberOfSubarraysWithBoundedMaximum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 区间子数组个数
+    /// https://leetcode-cn.com/problems/number-of-subarrays-with-bounded-maximum/
+    public class P0795_NumberOfSubarraysWithBoundedMaximum
+    {
+        public int NumSubarrayBoundedMax(int[] A, int L, int R)
+        {
+            return Count(A, R) - Count(A, L - 1);
+        }
+
+        public int Count(int[] A, int bound)
+        {
+            int ans = 0, cur = 0;
+            foreach (var num in A)
+            {
+                if (num <= bound)
+                {
+                    cur++;
+                    ans += cur;
+                }
+                else
+                {
+                    cur = 0;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
