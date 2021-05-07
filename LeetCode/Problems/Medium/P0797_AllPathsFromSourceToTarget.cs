9981cd036ee9fe3c3fed55fd463a4adff8290a30
谢在奎 <jackshea@163.com>
2021-05-07 10:51:03 +0800

    P0797 所有可能的路径

---
+++ LeetCode/Problems/Medium/P0797_AllPathsFromSourceToTarget.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 所有可能的路径
+    /// https://leetcode-cn.com/problems/all-paths-from-source-to-target/
+    public class P0797_AllPathsFromSourceToTarget
+    {
+        private IList<IList<int>> paths = new List<IList<int>>();
+        private HashSet<int> visited = new HashSet<int>();
+        public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+        {
+            DFS(graph, 0, new List<int>());
+            return paths;
+        }
+
+        public void DFS(int[][] graph, int start, List<int> curPath)
+        {
+            if (visited.Contains(start))
+            {
+                return;
+            }
+            curPath.Add(start);
+            visited.Add(start);
+            if (start == graph.Length - 1)
+            {
+                paths.Add(new List<int>(curPath));
+            }
+            else
+            {
+                foreach (var g in graph[start])
+                {
+                    DFS(graph, g, curPath);
+                }
+            }
+
+            visited.Remove(start);
+            curPath.RemoveAt(curPath.Count - 1);
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0797_AllPathsFromSourceToTargetTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0797_AllPathsFromSourceToTargetTests
+    {
+        [Test()]
+        public void AllPathsSourceTargetTest()
+        {
+            var test = new P0797_AllPathsFromSourceToTarget();
+            int[][] g = Utils.Read2DArray(" [[1,2],[3],[3],[]]");
+            int[][] expect = Utils.Read2DArray("[[0,1,3],[0,2,3]]");
+            var ans = test.AllPathsSourceTarget(g);
+            CollectionAssert.AreEquivalent(expect, ans);
+        }
+    }
+}
