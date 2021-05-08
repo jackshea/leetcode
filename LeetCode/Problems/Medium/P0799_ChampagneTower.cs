2c69e939c0e3a3edf530258aea9c8fe670a47e95
谢在奎 <jackshea@163.com>
2021-05-08 11:12:48 +0800

    P0799 香槟塔

---
+++ LeetCode/Problems/Medium/P0799_ChampagneTower.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 香槟塔
+    /// https://leetcode-cn.com/problems/champagne-tower/
+    public class P0799_ChampagneTower
+    {
+        public double ChampagneTower(int poured, int query_row, int query_glass)
+        {
+            double[] bottomRow = new double[101];
+            bottomRow[0] = poured;
+            for (int i = 1; i <= query_row; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    bottomRow[j] = bottomRow[j] > 1 ? bottomRow[j] - 1 : 0;
+                }
+                for (int j = i; j > 0; j--)
+                {
+                    bottomRow[j] = (bottomRow[j] + bottomRow[j - 1]) / 2.0;
+                }
+
+                bottomRow[0] = bottomRow[0] / 2.0;
+            }
+
+            return bottomRow[query_glass] >= 1 ? 1 : bottomRow[query_glass];
+        }
+    }
+}
