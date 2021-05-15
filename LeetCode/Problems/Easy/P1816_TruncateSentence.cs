4c8bbeab8fcb9928a56cd8989fa77815c25a7de9
谢在奎 <jackshea@163.com>
2021-05-15 22:57:51 +0800

    P1816 截断句子

---
+++ LeetCode/Problems/Easy/P1816_TruncateSentence.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    public class P1816_TruncateSentence
+    {
+        /// 截断句子
+        /// https://leetcode-cn.com/problems/truncate-sentence/
+        public string TruncateSentence(string s, int k)
+        {
+            int i = 0;
+            for (; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    k--;
+                }
+
+                if (k <= 0)
+                {
+                    break;
+                }
+            }
+
+            return s.Substring(0, i);
+        }
+    }
+}
