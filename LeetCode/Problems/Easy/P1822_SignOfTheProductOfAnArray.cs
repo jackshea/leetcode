5940007cdbc552d00472bb14b17e8ae1eaf38a41
谢在奎 <jackshea@163.com>
2021-05-16 21:58:48 +0800

    P1822 数组元素积的符号

---
+++ LeetCode/Problems/Easy/P1822_SignOfTheProductOfAnArray.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 数组元素积的符号
+    /// https://leetcode-cn.com/problems/sign-of-the-product-of-an-array/
+    public class P1822_SignOfTheProductOfAnArray
+    {
+        public int ArraySign(int[] nums)
+        {
+            int sign = 1;
+            foreach (var num in nums)
+            {
+                if (num < 0)
+                {
+                    sign = -sign;
+                }
+                else if (num == 0)
+                {
+                    return 0;
+                }
+            }
+
+            return sign;
+        }
+    }
+}
