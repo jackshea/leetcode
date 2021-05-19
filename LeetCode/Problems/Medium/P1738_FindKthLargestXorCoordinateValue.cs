432045f9c9c622128d54e1e75e0f314273e220b2
谢在奎 <jackshea@163.com>
2021-05-19 10:19:25 +0800

    P1738 找出第 K 大的异或坐标值

---
+++ LeetCode/Problems/Medium/P1738_FindKthLargestXorCoordinateValue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 找出第 K 大的异或坐标值
+    /// https://leetcode-cn.com/problems/find-kth-largest-xor-coordinate-value/
+    public class P1738_FindKthLargestXorCoordinateValue
+    {
+        public int KthLargestValue(int[][] matrix, int k)
+        {
+            var m = matrix.Length;
+            var n = matrix[0].Length;
+            int[,] dp = new int[m + 1, n + 1];
+            List<int> res = new List<int>();
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    dp[i, j] = dp[i - 1, j - 1] ^ dp[i, j - 1] ^ dp[i - 1, j] ^ matrix[i - 1][j - 1];
+                    res.Add(dp[i, j]);
+                }
+            }
+            res.Sort((a,b)=>-a.CompareTo(b));
+            return res[k - 1];
+        }
+    }
+}
