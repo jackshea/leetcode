d04fb8867600eac1371ff3ed373904fd977f72c4
谢在奎 <jackshea@163.com>
2021-05-20 10:40:21 +0800

    P0808 分汤

---
+++ LeetCode/Problems/Medium/P0808_SoupServings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 分汤
+    /// https://leetcode-cn.com/problems/soup-servings/
+    public class P0808_SoupServings
+    {
+        public double SoupServings(int n)
+        {
+            n = n / 25 + (n % 25 > 0 ? 1 : 0);
+            if (n >= 500)
+            {
+                return 1.0;
+            }
+            double[,] memo = new double[n + 1, n + 1];
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    int k = i - j;
+                    if (k < 0 || k > n)
+                    {
+                        continue;
+                    }
+
+                    double ans = 0;
+                    if (j == 0)
+                    {
+                        ans = 1;
+                    }
+                    if (j == 0 && k == 0)
+                    {
+                        ans = 0.5;
+                    }
+                    if (j > 0 && k > 0)
+                    {
+                        ans = 0.25 * (memo[M(j - 4), k] + memo[M(j - 3), M(k - 1)] +
+                                      memo[M(j - 2), M(k - 2)] + memo[M(j - 1), M(k - 3)]);
+                    }
+
+                    memo[j, k] = ans;
+                }
+            }
+
+            return memo[n, n];
+        }
+
+        public int M(int x) { return Math.Max(0, x); }
+    }
+}
