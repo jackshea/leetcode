8ef205af5a57bff2de8655d70e2945ca05c1248b
谢在奎 <jackshea@163.com>
2021-05-22 23:32:53 +0800

    P0810 黑板异或游戏

---
+++ LeetCode/Problems/Hard/P0810_ChalkboardXorGame.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems.Hard
+{
+    /// 黑板异或游戏
+    /// https://leetcode-cn.com/problems/chalkboard-xor-game/
+    public class P0810_ChalkboardXorGame
+    {
+        public bool XorGame(int[] nums)
+        {
+            if (nums.Length%2==0)
+            {
+                return true;
+            }
+
+            int xor = 0;
+            foreach (var num in nums)
+            {
+                xor ^= num;
+            }
+
+            return xor == 0;
+        }
+    }
+}
