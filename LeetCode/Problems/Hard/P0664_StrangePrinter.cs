5504b72451a76d18b3d523651cb8c9bea6a84bb9
谢在奎 <jackshea@163.com>
2021-05-24 10:53:33 +0800

    P0664 奇怪的打印机

---
+++ LeetCode/Problems/Hard/P0664_StrangePrinter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.Problems.Hard
+{
+    /// 奇怪的打印机
+    /// https://leetcode-cn.com/problems/strange-printer/
+    public class P0664_StrangePrinter
+    {
+        public int StrangePrinter(string s)
+        {
+            int n = s.Length;
+            int[,] dp = new int[n, n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                dp[i, i] = 1;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        dp[i, j] = dp[i, j - 1];
+                    }
+                    else
+                    {
+                        int min = int.MaxValue;
+                        for (int k = i; k < j; k++)
+                        {
+                            min = Math.Min(min, dp[i, k] + dp[k + 1, j]);
+                        }
+
+                        dp[i, j] = min;
+                    }
+                }
+            }
+
+            return dp[0, n - 1];
+        }
+    }
+}
