22a9bfedcd0d3103d59c51e02cb18ab8c97bfae3
谢在奎 <jackshea@163.com>
2021-05-28 17:08:26 +0800

    P1827 最少操作使数组递增

---
+++ LeetCode/Problems/Easy/P1827_MinimumOperationsToMakeTheArrayIncreasing.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 最少操作使数组递增
+    /// https://leetcode-cn.com/problems/minimum-operations-to-make-the-array-increasing/
+    public class P1827_MinimumOperationsToMakeTheArrayIncreasing
+    {
+        public int MinOperations(int[] nums)
+        {
+            int ans = 0;
+            int max = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                }
+                else
+                {
+                    max++;
+                    ans += max - nums[i];
+                }
+            }
+
+            return ans;
+        }
+    }
+}
