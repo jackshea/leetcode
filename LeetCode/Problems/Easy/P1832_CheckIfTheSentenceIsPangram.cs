e12b8130d6388e77033ed1b37003c60fdcdf2880
谢在奎 <jackshea@163.com>
2021-05-30 22:50:50 +0800

    P1832 判断句子是否为全字母句

---
+++ LeetCode/Problems/Easy/P1832_CheckIfTheSentenceIsPangram.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 判断句子是否为全字母句
+    /// https://leetcode-cn.com/problems/check-if-the-sentence-is-pangram/
+    public class P1832_CheckIfTheSentenceIsPangram
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            int count = 0;
+            int[] freq = new int[26];
+            foreach (var c in sentence)
+            {
+                if (freq[c - 'a'] == 0)
+                {
+                    count++;
+                    if (count >= 26)
+                    {
+                        return true;
+                    }
+                }
+
+                freq[c - 'a']++;
+            }
+
+            return false;
+        }
+    }
+}
