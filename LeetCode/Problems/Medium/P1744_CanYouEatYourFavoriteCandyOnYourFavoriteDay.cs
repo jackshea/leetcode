e8df49aefda53a201de5acb408fdb47865b0b61d
谢在奎 <jackshea@163.com>
2021-06-01 11:23:58 +0800

    P1744 你能在你最喜欢的那天吃到你最喜欢的糖果吗?

---
+++ LeetCode/Problems/Medium/P1744_CanYouEatYourFavoriteCandyOnYourFavoriteDay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 你能在你最喜欢的那天吃到你最喜欢的糖果吗？
+    /// https://leetcode-cn.com/problems/can-you-eat-your-favorite-candy-on-your-favorite-day/
+    public class P1744_CanYouEatYourFavoriteCandyOnYourFavoriteDay
+    {
+        public bool[] CanEat(int[] candiesCount, int[][] queries)
+        {
+            long[] preSum = new long[candiesCount.Length];
+            preSum[0] = candiesCount[0];
+            for (int i = 1; i < candiesCount.Length; i++)
+            {
+                preSum[i] = preSum[i - 1] + candiesCount[i];
+            }
+
+            bool[] ans = new bool[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                var query = queries[i];
+                int favoriteType = query[0], favoriteDay = query[1], dailyCap = query[2];
+
+                long x1 = favoriteDay + 1;
+                long y1 = x1 * dailyCap;
+                long x2 = favoriteType == 0 ? 1 : preSum[favoriteType - 1] + 1;
+                long y2 = preSum[favoriteType];
+
+                ans[i] = !(x1 > y2 || y1 < x2);
+            }
+
+            return ans;
+        }
+    }
+}
