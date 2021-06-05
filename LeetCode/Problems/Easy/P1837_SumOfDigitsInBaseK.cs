2981f6a86523aad5d9b9eb13cb7221d27598f5a7
谢在奎 <jackshea@163.com>
2021-06-05 23:32:43 +0800

    P1837 K 进制表示下的各位数字总和

---
+++ LeetCode/Problems/Easy/P1837_SumOfDigitsInBaseK.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// K 进制表示下的各位数字总和
+    /// https://leetcode-cn.com/problems/sum-of-digits-in-base-k/
+    public class P1837_SumOfDigitsInBaseK
+    {
+        public int SumBase(int n, int k)
+        {
+            int ans = 0;
+            while (n > 0)
+            {
+                ans += n % k;
+                n /= k;
+            }
+
+            return ans;
+        }
+    }
+}
