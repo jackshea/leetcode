bfffb61351b8311162feb5ac1b816422b2308b2e
谢在奎 <jackshea@163.com>
2021-06-06 20:21:11 +0800

    P1844 将所有数字用字符替换

---
+++ LeetCode/Problems/Easy/P1844_ReplaceAllDigitsWithCharacters.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 将所有数字用字符替换
+    /// https://leetcode-cn.com/problems/replace-all-digits-with-characters/
+    public class P1844_ReplaceAllDigitsWithCharacters
+    {
+        public string ReplaceDigits(string s)
+        {
+            var charArray = s.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    charArray[i] = (char)(charArray[i - 1] + charArray[i] - '0');
+                }
+            }
+            return new string(charArray);
+        }
+    }
+}
