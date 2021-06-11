1f2e9bc03d2e1e02aac30e45e52e28813e7374c1
谢在奎 <jackshea@163.com>
2021-06-11 10:10:58 +0800

    P1848 到目标元素的最小距离

---
+++ LeetCode/Problems/Easy/P1848_MinimumDistanceToTheTargetElement.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 到目标元素的最小距离
+    /// https://leetcode-cn.com/problems/minimum-distance-to-the-target-element/
+    public class P1848_MinimumDistanceToTheTargetElement
+    {
+        public int GetMinDistance(int[] nums, int target, int start)
+        {
+            if (nums[start] == target)
+            {
+                return 0;
+            }
+
+            int dist = 1;
+            var n = nums.Length;
+            while (start - dist >= 0 || start + dist < n)
+            {
+                if (start - dist >= 0 && nums[start - dist] == target)
+                {
+                    return dist;
+                }
+
+                if (start + dist < n && nums[start + dist] == target)
+                {
+                    return dist;
+                }
+
+                dist++;
+            }
+
+            return -1;
+        }
+    }
+}
