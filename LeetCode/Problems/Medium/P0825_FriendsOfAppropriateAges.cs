d84a52b3ac6f127279c64d85fe3bf569f5442d6a
谢在奎 <jackshea@163.com>
2021-06-15 14:11:45 +0800

    P0825 适龄的朋友

---
+++ LeetCode/Problems/Medium/P0825_FriendsOfAppropriateAges.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 适龄的朋友
+    /// https://leetcode-cn.com/problems/friends-of-appropriate-ages/
+    public class P0825_FriendsOfAppropriateAges
+    {
+        public int NumFriendRequests(int[] ages)
+        {
+            int[] ageSegm = new int[121];
+            foreach (var age in ages)
+            {
+                for (int i = age; i < ageSegm.Length; i++)
+                {
+                    ageSegm[i]++;
+                }
+            }
+
+            int ans = 0;
+            foreach (var age in ages)
+            {
+                int min = age / 2 + 7;
+                if (min >= age)
+                {
+                    continue;
+                }
+                int max = age >= 100 ? age : Math.Min(age, 100);
+                ans += ageSegm[max] - ageSegm[min] - 1;
+            }
+
+            return ans;
+        }
+    }
+}
