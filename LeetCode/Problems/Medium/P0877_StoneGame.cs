9be1d115506100f12f76f1758153c2bc738b2f94
谢在奎 <jackshea@163.com>
2021-06-16 23:21:37 +0800

    P0877 石子游戏

---
+++ LeetCode/Problems/Medium/P0877_StoneGame.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 石子游戏
+    /// https://leetcode-cn.com/problems/stone-game/
+    public class P0877_StoneGame
+    {
+        public bool StoneGame(int[] piles)
+        {
+            return true;
+        }
+    }
+}
