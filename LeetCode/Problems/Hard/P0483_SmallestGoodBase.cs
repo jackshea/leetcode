a9d3636905f87da16f18a9d847135a01482b25b2
谢在奎 <jackshea@163.com>
2021-06-18 10:11:45 +0800

    P0483 最小好进制

---
+++ LeetCode/Problems/Hard/P0483_SmallestGoodBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems.Hard
+{
+    /// 最小好进制
+    /// https://leetcode-cn.com/problems/smallest-good-base/
+    public class P0483_SmallestGoodBase
+    {
+        public string SmallestGoodBase(string n)
+        {
+            long nVal = long.Parse(n);
+            int mMax = (int)Math.Floor(Math.Log(nVal) / Math.Log(2));
+            for (int m = mMax; m > 1; m--)
+            {
+                int k = (int)Math.Pow(nVal, 1.0 / m);
+                long mul = 1, sum = 1;
+                for (int i = 0; i < m; i++)
+                {
+                    mul *= k;
+                    sum += mul;
+                }
+                if (sum == nVal)
+                {
+                    return k.ToString();
+                }
+            }
+            return (nVal - 1).ToString();
+        }
+    }
+}
