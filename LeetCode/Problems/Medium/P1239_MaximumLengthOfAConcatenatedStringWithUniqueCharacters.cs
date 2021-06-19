5d2c4c15d71bb01cd478396f7b23605aadd731f1
谢在奎 <jackshea@163.com>
2021-06-19 12:36:22 +0800

    P1239 串联字符串的最大长度

---
+++ LeetCode/Problems/Medium/P1239_MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 串联字符串的最大长度
+    /// https://leetcode-cn.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+    public class P1239_MaximumLengthOfAConcatenatedStringWithUniqueCharacters
+    {
+        private int ans;
+
+        public int MaxLength(IList<string> arr)
+        {
+            List<int> masks = new List<int>();
+            foreach (var word in arr)
+            {
+                int mask = 0;
+                foreach (var c in word)
+                {
+                    int bit = 1 << (c - 'a');
+                    if ((mask & bit) != 0)
+                    {
+                        mask = -1;
+                        break;
+                    }
+
+                    mask |= bit;
+                }
+
+                if (mask > 0)
+                {
+                    masks.Add(mask);
+                }
+            }
+
+            Backtrack(masks, 0, 0);
+            return ans;
+        }
+
+        public void Backtrack(List<int> masks, int start, int curMask)
+        {
+            if (start >= masks.Count)
+            {
+                ans = Math.Max(ans, HammingWeight(curMask));
+                return;
+            }
+
+            var mask = masks[start];
+            if ((curMask & mask) == 0)
+            {
+                Backtrack(masks, start + 1, curMask | mask);
+            }
+            Backtrack(masks, start + 1, curMask);
+        }
+
+        public int HammingWeight(int n)
+        {
+            int ans = 0;
+            while (n != 0)
+            {
+                n &= n - 1;
+                ans++;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P1239_MaximumLengthOfAConcatenatedStringWithUniqueCharactersTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P1239_MaximumLengthOfAConcatenatedStringWithUniqueCharactersTests
+    {
+        [Test()]
+        public void MaxLengthTest()
+        {
+            var test = new P1239_MaximumLengthOfAConcatenatedStringWithUniqueCharacters();
+            var input = new List<string>() { "un", "iq", "ue" };
+            Assert.AreEqual(4, test.MaxLength(input));
+        }
+    }
+}
