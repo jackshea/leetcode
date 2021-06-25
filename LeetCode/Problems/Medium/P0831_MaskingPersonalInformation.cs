eb5c56162ff342a561eabc725aa6b615ca9c466a
谢在奎 <jackshea@163.com>
2021-06-25 11:37:03 +0800

    P0831 隐藏个人信息

---
+++ LeetCode/Problems/Medium/P0831_MaskingPersonalInformation.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 隐藏个人信息
+    /// https://leetcode-cn.com/problems/masking-personal-information/
+    public class P0831_MaskingPersonalInformation
+    {
+        public string MaskPII(string s)
+        {
+            if (s.Contains('@'))
+            {
+                var names = s.Split(new[] { '@', '.' });
+                names[0] = names[0][0] + "*****" + names[0][names[0].Length - 1];
+                return $"{names[0]}@{names[1]}.{names[2]}".ToLower();
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var c in s)
+                {
+                    if (!char.IsDigit(c))
+                    {
+                        continue;
+                    }
+
+                    sb.Append(c);
+                }
+
+                StringBuilder pre = new StringBuilder();
+                if (sb.Length > 10)
+                {
+                    pre.Append('+');
+                    for (int i = 0; i < sb.Length - 10; i++)
+                    {
+                        pre.Append('*');
+                    }
+
+                    pre.Append('-');
+                }
+
+                return pre + "***-***-" + sb.ToString().Substring(sb.Length - 4, 4);
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P0831_MaskingPersonalInformationTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P0831_MaskingPersonalInformationTests
+    {
+        [Test()]
+        public void MaskPIITest()
+        {
+            var test = new P0831_MaskingPersonalInformation();
+            var input = "LeetCode@LeetCode.com";
+            Assert.AreEqual("l*****e@leetcode.com", test.MaskPII(input));
+        }
+
+        [Test()]
+        public void MaskPIITest_1()
+        {
+            var test = new P0831_MaskingPersonalInformation();
+            var input = "AB@qq.com";
+            Assert.AreEqual("a*****b@qq.com", test.MaskPII(input));
+        }
+
+        [Test()]
+        public void MaskPIITest_2()
+        {
+            var test = new P0831_MaskingPersonalInformation();
+            var input = "1(234)567-890";
+            Assert.AreEqual("***-***-7890", test.MaskPII(input));
+        }
+
+        [Test()]
+        public void MaskPIITest_3()
+        {
+            var test = new P0831_MaskingPersonalInformation();
+            var input = "86-(10)12345678";
+            Assert.AreEqual("+**-***-***-5678", test.MaskPII(input));
+        }
+
+        [Test()]
+        public void MaskPIITest_4()
+        {
+            var test = new P0831_MaskingPersonalInformation();
+            var input = "+(501321)-50-23431";
+            Assert.AreEqual("+***-***-***-3431", test.MaskPII(input));
+        }
+
+        [Test()]
+        public void MaskPIITest_5()
+        {
+            var test = new P0831_MaskingPersonalInformation();
+            var input = "+86(88)1513-7-74";
+            Assert.AreEqual("+*-***-***-3774", test.MaskPII(input));
+        }
+    }
+}
