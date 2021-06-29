00d9c21c73847d2c9c5bb1b0b9b423d515857dc0
谢在奎 <jackshea@163.com>
2021-06-29 11:42:03 +0800

    P0833 字符串中的查找与替换

---
+++ LeetCode/Problems/Medium/P0833_FindAndReplaceInString.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 字符串中的查找与替换
+    /// https://leetcode-cn.com/problems/find-and-replace-in-string/
+    public class P0833_FindAndReplaceInString
+    {
+        public string FindReplaceString(string s, int[] indices, string[] sources, string[] targets)
+        {
+            var n = s.Length;
+            int[] matchIndex = new int[n];
+            Array.Fill(matchIndex, -1);
+            for (int i = 0; i < sources.Length; i++)
+            {
+                bool check = true;
+                for (var j = 0; j < sources[i].Length; j++)
+                {
+                    if (sources[i][j] != s[indices[i]+j])
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if (check)
+                {
+                    matchIndex[indices[i]] = i;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int index = 0;
+            while (index < n)
+            {
+                if (matchIndex[index] >= 0)
+                {
+                    sb.Append(targets[matchIndex[index]]);
+                    index += sources[matchIndex[index]].Length;
+                }
+                else
+                {
+                    sb.Append(s[index++]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
