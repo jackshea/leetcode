fa75f0215c51b1b5d4a1f9c99b0e4b7b47774004
谢在奎 <jackshea@163.com>
2021-06-30 21:47:01 +0800

    P1791 找出星型图的中心节点

---
+++ LeetCode/Problems/Easy/P1791_FindCenterOfStarGraph.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 找出星型图的中心节点
+    /// https://leetcode-cn.com/problems/find-center-of-star-graph/
+    public class P1791_FindCenterOfStarGraph
+    {
+        public int FindCenter(int[][] edges)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (edges[0][i] == edges[1][j])
+                    {
+                        return edges[0][i];
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
