c7754e5aba859554eadeeba389284620fcab234e
谢在奎 <jackshea@163.com>
2021-07-02 10:01:55 +0800

    P1833 雪糕的最大数量

---
+++ LeetCode/Problems/Medium/P1833_MaximumIceCreamBars.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 雪糕的最大数量
+    /// https://leetcode-cn.com/problems/maximum-ice-cream-bars/
+    public class P1833_MaximumIceCreamBars
+    {
+        public int MaxIceCream(int[] costs, int coins)
+        {
+            Array.Sort(costs);
+            int ans = 0;
+            foreach (var cost in costs)
+            {
+                coins -= cost;
+                if (coins >= 0)
+                {
+                    ans++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
