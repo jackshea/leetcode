7047cef59bd176dbce8fa7858e3f0e986f09e826
谢在奎 <jackshea@163.com>
2021-07-03 21:24:03 +0800

    P1859 将句子排序

---
+++ LeetCode/Problems/Medium/P1859_SortingTheSentence.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 将句子排序
+    /// https://leetcode-cn.com/problems/sorting-the-sentence/
+    public class P1859_SortingTheSentence
+    {
+        public string SortSentence(string s)
+        {
+            var words = s.Split(' ');
+            string[] newWords = new string[words.Length + 1];
+            foreach (var word in words)
+            {
+                int num = 0;
+                string w = "";
+                for (var i = 0; i < word.Length; i++)
+                {
+                    var c = word[i];
+                    if (char.IsDigit(c))
+                    {
+                        num = num * 10 + c - '0';
+                        if (string.IsNullOrEmpty(w))
+                        {
+                            w = word.Substring(0, i);
+                        }
+                    }
+                    newWords[num] = w;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (var i = 1; i < newWords.Length; i++)
+            {
+                var newWord = newWords[i];
+                if (sb.Length != 0)
+                {
+                    sb.Append(" ");
+                }
+                sb.Append(newWord);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
