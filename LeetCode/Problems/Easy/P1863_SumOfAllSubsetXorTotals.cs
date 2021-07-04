5da4c8af43d37082f9d58a1b936268d8e0b10510
谢在奎 <jackshea@163.com>
2021-07-04 20:28:22 +0800

    P1863 找出所有子集的异或总和再求和

---
+++ LeetCode/Problems/Easy/P1863_SumOfAllSubsetXorTotals.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 找出所有子集的异或总和再求和
+    /// https://leetcode-cn.com/problems/sum-of-all-subset-xor-totals/
+    public class P1863_SumOfAllSubsetXorTotals
+    {
+        public int SubsetXORSum(int[] nums)
+        {
+            int ans = 0;
+            foreach (var num in nums)
+            {
+                ans |= num;
+            }
+
+            return ans << nums.Length - 1;
+        }
+    }
+}
