17df98dd435374ddacc2259a698a574bb593e075
谢在奎 <jackshea@163.com>
2021-07-07 10:37:02 +0800

    P1711 大餐计数

---
+++ LeetCode/Problems/Medium/P1711_CountGoodMeals.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 大餐计数
+    /// https://leetcode-cn.com/problems/count-good-meals/
+    public class P1711_CountGoodMeals
+    {
+        public int CountPairs(int[] deliciousness)
+        {
+            int MOD = (int)1E9 + 7;
+            int maxSum = 0;
+            foreach (var d in deliciousness)
+            {
+                if (d > maxSum)
+                {
+                    maxSum = d;
+                }
+            }
+
+            maxSum <<= 1;
+            int ans = 0;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (var d in deliciousness)
+            {
+                for (int i = 1; i <= maxSum; i <<= 1)
+                {
+                    if (i - d < 0)
+                    {
+                        continue;
+                    }
+
+                    dic.TryGetValue(i - d, out var cnt);
+                    ans = (ans + cnt) % MOD;
+                }
+
+                dic.TryGetValue(d, out var count);
+                dic[d] = count + 1;
+            }
+
+            return ans;
+        }
+    }
+}
