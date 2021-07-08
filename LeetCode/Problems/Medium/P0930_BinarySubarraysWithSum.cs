46be6d7ff891b5badd220049b9ddc92dcd25fa0a
谢在奎 <jackshea@163.com>
2021-07-08 14:33:56 +0800

    P0930 和相同的二元子数组

---
+++ LeetCode/Problems/Medium/P0930_BinarySubarraysWithSum.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 和相同的二元子数组
+    /// https://leetcode-cn.com/problems/binary-subarrays-with-sum/
+    public class P0930_BinarySubarraysWithSum
+    {
+        public int NumSubarraysWithSum(int[] nums, int goal)
+        {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            int sum = 0;
+            int ans = 0;
+            freq[0] = 1;
+            foreach (var num in nums)
+            {
+                sum += num;
+                var cnt = 0;
+                if (freq.TryGetValue(sum - goal, out cnt))
+                {
+                    ans += cnt;
+                }
+
+                freq.TryGetValue(sum, out cnt);
+                freq[sum] = cnt + 1;
+            }
+
+            return ans;
+        }
+    }
+}
