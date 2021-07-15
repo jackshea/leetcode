57b024981f968f01f73a731353392d2c8529efe0
谢在奎 <jackshea@163.com>
2021-07-15 10:27:11 +0800

    P1864 减小和重新排列数组后的最大元素

---
+++ LeetCode/Problems/Medium/P1846_MaximumElementAfterDecreasingAndRearranging.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 减小和重新排列数组后的最大元素
+    /// https://leetcode-cn.com/problems/maximum-element-after-decreasing-and-rearranging/
+    public class P1846_MaximumElementAfterDecreasingAndRearranging
+    {
+        public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+        {
+            Array.Sort(arr);
+            int max = 0;
+            foreach (var num in arr)
+            {
+                max = Math.Min(num, max + 1);
+            }
+
+            return max;
+        }
+    }
+}
