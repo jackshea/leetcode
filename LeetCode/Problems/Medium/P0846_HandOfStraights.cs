9e3ce3f7fe8ce32b19dd1ed2a0ddf83488192b77
谢在奎 <jackshea@163.com>
2021-07-16 10:42:19 +0800

    P0846 一手顺子

---
+++ LeetCode/Problems/Medium/P0846_HandOfStraights.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 一手顺子
+    /// https://leetcode-cn.com/problems/hand-of-straights/
+    public class P0846_HandOfStraights
+    {
+        public bool IsNStraightHand(int[] hand, int groupSize)
+        {
+            SortedList<int, int> dic = new SortedList<int, int>();
+            foreach (var card in hand)
+            {
+                dic.TryGetValue(card, out var cnt);
+                dic[card] = cnt + 1;
+            }
+
+            while (dic.Keys.Count >= groupSize)
+            {
+                var start = dic.Keys[0];
+                for (int i = start; i < start + groupSize; i++)
+                {
+                    if (dic.TryGetValue(i, out var cnt))
+                    {
+                        if (cnt <= 1)
+                        {
+                            dic.Remove(i);
+                        }
+                        else
+                        {
+                            dic[i] = cnt - 1;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return dic.Count == 0;
+        }
+    }
+}
