e4524d9da12b9da8841cfba0eff6c08b1c7aed9c
谢在奎 <jackshea@163.com>
2021-07-18 22:43:08 +0800

    P1880 检查某单词是否等于两单词之和

---
+++ LeetCode/Problems/Easy/P1880_CheckIfWordEqualsSummationOfTwoWords.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 检查某单词是否等于两单词之和
+    /// https://leetcode-cn.com/problems/check-if-word-equals-summation-of-two-words/
+    public class P1880_CheckIfWordEqualsSummationOfTwoWords
+    {
+        public bool IsSumEqual(string firstWord, string secondWord, string targetWord)
+        {
+            return WordValue(firstWord) + WordValue(secondWord) == WordValue(targetWord);
+        }
+
+        private int WordValue(string word)
+        {
+            int num = 0;
+            foreach (var c in word)
+            {
+                num = num * 10 + c - 'a';
+            }
+
+            return num;
+        }
+    }
+}
