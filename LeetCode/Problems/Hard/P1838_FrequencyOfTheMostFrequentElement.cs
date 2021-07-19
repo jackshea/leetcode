9611b6b59e52aec504ad8043d1ddfc2306da3dd9
谢在奎 <jackshea@163.com>
2021-07-19 11:35:10 +0800

    P1838 最高频元素的频数

---
+++ LeetCode/Problems/Hard/P1838_FrequencyOfTheMostFrequentElement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems.Hard
+{
+    /// 最高频元素的频数
+    /// https://leetcode-cn.com/problems/frequency-of-the-most-frequent-element/
+    public class P1838_FrequencyOfTheMostFrequentElement
+    {
+        public int MaxFrequency(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int left = 0, right = 0, ans = 1;
+            long kSum = 0;
+            while (right < nums.Length - 1)
+            {
+                right++;
+                kSum += (right - left) * (nums[right] - nums[right - 1]);
+                while (kSum > k)
+                {
+                    kSum -= nums[right] - nums[left];
+                    left++;
+                }
+
+                ans = Math.Max(ans, right - left + 1);
+            }
+
+            return ans;
+        }
+    }
+}
