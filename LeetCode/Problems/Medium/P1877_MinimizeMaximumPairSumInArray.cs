2fa7b762ed9e983a9f43d261cce41cd141312476
谢在奎 <jackshea@163.com>
2021-07-20 10:27:27 +0800

    P1877 数组中最大数对和的最小值

---
+++ LeetCode/Problems/Medium/P1877_MinimizeMaximumPairSumInArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 数组中最大数对和的最小值
+    /// https://leetcode-cn.com/problems/minimize-maximum-pair-sum-in-array/
+    public class P1877_MinimizeMaximumPairSumInArray
+    {
+        public int MinPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            int left = 0, right = nums.Length - 1, max = 0;
+            while (left < right)
+            {
+                max = Math.Max(max, nums[left++] + nums[right--]);
+            }
+
+            return max;
+        }
+    }
+}
