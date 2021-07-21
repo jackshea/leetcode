b5fd7c6774d800090579b955c5092fce97b30a51
谢在奎 <jackshea@163.com>
2021-07-21 10:01:32 +0800

    P0848 字母移位

---
+++ LeetCode/Problems/Medium/P0848_ShiftingLetters.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 字母移位
+    /// https://leetcode-cn.com/problems/shifting-letters/
+    public class P0848_ShiftingLetters
+    {
+        public string ShiftingLetters(string s, int[] shifts)
+        {
+            var chars = s.ToCharArray();
+            int shift = 0;
+            for (int i = shifts.Length - 1; i >= 0; i--)
+            {
+                shift = (shift + shifts[i]) % 26;
+                chars[i] = (char)((chars[i] - 'a' + shift) % 26 + 'a');
+            }
+            return new string(chars);
+        }
+    }
+}
