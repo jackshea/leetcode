47014567fb34fd3b29589d4b7c7c8f4e94176ebb
谢在奎 <jackshea@163.com>
2021-07-22 13:16:59 +0800

    P0861 喧闹和富有

---
+++ LeetCode/Problems/Medium/P0851_LoudAndRich.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 喧闹和富有
+    /// https://leetcode-cn.com/problems/loud-and-rich/
+    public class P0851_LoudAndRich
+    {
+        private List<int>[] graph;
+        private int[] ans;
+        private int[] quiet;
+        public int[] LoudAndRich(int[][] richer, int[] quiet)
+        {
+            this.quiet = quiet;
+            int n = quiet.Length;
+            ans = new int[n];
+            graph = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                graph[i] = new List<int>();
+            }
+
+            foreach (var r in richer)
+            {
+                graph[r[1]].Add(r[0]);
+            }
+
+            Array.Fill(ans, -1);
+
+            for (int i = 0; i < n; i++)
+            {
+                DFS(i);
+            }
+
+            return ans;
+        }
+
+        public int DFS(int node)
+        {
+            if (ans[node] != -1)
+            {
+                return ans[node];
+            }
+
+            ans[node] = node;
+            foreach (var child in graph[node])
+            {
+                int cand = DFS(child);
+                if (quiet[cand] < quiet[ans[node]])
+                {
+                    ans[node] = cand;
+                }
+            }
+
+            return ans[node];
+        }
+    }
+}
