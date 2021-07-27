3818e4cd88ed2da250137144c9758b1e4b7aad67
谢在奎 <jackshea@163.com>
2021-07-27 10:27:26 +0800

    P0853 车队

---
+++ LeetCode/Problems/Medium/P0853_CarFleet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 车队
+    /// https://leetcode-cn.com/problems/car-fleet/
+    public class P0853_CarFleet
+    {
+        public int CarFleet(int target, int[] position, int[] speed)
+        {
+            int n = position.Length;
+            Car[] cars = new Car[n];
+            for (int i = 0; i < n; i++)
+            {
+                cars[i] = new Car { Position = position[i], Time = 1.0 * (target - position[i]) / speed[i] };
+            }
+            Array.Sort(cars, (a, b) => a.Position.CompareTo(b.Position));
+            int ans = 0, t = n;
+            while (--t > 0)
+            {
+                if (cars[t].Time < cars[t - 1].Time)
+                {
+                    ans++;
+                }
+                else
+                {
+                    cars[t - 1].Time = cars[t].Time;
+                }
+            }
+
+            return ans + (t == 0 ? 1 : 0);
+        }
+
+        class Car
+        {
+            public int Position;
+            public double Time;
+        }
+    }
+}
