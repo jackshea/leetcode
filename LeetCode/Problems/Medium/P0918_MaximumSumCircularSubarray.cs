7f263bd05e794c135a98be5a2f354673319536f7
谢在奎 <jackshea@163.com>
2021-07-30 10:37:12 +0800

    P0918 环形子数组的最大和

---
+++ LeetCode/Problems/Medium/P0918_MaximumSumCircularSubarray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 环形子数组的最大和
+    /// https://leetcode-cn.com/problems/maximum-sum-circular-subarray/
+    public class P0918_MaximumSumCircularSubarray
+    {
+        public int MaxSubarraySumCircular(int[] nums)
+        {
+            int curMax, max, curMin, min, sum;
+            curMax = max = curMin = min = sum = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                curMax = curMax > 0 ? curMax + nums[i] : nums[i];
+                max = Math.Max(max, curMax);
+                curMin = curMin < 0 ? curMin + nums[i] : nums[i];
+                min = Math.Min(min, curMin);
+            }
+
+            if (max < 0)
+            {
+                return max;
+            }
+
+            return Math.Max(sum - min, max);
+        }
+    }
+}
