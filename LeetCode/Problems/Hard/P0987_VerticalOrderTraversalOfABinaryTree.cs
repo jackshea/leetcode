92416f1c9901de8066aa3df90e111eb39b9dc9cf
谢在奎 <jackshea@163.com>
2021-07-31 20:24:24 +0800

    P0987 二叉树的垂序遍历

---
+++ LeetCode/Problems/Hard/P0987_VerticalOrderTraversalOfABinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using LeetCode.Common;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Hard
+{
+    /// 二叉树的垂序遍历
+    /// https://leetcode-cn.com/problems/vertical-order-traversal-of-a-binary-tree/
+    public class P0987_VerticalOrderTraversalOfABinaryTree
+    {
+        private List<Tuple<int, int, int>> nodes = new List<Tuple<int, int, int>>();
+        public IList<IList<int>> VerticalTraversal(TreeNode root)
+        {
+            DFS(root, 0, 0);
+            nodes.Sort((a, b) =>
+            {
+                if (a.Item1 != b.Item1)
+                {
+                    return a.Item1.CompareTo(b.Item1);
+                }
+
+                if (a.Item2!=b.Item2)
+                {
+                    return a.Item2.CompareTo(b.Item2);
+                }
+
+                return a.Item3.CompareTo(b.Item3);
+            });
+            IList<IList<int>> ans = new List<IList<int>>();
+            int size = 0;
+            int lastcol = int.MinValue;
+            foreach (Tuple<int, int, int> tuple in nodes)
+            {
+                int col = tuple.Item1, row = tuple.Item2, value = tuple.Item3;
+                if (col != lastcol)
+                {
+                    lastcol = col;
+                    ans.Add(new List<int>());
+                    size++;
+                }
+                ans[size - 1].Add(value);
+            }
+            return ans;
+        }
+
+        void DFS(TreeNode node, int row, int col)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            nodes.Add(new Tuple<int, int, int>(row, col, node.val));
+            DFS(node.left, row + 1, col - 1);
+            DFS(node.left, row + 1, col + 1);
+        }
+    }
+}
