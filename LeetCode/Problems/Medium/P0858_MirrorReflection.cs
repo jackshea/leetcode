7de2a5aa90e15495eb16958c551115b94e1ec9e5
谢在奎 <jackshea@163.com>
2021-08-03 10:27:24 +0800

    P0858 镜面反射

---
+++ LeetCode/Problems/Medium/P0858_MirrorReflection.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 镜面反射
+    /// https://leetcode-cn.com/problems/mirror-reflection/
+    public class P0858_MirrorReflection
+    {
+        public int MirrorReflection(int p, int q)
+        {
+            int g = gcd(p, q);
+            p /= g;
+            p %= 2;
+            q /= g;
+            q %= 2;
+            if (p == 1 && q == 1)
+            {
+                return 1;
+            }
+
+            return p == 1 ? 0 : 2;
+        }
+
+        public int gcd(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+
+            return gcd(b, a % b);
+        }
+    }
+}
