679c3b2c6453e0992a5791d9c7029419c9308576
谢在奎 <jackshea@163.com>
2021-08-08 21:24:00 +0800

    P1897 重新分配字符使所有字符串都相等

---
+++ LeetCode/Problems/Easy/P1897_RedistributeCharactersToMakeAllStringsEqual.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 重新分配字符使所有字符串都相等
+    /// https://leetcode-cn.com/problems/redistribute-characters-to-make-all-strings-equal/
+    public class P1897_RedistributeCharactersToMakeAllStringsEqual
+    {
+        public bool MakeEqual(string[] words)
+        {
+            int[] freq = new int[26];
+            foreach (var word in words)
+            {
+                foreach (var c in word)
+                {
+                    freq[c - 'a']++;
+                }
+            }
+
+            int n = words.Length;
+            foreach (var f in freq)
+            {
+                if (f % n != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
