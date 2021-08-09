12ad659e2f441c3299f425c0f9dad473c2bfd611
谢在奎 <jackshea@163.com>
2021-08-09 23:33:00 +0800

    P1314 矩阵区域和

---
+++ LeetCode/Problems/Medium/P1314_MatrixBlockSum.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 矩阵区域和
+    /// https://leetcode-cn.com/problems/matrix-block-sum/
+    public class P1314_MatrixBlockSum
+    {
+        private int m;
+        private int n;
+        private int[,] sum;
+
+        public int[][] MatrixBlockSum(int[][] mat, int k)
+        {
+            m = mat.Length;
+            n = mat[0].Length;
+            sum = new int[m + 1, n + 1];
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    sum[i, j] = sum[i - 1, j] + sum[i, j - 1] - sum[i - 1, j - 1] + mat[i - 1][j - 1];
+                }
+            }
+
+            int[][] ans = new int[m][];
+            for (int i = 0; i < m; i++)
+            {
+                ans[i] = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    ans[i][j] = Get(i + k + 1, j + k + 1) - Get(i - k, j + k + 1) - Get(i + k + 1, j - k) + Get(i - k, j - k);
+                }
+            }
+
+            return ans;
+        }
+
+        public int Get(int x, int y)
+        {
+            x = x < 0 ? 0 : x > m ? m : x;
+            y = y < 0 ? 0 : y > n ? n : y;
+            return sum[x, y];
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/Medium/P1314_MatrixBlockSumTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using LeetCode.Problems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Common;
+
+namespace LeetCode.Problems.Medium.Tests
+{
+    [TestFixture()]
+    public class P1314_MatrixBlockSumTests
+    {
+        [Test()]
+        public void MatrixBlockSumTest()
+        {
+            var input = Utils.Read2DArray("[[1,2,3],[4,5,6],[7,8,9]]");
+            var test = new P1314_MatrixBlockSum();
+            var ans = test.MatrixBlockSum(input, 1);
+            CollectionAssert.AreEqual(Utils.Read2DArray("[[12,21,16],[27,45,33],[24,39,28]]"), ans);
+        }
+    }
+}
