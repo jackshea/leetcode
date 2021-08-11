029831cf74cd03d05fd155daf58e411fd45937b4
谢在奎 <jackshea@163.com>
2021-08-11 19:58:45 +0800

    P0446 等差数列划分 II - 子序列

---
+++ LeetCode/Problems/Hard/P0446_ArithmeticSlicesIISubsequence.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Hard
+{
+    /// 等差数列划分 II - 子序列
+    /// https://leetcode-cn.com/problems/arithmetic-slices-ii-subsequence/
+    public class P0446_ArithmeticSlicesIISubsequence
+    {
+        public int NumberOfArithmeticSlices(int[] nums)
+        {
+            int ans = 0;
+            int n = nums.Length;
+            Dictionary<long, int>[] f = new Dictionary<long, int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                f[i] = new Dictionary<long, int>();
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    long d = 1L * nums[i] - nums[j];
+                    f[j].TryGetValue(d, out var cnt);
+                    ans += cnt;
+                    f[i].TryGetValue(d, out var cnti);
+                    f[i][d] = cnti + cnt + 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ LeetCode/Problems/Medium/P0869_ReorderedPowerOf2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 重新排序得到 2 的幂
+    /// https://leetcode-cn.com/problems/reordered-power-of-2/
+    public class P0869_ReorderedPowerOf2
+    {
+        public bool ReorderedPowerOf2(int n)
+        {
+            var ns = n.ToString().ToCharArray();
+            Array.Sort(ns);
+            for (int i = 0; i <= 31; i++)
+            {
+                var s = (1 << i).ToString();
+                if (s.Length < ns.Length)
+                {
+                    continue;
+                }
+                else if (s.Length > ns.Length)
+                {
+                    break;
+                }
+                else
+                {
+                    var charArray = s.ToCharArray();
+                    Array.Sort(charArray);
+                    bool check = true;
+                    for (int j = 0; j < ns.Length; j++)
+                    {
+                        if (charArray[j] != ns[j])
+                        {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
