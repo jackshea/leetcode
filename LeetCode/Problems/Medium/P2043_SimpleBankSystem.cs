f4a662144579b4ada509068a598a701b71ed6e16
谢在奎 <xiezk@feiyu.com>
2022-03-18 10:28:00 +0800

    P2043 简易银行系统

---
+++ LeetCode/Problems/Medium/P2043_SimpleBankSystem.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    public class Bank
+    {
+        private long[] balance;
+        private int accountQty = 0;
+        public Bank(long[] balance)
+        {
+            this.balance = balance;
+            accountQty = balance.Length;
+        }
+
+        public bool Transfer(int account1, int account2, long money)
+        {
+            if (!CheckAccount(account1, money) || !CheckAccount(account2, 0))
+            {
+                return false;
+            }
+            balance[account1 - 1] -= money;
+            balance[account2 - 1] += money;
+            return true;
+        }
+
+        public bool Deposit(int account, long money)
+        {
+            if (!CheckAccount(account, 0))
+            {
+                return false;
+            }
+
+            balance[account - 1] += money;
+            return true;
+        }
+
+        public bool Withdraw(int account, long money)
+        {
+            if (!CheckAccount(account, money))
+            {
+                return false;
+            }
+
+            balance[account - 1] -= money;
+            return true;
+        }
+
+        private bool CheckAccount(int account, long money)
+        {
+            return account >= 1 && account <= accountQty && balance[account - 1] >= money;
+        }
+    }
+}
