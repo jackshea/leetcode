f1cf962a8ddb1f14481eb45af30213d882e83c96
谢在奎 <xiezk@feiyu.com>
2022-12-15 10:03:18 +0800

    P1945 字符串转化后的各位数字之和

---
+++ LeetCode/Problems/Easy/P1945_SumOfDigitsOfStringAfterConvert.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 字符串转化后的各位数字之和
+    /// https://leetcode.cn/problems/sum-of-digits-of-string-after-convert/
+    public class P1945_SumOfDigitsOfStringAfterConvert
+    {
+        public int GetLucky(string s, int k)
+        {
+            int sum = 0;
+            foreach (char c in s)
+            {
+                sum += DigitSum(c - 'a' + 1);
+            }
+
+            for (int i = 0; i < k-1; i++)
+            {
+                sum = DigitSum(sum);
+            }
+
+            return sum;
+        }
+
+        public int DigitSum(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number = number / 10;
+            }
+
+            return sum;
+        }
+    }
+}
