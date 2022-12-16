ae90a5f4955655fe8266fbdffec10762bf0b64b8
谢在奎 <xiezk@feiyu.com>
2022-12-16 10:25:58 +0800

    P1786 构成特定和需要添加的最少元素

---
+++ LeetCode/Problems/Medium/P1785_MinimumElementsToAddToFormAGivenSum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 构成特定和需要添加的最少元素
+    /// https://leetcode.cn/problems/minimum-elements-to-add-to-form-a-given-sum/
+    public class P1785_MinimumElementsToAddToFormAGivenSum
+    {
+        public int MinElements(int[] nums, int limit, int goal)
+        {
+            long sum = 0;
+            foreach (var num in nums)
+            {
+                sum += num;
+            }
+
+            return (int)Math.Ceiling(1.0 * Math.Abs(goal - sum) / limit);
+        }
+    }
+}
