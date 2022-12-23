ba6ddec4a01a90b6741a8d6da0e38d1f897f4f20
谢在奎 <xiezk@feiyu.com>
2022-12-23 09:55:49 +0800

    P2011 执行操作后的变量值

---
+++ LeetCode/Problems/Easy/P2011_FinalValueOfVariableAfterPerformingOperations.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 执行操作后的变量值
+    /// https://leetcode.cn/problems/final-value-of-variable-after-performing-operations/
+    public class P2011_FinalValueOfVariableAfterPerformingOperations
+    {
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int ans = 0;
+            foreach (var operation in operations)
+            {
+                if (operation=="X++" || operation=="++X")
+                {
+                    ans++;
+                }
+                else
+                {
+                    ans--;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
