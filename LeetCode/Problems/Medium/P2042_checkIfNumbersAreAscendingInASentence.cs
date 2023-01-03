e4298b6f10f84014400853c7490be692ad7773ba
谢在奎 <xiezk@feiyu.com>
2023-01-03 09:58:47 +0800

    P2042 检查句子中的数字是否递增

---
+++ LeetCode/Problems/Medium/P2042_checkIfNumbersAreAscendingInASentence.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Medium
+{
+    /// 检查句子中的数字是否递增
+    /// https://leetcode.cn/problems/check-if-numbers-are-ascending-in-a-sentence/
+    public class P2042_CheckIfNumbersAreAscendingInASentence
+    {
+        public bool AreNumbersAscending(string s)
+        {
+            string[] words = s.Split(' ');
+            int last = 0;
+            foreach (var word in words)
+            {
+                if (int.TryParse(word, out var number))
+                {
+                    if (number > last)
+                    {
+                        last = number;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
