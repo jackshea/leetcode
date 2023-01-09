ef662c14d12db0fe36b0cc577bbfa294a4cc1af0
谢在奎 <xiezk@feiyu.com>
2023-01-09 10:24:46 +0800

    P1806 还原排列的最少操作步数

---
+++ LeetCode/Problems/Medium/P1806_MinimumNumberOfOperationsToReinitializeAPermutation.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 还原排列的最少操作步数
+    /// https://leetcode.cn/problems/minimum-number-of-operations-to-reinitialize-a-permutation/
+    public class P1806_MinimumNumberOfOperationsToReinitializeAPermutation
+    {
+        public int ReinitializePermutation(int n)
+        {
+            int[] raw = new int[n];
+            int[] perm = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                raw[i] = i;
+                perm[i] = i;
+            }
+
+            int step = 0;
+            while (true)
+            {
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    if (i%2==0)
+                    {
+                        arr[i] = perm[i / 2];
+                    }
+                    else
+                    {
+                        arr[i] = perm[n / 2 + (i - 1) / 2];
+                    }
+                }
+
+                perm = arr;
+                step++;
+                if (Enumerable.SequenceEqual(raw,perm))
+                {
+                    break;
+                }
+            }
+
+            return step;
+        }
+    }
+}
