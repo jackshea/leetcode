35769cb1ac5282687cec20346399f6aa02a19a7f
谢在奎 <xiezk@feiyu.com>
2023-01-11 09:56:43 +0800

    P2283 判断一个数的数字计数是否等于数位的值

---
+++ LeetCode/Problems/Easy/P2283_CheckIfNumberHasEqualDigitCountAndDigitValue.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 判断一个数的数字计数是否等于数位的值
+    /// https://leetcode.cn/problems/check-if-number-has-equal-digit-count-and-digit-value/
+    public class P2283_CheckIfNumberHasEqualDigitCountAndDigitValue
+    {
+        public bool DigitCount(string num)
+        {
+            int[] freq = new int[10];
+            foreach (var digit in num)
+            {
+                freq[digit - '0']++;
+            }
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (freq[i] != num[i] - '0')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
