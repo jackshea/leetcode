198ba69379c400c912cbdadf27c0463f3ec43f7f
谢在奎 <xiezk@feiyu.com>
2023-01-19 10:03:39 +0800

    P2299 强密码检验器 II

---
+++ LeetCode/Problems/Easy/P2299_StrongPasswordCheckerII.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 强密码检验器 II
+    /// https://leetcode.cn/problems/strong-password-checker-ii/
+    public class P2299_StrongPasswordCheckerII
+    {
+        public bool StrongPasswordCheckerII(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            var set = "!@#$%^&*()-+".ToHashSet();
+
+            int ans = 0;
+            bool chk6 = true;
+            for (int i = 0; i < password.Length; i++)
+            {
+                var c = password[i];
+                if (c >= 'a' && c <= 'z')
+                {
+                    ans |= 1;
+                }
+                if (c >= 'A' && c <= 'Z')
+                {
+                    ans |= 0b10;
+                }
+                if (c >= '0' && c <= '9')
+                {
+                    ans |= 0b100;
+                }
+                if (set.Contains(c))
+                {
+                    ans |= 0b1000;
+                }
+                if (i < password.Length - 1 && c == password[i + 1])
+                {
+                    chk6 = false;
+                }
+            }
+
+            return ans == 0b1111 && chk6;
+        }
+    }
+}
