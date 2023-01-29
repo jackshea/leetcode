be94489cc86655b675bc5d30b77f6d88e3bdc8d0
谢在奎 <xiezk@feiyu.com>
2023-01-29 10:07:03 +0800

    P2315 统计星号

---
+++ LeetCode/Problems/Easy/P2315_CountAsterisks.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    /// 统计星号
+    /// https://leetcode.cn/problems/count-asterisks/
+    public class P2315_CountAsterisks
+    {
+        public int CountAsterisks(string s)
+        {
+            int ans = 0;
+            bool external = true;
+            foreach (var c in s)
+            {
+                if (c=='|')
+                {
+                    external = !external;
+                }
+
+                if (external && c=='*')
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
