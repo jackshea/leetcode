dc115e32b3a43ef0d57ef97fd96808fd5e29ce27
谢在奎 <xiezk@feiyu.com>
2023-02-09 14:58:55 +0800

    P1797 设计一个验证系统

---
+++ LeetCode/Problems/Medium/P1797_DesignAuthenticationManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium
+{
+    /// 设计一个验证系统
+    /// https://leetcode.cn/problems/design-authentication-manager/
+    public class AuthenticationManager
+    {
+        private int timeToLive;
+        private Dictionary<string, int> dic = new Dictionary<string, int>();
+
+        public AuthenticationManager(int timeToLive)
+        {
+            this.timeToLive = timeToLive;
+        }
+
+        public void Generate(string tokenId, int currentTime)
+        {
+            dic[tokenId] = currentTime + timeToLive;
+        }
+
+        public void Renew(string tokenId, int currentTime)
+        {
+            if (dic.TryGetValue(tokenId, out var expireTime) && expireTime > currentTime)
+            {
+                dic[tokenId] = currentTime + timeToLive;
+            }
+        }
+
+        public int CountUnexpiredTokens(int currentTime)
+        {
+            int count = 0;
+            List<string> expireKeys = new List<string>();
+            foreach (var kvp in dic)
+            {
+                if (kvp.Value > currentTime)
+                {
+                    count++;
+                }
+                else
+                {
+                    expireKeys.Add(kvp.Key);
+                }
+            }
+            foreach (var expireKey in expireKeys)
+            {
+                dic.Remove(expireKey);
+            }
+
+            return count;
+        }
+    }
+}
