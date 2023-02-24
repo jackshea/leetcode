1cc71246e284bd434fd39b3e25d00e36f634ce10
谢在奎 <xiezk@feiyu.com>
2023-02-24 11:23:39 +0800

    P2357 使数组中所有元素都等于零

---
+++ LeetCode/Problems/Easy/P2357_MakeArrayZeroBySubtractingEqualAmounts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy
+{
+    /// 使数组中所有元素都等于零
+    /// https://leetcode.cn/problems/make-array-zero-by-subtracting-equal-amounts/
+    public class P2357_MakeArrayZeroBySubtractingEqualAmounts
+    {
+        public int MinimumOperations(int[] nums)
+        {
+            Array.Sort(nums);
+            int max = 0;
+            int ans = 0;
+            foreach (var num in nums)
+            {
+                if (num > max)
+                {
+                    max = num;
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
