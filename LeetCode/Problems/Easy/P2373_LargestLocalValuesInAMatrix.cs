adc957e9d27a8609f5d031be521222419d705e57
谢在奎 <xiezk@feiyu.com>
2023-03-01 11:18:32 +0800

    P2373 矩阵中的局部最大值

---
+++ LeetCode/Problems/Easy/P2373_LargestLocalValuesInAMatrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy;
+
+/// 矩阵中的局部最大值
+/// https://leetcode.cn/problems/largest-local-values-in-a-matrix/
+public class P2373_LargestLocalValuesInAMatrix
+{
+    public int[][] LargestLocal(int[][] grid)
+    {
+        int n = grid.Length - 2;
+        int[][] ans = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = new int[n];
+            for (int j = 0; j < n; j++)
+            {
+                ans[i][j] = Max(grid, i + 1, j + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    public int Max(int[][] grid, int x, int y)
+    {
+        int max = 0;
+        for (int i = x - 1; i <= x + 1; i++)
+        {
+            for (int j = y - 1; j <= y + 1; j++)
+            {
+                max = Math.Max(max, grid[i][j]);
+            }
+        }
+        return max;
+    }
+}
