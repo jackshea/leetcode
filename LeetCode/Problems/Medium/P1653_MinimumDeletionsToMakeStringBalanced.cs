8d8121b611d282800ee7ad2b54994523c704affe
谢在奎 <xiezk@feiyu.com>
2023-03-06 15:42:06 +0800

    P1653 使字符串平衡的最少删除次数

---
+++ LeetCode/Problems/Medium/P1653_MinimumDeletionsToMakeStringBalanced.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Problems.Medium;
+
+/// 使字符串平衡的最少删除次数
+/// https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced/
+public class P1653_MinimumDeletionsToMakeStringBalanced
+{
+    public int MinimumDeletions(string s)
+    {
+        int countB = 0, ans = 0;
+        foreach (var c in s)
+        {
+            if (c == 'a')
+            {
+                ans = Math.Min(ans + 1, countB );
+            }
+            else
+            {
+                countB++;
+            }
+        }
+
+        return ans;
+    }
+}
