16a16768941b752f7620820981f48e5360afea9a
谢在奎 <xiezk@feiyu.com>
2023-03-17 10:28:50 +0800

    P2389 和有限的最长子序列

---
+++ LeetCode/Problems/Easy/P2389_LongestSubsequenceWithLimitedSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy;
+
+/// 和有限的最长子序列
+/// https://leetcode.cn/problems/longest-subsequence-with-limited-sum/
+public class P2389_LongestSubsequenceWithLimitedSum
+{
+    public int[] AnswerQueries(int[] nums, int[] queries)
+    {
+        Array.Sort(nums);
+        for (int i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+
+        int[] ans=new int[queries.Length];
+        for (int i = 0; i < queries.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (queries[i] < nums[j])
+                {
+                    ans[i] = j ;
+                    goto end;
+                }
+            }
+
+            ans[i] = nums.Length;
+            end: ;
+        }
+
+        return ans;
+    }
+}
