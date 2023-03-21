edf071b1079935fadcd65137d6a3625f8f5b30be
谢在奎 <xiezk@feiyu.com>
2023-03-21 09:51:21 +0800

    P2469 温度转换

---
+++ LeetCode/Problems/Easy/P2469_ConvertTheTemperature.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Problems.Easy;
+
+/// 温度转换
+/// https://leetcode.cn/problems/convert-the-temperature/
+public class P2469_ConvertTheTemperature
+{
+    public double[] ConvertTemperature(double celsius)
+    {
+        return new[] {celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+}
