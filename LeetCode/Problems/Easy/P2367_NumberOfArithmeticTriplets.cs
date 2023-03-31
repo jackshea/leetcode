6ae5d2d6604611aa909914f61f47b1d835a1f6ab
谢在奎 <xiezk@feiyu.com>
2023-03-31 13:44:29 +0800

    P2367 算术三元组的数目

---
+++ LeetCode/Problems/Easy/P2367_NumberOfArithmeticTriplets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Easy;
+
+/// 算术三元组的数目
+/// https://leetcode.cn/problems/number-of-arithmetic-triplets/
+public class P2367_NumberOfArithmeticTriplets
+{
+    public int ArithmeticTriplets(int[] nums, int diff)
+    {
+        HashSet<int> set=new HashSet<int>();
+        foreach (var num in nums)
+        {
+            set.Add(num);
+        }
+
+        int ans = 0;
+        foreach (var num in nums)
+        {
+            if (set.Contains(num+diff) && set.Contains(num+diff*2))
+            {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
