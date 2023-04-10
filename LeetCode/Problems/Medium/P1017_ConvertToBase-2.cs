c24f5be70f3c3c6ae1d0e9f10821ffcc9284d038
谢在奎 <xiezk@feiyu.com>
2023-04-10 10:14:57 +0800

    P1019 链表中的下一个更大节点

---
+++ LeetCode/Problems/Medium/P1017_ConvertToBase-2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Problems.Medium;
+
+public class P1017_ConvertToBase_2
+{
+    public string BaseNeg2(int n)
+    {
+        StringBuilder ans = new StringBuilder();
+        while (n != 0)
+        {
+            int remain = n % -2;
+            ans.Append(Math.Abs(remain).ToString());
+            n = remain < 0 ? n / -2 + 1 : n / -2;
+        }
+        if (ans.Length == 0)
+        {
+            return "0";
+        }
+
+        var charArray = ans.ToString().ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
---
+++ LeetCode/Problems/Medium/P1019_NextGreaterNodeInLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Common;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Medium;
+
+/// 链表中的下一个更大节点
+/// https://leetcode.cn/problems/next-greater-node-in-linked-list/
+public class P1019_NextGreaterNodeInLinkedList
+{
+    public int[] NextLargerNodes(ListNode head)
+    {
+        Stack<ListNodeWithIndex> stack=new Stack<ListNodeWithIndex>();
+        List<int> ans=new List<int>();
+        ListNode cur=head;
+        int index = 0;
+        while (cur != null)
+        {
+            ans.Add(0);
+            while (stack.TryPeek(out var top) && top.node.val<cur.val)
+            {
+                ans[top.index] = cur.val;
+                stack.Pop();
+            }
+
+            stack.Push(new ListNodeWithIndex{node = cur,index = index});
+
+            cur = cur.next;
+            index++;
+        }
+        return ans.ToArray();
+    }
+
+    public class ListNodeWithIndex
+    {
+        public ListNode node;
+        public int index;
+    }
+}
