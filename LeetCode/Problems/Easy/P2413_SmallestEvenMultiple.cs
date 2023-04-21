ed2187c659e70cf5c927c4f4d920846da42b6fbe
谢在奎 <xiezk@feiyu.com>
2023-04-21 11:16:31 +0800

    P2413 最小偶倍数

---
+++ LeetCode/Problems/Easy/P2413_SmallestEvenMultiple.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Problems.Easy;
+
+/// 最小偶倍数
+/// https://leetcode.cn/problems/smallest-even-multiple/
+public class P2413_SmallestEvenMultiple
+{
+    public int SmallestEvenMultiple(int n)
+    {
+        if (n % 2 == 0)
+        {
+            return n;
+        }
+        else
+        {
+            return 2 * n;
+        }
+    }
+}
