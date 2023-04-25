d84c607a04797fc400a8bbd0a74bd3821d4da85d
谢在奎 <xiezk@feiyu.com>
2023-04-25 15:29:18 +0800

    P2418 按身高排序

---
+++ LeetCode/Problems/Easy/P2418_SortThePeople.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Problems.Easy;
+
+/// 按身高排序
+/// https://leetcode.cn/problems/sort-the-people/
+public class P2418_SortThePeople
+{
+    public string[] SortPeople(string[] names, int[] heights)
+    {
+        int[] indexes = new int[names.Length];
+        for (int i = 0; i < names.Length; i++)
+        {
+            indexes[i] = i;
+        }
+        Array.Sort(indexes, (a, b) => -heights[a].CompareTo(heights[b]));
+        string[] ans = new string[names.Length];
+        for (int i = 0; i < indexes.Length; i++)
+        {
+            ans[i] = names[indexes[i]];
+        }
+
+        return ans;
+    }
+}
